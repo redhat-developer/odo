8a33b99056a7aa8a210ff446c7ff8dfd4ee95fee
Suraj Narwade <surajnarwade353@gmail.com>
2018-01-30 12:35:58 +0530

    Added basic project and Cobra CLI structure

---
+++ scripts/check-gofmt.sh
@@ -0,0 +1,19 @@
+GO_FILES=$(find . -path ./vendor -prune -o -name '*.go' -print )
+
+for file in $GO_FILES; do
+	gofmtOutput=$(gofmt -l "$file")
+	if [ "$gofmtOutput" ]; then
+		errors+=("$gofmtOutput")
+	fi
+done
+
+
+if [ ${#errors[@]} -eq 0 ]; then
+	echo "gofmt OK"
+else
+	echo "gofmt ERROR - These files are not formated by gofmt:"
+	for err in "${errors[@]}"; do
+		echo "$err"
+	done
+	exit 1
+fi
