f2bac3c3e515fc73f189bbf884abda220c905ad6
Shubham Minglani <shubham@linux.com>
2018-02-06 18:31:42 +0530

    Modify application create behavior
    This commit moves the setting defaultApplication logic from
    pkg/application/application.go to cmd/app.go. Also, pretty prints
    the log messages now.

--- pkg/application/application.go
+++ pkg/application/application.go
@@ -3,28 +3,17 @@ package application
 import (
 	"github.com/pkg/errors"
 	"github.com/redhat-developer/ocdev/pkg/occlient"
-	log "github.com/sirupsen/logrus"
 )
 
-const defaultApplication = "app"
-
 // Create creates a new application and switches to it.
-
-// If no name is provided, the application is named as in the constant
-// "defaultApplication".
 
 // If application already exists, this errors out.
 
 // If no project is set, this errors out.
 func Create(name string) error {
-	// Set default application name if not set
-	if len(name) == 0 {
-		name = defaultApplication
-	}
 	if err := occlient.CreateNewProject(name); err != nil {
 		return errors.Wrapf(err, "unable to create application: %v", name)
 	}
-	log.Infof("Switching to application: %v", name)
 	return nil
 }
 
