165dfdb011f3a0dcaa6bee904e9ff6562a168ce9
Shubham Minglani <shubham@linux.com>
2018-02-08 23:29:24 +0530

    Implement `ocdev storage remove`

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -11,7 +11,18 @@ func Add(config *occlient.VolumeConfig) (string, error) {
 			Add: true,
 		})
 	if err != nil {
-		return "", errors.Wrap(err, "unable to create volume")
+		return "", errors.Wrap(err, "unable to create storage")
+	}
+	return output, nil
+}
+
+func Remove(config *occlient.VolumeConfig) (string, error) {
+	output, err := occlient.SetVolumes(config,
+		&occlient.VolumeOpertaions{
+			Remove: true,
+		})
+	if err != nil {
+		return "", errors.Wrap(err, "unable to remove storage")
 	}
 	return output, nil
 }
