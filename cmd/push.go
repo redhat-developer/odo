6324787dff209492c8cba43e6705c41d9b297b6e
Tomas Kral <tkral@redhat.com>
2018-03-21 15:14:07 +0100

    Make push smart
    When a component is created type and path to the source is saved to objects
    annotations.
    Later when `ocdev push` is called it detects what source was used and
    uses the same source for a new build. (If it was a local source, the same dir is pushed to
    BuildConfig, if it was built from remote git it starts new BuildConfig
    with to rebuild from updated git.)

--- cmd/push.go
+++ cmd/push.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
+	"github.com/redhat-developer/ocdev/pkg/application"
 	"github.com/redhat-developer/ocdev/pkg/component"
 
 	"github.com/pkg/errors"
@@ -23,8 +25,20 @@ var pushCmd = &cobra.Command{
 	`,
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Debug("component push called")
 		client := getOcClient()
+		// TODO: use project abstraction
+		projectName, err := client.GetCurrentProjectName()
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "unable to get current project"))
+			os.Exit(1)
+		}
+
+		applicationName, err := application.GetCurrent(client)
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "unable to get current application"))
+			os.Exit(1)
+		}
+
 		var componentName string
 		if len(args) == 0 {
 			var err error
@@ -34,19 +48,56 @@ var pushCmd = &cobra.Command{
 				fmt.Println(errors.Wrap(err, "unable to get current component"))
 				os.Exit(1)
 			}
+			if componentName == "" {
+				fmt.Println("No component is set as active.")
+				fmt.Println("Use 'ocdev component set <component name> to set and existing component as active or call this command with component name as and argument.")
+				os.Exit(1)
+			}
 		} else {
 			componentName = args[0]
 		}
 		fmt.Printf("pushing changes to component: %v\n", componentName)
 
-		if len(componentDir) == 0 {
-			componentDir = "."
-		}
-
-		if _, err := component.Push(client, componentName, componentDir); err != nil {
-			fmt.Printf("failed to push component: %v", componentName)
+		sourceType, sourcePath, err := component.GetComponentSource(client, componentName, applicationName, projectName)
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "unable to get current component"))
 			os.Exit(1)
 		}
+
+		switch sourceType {
+		case "local":
+			// use value of '--dir' as source if it was used
+			if len(componentDir) != 0 {
+				sourcePath = componentDir
+			}
+			u, err := url.Parse(sourcePath)
+			if err != nil {
+				fmt.Printf("Unable to parse source %s from component %s", sourcePath, componentName)
+				os.Exit(1)
+			}
+			if u.Scheme != "" && u.Scheme != "file" {
+				fmt.Printf("Component %s has invalid source path %s", componentName, u.Scheme)
+				os.Exit(1)
+			}
+
+			if err := component.PushLocal(client, componentName, u.Path); err != nil {
+				fmt.Printf("failed to push component: %v", componentName)
+				os.Exit(1)
+			}
+		case "git":
+			// currently we don't support changing build type
+			// it doesn't make sense to use --dir with git build
+			if len(componentDir) != 0 {
+				fmt.Println("unable to push local directory to component that uses git repository as source")
+				os.Exit(1)
+			}
+			if err := component.RebuildGit(client, componentName); err != nil {
+				fmt.Printf("failed to push component: %v", componentName)
+				os.Exit(1)
+			}
+
+		}
+
 		fmt.Printf("changes successfully pushed to component: %v\n", componentName)
 	},
 }
