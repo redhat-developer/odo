0a5c2397c1ab0e3aa14d2b466820e88ac4996281
Charlie Drage <charlie@charliedrage.com>
2018-06-27 10:33:24 -0400

    Add terminal command, moves completion and terminal commands to utils
    This combines both autocompletion and "terminal" commands into one
    category (`odo utils`).
    
    The new command added is `odo utils terminal`
    
    This can be used with either bash/zsh by: source <(odo utils terminal
    bash/zsh)

---
+++ cmd/terminal.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// terminalCmd represents the terminal command
+var terminalCmd = &cobra.Command{
+	Use:   "terminal",
+	Short: "Add Odo terminal support to your development environment",
+	Long: `Add Odo terminal support to your development environment. 
+
+This will append your PS1 environment variable with Odo component and application information.`,
+	Example: `  # Bash terminal PS1 support
+  source <(odo utils terminal bash)
+
+  # Zsh terminal PS1 support
+  source <(odo utils terminal zsh)
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		err := TerminalGenerate(os.Stdout, cmd, args)
+		checkError(err, "")
+
+		return nil
+	},
+}
+
+// Generates the PS1 output for Odo terminal support (appends to current PS1 environment variable)
+func TerminalGenerate(out io.Writer, cmd *cobra.Command, args []string) error {
+	// Check the passed in arguments
+	if len(args) == 0 {
+		return fmt.Errorf("Shell not specified. ex. odo completion [bash|zsh]")
+	}
+	if len(args) > 1 {
+		return fmt.Errorf("Too many arguments. Expected only the shell type. ex. odo completion [bash|zsh]")
+	}
+	shell := args[0]
+
+	// sh function for retrieving component information
+	var PS1 = `
+__odo_ps1() {
+
+    # Get current context
+    COMPONENT_GET=$(odo component get --skip-connection-check | sed "s/The current component is: //g")
+		if [ $? != 0 ]; then
+		COMPONENT="Unable to retrieve component"
+		else
+    COMPONENT=$(echo $COMPONENT_GET | sed "s/The current component is: //g")
+		fi
+
+		# Get application context
+    APP_GET=$(odo application get --skip-connection-check | sed "s/The current application is: //g")
+		if [ $? != 0 ]; then
+		APP="Unable to retrieve app"
+		else
+    APP=$(echo $APP_GET | sed "s/The current application is: //g")
+		fi
+
+    if [ -n "$COMPONENT" ] || [ -n "$APP" ]; then
+        echo "[${APP}/${COMPONENT}]"
+    fi
+}
+`
+
+	// Bash output
+	var bashPS1Output = PS1 + `
+PS1='$(__odo_ps1)'$PS1
+`
+
+	// Zsh output
+	var zshPS1Output = PS1 + `
+setopt prompt_subst
+PROMPT='$(__odo_ps1)'$PROMPT
+`
+
+	if shell == "bash" {
+		out.Write([]byte(bashPS1Output))
+	} else if shell == "zsh" {
+		out.Write([]byte(zshPS1Output))
+	} else {
+		return fmt.Errorf("not a compatible shell, bash and zsh are only supported")
+	}
+
+	return nil
+}
