a5592b58a2f37904811304d0c7ecf502091c4d32
mdas <mrinald7@gmail.com>
2018-07-12 21:12:47 +0530

    Revamps the URL command and adds support for custom names for url.
    It also adds a component flag to the create command for URL to specify the component. It also changes the behaviour of the odo delete command and now a component flag can be passed to delete the url from or else the current component is used.
    
    Signed-off-by: mdas <mrinald7@gmail.com>

---
+++ pkg/url/labels/labels_test.go
@@ -0,0 +1,72 @@
+package labels
+
+import (
+	"reflect"
+	"testing"
+
+	applabels "github.com/redhat-developer/odo/pkg/application/labels"
+	componentlabels "github.com/redhat-developer/odo/pkg/component/labels"
+)
+
+func TestGetLabels(t *testing.T) {
+	type args struct {
+		urlName         string
+		componentName   string
+		applicationName string
+		additional      bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "everything filled",
+			args: args{
+				urlName:         "urlname",
+				componentName:   "componentname",
+				applicationName: "applicationame",
+				additional:      false,
+			},
+			want: map[string]string{
+				applabels.ApplicationLabel:     "applicationame",
+				componentlabels.ComponentLabel: "componentname",
+				UrlLabel:                       "urlname",
+			},
+		}, {
+			name: "no storage name",
+			args: args{
+				urlName:         "",
+				componentName:   "componentname",
+				applicationName: "applicationame",
+				additional:      false,
+			},
+			want: map[string]string{
+				applabels.ApplicationLabel:     "applicationame",
+				componentlabels.ComponentLabel: "componentname",
+				UrlLabel:                       "",
+			},
+		}, {
+			name: "everything with additional",
+			args: args{
+				urlName:         "urlname",
+				componentName:   "componentname",
+				applicationName: "applicationame",
+				additional:      true,
+			},
+			want: map[string]string{
+				applabels.ApplicationLabel:               "applicationame",
+				applabels.AdditionalApplicationLabels[0]: "applicationame",
+				componentlabels.ComponentLabel:           "componentname",
+				UrlLabel:                                 "urlname",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetLabels(tt.args.urlName, tt.args.componentName, tt.args.applicationName, tt.args.additional); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetLabels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
