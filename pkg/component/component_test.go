83f6a901dc851ab69078e82f864be75c204fa242
anmolbabu <anmolbudugutta@gmail.com>
2018-08-04 13:25:54 +0530

    Add additional tests
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- pkg/component/component_test.go
+++ pkg/component/component_test.go
@@ -2,6 +2,7 @@ package component
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 
 	"github.com/redhat-developer/odo/pkg/occlient"
@@ -14,9 +15,6 @@ func TestGetComponentPorts(t *testing.T) {
 	type args struct {
 		componentName   string
 		applicationName string
-		namespace       string
-		componentType   string
-		containerPort   int32
 	}
 
 	tests := []struct {
@@ -28,25 +26,37 @@ func TestGetComponentPorts(t *testing.T) {
 		{
 			name: "Case 1: Invalid/Non-existant component name",
 			args: args{
-				componentName:   "nodejs",
-				applicationName: "nodejs",
-				namespace:       "myproject",
-				componentType:   "nodejs",
-				containerPort:   8080,
+				componentName:   "r",
+				applicationName: "app",
 			},
 			wantErr: true,
 			output:  []string{},
 		},
 		{
-			name: "Case 2: Valid params",
+			name: "Case 2: Valid params with multiple containers each with multiple ports",
 			args: args{
 				componentName:   "python",
 				applicationName: "app",
-				namespace:       "myproject",
-				componentType:   "python",
-				containerPort:   8080,
+			},
+			output:  []string{"10080/TCP", "8080/TCP", "9090/UDP", "10090/UDP"},
+			wantErr: false,
+		},
+		{
+			name: "Case 3: Valid params with single container and single port",
+			args: args{
+				componentName:   "nodejs",
+				applicationName: "app",
 			},
 			output:  []string{"8080/TCP"},
+			wantErr: false,
+		},
+		{
+			name: "Case 4: Valid params with single container and multiple port",
+			args: args{
+				componentName:   "wildfly",
+				applicationName: "app",
+			},
+			output:  []string{"8090/TCP", "8080/TCP"},
 			wantErr: false,
 		},
 	}
@@ -56,7 +66,8 @@ func TestGetComponentPorts(t *testing.T) {
 			// Fake the client with the appropriate arguments
 			client, fakeClientSet := occlient.FakeNew()
 			fakeClientSet.AppsClientset.PrependReactor("list", "deploymentconfigs", func(action ktesting.Action) (bool, runtime.Object, error) {
-				return true, testingutil.FakeDeploymentConfigs(tt.args.namespace, tt.args.componentName, tt.args.componentType, tt.args.applicationName, tt.args.containerPort), nil
+				//return true, testingutil.FakeDeploymentConfigs(tt.args.namespace, tt.args.componentName, tt.args.componentType, tt.args.applicationName, tt.args.containerPort), nil
+				return true, testingutil.FakeDeploymentConfigs(), nil
 			})
 
 			// The function we are testing
@@ -66,6 +77,10 @@ func TestGetComponentPorts(t *testing.T) {
 			if !tt.wantErr == (err != nil) {
 				t.Errorf("component List() unexpected error %v, wantErr %v", err, tt.wantErr)
 			}
+
+			// Sort the output and expected o/p in-order to avoid issues due to order as its not important
+			sort.Strings(output)
+			sort.Strings(tt.output)
 
 			// Check if the output is the same as what's expected (tags)
 			// and only if output is more than 0 (something is actually returned)
--- pkg/testingutil/deploymentconfigs.go
+++ pkg/testingutil/deploymentconfigs.go
@@ -8,8 +8,151 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func getContainerPort(containerPort int32, containerProtocol corev1.Protocol) (container corev1.ContainerPort) {
+	return corev1.ContainerPort{
+		Name:          fmt.Sprintf("%v/%v", containerPort, containerProtocol),
+		ContainerPort: containerPort,
+		Protocol:      containerProtocol,
+	}
+}
+
+func getContainer(componentName string, applicationName string, ports []corev1.ContainerPort) corev1.Container {
+	return corev1.Container{
+		Name:  fmt.Sprintf("%v-%v", componentName, applicationName),
+		Image: fmt.Sprintf("%v-%v", componentName, applicationName),
+		Ports: ports,
+	}
+}
+
+func getDeploymentConfig(namespace string, componentName string, componentType string, applicationName string, containers []corev1.Container) v1.DeploymentConfig {
+	return v1.DeploymentConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "DeploymentConfig",
+			APIVersion: "apps.openshift.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%v-%v", componentName, applicationName),
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app": "app",
+				"app.kubernetes.io/component-name": componentName,
+				"app.kubernetes.io/component-type": componentType,
+				"app.kubernetes.io/name":           applicationName,
+			},
+			Annotations: map[string]string{ // Convert into separate function when other source types required in tests
+				"app.kubernetes.io/component-source-type": "git",
+				"app.kubernetes.io/url":                   fmt.Sprintf("https://github.com/%s/%s.git", componentName, applicationName),
+			},
+		},
+		Spec: v1.DeploymentConfigSpec{
+			Replicas: 1,
+			Selector: map[string]string{
+				"deploymentconfig": fmt.Sprintf("%v-%v", componentName, applicationName),
+			},
+			Template: &corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"deploymentconfig": fmt.Sprintf("%v-%v", componentName, applicationName),
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: containers,
+				},
+			},
+		},
+	}
+}
+
+func FakeDeploymentConfigs() *v1.DeploymentConfigList {
+
+	var componentName string
+	var applicationName string
+	var componentType string
+
+	//DC1 with multiple containers each with multiple ports
+	componentType = "python"
+	componentName = "python"
+	applicationName = "app"
+	c1 := getContainer(componentName, applicationName, []corev1.ContainerPort{
+		{
+			Name:          fmt.Sprintf("%v-%v-p1", componentName, applicationName),
+			ContainerPort: 8080,
+			Protocol:      corev1.ProtocolTCP,
+		},
+		{
+			Name:          fmt.Sprintf("%v-%v-p2", componentName, applicationName),
+			ContainerPort: 9090,
+			Protocol:      corev1.ProtocolUDP,
+		},
+	})
+	c2 := getContainer(componentName, applicationName, []corev1.ContainerPort{
+		{
+			Name:          fmt.Sprintf("%v-%v-p1", componentName, applicationName),
+			ContainerPort: 10080,
+			Protocol:      corev1.ProtocolTCP,
+		},
+		{
+			Name:          fmt.Sprintf("%v-%v-p2", componentName, applicationName),
+			ContainerPort: 10090,
+			Protocol:      corev1.ProtocolUDP,
+		},
+	})
+	dc1 := getDeploymentConfig("myproject", componentName, componentType, applicationName, []corev1.Container{c1, c2})
+
+	//DC2 with single container and single port
+	componentType = "nodejs"
+	componentName = "nodejs"
+	applicationName = "app"
+	c3 := getContainer(componentName, applicationName, []corev1.ContainerPort{
+		{
+			Name:          fmt.Sprintf("%v-%v-p1", componentName, applicationName),
+			ContainerPort: 8080,
+			Protocol:      corev1.ProtocolTCP,
+		},
+	})
+	dc2 := getDeploymentConfig("myproject", componentName, componentType, applicationName, []corev1.Container{c3})
+
+	//DC3 with single container and multiple ports
+	componentType = "wildfly"
+	componentName = "wildfly"
+	applicationName = "app"
+	c4 := getContainer(componentName, applicationName, []corev1.ContainerPort{
+		{
+			Name:          fmt.Sprintf("%v-%v-p1", componentName, applicationName),
+			ContainerPort: 8080,
+			Protocol:      corev1.ProtocolTCP,
+		},
+		{
+			Name:          fmt.Sprintf("%v-%v-p1", componentName, applicationName),
+			ContainerPort: 8090,
+			Protocol:      corev1.ProtocolTCP,
+		},
+	})
+	dc3 := getDeploymentConfig("myproject", componentName, componentType, applicationName, []corev1.Container{c4})
+
+	return &v1.DeploymentConfigList{
+		Items: []v1.DeploymentConfig{
+			dc1,
+			dc2,
+			dc3,
+		},
+	}
+}
+
+/*
+func FakeDeploymentConfigss(namespace string, componentName string, componentType string, applicationName string, containerPort int32) *v1.DeploymentConfigList {
+
+	dc := getDeploymentConfig("myproject", "python", "python", "app", 8080)
+	return &v1.DeploymentConfigList{
+		Items: []v1.DeploymentConfig{
+			dc,
+		},
+	}
+}
+*/
+/*
 // getDeploymentConfig("myproject", "python", "python", "app", 8080)
-func getDeploymentConfig(namespace string, componentName string, componentType string, applicationName string, containerPort int32) v1.DeploymentConfig {
+func getDeploymentConfigg(namespace string, componentName string, componentType string, applicationName string, containerPort int32) v1.DeploymentConfig {
 	return v1.DeploymentConfig{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "DeploymentConfig",
@@ -26,7 +169,7 @@ func getDeploymentConfig(namespace string, componentName string, componentType s
 			},
 			Annotations: map[string]string{
 				"app.kubernetes.io/component-source-type": "git",
-				"app.kubernetes.io/url":                   "https://github.com/openshift/django-ex.git",
+				"app.kubernetes.io/url":                   fmt.Sprintf("https://github.com/%s/%s.git", componentName, applicationName),
 			},
 		},
 		Spec: v1.DeploymentConfigSpec{
@@ -59,12 +202,4 @@ func getDeploymentConfig(namespace string, componentName string, componentType s
 		},
 	}
 }
-
-func FakeDeploymentConfigs(namespace string, componentName string, componentType string, applicationName string, containerPort int32) *v1.DeploymentConfigList {
-	dc := getDeploymentConfig("myproject", "python", "python", "app", 8080)
-	return &v1.DeploymentConfigList{
-		Items: []v1.DeploymentConfig{
-			dc,
-		},
-	}
-}
+*/
