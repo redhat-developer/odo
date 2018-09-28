e4543cab9bcdc4ac82143a00043e35de678ef16b
Charlie Drage <charlie@charliedrage.com>
2018-09-28 12:19:37 -0400

    0.0.13 release
    0.0.13 release

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ set -e
 ODO_VERSION="latest"
 
 # Latest released odo version
-LATEST_VERSION="v0.0.12"
+LATEST_VERSION="v0.0.13"
 
 GITHUB_RELEASES_URL="https://github.com/redhat-developer/odo/releases/download/${LATEST_VERSION}"
 BINTRAY_URL="https://dl.bintray.com/odo/odo/latest"
