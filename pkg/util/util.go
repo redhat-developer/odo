4c025280b2b5b8303553e7dd08812d75e8b7ce5e
anmolbabu <anmolbudugutta@gmail.com>
2018-10-19 12:43:56 +0530

    Rebase
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"fmt"
-	"github.com/golang/glog"
 	"math/rand"
 	"net/url"
 	"os"
@@ -11,6 +10,8 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+
+	"github.com/golang/glog"
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
@@ -49,6 +50,100 @@ func GenerateRandomString(n int) string {
 	return string(b)
 }
 
+// Hyphenate applicationName and componentName
+func NamespaceOpenShiftObject(componentName string, applicationName string) (string, error) {
+
+	// Error if it's blank
+	if componentName == "" {
+		return "", errors.New("namespacing: component name cannot be blank")
+	}
+
+	// Error if it's blank
+	if applicationName == "" {
+		return "", errors.New("namespacing: application name cannot be blank")
+	}
+
+	// Return the hyphenated namespaced name
+	return fmt.Sprintf("%s-%s", strings.Replace(componentName, "/", "-", -1), applicationName), nil
+}
+
+// ExtractComponentType returns only component type part from passed component type(default unqualified, fully qualified, versioned, etc...and their combinations) for use as component name
+// Possible types of parameters:
+// 1. "myproject/python:3.5" -- Return python
+// 2. "python:3.5" -- Return python
+// 3. nodejs -- Return nodejs
+func ExtractComponentType(namespacedVersionedComponentType string) string {
+	s := strings.Split(namespacedVersionedComponentType, "/")
+	versionedString := s[0]
+	if len(s) == 2 {
+		versionedString = s[1]
+	}
+	s = strings.Split(versionedString, ":")
+	return s[0]
+}
+
+// parseCreateCmdArgs returns
+// 1. image name
+// 2. component type i.e, builder image name
+// 3. component name default value is component type else the user requested component name
+// 4. component version which is by default latest else version passed with builder image name
+func ParseCreateCmdArgs(args []string) (string, string, string, string) {
+	// We don't have to check it anymore, Args check made sure that args has at least one item
+	// and no more than two
+
+	// "Default" values
+	componentImageName := args[0]
+	componentType := args[0]
+	componentName := ExtractComponentType(componentType)
+	componentVersion := "latest"
+
+	// Check if componentType includes ":", if so, then we need to spit it into using versions
+	if strings.ContainsAny(componentImageName, ":") {
+		versionSplit := strings.Split(args[0], ":")
+		componentType = versionSplit[0]
+		componentName = ExtractComponentType(componentType)
+		componentVersion = versionSplit[1]
+	}
+	return componentImageName, componentType, componentName, componentVersion
+}
+
+const WIN = "windows"
+
+// Reads file path form URL file:///C:/path/to/file to C:\path\to\file
+func ReadFilePath(u *url.URL, os string) string {
+	location := u.Path
+	if os == WIN {
+		location = strings.Replace(u.Path, "/", "\\", -1)
+		location = location[1:]
+	}
+	return location
+}
+
+// Converts file path on windows to /C:/path/to/file to work in URL
+func GenFileUrl(location string, os string) string {
+	urlPath := location
+	if os == WIN {
+		urlPath = "/" + strings.Replace(location, "\\", "/", -1)
+	}
+	return "file://" + urlPath
+}
+
+// ConvertKeyValueStringToMap converts String Slice of Parameters to a Map[String]string
+// Each value of the slice is expected to be in the key=value format
+// Values that do not conform to this "spec", will be ignored
+func ConvertKeyValueStringToMap(params []string) map[string]string {
+	result := make(map[string]string, len(params))
+	for _, param := range params {
+		str := strings.Split(param, "=")
+		if len(str) != 2 {
+			glog.Fatalf("Parameter %s is not in the expected key=value format", param)
+		} else {
+			result[str[0]] = str[1]
+		}
+	}
+	return result
+}
+
 // TruncateString truncates passed string to given length
 // Note: if -1 is passed, the original string is returned
 func TruncateString(str string, maxLen int) string {
@@ -59,19 +154,6 @@ func TruncateString(str string, maxLen int) string {
 		return str[:maxLen]
 	}
 	return str
-}
-
-// StringInSlice returns if a passed string exists in the passed slice of strings
-func StringInSlice(checkStr string, strSlice []string) bool {
-	// Iterate the slice
-	for _, ele := range strSlice {
-		// if current element is same as element to search(checkStr), return true indicating checkStr found
-		if ele == checkStr {
-			return true
-		}
-	}
-	// Exited loop without finding required element(checkStr) in slice, so return false indicating checkStr not found in slice
-	return false
 }
 
 // GetRandomName returns a randomly generated name which can be used for naming odo and/or openshift entities
@@ -158,100 +240,6 @@ func GetComponentDir(path string, paramType ComponentCreateType) (string, error)
 	return retVal, nil
 }
 
-// Hyphenate applicationName and componentName
-func NamespaceOpenShiftObject(componentName string, applicationName string) (string, error) {
-
-	// Error if it's blank
-	if componentName == "" {
-		return "", errors.New("namespacing: component name cannot be blank")
-	}
-
-	// Error if it's blank
-	if applicationName == "" {
-		return "", errors.New("namespacing: application name cannot be blank")
-	}
-
-	// Return the hyphenated namespaced name
-	return fmt.Sprintf("%s-%s", strings.Replace(componentName, "/", "-", -1), applicationName), nil
-}
-
-// ExtractComponentType returns only component type part from passed component type(default unqualified, fully qualified, versioned, etc...and their combinations) for use as component name
-// Possible types of parameters:
-// 1. "myproject/python:3.5" -- Return python
-// 2. "python:3.5" -- Return python
-// 3. nodejs -- Return nodejs
-func ExtractComponentType(namespacedVersionedComponentType string) string {
-	s := strings.Split(namespacedVersionedComponentType, "/")
-	versionedString := s[0]
-	if len(s) == 2 {
-		versionedString = s[1]
-	}
-	s = strings.Split(versionedString, ":")
-	return s[0]
-}
-
-// parseCreateCmdArgs returns
-// 1. image name
-// 2. component type i.e, builder image name
-// 3. component name default value is component type else the user requested component name
-// 4. component version which is by default latest else version passed with builder image name
-func ParseCreateCmdArgs(args []string) (string, string, string, string) {
-	// We don't have to check it anymore, Args check made sure that args has at least one item
-	// and no more than two
-
-	// "Default" values
-	componentImageName := args[0]
-	componentType := args[0]
-	componentName := ExtractComponentType(componentType)
-	componentVersion := "latest"
-
-	// Check if componentType includes ":", if so, then we need to spit it into using versions
-	if strings.ContainsAny(componentImageName, ":") {
-		versionSplit := strings.Split(args[0], ":")
-		componentType = versionSplit[0]
-		componentName = ExtractComponentType(componentType)
-		componentVersion = versionSplit[1]
-	}
-	return componentImageName, componentType, componentName, componentVersion
-}
-
-const WIN = "windows"
-
-// Reads file path form URL file:///C:/path/to/file to C:\path\to\file
-func ReadFilePath(u *url.URL, os string) string {
-	location := u.Path
-	if os == WIN {
-		location = strings.Replace(u.Path, "/", "\\", -1)
-		location = location[1:]
-	}
-	return location
-}
-
-// Converts file path on windows to /C:/path/to/file to work in URL
-func GenFileUrl(location string, os string) string {
-	urlPath := location
-	if os == WIN {
-		urlPath = "/" + strings.Replace(location, "\\", "/", -1)
-	}
-	return "file://" + urlPath
-}
-
-<<<<<<< HEAD
-// ConvertKeyValueStringToMap converts String Slice of Parameters to a Map[String]string
-// Each value of the slice is expected to be in the key=value format
-// Values that do not conform to this "spec", will be ignored
-func ConvertKeyValueStringToMap(params []string) map[string]string {
-	result := make(map[string]string, len(params))
-	for _, param := range params {
-		str := strings.Split(param, "=")
-		if len(str) != 2 {
-			glog.Fatalf("Parameter %s is not in the expected key=value format", param)
-		} else {
-			result[str[0]] = str[1]
-		}
-	}
-	return result
-=======
 // GetDNS1123Name Converts passed string into DNS-1123 string
 func GetDNS1123Name(str string) string {
 	replacer := strings.NewReplacer(
@@ -265,5 +253,4 @@ func GetDNS1123Name(str string) string {
 		"--", "-",
 	)
 	return strings.TrimSpace(replacer.Replace(strings.ToLower(str)))
->>>>>>> Fix travis failures
 }
