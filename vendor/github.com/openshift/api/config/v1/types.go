30bbd8956562b8415522eb44d1a51b29ea5a4efa
Chris Laprun <metacosm@gmail.com>
2018-10-24 10:59:30 +0200

    Update glide and vendor.

--- vendor/github.com/openshift/api/config/v1/types.go
+++ vendor/github.com/openshift/api/config/v1/types.go
@@ -5,6 +5,81 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Image holds cluster-wide information about how to handle images.  The canonical name is `cluster`
+type Image struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// spec holds user settable values for configuration
+	Spec ImageSpec `json:"spec"`
+	// status holds observed values from the cluster. They may not be overridden.
+	Status ImageStatus `json:"status"`
+}
+
+type ImageSpec struct {
+	// AllowedRegistriesForImport limits the docker registries that normal users may import
+	// images from. Set this list to the registries that you trust to contain valid Docker
+	// images and that you want applications to be able to import from. Users with
+	// permission to create Images or ImageStreamMappings via the API are not affected by
+	// this policy - typically only administrators or system integrations will have those
+	// permissions.
+	AllowedRegistriesForImport []RegistryLocation `json:"allowedRegistriesForImport,omitempty"`
+
+	// ExternalRegistryHostname sets the hostname for the default external image
+	// registry. The external hostname should be set only when the image registry
+	// is exposed externally. The value is used in 'publicDockerImageRepository'
+	// field in ImageStreams. The value must be in "hostname[:port]" format.
+	ExternalRegistryHostname string `json:"externalRegistryHostname,omitempty"`
+
+	// AdditionalTrustedCA is a reference to a ConfigMap containing additional CAs that
+	// should be trusted during imagestream import.
+	AdditionalTrustedCA ConfigMapReference `json:"additionalTrustedCA,omitempty"`
+}
+
+type ImageStatus struct {
+
+	// this value is set by the image registry operator which controls the internal registry hostname
+	// InternalRegistryHostname sets the hostname for the default internal image
+	// registry. The value must be in "hostname[:port]" format.
+	// For backward compatibility, users can still use OPENSHIFT_DEFAULT_REGISTRY
+	// environment variable but this setting overrides the environment variable.
+	InternalRegistryHostname string `json:"internalRegistryHostname,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ImageList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Image `json:"items"`
+}
+
+// RegistryLocation contains a location of the registry specified by the registry domain
+// name. The domain name might include wildcards, like '*' or '??'.
+type RegistryLocation struct {
+	// DomainName specifies a domain name for the registry
+	// In case the registry use non-standard (80 or 443) port, the port should be included
+	// in the domain name as well.
+	DomainName string `json:"domainName"`
+	// Insecure indicates whether the registry is secure (https) or insecure (http)
+	// By default (if not specified) the registry is assumed as secure.
+	Insecure bool `json:"insecure,omitempty"`
+}
+
+// ConfigMapReference references the location of a configmap.
+type ConfigMapReference struct {
+	Namespace string `json:"namespace"`
+	Name      string `json:"name"`
+	// Key allows pointing to a specific key/value inside of the configmap.  This is useful for logical file references.
+	Key string `json:"filename,omitempty"`
+}
+
 // HTTPServingInfo holds configuration for serving HTTP
 type HTTPServingInfo struct {
 	// ServingInfo is the HTTP serving information
--- vendor/github.com/openshift/api/config/v1/types_swagger_doc_generated.go
+++ vendor/github.com/openshift/api/config/v1/types_swagger_doc_generated.go
@@ -60,6 +60,15 @@ func (ClientConnectionOverrides) SwaggerDoc() map[string]string {
 	return map_ClientConnectionOverrides
 }
 
+var map_ConfigMapReference = map[string]string{
+	"":         "ConfigMapReference references the location of a configmap.",
+	"filename": "Key allows pointing to a specific key/value inside of the configmap.  This is useful for logical file references.",
+}
+
+func (ConfigMapReference) SwaggerDoc() map[string]string {
+	return map_ConfigMapReference
+}
+
 var map_EtcdConnectionInfo = map[string]string{
 	"":     "EtcdConnectionInfo holds information necessary for connecting to an etcd server",
 	"urls": "URLs are the URLs for etcd",
@@ -100,6 +109,43 @@ func (HTTPServingInfo) SwaggerDoc() map[string]string {
 	return map_HTTPServingInfo
 }
 
+var map_Image = map[string]string{
+	"":         "Image holds cluster-wide information about how to handle images.  The canonical name is `cluster`",
+	"metadata": "Standard object's metadata.",
+	"spec":     "spec holds user settable values for configuration",
+	"status":   "status holds observed values from the cluster. They may not be overridden.",
+}
+
+func (Image) SwaggerDoc() map[string]string {
+	return map_Image
+}
+
+var map_ImageList = map[string]string{
+	"metadata": "Standard object's metadata.",
+}
+
+func (ImageList) SwaggerDoc() map[string]string {
+	return map_ImageList
+}
+
+var map_ImageSpec = map[string]string{
+	"allowedRegistriesForImport": "AllowedRegistriesForImport limits the docker registries that normal users may import images from. Set this list to the registries that you trust to contain valid Docker images and that you want applications to be able to import from. Users with permission to create Images or ImageStreamMappings via the API are not affected by this policy - typically only administrators or system integrations will have those permissions.",
+	"externalRegistryHostname":   "ExternalRegistryHostname sets the hostname for the default external image registry. The external hostname should be set only when the image registry is exposed externally. The value is used in 'publicDockerImageRepository' field in ImageStreams. The value must be in \"hostname[:port]\" format.",
+	"additionalTrustedCA":        "AdditionalTrustedCA is a reference to a ConfigMap containing additional CAs that should be trusted during imagestream import.",
+}
+
+func (ImageSpec) SwaggerDoc() map[string]string {
+	return map_ImageSpec
+}
+
+var map_ImageStatus = map[string]string{
+	"internalRegistryHostname": "this value is set by the image registry operator which controls the internal registry hostname InternalRegistryHostname sets the hostname for the default internal image registry. The value must be in \"hostname[:port]\" format. For backward compatibility, users can still use OPENSHIFT_DEFAULT_REGISTRY environment variable but this setting overrides the environment variable.",
+}
+
+func (ImageStatus) SwaggerDoc() map[string]string {
+	return map_ImageStatus
+}
+
 var map_KubeClientConfig = map[string]string{
 	"kubeConfig":          "kubeConfig is a .kubeconfig filename for going to the owning kube-apiserver.  Empty uses an in-cluster-config",
 	"connectionOverrides": "connectionOverrides specifies client overrides for system components to loop back to this master.",
@@ -130,6 +176,16 @@ var map_NamedCertificate = map[string]string{
 
 func (NamedCertificate) SwaggerDoc() map[string]string {
 	return map_NamedCertificate
+}
+
+var map_RegistryLocation = map[string]string{
+	"":           "RegistryLocation contains a location of the registry specified by the registry domain name. The domain name might include wildcards, like '*' or '??'.",
+	"domainName": "DomainName specifies a domain name for the registry In case the registry use non-standard (80 or 443) port, the port should be included in the domain name as well.",
+	"insecure":   "Insecure indicates whether the registry is secure (https) or insecure (http) By default (if not specified) the registry is assumed as secure.",
+}
+
+func (RegistryLocation) SwaggerDoc() map[string]string {
+	return map_RegistryLocation
 }
 
 var map_RemoteConnectionInfo = map[string]string{
