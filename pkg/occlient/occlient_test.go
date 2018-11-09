0972b1bf0f6d6122561fda0214b6552c51b47a48
Charlie Drage <charlie@charliedrage.com>
2018-11-09 15:30:46 -0500

    Add CreateBuildConfig test
    Adds a test that covers CreateBuildConfig in occlient.go
    
    Closes https://github.com/redhat-developer/odo/issues/828

--- pkg/occlient/occlient_test.go
+++ pkg/occlient/occlient_test.go
@@ -4343,3 +4343,183 @@ func TestWaitAndGetDC(t *testing.T) {
 		})
 	}
 }
+
+func TestCreateBuildConfig(t *testing.T) {
+	type args struct {
+		commonObjectMeta metav1.ObjectMeta
+		namespace        string
+		builderImage     string
+		gitURL           string
+		envVars          []corev1.EnvVar
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		actions int
+	}{
+		{
+			name: "Case 1 - Generate and create the BuildConfig",
+			args: args{
+				builderImage: "ruby:latest",
+				namespace:    "testing",
+				gitURL:       "https://github.com/openshift/ruby",
+				commonObjectMeta: metav1.ObjectMeta{
+					Name: "ruby",
+					Labels: map[string]string{
+						"app":                              "apptmp",
+						"app.kubernetes.io/component-name": "ruby",
+						"app.kubernetes.io/component-type": "ruby",
+						"app.kubernetes.io/name":           "apptmp",
+					},
+					Annotations: map[string]string{
+						"app.kubernetes.io/url":                   "https://github.com/openshift/ruby",
+						"app.kubernetes.io/component-source-type": "git",
+					},
+				},
+				envVars: []corev1.EnvVar{
+					{
+						Name:  "key",
+						Value: "value",
+					},
+					{
+						Name:  "key1",
+						Value: "value1",
+					},
+				},
+			},
+			wantErr: false,
+			actions: 1,
+		},
+		{
+			name: "Case 2 - Generate and create the BuildConfig but fail with unable to find image name",
+			args: args{
+				builderImage: "fakeimagename:notlatest",
+				namespace:    "testing",
+				gitURL:       "https://github.com/openshift/ruby",
+				commonObjectMeta: metav1.ObjectMeta{
+					Name: "ruby",
+					Labels: map[string]string{
+						"app":                              "apptmp",
+						"app.kubernetes.io/component-name": "ruby",
+						"app.kubernetes.io/component-type": "ruby",
+						"app.kubernetes.io/name":           "apptmp",
+					},
+					Annotations: map[string]string{
+						"app.kubernetes.io/url":                   "https://github.com/openshift/ruby",
+						"app.kubernetes.io/component-source-type": "git",
+					},
+				},
+				envVars: []corev1.EnvVar{
+					{
+						Name:  "key",
+						Value: "value",
+					},
+					{
+						Name:  "key1",
+						Value: "value1",
+					},
+				},
+			},
+			wantErr: true,
+			actions: 1,
+		},
+		{
+			name: "Case 3 - Generate and create the BuildConfig but fail with unable to parse image name",
+			args: args{
+				builderImage: "::",
+				namespace:    "testing",
+				gitURL:       "https://github.com/openshift/ruby",
+				commonObjectMeta: metav1.ObjectMeta{
+					Name: "ruby",
+					Labels: map[string]string{
+						"app":                              "apptmp",
+						"app.kubernetes.io/component-name": "ruby",
+						"app.kubernetes.io/component-type": "ruby",
+						"app.kubernetes.io/name":           "apptmp",
+					},
+					Annotations: map[string]string{
+						"app.kubernetes.io/url":                   "https://github.com/openshift/ruby",
+						"app.kubernetes.io/component-source-type": "git",
+					},
+				},
+				envVars: []corev1.EnvVar{
+					{
+						Name:  "key",
+						Value: "value",
+					},
+					{
+						Name:  "key1",
+						Value: "value1",
+					},
+				},
+			},
+			wantErr: true,
+			actions: 1,
+		},
+		{
+			name: "Case 4 - Generate and create the BuildConfig and pass in no envVars",
+			args: args{
+				builderImage: "ruby:latest",
+				namespace:    "testing",
+				gitURL:       "https://github.com/openshift/ruby",
+				commonObjectMeta: metav1.ObjectMeta{
+					Name: "ruby",
+					Labels: map[string]string{
+						"app":                              "apptmp",
+						"app.kubernetes.io/component-name": "ruby",
+						"app.kubernetes.io/component-type": "ruby",
+						"app.kubernetes.io/name":           "apptmp",
+					},
+					Annotations: map[string]string{
+						"app.kubernetes.io/url":                   "https://github.com/openshift/ruby",
+						"app.kubernetes.io/component-source-type": "git",
+					},
+				},
+				envVars: []corev1.EnvVar{},
+			},
+			wantErr: false,
+			actions: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakeClient, fakeClientSet := FakeNew()
+
+			fakeClientSet.ImageClientset.PrependReactor("get", "imagestreams", func(action ktesting.Action) (bool, runtime.Object, error) {
+				return true, fakeImageStream(tt.args.commonObjectMeta.Name, tt.args.commonObjectMeta.Namespace, []string{"latest"}), nil
+			})
+
+			// Run function CreateBuildConfig
+			bc, err := fakeClient.CreateBuildConfig(tt.args.commonObjectMeta, tt.args.builderImage, tt.args.gitURL, tt.args.envVars)
+
+			if err == nil && !tt.wantErr {
+				// Check to see how many actions are being ran
+				if (len(fakeClientSet.ImageClientset.Actions()) != tt.actions) && !tt.wantErr {
+					t.Errorf("expected %v action(s) in CreateBuildConfig got %v: %v", tt.actions, len(fakeClientSet.ImageClientset.Actions()), fakeClientSet.ImageClientset.Actions())
+				}
+
+				// Check to see that names match
+				if bc.ObjectMeta.Name != tt.args.commonObjectMeta.Name {
+					t.Errorf("Expected buildConfig name %s, got '%s'", tt.args.commonObjectMeta.Name, bc.ObjectMeta.Name)
+				}
+
+				// Check to see that labels match
+				if !reflect.DeepEqual(tt.args.commonObjectMeta.Labels, bc.ObjectMeta.Labels) {
+					t.Errorf("Expected equal labels, got %+v, expected %+v", tt.args.commonObjectMeta.Labels, bc.ObjectMeta.Labels)
+				}
+
+				// Check to see that annotations match
+				if !reflect.DeepEqual(tt.args.commonObjectMeta.Annotations, bc.ObjectMeta.Annotations) {
+					t.Errorf("Expected equal annotations, got %+v, expected %+v", tt.args.commonObjectMeta.Annotations, bc.ObjectMeta.Annotations)
+				}
+
+			} else if err == nil && tt.wantErr {
+				t.Error("test failed, expected: false, got true")
+			} else if err != nil && !tt.wantErr {
+				t.Errorf("test failed, expected: no error, got error: %s", err.Error())
+			}
+
+		})
+	}
+}
