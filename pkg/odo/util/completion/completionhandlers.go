fdb2bad291c1e2169d58c6dd128c3f2b9ee26814
Suraj Narwade <surajnarwade@users.noreply.github.com>
2018-11-12 17:48:53 +0530

    Added command completion for `odo app` commands (#934)
    * `odo app describe`
    * `odo app delete`
    * `odo app set`

--- pkg/odo/util/completion/completionhandlers.go
+++ pkg/odo/util/completion/completionhandlers.go
@@ -2,6 +2,7 @@ package completion
 
 import (
 	"github.com/posener/complete"
+	"github.com/redhat-developer/odo/pkg/application"
 	"github.com/redhat-developer/odo/pkg/occlient"
 	"github.com/redhat-developer/odo/pkg/odo/util"
 	"github.com/redhat-developer/odo/pkg/service"
@@ -37,6 +38,22 @@ var ServiceClassCompletionHandler = func(args complete.Args, client *occlient.Cl
 		completions = append(completions, class.Spec.ExternalName)
 	}
 
+	return completions
+}
+
+// AppCompletionHandler provides completion for the app commands
+var AppCompletionHandler = func(args complete.Args, client *occlient.Client) (completions []string) {
+	completions = make([]string, 0)
+	util.GetAndSetNamespace(client)
+
+	applications, err := application.List(client)
+	if err != nil {
+		return completions
+	}
+
+	for _, app := range applications {
+		completions = append(completions, app.Name)
+	}
 	return completions
 }
 
