b8bfa9eb03a6f04f5f6ef3662bd237f9e3e4ee85
Georgios Andrianakis <geoand@gmail.com>
2018-11-14 16:58:36 +0200

    Add go completion for odo url delete
    Fixes: #938

--- pkg/odo/util/completion/completionhandlers.go
+++ pkg/odo/util/completion/completionhandlers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	"github.com/redhat-developer/odo/pkg/project"
 	"github.com/redhat-developer/odo/pkg/service"
+	"github.com/redhat-developer/odo/pkg/url"
 	"github.com/spf13/cobra"
 )
 
@@ -78,4 +79,24 @@ var ProjectNameCompletionHandler = func(cmd *cobra.Command, args parsedArgs, con
 		completions = append(completions, project.Name)
 	}
 	return completions
+}
+
+// URLCompletionHandler provides completion for the url commands
+var URLCompletionHandler = func(cmd *cobra.Command, args parsedArgs, context *genericclioptions.Context) (completions []string) {
+	completions = make([]string, 0)
+
+	urls, err := url.List(context.Client, context.Component(), context.Application)
+	if err != nil {
+		return completions
+	}
+
+	for _, url := range urls {
+		// we found the url in the list which means
+		// that the url name has been already selected by the user so no need to suggest more
+		if val, ok := args.commands[url.Name]; ok && val {
+			return nil
+		}
+		completions = append(completions, url.Name)
+	}
+	return
 }
