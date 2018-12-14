5d450bce098c5d55e2e21e8768c67e315b56936a
Girish Ramnani <girishramnani95@gmail.com>
2018-12-14 16:49:44 +0530

    using double quotes and new line in error logs

--- pkg/odo/cli/component/push.go
+++ pkg/odo/cli/component/push.go
@@ -53,8 +53,9 @@ var pushCmd = &cobra.Command{
 
 		// if the componentName is blank then there is no active component set
 		if len(componentName) == 0 {
-			log.Error(`No component is set as active.
-Use 'odo component set' to set and existing component as active or call this command with component name as an argument.`)
+			log.Error("No component is set as active.\n" +
+				"Use 'odo component set' to set and existing component as active \n" +
+				"or call this command with component name as an argument.")
 			os.Exit(1)
 		}
 
