fa93d66a5f180549596f748a28d700bb99e7885a
Chris Laprun <metacosm@gmail.com>
2019-01-21 17:18:44 +0100

    Refactor catalog search service according to complete / validate / run pattern.

--- pkg/odo/cli/catalog/search/service.go
+++ pkg/odo/cli/catalog/search/service.go
@@ -2,13 +2,12 @@ package search
 
 import (
 	"fmt"
-	"github.com/redhat-developer/odo/pkg/log"
+	"github.com/redhat-developer/odo/pkg/occlient"
 	"github.com/redhat-developer/odo/pkg/odo/cli/catalog/util"
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	odoutil "github.com/redhat-developer/odo/pkg/odo/util"
 	svc "github.com/redhat-developer/odo/pkg/service"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 const serviceRecommendedCommandName = "service"
@@ -16,7 +15,50 @@ const serviceRecommendedCommandName = "service"
 var serviceExample = `  # Search for a service
   %[1]s mysql`
 
+// SearchServiceOptions encapsulates the options for the odo catalog describe service command
+type SearchServiceOptions struct {
+	searchTerm string
+	services   []occlient.Service
+	// generic context options common to all commands
+	*genericclioptions.Context
+}
+
+// NewSearchServiceOptions creates a new SearchServiceOptions instance
+func NewSearchServiceOptions() *SearchServiceOptions {
+	return &SearchServiceOptions{}
+}
+
+// Complete completes SearchServiceOptions after they've been created
+func (o *SearchServiceOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	o.Context = genericclioptions.NewContext(cmd)
+	o.searchTerm = args[0]
+
+	o.services, err = svc.Search(o.Client, o.searchTerm)
+	if err != nil {
+		return fmt.Errorf("unable to list services because Service Catalog is not enabled in your cluster: %v", err)
+	}
+	o.services = util.FilterHiddenServices(o.services)
+
+	return err
+}
+
+// Validate validates the SearchServiceOptions based on completed values
+func (o *SearchServiceOptions) Validate() (err error) {
+	if len(o.services) == 0 {
+		return fmt.Errorf("no service matched the query: %s", o.searchTerm)
+	}
+
+	return
+}
+
+// Run contains the logic for the odo catalog describe service command
+func (o *SearchServiceOptions) Run() (err error) {
+	util.DisplayServices(o.services)
+	return
+}
+
 func NewCmdCatalogSearchService(name, fullName string) *cobra.Command {
+	o := NewSearchServiceOptions()
 	return &cobra.Command{
 		Use:   name,
 		Short: "Search service type in catalog",
@@ -28,19 +70,9 @@ services from service catalog.
 		Example: fmt.Sprintf(serviceExample, fullName),
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			client := genericclioptions.Client(cmd)
-			searchTerm := args[0]
-			services, err := svc.Search(client, searchTerm)
-			odoutil.LogErrorAndExit(err, "unable to search for services")
-			services = util.FilterHiddenServices(services)
-
-			switch len(services) {
-			case 0:
-				log.Errorf("No service matched the query: %v", searchTerm)
-				os.Exit(1)
-			default:
-				util.DisplayServices(services)
-			}
+			odoutil.LogErrorAndExit(o.Complete(name, cmd, args), "")
+			odoutil.LogErrorAndExit(o.Validate(), "")
+			odoutil.LogErrorAndExit(o.Run(), "")
 		},
 	}
 
