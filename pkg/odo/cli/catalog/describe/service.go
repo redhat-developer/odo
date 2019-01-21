b24d07c608b1613bbb71ee0f3b43160cf7d3b373
Chris Laprun <metacosm@gmail.com>
2019-01-21 15:11:58 +0100

    Refactor describe service according to complete / validate / run pattern.

--- pkg/odo/cli/catalog/describe/service.go
+++ pkg/odo/cli/catalog/describe/service.go
@@ -3,7 +3,6 @@ package describe
 import (
 	"fmt"
 	"github.com/olekukonko/tablewriter"
-	"github.com/redhat-developer/odo/pkg/log"
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	odoutil "github.com/redhat-developer/odo/pkg/odo/util"
 	svc "github.com/redhat-developer/odo/pkg/service"
@@ -25,7 +24,110 @@ This describes the service and the associated plans.
 `)
 )
 
+// DescribeServiceOptions encapsulates the options for the odo describe service command
+type DescribeServiceOptions struct {
+	// name of the service to describe, from command arguments
+	serviceName string
+	// resolved service
+	service svc.ServiceClass
+	plans   []svc.ServicePlan
+	// generic context options common to all commands
+	*genericclioptions.Context
+}
+
+// NewDescribeServiceOptions creates a new DescribeServiceOptions instance
+func NewDescribeServiceOptions() *DescribeServiceOptions {
+	return &DescribeServiceOptions{}
+}
+
+// Complete completes DescribeServiceOptions after they've been created
+func (o *DescribeServiceOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	o.Context = genericclioptions.NewContext(cmd)
+	o.serviceName = args[0]
+
+	return
+}
+
+// Validate validates the DescribeServiceOptions based on completed values
+func (o *DescribeServiceOptions) Validate() (err error) {
+	o.service, o.plans, err = svc.GetServiceClassAndPlans(o.Client, o.serviceName)
+	return err
+}
+
+// Run contains the logic for the odo catalog describe service command
+func (o *DescribeServiceOptions) Run() (err error) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetBorder(false)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+
+	serviceData := [][]string{
+		{"Name", o.service.Name},
+		{"Bindable", fmt.Sprint(o.service.Bindable)},
+		{"Operated by the broker", o.service.ServiceBrokerName},
+		{"Short Description", o.service.ShortDescription},
+		{"Long Description", o.service.LongDescription},
+		{"Versions Available", strings.Join(o.service.VersionsAvailable, ",")},
+		{"Tags", strings.Join(o.service.Tags, ",")},
+	}
+
+	table.AppendBulk(serviceData)
+
+	table.Append([]string{""})
+
+	if len(o.plans) > 0 {
+		table.Append([]string{"PLANS"})
+
+		for _, plan := range o.plans {
+
+			// create the display values for required  and optional parameters
+			requiredWithMandatoryUserInputParameterNames := []string{}
+			requiredWithOptionalUserInputParameterNames := []string{}
+			optionalParameterDisplay := []string{}
+			for _, parameter := range plan.Parameters {
+				if parameter.Required {
+					// until we have a better solution for displaying the plan data (like a separate table perhaps)
+					// this is simplest thing to do
+					if parameter.HasDefaultValue {
+						requiredWithOptionalUserInputParameterNames = append(
+							requiredWithOptionalUserInputParameterNames,
+							fmt.Sprintf("%s (default: '%s')", parameter.Name, parameter.Default))
+					} else {
+						requiredWithMandatoryUserInputParameterNames = append(requiredWithMandatoryUserInputParameterNames, parameter.Name)
+					}
+
+				} else {
+					optionalParameterDisplay = append(optionalParameterDisplay, parameter.Name)
+				}
+			}
+
+			table.Append([]string{"***********************", "*****************************************************"})
+			planLineSeparator := []string{"-----------------", "-----------------"}
+
+			planData := [][]string{
+				{"Name", plan.Name},
+				planLineSeparator,
+				{"Display Name", plan.DisplayName},
+				planLineSeparator,
+				{"Short Description", plan.Description},
+				planLineSeparator,
+				{"Required Params without a default value", strings.Join(requiredWithMandatoryUserInputParameterNames, ", ")},
+				planLineSeparator,
+				{"Required Params with a default value", strings.Join(requiredWithOptionalUserInputParameterNames, ", ")},
+				planLineSeparator,
+				{"Optional Params", strings.Join(optionalParameterDisplay, ", ")},
+				{"", ""},
+			}
+			table.AppendBulk(planData)
+		}
+		table.Render()
+	} else {
+		return fmt.Errorf("no plans found for service %s", o.serviceName)
+	}
+	return
+}
+
 func NewCmdCatalogDescribeService(name, fullName string) *cobra.Command {
+	o := NewDescribeServiceOptions()
 	command := &cobra.Command{
 		Use:     name,
 		Short:   "Describe a service",
@@ -33,80 +135,11 @@ func NewCmdCatalogDescribeService(name, fullName string) *cobra.Command {
 		Example: fmt.Sprintf(serviceExample, fullName),
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			client := genericclioptions.Client(cmd)
-			serviceName := args[0]
-			service, plans, err := svc.GetServiceClassAndPlans(client, serviceName)
-			odoutil.LogErrorAndExit(err, "")
-
-			table := tablewriter.NewWriter(os.Stdout)
-			table.SetBorder(false)
-			table.SetAlignment(tablewriter.ALIGN_LEFT)
-
-			serviceData := [][]string{
-				{"Name", service.Name},
-				{"Bindable", fmt.Sprint(service.Bindable)},
-				{"Operated by the broker", service.ServiceBrokerName},
-				{"Short Description", service.ShortDescription},
-				{"Long Description", service.LongDescription},
-				{"Versions Available", strings.Join(service.VersionsAvailable, ",")},
-				{"Tags", strings.Join(service.Tags, ",")},
-			}
-
-			table.AppendBulk(serviceData)
-
-			table.Append([]string{""})
-
-			if len(plans) > 0 {
-				table.Append([]string{"PLANS"})
-
-				for _, plan := range plans {
-
-					// create the display values for required  and optional parameters
-					requiredWithMandatoryUserInputParameterNames := []string{}
-					requiredWithOptionalUserInputParameterNames := []string{}
-					optionalParameterDisplay := []string{}
-					for _, parameter := range plan.Parameters {
-						if parameter.Required {
-							// until we have a better solution for displaying the plan data (like a separate table perhaps)
-							// this is simplest thing to do
-							if parameter.HasDefaultValue {
-								requiredWithOptionalUserInputParameterNames = append(
-									requiredWithOptionalUserInputParameterNames,
-									fmt.Sprintf("%s (default: '%s')", parameter.Name, parameter.Default))
-							} else {
-								requiredWithMandatoryUserInputParameterNames = append(requiredWithMandatoryUserInputParameterNames, parameter.Name)
-							}
-
-						} else {
-							optionalParameterDisplay = append(optionalParameterDisplay, parameter.Name)
-						}
-					}
-
-					table.Append([]string{"***********************", "*****************************************************"})
-					planLineSeparator := []string{"-----------------", "-----------------"}
-
-					planData := [][]string{
-						{"Name", plan.Name},
-						planLineSeparator,
-						{"Display Name", plan.DisplayName},
-						planLineSeparator,
-						{"Short Description", plan.Description},
-						planLineSeparator,
-						{"Required Params without a default value", strings.Join(requiredWithMandatoryUserInputParameterNames, ", ")},
-						planLineSeparator,
-						{"Required Params with a default value", strings.Join(requiredWithOptionalUserInputParameterNames, ", ")},
-						planLineSeparator,
-						{"Optional Params", strings.Join(optionalParameterDisplay, ", ")},
-						{"", ""},
-					}
-					table.AppendBulk(planData)
-				}
-				table.Render()
-			} else {
-				log.Errorf("No plans found for service %s", serviceName)
-				os.Exit(1)
-			}
+			odoutil.LogErrorAndExit(o.Complete(name, cmd, args), "")
+			odoutil.LogErrorAndExit(o.Validate(), "")
+			odoutil.LogErrorAndExit(o.Run(), "")
 		},
 	}
+
 	return command
 }
