0abce6144b8b87892e333b181490f7af018f1d0a
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-01-21 12:28:41 +0530

    WIP Fixing #1195
    Changing odo version to follow patterns of odo service which
    already follows complete-validate-run pattern
    
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/odo/cli/version/version.go
+++ pkg/odo/cli/version/version.go
@@ -5,12 +5,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/golang/glog"
 	"github.com/redhat-developer/odo/pkg/notify"
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	"github.com/redhat-developer/odo/pkg/odo/util"
-
-	"github.com/golang/glog"
 	"github.com/spf13/cobra"
+	ktemplates "k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 )
 
 var (
@@ -23,58 +23,66 @@ var (
 	GITCOMMIT = "HEAD"
 )
 
+// RecommendedCommandName is the recommended service command name
+const RecommendedCommandName = "version"
+
+var versonLongDesc = ktemplates.LongDesc("Print the client version information")
+
 var clientFlag bool
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the client version information",
-	Long:  "Print the client version information",
-	Example: `  # Print the client version of Odo
-  odo version
-	`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-		// If verbose mode is enabled, dump all KUBECLT_* env variables
-		// this is usefull for debuging oc plugin integration
-		for _, v := range os.Environ() {
-			if strings.HasPrefix(v, "KUBECTL_") {
-				glog.V(4).Info(v)
-			}
-		}
-
-		fmt.Println("odo " + VERSION + " (" + GITCOMMIT + ")")
-
-		if !clientFlag {
-			// Let's fetch the info about the server
-			serverInfo, err := genericclioptions.ClientWithConnectionCheck(cmd, true).GetServerVersion()
-			if err == nil {
-				// make sure we only include Openshift info if we actually have it
-				openshiftStr := ""
-				if len(serverInfo.OpenShiftVersion) > 0 {
-					openshiftStr = fmt.Sprintf("OpenShift: %v\n", serverInfo.OpenShiftVersion)
-				}
-				fmt.Printf("\n"+
-					"Server: %v\n"+
-					"%v"+
-					"Kubernetes: %v\n",
-					serverInfo.Address,
-					openshiftStr,
-					serverInfo.KubernetesVersion)
-			}
-		}
-	},
-}
-
 // NewCmdVersion implements the version odo command
-func NewCmdVersion() *cobra.Command {
+func NewCmdVersion(name, fullName string) *cobra.Command {
 	// Add a defined annotation in order to appear in the help menu
+
+	// versionCmd represents the version command
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the client version information",
+		Long:  versonLongDesc,
+		Example: `  # Print the client version of Odo
+	  odo version
+		`,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			// If verbose mode is enabled, dump all KUBECLT_* env variables
+			// this is usefull for debuging oc plugin integration
+			for _, v := range os.Environ() {
+				if strings.HasPrefix(v, "KUBECTL_") {
+					glog.V(4).Info(v)
+				}
+			}
+
+			fmt.Println("odo " + VERSION + " (" + GITCOMMIT + ")")
+
+			if !clientFlag {
+				// Let's fetch the info about the server
+				serverInfo, err := genericclioptions.ClientWithConnectionCheck(cmd, true).GetServerVersion()
+				if err == nil {
+					// make sure we only include Openshift info if we actually have it
+					openshiftStr := ""
+					if len(serverInfo.OpenShiftVersion) > 0 {
+						openshiftStr = fmt.Sprintf("OpenShift: %v\n", serverInfo.OpenShiftVersion)
+					}
+					fmt.Printf("\n"+
+						"Server: %v\n"+
+						"%v"+
+						"Kubernetes: %v\n",
+						serverInfo.Address,
+						openshiftStr,
+						serverInfo.KubernetesVersion)
+				}
+			}
+		},
+	}
+
 	versionCmd.Annotations = map[string]string{"command": "utility"}
 	versionCmd.SetUsageTemplate(util.CmdUsageTemplate)
 	versionCmd.Flags().BoolVar(&clientFlag, "client", false, "Client version only (no server required).")
 
 	return versionCmd
 }
+
+// GetLatestReleaseInfo Gets information about the latest release
 func GetLatestReleaseInfo(info chan<- string) {
 	newTag, err := notify.CheckLatestReleaseTag(VERSION)
 	if err != nil {
