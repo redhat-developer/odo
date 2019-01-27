2c3730405391c37aee1d0978b6d0031db5c8da40
Chris Laprun <metacosm@gmail.com>
2019-01-27 21:18:47 +0100

    Add more tests.

--- pkg/odo/events/bus_test.go
+++ pkg/odo/events/bus_test.go
@@ -2,14 +2,19 @@ package events
 
 import (
 	"github.com/metacosm/odo-event-api/odo/api/events"
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
 
-type testListener struct{}
+var received events.Event
 
-func (testListener) OnEvent(event events.Event) error {
-	panic("implement me")
+type testListener struct {
+}
+
+func (t testListener) OnEvent(event events.Event) error {
+	received = event
+	return nil
 }
 
 func (testListener) OnAbort(abortError events.EventCausedAbortError) {
@@ -24,8 +29,74 @@ func TestEventBus_RegisterSingle(t *testing.T) {
 	listener := testListener{}
 	name := "foo"
 	eventType := events.PostComplete
+
+	// initial state should be empty
+	assert.Equal(t, len(bus.listeners), 0)
+	assert.NotContains(t, bus.listeners, name)
+
+	// we should now only one listener
 	bus.RegisterSingle(name, eventType, listener)
+	assert.Equal(t, len(bus.listeners), 1)
+	assert.Contains(t, bus.listeners, name)
+	assert.Equal(t, len(bus.listeners[name]), 1)
+	assert.Contains(t, bus.listeners[name], eventType)
 	if !reflect.DeepEqual(bus.getListenersFor(name, eventType)[0], listener) {
 		t.Error("failed, expected bus listeners to contain registered listener")
 	}
+
+	// we should still have one listener
+	bus.RegisterSingle(name, eventType, nil)
+	assert.Equal(t, len(bus.listeners), 1)
+	assert.Contains(t, bus.listeners, name)
+	assert.Equal(t, len(bus.listeners[name]), 1)
+	assert.Contains(t, bus.listeners[name], eventType)
+
+	// register a listener for a different event and type and check that it's properly registered
+	bar := "bar"
+	eventType2 := events.PostValidate
+	bus.RegisterSingle(bar, eventType2, listener)
+	assert.Equal(t, len(bus.listeners), 2)
+	assert.Contains(t, bus.listeners, name)
+	assert.Contains(t, bus.listeners, bar)
+	assert.Equal(t, len(bus.listeners[name]), 1)
+	assert.Equal(t, len(bus.listeners[bar]), 1)
+	assert.Contains(t, bus.listeners[name], eventType)
+	assert.Contains(t, bus.listeners[bar], eventType2)
+	if !reflect.DeepEqual(bus.getListenersFor(bar, eventType2)[0], listener) {
+		t.Error("failed, expected bus listeners to contain registered listener")
+	}
+
+	// reset state
+	delete(bus.listeners, name)
+	delete(bus.listeners, bar)
+	assert.Equal(t, len(bus.listeners), 0)
+}
+
+func TestDispatchEvent(t *testing.T) {
+	foo := "foo"
+	eventType := events.PostValidate
+	event := events.Event{
+		Name: foo,
+		Type: eventType,
+	}
+
+	// received shouldn't changed when no listeners are registered
+	err := bus.DispatchEvent(event)
+	if err != nil {
+		t.Errorf("failed, got unexpected error: %v", err)
+	}
+	assert.EqualValues(t, received, events.Event{})
+
+	// check that received is indeed equal to the dispatched event
+	listener := testListener{}
+	bus.RegisterSingle(foo, eventType, listener)
+	err = bus.DispatchEvent(event)
+	if err != nil {
+		t.Errorf("failed, got unexpected error: %v", err)
+	}
+	assert.EqualValues(t, event, received)
+
+	// reset state
+	delete(bus.listeners, foo)
+	received = events.Event{}
 }
