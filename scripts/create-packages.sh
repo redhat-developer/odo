c9a37b469ffd486019c562237109d4b0ce6192ee
Tomas Kral <tkral@redhat.com>
2019-01-29 10:16:59 +0100

    use 'odo version --client' in create-package.sh

--- scripts/create-packages.sh
+++ scripts/create-packages.sh
@@ -14,7 +14,7 @@ PKG_VERSION=$(date "+%Y%m%d%H%M%S")
 if [[ -n $TRAVIS_TAG ]]; then
     echo "Checking if odo version was set to the same version as current tag"
     # use sed to get only semver part
-    bin_version=$(${BIN_DIR}/linux-amd64/odo version | head -1 | sed "s/^odo \(.*\) (.*)$/\1/")
+    bin_version=$(${BIN_DIR}/linux-amd64/odo version --client | head -1 | sed "s/^odo \(.*\) (.*)$/\1/")
     if [ "$TRAVIS_TAG" == "${bin_version}" ]; then
         echo "OK: odo version output is matching current tag"
     else
