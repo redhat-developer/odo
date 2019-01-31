f4dca2e0be99e9a989ee04d95b6c1f2cdb3f568c
Chris Laprun <metacosm@gmail.com>
2019-01-31 13:19:16 +0100

    Implement Complete/Validate/Run pattern for set.

--- pkg/odo/cli/utils/config/config.go
+++ pkg/odo/cli/utils/config/config.go
@@ -7,14 +7,15 @@ import (
 	ktemplates "k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 )
 
+var supportedParameters = map[string]string{
+	"UpdateNotification": "Controls if an update notification is shown or not (true or false)",
+	"NamePrefix":         "Default prefix is the current directory name. Use this value to set a default name prefix",
+	"Timeout":            "Timeout (in seconds) for OpenShift server connection check",
+}
+
 const RecommendedCommandName = "config"
 
-var configLongDesc = ktemplates.LongDesc(`Modifies Odo specific configuration settings within the config file.
-
-Available Parameters:
-UpdateNotification - Controls if an update notification is shown or not (true or false)
-NamePrefix - Default prefix is the current directory name. Use this value to set a default name prefix
-Timeout - Timeout (in seconds) for OpenShift server connection check`)
+var configLongDesc = ktemplates.LongDesc(fmt.Sprintf("Modifies Odo specific configuration settings within the config file.\n%s", formatSupportedParameters()))
 
 // NewCmdConfiguration implements the utils config odo command
 func NewCmdConfiguration(name, fullName string) *cobra.Command {
@@ -35,4 +36,23 @@ func NewCmdConfiguration(name, fullName string) *cobra.Command {
 	configurationCmd.SetUsageTemplate(util.CmdUsageTemplate)
 
 	return configurationCmd
+}
+
+func formatSupportedParameters() (result string) {
+	for k, v := range supportedParameters {
+		result = result + "\n" + k + " - " + v
+	}
+	return
+}
+
+func getSupportedParameters() []string {
+	keys := make([]string, len(supportedParameters))
+
+	i := 0
+	for k := range supportedParameters {
+		keys[i] = k
+		i++
+	}
+
+	return keys
 }
--- pkg/odo/cli/utils/config/set.go
+++ pkg/odo/cli/utils/config/set.go
@@ -2,47 +2,81 @@ package config
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"github.com/redhat-developer/odo/pkg/config"
+	"github.com/redhat-developer/odo/pkg/odo/util"
 	"github.com/spf13/cobra"
+	ktemplates "k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 	"strings"
 )
 
 const setCommandName = "set"
 
+var (
+	setLongDesc = ktemplates.LongDesc(fmt.Sprintf("Set an individual value in the Odo configuration file.\n%s", formatSupportedParameters()))
+	setExample  = ktemplates.Examples(`
+   # Set a configuration value
+   %[1]s UpdateNotification false
+   %[1]s NamePrefix "app"
+   %[1]s Timeout 20
+	`)
+)
+
+// SetOptions encapsulates the options for the command
+type SetOptions struct {
+	paramName  string
+	paramValue string
+}
+
+// NewSetOptions creates a new SetOptions instance
+func NewSetOptions() *SetOptions {
+	return &SetOptions{}
+}
+
+// Complete completes SetOptions after they've been created
+func (o *SetOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	o.paramName = strings.ToLower(args[0])
+	o.paramValue = args[1]
+	return
+}
+
+// Validate validates the SetOptions based on completed values
+func (o *SetOptions) Validate() (err error) {
+	if _, ok := supportedParameters[o.paramName]; !ok {
+		return fmt.Errorf("unknown parameter '%s', supported parameters: %v", o.paramName, getSupportedParameters())
+	}
+	return
+}
+
+// Run contains the logic for the command
+func (o *SetOptions) Run() (err error) {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	return cfg.SetConfiguration(o.paramName, o.paramValue)
+}
+
 // NewCmdSet implements the config set odo command
 func NewCmdSet(name, fullName string) *cobra.Command {
-
+	o := NewSetOptions()
 	configurationSetCmd := &cobra.Command{
-		Use:   name,
-		Short: "Set a value in odo config file",
-		Long: `Set an individual value in the Odo configuration file
-
-Available Parameters:
-UpdateNotification - Controls if an update notification is shown or not (true or false)
-NamePrefix - Default prefix is the current directory name. Use this value to set a default name prefix.
-Timeout - Timeout (in seconds) for OpenShift server connection check`,
-		Example: `
-   # Set a configuration value
-   odo utils config set UpdateNotification false
-   odo utils config set NamePrefix "app"
-   odo utils config set Timeout 20
-	`,
+		Use:     name,
+		Short:   "Set a value in odo config file",
+		Long:    setLongDesc,
+		Example: fmt.Sprintf(setExample, fullName),
 		Args: func(cmd *cobra.Command, args []string) error {
 			if len(args) < 2 {
-				return fmt.Errorf("Please provide a parameter name and value")
+				return fmt.Errorf("please provide a parameter name and value")
 			} else if len(args) > 2 {
-				return fmt.Errorf("Only one value per parameter is allowed")
+				return fmt.Errorf("only one value per parameter is allowed")
 			} else {
 				return nil
 			}
 
-		}, RunE: func(cmd *cobra.Command, args []string) error {
-			cfg, err := config.New()
-			if err != nil {
-				return errors.Wrapf(err, "unable to set configuration")
-			}
-			return cfg.SetConfiguration(strings.ToLower(args[0]), args[1])
+		}, Run: func(cmd *cobra.Command, args []string) {
+			util.LogErrorAndExit(o.Complete(name, cmd, args), "")
+			util.LogErrorAndExit(o.Validate(), "")
+			util.LogErrorAndExit(o.Run(), "")
 		},
 	}
 
