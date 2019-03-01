109b5cfaf8f0e6edaaa35ac7ee8844b39e3273c4
Suraj Narwade <surajnarwade353@gmail.com>
2019-03-01 09:48:59 +0530

    Added unit test

--- pkg/application/application_test.go
+++ pkg/application/application_test.go
@@ -2,11 +2,21 @@ package application
 
 import (
 	"os"
+	"reflect"
 	"regexp"
 	"testing"
 
+	appsv1 "github.com/openshift/api/apps/v1"
+	applabels "github.com/redhat-developer/odo/pkg/application/labels"
+	"github.com/redhat-developer/odo/pkg/component"
+	componentlabels "github.com/redhat-developer/odo/pkg/component/labels"
 	"github.com/redhat-developer/odo/pkg/config"
+	"github.com/redhat-developer/odo/pkg/occlient"
 	"github.com/redhat-developer/odo/pkg/testingutil"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ktesting "k8s.io/client-go/testing"
 )
 
 func TestGetDefaultAppName(t *testing.T) {
@@ -69,6 +79,187 @@ func TestGetDefaultAppName(t *testing.T) {
 			if !match {
 				fetchedConfig, _ := config.New()
 				t.Errorf("randomly generated application name %s does not match regexp %s and config is %+v\nthe prefix is %s", name, tt.wantRE, fetchedConfig, *fetchedConfig.OdoSettings.NamePrefix)
+			}
+		})
+	}
+}
+
+func TestGetMachineReadableFormat(t *testing.T) {
+	type args struct {
+		// client      *occlient.Client
+		appName     string
+		projectName string
+		active      bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want App
+	}{
+		{
+
+			name: "Test Case: machine readable output for application",
+			args: args{
+				appName:     "myapp",
+				projectName: "myproject",
+				active:      true,
+			},
+			want: App{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "app",
+					APIVersion: "odo.openshift.io/v1alpha1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "myapp",
+					Namespace: "myproject",
+				},
+				Spec: AppSpec{
+					Components: []string{"frontend"},
+				},
+				Status: AppStatus{
+					Active: true,
+				},
+			},
+		},
+	}
+
+	dcList := appsv1.DeploymentConfigList{
+		Items: []appsv1.DeploymentConfig{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "frontend-myapp",
+					Namespace: "myproject",
+					Labels: map[string]string{
+						applabels.ApplicationLabel:         "myapp",
+						componentlabels.ComponentLabel:     "frontend",
+						componentlabels.ComponentTypeLabel: "nodejs",
+					},
+					Annotations: map[string]string{
+						component.ComponentSourceTypeAnnotation: "local",
+					},
+				},
+				Spec: appsv1.DeploymentConfigSpec{
+					Template: &corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name: "dummyContainer",
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "backend-app",
+					Namespace: "myproject",
+					Labels: map[string]string{
+						applabels.ApplicationLabel:         "app",
+						componentlabels.ComponentLabel:     "backend",
+						componentlabels.ComponentTypeLabel: "java",
+					},
+					Annotations: map[string]string{
+						component.ComponentSourceTypeAnnotation: "local",
+					},
+				},
+				Spec: appsv1.DeploymentConfigSpec{
+					Template: &corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name: "dummyContainer",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Fake the client with the appropriate arguments
+			client, fakeClientSet := occlient.FakeNew()
+			//fake the dcs
+			fakeClientSet.AppsClientset.PrependReactor("list", "deploymentconfigs", func(action ktesting.Action) (bool, runtime.Object, error) {
+				return true, &dcList, nil
+			})
+
+			for i := range dcList.Items {
+				fakeClientSet.AppsClientset.PrependReactor("get", "deploymentconfigs", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, &dcList.Items[i], nil
+				})
+			}
+			if got := GetMachineReadableFormat(client, tt.args.appName, tt.args.projectName, tt.args.active); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetMachineReadableFormat() = %v,\n want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetMachineReadableFormatForList(t *testing.T) {
+	type args struct {
+		apps []App
+	}
+	tests := []struct {
+		name string
+		args args
+		want AppList
+	}{
+		{
+			name: "Test Case: Machine Readable for Application List",
+			args: args{
+				apps: []App{
+					{
+						TypeMeta: metav1.TypeMeta{
+							Kind:       "app",
+							APIVersion: "odo.openshift.io/v1alpha1",
+						},
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "myapp",
+						},
+						Spec: AppSpec{
+							Components: []string{"frontend"},
+						},
+						Status: AppStatus{
+							Active: true,
+						},
+					},
+				},
+			},
+			want: AppList{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "List",
+					APIVersion: "odo.openshift.io/v1alpha1",
+				},
+				ListMeta: metav1.ListMeta{},
+				Items: []App{
+					{
+						TypeMeta: metav1.TypeMeta{
+							Kind:       "app",
+							APIVersion: "odo.openshift.io/v1alpha1",
+						},
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "myapp",
+						},
+						Spec: AppSpec{
+							Components: []string{"frontend"},
+						},
+						Status: AppStatus{
+							Active: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetMachineReadableFormatForList(tt.args.apps); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetMachineReadableFormatForList() = %v, want %v", got, tt.want)
 			}
 		})
 	}
