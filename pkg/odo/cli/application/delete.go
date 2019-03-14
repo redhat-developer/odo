179a4967797255658783f6dca8074998e4a8b689
syamgk <syamgk01@gmail.com>
2019-03-14 15:27:00 +0530

    add -o json flag to `odo app delete`

--- pkg/odo/cli/application/delete.go
+++ pkg/odo/cli/application/delete.go
@@ -9,6 +9,7 @@ import (
 	"github.com/redhat-developer/odo/pkg/odo/cli/ui"
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	"github.com/redhat-developer/odo/pkg/odo/util/completion"
+	"github.com/redhat-developer/odo/pkg/util"
 	"github.com/spf13/cobra"
 	ktemplates "k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 )
@@ -45,6 +46,9 @@ func (o *DeleteOptions) Complete(name string, cmd *cobra.Command, args []string)
 
 // Validate validates the DeleteOptions based on completed values
 func (o *DeleteOptions) Validate() (err error) {
+	if !util.CheckOutputFlag(o.OutputFlag) {
+		return fmt.Errorf("given output format %s is not supported", o.OutputFlag)
+	}
 	return ensureAppExists(o.Client, o.appName, o.Project)
 }
 
@@ -56,7 +60,12 @@ func (o *DeleteOptions) Run() (err error) {
 		return err
 	}
 
-	if o.force || ui.Proceed(fmt.Sprintf("Are you sure you want to delete the application: %v from project: %v", o.appName, o.Project)) {
+	if o.OutputFlag == "json" {
+		err = application.Delete(o.Client, o.appName)
+		if err != nil {
+			return err
+		}
+	} else if o.force || ui.Proceed(fmt.Sprintf("Are you sure you want to delete the application: %v from project: %v", o.appName, o.Project)) {
 		err = application.Delete(o.Client, o.appName)
 		if err != nil {
 			return err
@@ -83,6 +92,7 @@ func NewCmdDelete(name, fullName string) *cobra.Command {
 	}
 
 	command.Flags().BoolVarP(&o.force, "force", "f", false, "Delete application without prompting")
+	genericclioptions.AddOutputFlag(command)
 
 	project.AddProjectFlag(command)
 	completion.RegisterCommandHandler(command, completion.AppCompletionHandler)
