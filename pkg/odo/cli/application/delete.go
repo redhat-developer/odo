ba7eb8a7f913ae93e5e9d36e06f63f6d29cc3569
Syam G Krishnan <syamgk01@gmail.com>
2019-03-19 17:52:40 +0530

    Add -o json  flag to `odo app delete`   (#1456)
    * add -o json flag to `odo app delete`
    
    * add e2e test to validate odo app delete foo -o json
    
    * disable showing appinfo when machinereadable flag is present

--- pkg/odo/cli/application/delete.go
+++ pkg/odo/cli/application/delete.go
@@ -9,6 +9,7 @@ import (
 	"github.com/redhat-developer/odo/pkg/odo/cli/ui"
 	"github.com/redhat-developer/odo/pkg/odo/genericclioptions"
 	"github.com/redhat-developer/odo/pkg/odo/util/completion"
+	"github.com/redhat-developer/odo/pkg/util"
 	"github.com/spf13/cobra"
 	ktemplates "k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 )
@@ -45,11 +46,22 @@ func (o *DeleteOptions) Complete(name string, cmd *cobra.Command, args []string)
 
 // Validate validates the DeleteOptions based on completed values
 func (o *DeleteOptions) Validate() (err error) {
+	if !util.CheckOutputFlag(o.OutputFlag) {
+		return fmt.Errorf("given output format %s is not supported", o.OutputFlag)
+	}
 	return ensureAppExists(o.Client, o.appName, o.Project)
 }
 
 // Run contains the logic for the odo command
 func (o *DeleteOptions) Run() (err error) {
+	if o.OutputFlag == "json" {
+		err = application.Delete(o.Client, o.appName)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
 	// Print App Information which will be deleted
 	err = printDeleteAppInfo(o.Client, o.appName, o.Project)
 	if err != nil {
@@ -83,6 +95,7 @@ func NewCmdDelete(name, fullName string) *cobra.Command {
 	}
 
 	command.Flags().BoolVarP(&o.force, "force", "f", false, "Delete application without prompting")
+	genericclioptions.AddOutputFlag(command)
 
 	project.AddProjectFlag(command)
 	completion.RegisterCommandHandler(command, completion.AppCompletionHandler)
