150ca3c2244e3e7a7612f09e91af9bca7f95b2e7
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-04-01 20:49:48 +0530

    Adding tests for app list

--- tests/e2e/component.go
+++ tests/e2e/component.go
@@ -69,6 +69,8 @@ func componentTests(componentCmdPrefix string) {
 			runCmdShouldPass("mv .odo .odo_tmp")
 			session := runCmdShouldFail("odo component list")
 			Expect(session).To(ContainSubstring("the current directory does not represent an odo component"))
+			session = runCmdShouldFail("odo app list")
+			Expect(session).To(ContainSubstring("the current directory does not represent an odo component"))
 			// clean up
 			runCmdShouldPass("mv .odo_tmp .odo")
 			os.RemoveAll(dirName)
