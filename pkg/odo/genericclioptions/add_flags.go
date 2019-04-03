fdb75ff4e5cabe5532d6b706f07714f4a41bf1e3
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-04-03 16:44:49 +0530

    Adding function to add context flag needed across most commands

--- pkg/odo/genericclioptions/add_flags.go
+++ pkg/odo/genericclioptions/add_flags.go
@@ -6,3 +6,8 @@ import "github.com/spf13/cobra"
 func AddOutputFlag(cmd *cobra.Command) {
 	cmd.Flags().StringP(OutputFlagName, "o", "", "Specify output format, supported format: json")
 }
+
+// AddContextFlag adds `context` flag to given cobra command
+func AddContextFlag(cmd *cobra.Command, setValueTo *string) {
+	cmd.Flags().StringVar(setValueTo, "context", "", "Use context to indicate the path where the component settings need to be saved and this directory should contain component source for local and binary components")
+}
