81af0888cb855d5f9f93c80da80002a5ca247961
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-04-04 10:21:09 +0530

    Updating AddContext to handle nil flag value pointer

--- pkg/odo/genericclioptions/add_flags.go
+++ pkg/odo/genericclioptions/add_flags.go
@@ -9,5 +9,9 @@ func AddOutputFlag(cmd *cobra.Command) {
 
 // AddContextFlag adds `context` flag to given cobra command
 func AddContextFlag(cmd *cobra.Command, setValueTo *string) {
-	cmd.Flags().StringVar(setValueTo, "context", "", "Use context to indicate the path where the component settings need to be saved and this directory should contain component source for local and binary components")
+	if setValueTo != nil {
+		cmd.Flags().StringVar(setValueTo, "context", "", "Use context to indicate the path where the component settings need to be saved and this directory should contain component source for local and binary components")
+	} else {
+		cmd.Flags().String("context", "", "Use context to indicate the path where the component settings need to be saved and this directory should contain component source for local and binary components")
+	}
 }
