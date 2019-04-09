45eec1fab583307804fcf13c6f9a3c498850ee32
Girish Ramnani <girishramnani95@gmail.com>
2019-04-09 14:25:46 +0530

    moved config file exist to complete part

--- pkg/odo/cli/component/create.go
+++ pkg/odo/cli/component/create.go
@@ -235,6 +235,13 @@ func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string
 	if err != nil {
 		return errors.Wrap(err, "failed intiating local config")
 	}
+
+	// check to see if config file exists or not, if it does that
+	// means we shouldn't allow the user to override the current component
+	if co.localConfigInfo.ConfigFileExists() {
+		return errors.New("this directory already contains a component")
+	}
+
 	co.componentSettings = co.localConfigInfo.GetComponentSettings()
 
 	co.Context = genericclioptions.NewContextCreatingAppIfNeeded(cmd)
@@ -356,10 +363,6 @@ func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string
 
 // Validate validates the create parameters
 func (co *CreateOptions) Validate() (err error) {
-	if co.localConfigInfo.ConfigFileExists() {
-		return errors.New("this directory already contains a component")
-	}
-
 	return component.ValidateComponentCreateRequest(co.Context.Client, co.componentSettings, false)
 }
 
