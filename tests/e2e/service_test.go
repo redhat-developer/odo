5ce9272dde2e61281b62d27070a2a6c368d1cfdc
Tomas Kral <tkral@redhat.com>
2019-04-11 18:26:08 +0200

    wait for brokers

--- tests/e2e/service_test.go
+++ tests/e2e/service_test.go
@@ -51,7 +51,6 @@ var _ = Describe("odoServiceE2e", func() {
 			runCmdShouldPass("oc login -u system:admin")
 			runCmdShouldPass("oc get pods -n openshift-automation-service-broker")
 			runCmdShouldPass("oc get clusterservicebroker")
-			runCmdShouldPass("oc describe clusterservicebroker openshift-automation-service-broker")
 			runCmdShouldPass("oc login -u developer")
 			runCmdShouldPass("oc get clusterserviceclasses")
 
