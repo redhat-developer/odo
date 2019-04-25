15a969031ad619f2216dd5fb479889fae790790f
Tomas Kral <tkral@redhat.com>
2019-04-25 19:51:42 +0200

    use Gomega matchers for json output (#1625)

--- tests/e2e/json_test.go
+++ tests/e2e/json_test.go
@@ -1,9 +1,7 @@
 package e2e
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,9 +18,7 @@ var _ = Describe("odojsonoutput", func() {
 		It("should be able to return empty list", func() {
 			actual := runCmdShouldPass("odo app list -o json --project json-test")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[]}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
-
+			Expect(desired).Should(MatchJSON(actual))
 		})
 		// Basic creation
 		It("Pre-Test Creation Json", func() {
@@ -33,8 +29,7 @@ var _ = Describe("odojsonoutput", func() {
 		It("should be able to list empty url list", func() {
 			actual := runCmdShouldPass("odo url list -o json")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":null}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 
 		})
 		// odo url create
@@ -56,45 +51,39 @@ var _ = Describe("odojsonoutput", func() {
 		It("should be able to create storage", func() {
 			actual := runCmdShouldPass("odo storage create mystorage --path=/opt/app-root/src/storage/ --size=1Gi -o json")
 			desired := `{"kind":"storage","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"mystorage","creationTimestamp":null},"spec":{"size":"1Gi"},"status":{"path":"/opt/app-root/src/storage/"}}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 		})
 		// odo project list -o json
 		It("should be able to list the projects", func() {
 			actual := runCmdShouldPass("odo project list -o json")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[{"kind":"Project","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"json-test","creationTimestamp":null},"spec":{"apps":["myapp"]},"status":{"active":true}},{"kind":"Project","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"myproject","creationTimestamp":null},"spec":{"apps":["myapp"]},"status":{"active":false}}]}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 
 		})
 		// odo app describe myapp -o json
 		It("should be able to describe app", func() {
 			desired := `{"kind":"app","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"myapp","namespace":"json-test","creationTimestamp":null},"spec":{"components":["nodejs"]},"status":{"active":false}}`
 			actual := runCmdShouldPass("odo app describe myapp -o json")
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 		})
 		// odo app list -o json
 		It("should be able to list the apps", func() {
 			actual := runCmdShouldPass("odo app list -o json")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[{"kind":"app","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"myapp","namespace":"json-test","creationTimestamp":null},"spec":{"components":["nodejs"]},"status":{"active":false}}]}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 
 		})
 		// odo describe nodejs -o json
 		It("should be able to describe component", func() {
 			actual := runCmdShouldPass("odo describe nodejs -o json")
 			desired := `{"kind":"Component","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"nodejs","creationTimestamp":null},"spec":{"type":"nodejs","source":"https://github.com/openshift/nodejs-ex","url":["myurl"],"storage":["mystorage"]},"status":{"active":false}}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 		})
 		// odo list -o json
 		It("should be able to list components", func() {
 			actual := runCmdShouldPass("odo list -o json")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[{"kind":"Component","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"nodejs","creationTimestamp":null},"spec":{"type":"nodejs","source":"https://github.com/openshift/nodejs-ex","url":["myurl"],"storage":["mystorage"]},"status":{"active":false}}]}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 
 		})
 		// odo url list -o json
@@ -102,8 +91,7 @@ var _ = Describe("odojsonoutput", func() {
 			actual := runCmdShouldPass("odo url list -o json")
 			url := runCmdShouldPass("oc get routes myurl-myapp -o jsonpath={.spec.host}")
 			desired := fmt.Sprintf(`{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[{"kind":"url","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"myurl","creationTimestamp":null},"spec":{"host":"%s","protocol":"http","port":8080}}]}`, url)
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
+			Expect(desired).Should(MatchJSON(actual))
 
 		})
 
@@ -111,9 +99,7 @@ var _ = Describe("odojsonoutput", func() {
 		It("should be able to list storage", func() {
 			actual := runCmdShouldPass("odo storage list -o json")
 			desired := `{"kind":"List","apiVersion":"odo.openshift.io/v1aplha1","metadata":{},"items":[{"kind":"Storage","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"mystorage","creationTimestamp":null},"spec":{"size":"1Gi"},"status":{"path":"/opt/app-root/src/storage/"}}]}`
-			areEqual, _ := compareJSON(desired, actual)
-			Expect(areEqual).To(BeTrue())
-
+			Expect(desired).Should(MatchJSON(actual))
 		})
 		// cleanup
 		It("Cleanup", func() {
@@ -123,17 +109,3 @@ var _ = Describe("odojsonoutput", func() {
 
 	})
 })
-
-func compareJSON(desired, actual string) (bool, error) {
-	var o1, o2 interface{}
-	err := json.Unmarshal([]byte(actual), &o1)
-	if err != nil {
-		return false, fmt.Errorf("Error mashalling string :: %s", err.Error())
-	}
-	err = json.Unmarshal([]byte(desired), &o2)
-	if err != nil {
-		return false, fmt.Errorf("Error mashalling string :: %s", err.Error())
-	}
-	return reflect.DeepEqual(o1, o2), nil
-
-}
