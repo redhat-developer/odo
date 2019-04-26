8331d64543913a6d00f39a9f7bc16f9a9689e1f0
cdrage <charlie@charliedrage.com>
2019-04-26 09:22:10 -0400

    Re-add the color

--- pkg/auth/login.go
+++ pkg/auth/login.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"os"
 
+	"github.com/fatih/color"
 	odolog "github.com/openshift/odo/pkg/log"
 	"github.com/openshift/origin/pkg/oc/cli/login"
 	"k8s.io/client-go/tools/clientcmd"
@@ -50,11 +51,14 @@ func Login(server, username, password, token, caAuth string, skipTLS bool) error
 
 	// 2. Gather information and connect
 	// We set the color to "yellow" to distinguish between odo and oc output
+	color.Set(color.FgYellow)
 	if err := a.GatherInfo(); err != nil {
 		// Make sure we newline between the "Connecting" and error-out messages
 		odolog.Info("")
+		color.Unset()
 		return err
 	}
+	color.Unset()
 
 	// 3. Output the information in a correct format
 	// In order to interpret the error message and manipulate the output of `oc`
