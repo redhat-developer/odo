60b2c8a9a01c6ef0362a077f2dc1a9a0a352cf82
cdrage <charlie@charliedrage.com>
2019-05-02 12:03:10 -0400

    Adds another fix with regards to filtering messages

--- pkg/auth/login.go
+++ pkg/auth/login.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/fatih/color"
@@ -15,6 +16,16 @@ import (
 
 // Login takes of authentication part and returns error if there any
 func Login(server, username, password, token, caAuth string, skipTLS bool) error {
+
+	// Here we are grabbing the stdout output and then
+	// throwing it through "copyAndFilter" in order to get
+	// a correctly filtered result from `odo login`
+	filteredReader, filteredWriter := io.Pipe()
+	go func() {
+		defer filteredWriter.Close()
+		_, _ = copyAndFilter(odolog.GetStdout(), filteredReader)
+	}()
+
 	a := login.LoginOptions{
 		Server:         server,
 		CommandName:    "odo",
@@ -26,7 +37,7 @@ func Login(server, username, password, token, caAuth string, skipTLS bool) error
 		Token:          token,
 		PathOptions:    &clientcmd.PathOptions{GlobalFile: clientcmd.RecommendedHomeFile, EnvVar: clientcmd.RecommendedConfigPathEnvVar, ExplicitFileFlag: "config", LoadingRules: &clientcmd.ClientConfigLoadingRules{ExplicitPath: ""}},
 		RequestTimeout: 0,
-		IOStreams:      genericclioptions.IOStreams{Out: odolog.GetStdout(), In: os.Stdin, ErrOut: odolog.GetStderr()},
+		IOStreams:      genericclioptions.IOStreams{Out: filteredWriter, In: os.Stdin, ErrOut: odolog.GetStderr()},
 	}
 
 	// initialize client-go client and read starting kubeconfig file
@@ -49,41 +60,34 @@ func Login(server, username, password, token, caAuth string, skipTLS bool) error
 	}
 
 	// 1. Say we're connecting
-	odolog.Info("Connecting to the OpenShift cluster")
+	odolog.Info("Connecting to the OpenShift cluster\n")
 
 	// 2. Gather information and connect
 	// We set the color to "yellow" to distinguish between odo and oc output
 	color.Set(color.FgYellow)
-	if err := a.GatherInfo(); err != nil {
-		// Make sure we newline between the "Connecting" and error-out messages
-		odolog.Info("")
-		color.Unset()
+	defer color.Unset()
 
-		// Handle the error messages here. This is copied over from:
-		// https://github.com/openshift/origin/blob/master/pkg/oc/cli/login/login.go#L60
-		// as unauthorization errors are handled MANUALLY by oc.
+	// Handle the error messages here. This is copied over from:
+	// https://github.com/openshift/origin/blob/master/pkg/oc/cli/login/login.go#L60
+	// as unauthorization errors are handled MANUALLY by oc.
+	if err := a.GatherInfo(); err != nil {
 		if kapierrors.IsUnauthorized(err) {
-			fmt.Fprintln(odolog.GetStdout(), "Login failed (401 Unauthorized)")
-			fmt.Fprintln(odolog.GetStdout(), "Verify you have provided correct credentials.")
+			fmt.Println("Login failed (401 Unauthorized)")
+			fmt.Println("Verify you have provided correct credentials.")
 
 			if err, isStatusErr := err.(*kapierrors.StatusError); isStatusErr {
 				if details := err.Status().Details; details != nil {
 					for _, cause := range details.Causes {
-						fmt.Fprintln(odolog.GetStdout(), cause.Message)
+						fmt.Println(cause.Message)
 					}
 				}
 			}
 		}
-
+		color.Unset()
 		return err
 	}
-	color.Unset()
 
-	// 3. Output the information in a correct format
-	// In order to interpret the error message and manipulate the output of `oc`
-	// we pass in a buffer in order to modify the output.
-	loginOutBuffer := &bytes.Buffer{}
-	a.IOStreams = genericclioptions.IOStreams{Out: loginOutBuffer, In: os.Stdin, ErrOut: os.Stderr}
+	// 3. Correctly save the configuration
 	newFileCreated, err := a.SaveConfig()
 	if err != nil {
 		return err
@@ -95,18 +99,42 @@ func Login(server, username, password, token, caAuth string, skipTLS bool) error
 		odolog.Infof("Welcome! See '%s help' to get started.", a.CommandName)
 	}
 
-	// Process the messages returned by openshift login code and print our message
-	originalOutMsg := loginOutBuffer.Bytes()
-	loginSuccessMsg := bytes.Replace(originalOutMsg, []byte("new-project"), []byte("project create"), -1)
-	loginSuccessMsg = bytes.Replace(loginSuccessMsg, []byte("<projectname>"), []byte("<project-name>"), -1)
-
-	// Add newline + output succeded
-	odolog.Info("")
-	if len(originalOutMsg) == 0 {
-		odolog.Success("Login succeeded")
-	} else {
-		odolog.Successf("%s", loginSuccessMsg)
-	}
-
 	return nil
+}
+
+// copyAndFilter captures the output, filters it and then spits it back out to stdout.
+// Kindly taken from https://stackoverflow.com/questions/54570268/filtering-the-output-of-a-terminal-output-using-golang
+func copyAndFilter(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(filteredInformation(d))
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
+	}
+}
+
+// filteredInformation takes a list of strings ([]byte), replaces them and spits it back out
+// This is used since we utilize `oc login` withi odo and require certain strings to be filtered / changed
+// to their odo equivilant
+func filteredInformation(s []byte) []byte {
+
+	// List of strings to correctly filter
+	s = bytes.Replace(s, []byte("new-project"), []byte("project create"), -1)
+	s = bytes.Replace(s, []byte("<projectname>"), []byte("<project-name>"), -1)
+
+	return s
 }
