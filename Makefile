766583bd7f3fc642411b9c998cc58ed2d6cd016a
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-05-22 14:49:44 +0530

    Adding url integration tests

--- Makefile
+++ Makefile
@@ -169,9 +169,20 @@ test-odo-login-e2e:
 test-odo-config:
 	go test -v github.com/openshift/odo/tests/integration --ginkgo.focus="odo config test" -ginkgo.slowSpecThreshold=$(SLOW_SPEC_THRESHOLD) -ginkgo.v -timeout $(TIMEOUT)
 
+<<<<<<< HEAD
 # Run all integration tests
 .PHONY: test-integration
 test-integration:
+=======
+# Run config tests
+.PHONY: test-odo-url-int
+test-odo-url-int:
+	go test -v github.com/openshift/odo/tests/integration --ginkgo.focus="odoURLIntegration" -ginkgo.slowSpecThreshold=$(SLOW_SPEC_THRESHOLD) -ginkgo.v -timeout $(TIMEOUT)
+
+# Run all e2e tests
+.PHONY: test-e2e
+test-e2e:
+>>>>>>> Adding url integration tests
 	go test -v github.com/openshift/odo/tests/integration -ginkgo.slowSpecThreshold=$(SLOW_SPEC_THRESHOLD) -ginkgo.v -timeout $(TIMEOUT)
 
 # Run e2e test scenarios
---
+++ tests/integration/url_test.go
@@ -0,0 +1,87 @@
+package integration
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/openshift/odo/tests/helper"
+)
+
+var _ = Describe("odoURLIntegration", func() {
+	//new clean project and context for each test
+	var project string
+	var context string
+
+	//  current directory and project (before eny test is run) so it can restored  after all testing is done
+	var originalDir string
+	var originalProject string
+	var oc helper.OcRunner
+
+	// Setup up state for each test spec
+	// create new project (not set as active) and new context directory for each test spec
+	// This is before every spec (It)
+	var _ = BeforeEach(func() {
+		SetDefaultEventuallyTimeout(10 * time.Minute)
+		oc = helper.NewOcRunner("oc")
+		project = helper.CreateRandProject()
+		context = helper.CreateNewContext()
+		originalDir = helper.Getwd()
+		originalProject = oc.GetCurrentProject()
+		helper.Chdir(context)
+		oc.SwitchProject(project)
+	})
+
+	// Clean up after the test
+	// This is run after every Spec (It)
+	var _ = AfterEach(func() {
+		helper.Chdir(originalDir)
+		oc.SwitchProject(originalProject)
+		helper.DeleteProject(project)
+		helper.DeleteDir(context)
+	})
+
+	Context("Listing urls", func() {
+		It("should list appropriate URLs and push message", func() {
+			var stdout string
+			url1 := helper.RandString(5)
+			url2 := helper.RandString(5)
+			componentName := helper.RandString(6)
+			helper.CmdShouldPass("odo", "create", "nodejs", "--project", project, componentName, "--ref", "master", "--git", "https://github.com/openshift/nodejs-ex")
+			helper.CmdShouldPass("odo", "push")
+			stdout = helper.CmdShouldFail("odo", "url", "list")
+			Expect(stdout).To(ContainSubstring("no URLs found"))
+			helper.CmdShouldPass("odo", "url", "create", url1, "--port", "8080")
+			stdout = helper.CmdShouldPass("odo", "url", "list")
+			Expect(stdout).To(ContainSubstring(url1))
+			Expect(stdout).To(ContainSubstring("<not created on cluster>"))
+			Expect(stdout).To(ContainSubstring("Present"))
+			Expect(stdout).To(ContainSubstring("To create URLS on the OpenShift Cluster, please use `odo push`"))
+			helper.CmdShouldPass("odo", "push")
+			stdout = helper.CmdShouldPass("odo", "url", "list")
+			Expect(stdout).To(ContainSubstring(url1))
+			Expect(stdout).ToNot(ContainSubstring("<not created on cluster>"))
+			Expect(stdout).To(ContainSubstring("Present"))
+			Expect(stdout).ToNot(ContainSubstring("To create URLS on the OpenShift Cluster, please use `odo push`"))
+			helper.CmdShouldPass("odo", "url", "delete", url1, "-f")
+			stdout = helper.CmdShouldPass("odo", "url", "list")
+			Expect(stdout).To(ContainSubstring(url1))
+			Expect(stdout).To(ContainSubstring("Absent"))
+			Expect(stdout).To(ContainSubstring("To delete URLS on the OpenShift Cluster, please use `odo push`"))
+			helper.CmdShouldPass("odo", "url", "create", url2, "--port", "8000")
+			stdout = helper.CmdShouldPass("odo", "url", "list")
+			Expect(stdout).To(ContainSubstring(url1))
+			Expect(stdout).To(ContainSubstring("Absent"))
+			Expect(stdout).To(ContainSubstring(url2))
+			Expect(stdout).To(ContainSubstring("Present"))
+			Expect(stdout).To(ContainSubstring("To create/delete URLS on the OpenShift Cluster, please use `odo push`"))
+			helper.CmdShouldPass("odo", "push")
+			stdout = helper.CmdShouldPass("odo", "url", "list")
+			Expect(stdout).ToNot(ContainSubstring(url1))
+			Expect(stdout).ToNot(ContainSubstring("Absent"))
+			Expect(stdout).To(ContainSubstring(url2))
+			Expect(stdout).To(ContainSubstring("Present"))
+			Expect(stdout).ToNot(ContainSubstring("URLS on the OpenShift Cluster, please use `odo push`"))
+		})
+	})
+})
