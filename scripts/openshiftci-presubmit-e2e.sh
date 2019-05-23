17984717be2c3921a3413a0fdfac6ec187f55c8a
arout <arout@redhat.com>
2019-05-23 14:48:44 +0530

    Installing ginkgo binary

--- scripts/openshiftci-presubmit-e2e.sh
+++ scripts/openshiftci-presubmit-e2e.sh
@@ -10,6 +10,9 @@ export TIMEOUT="30m"
 make configure-installer-tests-cluster
 make bin
 echo $GOPATH
+mkdir -p $GOPATH/bin
+go get -u github.com/onsi/ginkgo/ginkgo
+ls $GOPATH/bin
 export PATH="$PATH:$(pwd):$GOPATH/bin"
 export CUSTOM_HOMEDIR="/tmp/artifacts"
 make clean-test
