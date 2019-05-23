25c7950b2f1de99a193707d40da93904a669e64e
arout <arout@redhat.com>
2019-05-23 17:36:39 +0530

    Removed debug line

--- scripts/openshiftci-presubmit-e2e.sh
+++ scripts/openshiftci-presubmit-e2e.sh
@@ -9,13 +9,10 @@ export CI="openshift"
 export TIMEOUT="30m"
 make configure-installer-tests-cluster
 make bin
-echo $GOPATH
 mkdir -p $GOPATH/bin
 go get -u github.com/onsi/ginkgo/ginkgo
-ls $GOPATH/bin
 export PATH="$PATH:$(pwd):$GOPATH/bin"
 export CUSTOM_HOMEDIR="/tmp/artifacts"
-make clean-test
 make test-generic
 make test-odo-login-e2e
 make test-json-format-output
