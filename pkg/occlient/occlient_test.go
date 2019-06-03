6ff4bf2dd0722f36aaaf5625a41e4e0177a86319
Tomas Kral <tkral@redhat.com>
2019-06-03 16:28:28 +0200

    fix tests for CreateNewProject

--- pkg/occlient/occlient_test.go
+++ pkg/occlient/occlient_test.go
@@ -2565,8 +2565,6 @@ func TestCreateNewProject(t *testing.T) {
 			wait:     true,
 			wantErr:  false,
 		},
-
-		// TODO: Currently fails. Enable once fixed.
 		// {
 		// 	name:     "Case 2: empty project name",
 		// 	projName: "",
@@ -2578,6 +2576,15 @@ func TestCreateNewProject(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			fkclient, fkclientset := FakeNew()
 
+			fkclientset.ProjClientset.PrependReactor("create", "projectrequests", func(action ktesting.Action) (bool, runtime.Object, error) {
+				proj := projectv1.Project{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: tt.projName,
+					},
+				}
+				return true, &proj, nil
+			})
+
 			if tt.wait {
 				fkWatch := watch.NewFake()
 				// Change the status
@@ -2588,7 +2595,7 @@ func TestCreateNewProject(t *testing.T) {
 						},
 					})
 				}()
-				fkclientset.ProjClientset.PrependWatchReactor("projects", func(action ktesting.Action) (handled bool, ret watch.Interface, err error) {
+				fkclientset.ProjClientset.AddWatchReactor("projects", func(action ktesting.Action) (handled bool, ret watch.Interface, err error) {
 					if len(tt.projName) == 0 {
 						return true, nil, fmt.Errorf("error watching project")
 					}
@@ -2596,31 +2603,22 @@ func TestCreateNewProject(t *testing.T) {
 				})
 			}
 
-			fkclientset.ProjClientset.PrependReactor("create", "projectrequests", func(action ktesting.Action) (bool, runtime.Object, error) {
-				proj := projectv1.Project{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: tt.projName,
-					},
-				}
-				return true, &proj, nil
-			})
-
 			err := fkclient.CreateNewProject(tt.projName, tt.wait)
 			if !tt.wantErr == (err != nil) {
 				t.Errorf("client.CreateNewProject(string) unexpected error %v, wantErr %v", err, tt.wantErr)
 			}
 
 			actions := fkclientset.ProjClientset.Actions()
-			actionsNb := len(actions)
-			if !tt.wait && actionsNb != 1 {
+			actionsLen := len(actions)
+			if !tt.wait && actionsLen != 1 {
 				t.Errorf("expected 1 action in CreateNewProject got: %v", actions)
 			}
-			if tt.wait && actionsNb != 2 {
+			if tt.wait && actionsLen != 2 {
 				t.Errorf("expected 2 actions in CreateNewProject when waiting for project creation got: %v", actions)
 			}
 
 			if err == nil {
-				createdProj := actions[0].(ktesting.CreateAction).GetObject().(*projectv1.ProjectRequest)
+				createdProj := actions[actionsLen-1].(ktesting.CreateAction).GetObject().(*projectv1.ProjectRequest)
 
 				if createdProj.Name != tt.projName {
 					t.Errorf("project name does not match the expected name, expected: %s, got: %s", tt.projName, createdProj.Name)
@@ -2628,7 +2626,7 @@ func TestCreateNewProject(t *testing.T) {
 
 				if tt.wait {
 					expectedFields := fields.OneTermEqualSelector("metadata.name", tt.projName)
-					gotFields := actions[1].(ktesting.WatchAction).GetWatchRestrictions().Fields
+					gotFields := actions[0].(ktesting.WatchAction).GetWatchRestrictions().Fields
 
 					if !reflect.DeepEqual(expectedFields, gotFields) {
 						t.Errorf("Fields not matching: expected: %s, got %s", expectedFields, gotFields)
