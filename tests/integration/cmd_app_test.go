f6fe3bd7ec3c05455c6bee2fcf72aaa078c7dd88
mik-dass <mrinald7@gmail.com>
2019-06-05 13:48:29 +0530

    Splits a e2e test into 2 specs

--- tests/integration/cmd_app_test.go
+++ tests/integration/cmd_app_test.go
@@ -4,7 +4,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/odo/tests/helper"
-	"os"
 	"path/filepath"
 	"time"
 )
@@ -12,6 +11,7 @@ import (
 var _ = Describe("odoCmdAppE2e", func() {
 	var project string
 	var context string
+	var originalDir string
 
 	appName := "app"
 	cmpName := "nodejs"
@@ -21,13 +21,14 @@ var _ = Describe("odoCmdAppE2e", func() {
 		SetDefaultEventuallyTimeout(10 * time.Minute)
 		project = helper.CreateRandProject()
 		context = helper.CreateNewContext()
+		originalDir = helper.Getwd()
 	})
 
 	// Clean up after the test
 	// This is run after every Spec (It)
 	var _ = AfterEach(func() {
 		helper.DeleteProject(project)
-		os.RemoveAll(".odo")
+		helper.DeleteDir(context)
 	})
 
 	Context("App test", func() {
@@ -38,23 +39,33 @@ var _ = Describe("odoCmdAppE2e", func() {
 
 			// changing directory to the context directory
 			helper.Chdir(context)
+			appListOutput := helper.CmdShouldPass("odo", "app", "list")
+			Expect(appListOutput).To(ContainSubstring(appName))
 			helper.CmdShouldPass("odo", "app", "describe")
-			helper.CmdShouldPass("odo", "app", "list")
 			helper.CmdShouldPass("odo", "app", "delete", "-f")
+			helper.Chdir(originalDir)
 		})
 
-		It("should fail outside a odo directory without app parameters(except the list command) and pass with app parameters", func() {
+		It("should fail outside a odo directory without app parameters(except the list command)", func() {
 			helper.CopyExample(filepath.Join("source", "nodejs"), context)
 			helper.CmdShouldPass("odo", "component", "create", "nodejs", cmpName, "--app", appName, "--project", project, "--context", context)
 			helper.CmdShouldPass("odo", "push", "--context", context)
 
 			// list should pass as the project exists
-			helper.CmdShouldPass("odo", "app", "list")
+			appListOutput := helper.CmdShouldPass("odo", "app", "list")
+			Expect(appListOutput).To(ContainSubstring(appName))
 			helper.CmdShouldFail("odo", "app", "describe")
 			helper.CmdShouldFail("odo", "app", "delete")
+		})
 
+		It("should pass outside a odo directory with app parameters", func() {
+			helper.CopyExample(filepath.Join("source", "nodejs"), context)
+			helper.CmdShouldPass("odo", "component", "create", "nodejs", cmpName, "--app", appName, "--project", project, "--context", context)
+			helper.CmdShouldPass("odo", "push", "--context", context)
+
+			appListOutput := helper.CmdShouldPass("odo", "app", "list")
+			Expect(appListOutput).To(ContainSubstring(appName))
 			helper.CmdShouldPass("odo", "app", "describe", appName)
-			helper.CmdShouldPass("odo", "app", "list")
 			helper.CmdShouldPass("odo", "app", "delete", appName, "-f")
 		})
 	})
