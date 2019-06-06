c3cc9add1a59a7c1c7b2a12bff54908b87ab2f93
Tomas Kral <tkral@redhat.com>
2019-06-06 13:32:24 +0200

    Add benchmark tests
    Tests are measuring speed of various odo commands for Java and NodeJS
    components.
    Results are displeyed on the output and also send to the Google Sheet table
    https://docs.google.com/spreadsheets/d/1o-GIoYlZoEyW1F25kAwvvEXzbW4tLck0x3EGCC4_L_A/

---
+++ tests/benchmark/benchmark_test.go
@@ -0,0 +1,25 @@
+package benchmark
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/openshift/odo/tests/helper/reporter"
+)
+
+func TestBenchmark(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	reporters := []Reporter{}
+
+	if os.Getenv("CI") == "openshift" {
+		// If running on OpenShift CI, add reporter that will submit measurements to Google Sheets table
+		// https://docs.google.com/spreadsheets/d/1o-GIoYlZoEyW1F25kAwvvEXzbW4tLck0x3EGCC4_L_A/
+		reporters = append(reporters, reporter.NewHTTPMeasurementReporter("https://script.google.com/macros/s/AKfycbyoeFrEXsrkjWOCCjsLOGY5a31Fsv5RTUvgqQP0E5vPo3YvDGE/exec"))
+	}
+
+	RunSpecsWithDefaultAndCustomReporters(t, "odo benchmark tests", reporters)
+}
