23f8f59e5e0b52ed96cf4aac1e8f49a2d4e90293
Dharmit Shah <shahdharmit@gmail.com>
2019-06-13 13:46:55 +0530

    Move URL name check to Validate function as per the review

--- pkg/odo/cli/url/create.go
+++ pkg/odo/cli/url/create.go
@@ -63,9 +63,6 @@ func (o *URLCreateOptions) Complete(name string, cmd *cobra.Command, args []stri
 	if len(args) == 0 {
 		o.urlName = url.GetURLName(o.Component(), o.componentPort)
 	} else {
-		if len(args[0]) > 63 {
-			return fmt.Errorf("url name must be shorter than 63 characters")
-		}
 		o.urlName = args[0]
 	}
 	o.localConfigInfo, err = config.NewLocalConfigInfo(o.componentContext)
@@ -83,9 +80,15 @@ func (o *URLCreateOptions) Validate() (err error) {
 		}
 	}
 
+	// Check if url name is more than 63 characters long
+	if len(o.urlName) > 63 {
+		return fmt.Errorf("url name must be shorter than 63 characters")
+	}
+
 	if !util.CheckOutputFlag(o.OutputFlag) {
 		return fmt.Errorf("given output format %s is not supported", o.OutputFlag)
 	}
+
 	return
 }
 
