a883a8bb1f3eb30f38b5d6e05754b8ebb2d8542b
Amit Rout <arout@redhat.com>
2019-06-25 16:52:33 +0530

    Run json format test in parallel (#1789)
    * Run json format test in parallel
    
    * Added ginkgo in travis CI
    
    * Added doc and updated Makefile
    
    * Updated doc

--- tests/integration/json_test.go
+++ tests/integration/json_test.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -13,21 +14,24 @@ import (
 )
 
 var _ = Describe("odojsonoutput", func() {
-	var project, tmpDir string
+	var project string
+	var context string
+	var originalDir string
 
 	// This is run after every Spec (It)
-	var _ = BeforeEach(func() {
+	BeforeEach(func() {
 		SetDefaultEventuallyTimeout(10 * time.Minute)
+		context = helper.CreateNewContext()
+		os.Setenv("GLOBALODOCONFIG", filepath.Join(context, "config.yaml"))
 		project = helper.CreateRandProject()
-		tmpDir = helper.CreateNewContext()
 	})
 
 	// Clean up after the test
 	// This is run after every Spec (It)
-	var _ = AfterEach(func() {
+	AfterEach(func() {
 		helper.DeleteProject(project)
-		os.RemoveAll(".odo")
-		helper.DeleteDir(tmpDir)
+		helper.DeleteDir(context)
+		os.Unsetenv("GLOBALODOCONFIG")
 	})
 
 	Context("odo machine readable output on empty project", func() {
@@ -48,6 +52,14 @@ var _ = Describe("odojsonoutput", func() {
 	})
 
 	Context("odo machine readable output on project nodejs is deployed", func() {
+		JustBeforeEach(func() {
+			originalDir = helper.Getwd()
+			helper.Chdir(context)
+		})
+
+		JustAfterEach(func() {
+			helper.Chdir(originalDir)
+		})
 		It("Shows storage, list, app in json format", func() {
 			helper.CmdShouldPass("odo", "create", "nodejs", "nodejs", "--app", "myapp", "--project", project, "--git", "https://github.com/openshift/nodejs-ex")
 			helper.CmdShouldPass("odo", "push")
@@ -65,7 +77,7 @@ var _ = Describe("odojsonoutput", func() {
 			// odo list -o json --path .
 			pwd := helper.Getwd()
 			desired := fmt.Sprintf(`{"kind":"List","apiVersion":"odo.openshift.io/v1alpha1","metadata":{},"items":[{"kind":"Component","apiVersion":"odo.openshift.io/v1alpha1","metadata":{"name":"nodejs","creationTimestamp":null},"spec":{"type":"nodejs","source":"https://github.com/openshift/nodejs-ex"},"status":{"context":"%s","state":"Pushed"}}]}`, strings.TrimSpace(pwd))
-			helper.Chdir(tmpDir)
+			helper.Chdir(context)
 			actual := helper.CmdShouldPass("odo", "list", "-o", "json", "--path", pwd)
 			helper.Chdir(pwd)
 			Expect(desired).Should(MatchJSON(actual))
@@ -74,6 +86,14 @@ var _ = Describe("odojsonoutput", func() {
 	})
 
 	Context("odo machine readable output on project nodejs is deployed", func() {
+		JustBeforeEach(func() {
+			originalDir = helper.Getwd()
+			helper.Chdir(context)
+		})
+
+		JustAfterEach(func() {
+			helper.Chdir(originalDir)
+		})
 		It("should be able to list url", func() {
 			helper.CmdShouldPass("odo", "create", "nodejs", "nodejs", "--app", "myapp", "--project", project, "--git", "https://github.com/openshift/nodejs-ex")
 			helper.CmdShouldPass("odo", "url", "create", "myurl", "--port", "8080")
