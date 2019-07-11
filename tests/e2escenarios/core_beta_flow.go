c4fd022d0333591781b4e17f79ffc004c9e82eaa
Suraj Narwade <surajnarwade353@gmail.com>
2019-07-11 11:28:04 +0530

    resolved conflicts

--- tests/e2escenarios/core_beta_flow.go
+++ tests/e2escenarios/core_beta_flow.go
@@ -103,90 +103,10 @@ var _ = Describe("Core beta flow", func() {
 			helper.Chdir(originalDir)
 		})
 
-<<<<<<< HEAD
 		It("'odo component' should fail if there already is .odo dir", func() {
 			helper.CmdShouldPass("odo", "component", "create", "nodejs", "--project", project)
 			helper.CmdShouldFail("odo", "component", "create", "nodejs", "--project", project)
 		})
-=======
-		// abstract main test to the function, to allow running the same test in a different context (slightly different arguments)
-		TestBasicCreateConfigPush := func(extraArgs ...string) {
-			createSession := helper.CmdShouldPass(odo, append([]string{"component", "create", "java", "mycomponent", "--app", "myapp"}, extraArgs...)...)
-			// output of the commands should point user to running "odo push"
-			Expect(createSession).Should(ContainSubstring("odo push"))
-			configFile := filepath.Join(context, ".odo", "config.yaml")
-			Expect(configFile).To(BeARegularFile())
-			helper.FileShouldContainSubstring(configFile, "Name: mycomponent")
-			helper.FileShouldContainSubstring(configFile, "Type: java")
-			helper.FileShouldContainSubstring(configFile, "Application: myapp")
-			helper.FileShouldContainSubstring(configFile, "SourceType: local")
-			// SourcePath should be relative
-			//helper.FileShouldContainSubstring(configFile, "SourceLocation: .")
-			helper.FileShouldContainSubstring(configFile, "Project: "+project)
-
-			configSession := helper.CmdShouldPass(odo, append([]string{"config", "set", "--env", "FOO=bar"}, extraArgs...)...)
-			// output of the commands should point user to running "odo push"
-			// currently failing
-			Expect(configSession).Should(ContainSubstring("odo push"))
-			helper.FileShouldContainSubstring(configFile, "Name: FOO")
-			helper.FileShouldContainSubstring(configFile, "Value: bar")
-
-			urlCreateSession := helper.CmdShouldPass(odo, append([]string{"url", "create", "--port", "8080"}, extraArgs...)...)
-			// output of the commands should point user to running "odo push"
-			Eventually(urlCreateSession).Should(ContainSubstring("odo push"))
-			helper.FileShouldContainSubstring(configFile, "Url:")
-			helper.FileShouldContainSubstring(configFile, "Port: 8080")
-
-			helper.CmdShouldPass(odo, append([]string{"push"}, extraArgs...)...)
-
-			dcSession := oc.GetComponentDC("mycomponent", "myapp", project)
-
-			Expect(dcSession).Should(ContainSubstring("app.kubernetes.io/component-name: mycomponent"))
-			Expect(dcSession).Should(ContainSubstring("app.kubernetes.io/component-source-type: local"))
-			Expect(dcSession).Should(ContainSubstring("app.kubernetes.io/component-type: java"))
-			Expect(dcSession).Should(ContainSubstring("app.kubernetes.io/name: myapp"))
-			Expect(dcSession).Should(ContainSubstring("name: mycomponent-myapp"))
-			// DC should have env variable
-			Expect(dcSession).Should(ContainSubstring("name: FOO"))
-			Expect(dcSession).Should(ContainSubstring("value: bar"))
-
-			routeSession := oc.GetComponentRoutes("mycomponent", "myapp", project)
-			// check that route is pointing gto right port and component
-			Expect(routeSession).Should(ContainSubstring("targetPort: 8080"))
-			Expect(routeSession).Should(ContainSubstring("name: mycomponent-myapp"))
-			url := oc.GetFirstURL("mycomponent", "myapp", project)
-
-			helper.HttpWaitFor("http://"+url, "Hello World from Javalin!", 10, 5)
-		}
-
-		Context("when component is in the current directory", func() {
-
-			// we will be testing components that are created from the current directory
-			// switch to the clean context dir before each test
-			JustBeforeEach(func() {
-				originalDir = helper.Getwd()
-				helper.Chdir(context)
-			})
-
-			// go back to original directory after each test
-			JustAfterEach(func() {
-				helper.Chdir(originalDir)
-			})
-
-			It("'odo component' should fail if there already is .odo dir", func() {
-				helper.CmdShouldPass("odo", "component", "create", "nodejs")
-				helper.CmdShouldFail("odo", "component", "create", "nodejs")
-			})
-
-			It("'odo config' should fail if there is no .odo dir", func() {
-				helper.CmdShouldFail("odo", "config", "set", "memory", "2Gi")
-			})
-
-			It("create local java component and push code", func() {
-				helper.CopyExample(filepath.Join("source", "openjdk"), context)
-				TestBasicCreateConfigPush()
-			})
->>>>>>> 54634013... fixed tests
 
 		It("'odo config' should fail if there is no .odo dir", func() {
 			helper.CmdShouldFail("odo", "config", "set", "memory", "2Gi")
