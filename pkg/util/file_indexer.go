ebadd2998bf634a5c247490855f3bc0eff14c421
Girish Ramnani <girishramnani95@gmail.com>
2019-08-13 12:08:37 +0530

    created a new struct for versioned file index

--- pkg/util/file_indexer.go
+++ pkg/util/file_indexer.go
@@ -8,10 +8,29 @@ import (
 	"time"
 
 	"github.com/golang/glog"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const fileIndexDirectory = ".odo"
 const fileIndexName = "odo-file-index.json"
+
+// FileIndex holds the file index used for storing local file state change
+type FileIndex struct {
+	metav1.TypeMeta
+	FileMap map[string]FileData
+}
+
+// NewFileIndex returns a fileIndex
+func NewFileIndex() *FileIndex {
+
+	return &FileIndex{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "FileIndex",
+			APIVersion: "v1",
+		},
+		FileMap: make(map[string]FileData),
+	}
+}
 
 type FileData struct {
 	Size             int64
@@ -20,11 +39,11 @@ type FileData struct {
 
 // read tries to read the odo index file from the given location and returns the data from the file
 // if no such file is present, it means the folder hasn't been walked and thus returns a empty list
-func read(filePath string) (map[string]FileData, error) {
+func readFileIndex(filePath string) (*FileIndex, error) {
 	// read operation
-	fileReadMap := make(map[string]FileData)
+	var fi FileIndex
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		return map[string]FileData{}, nil
+		return NewFileIndex(), nil
 	}
 
 	byteValue, err := ioutil.ReadFile(filePath)
@@ -32,11 +51,16 @@ func read(filePath string) (map[string]FileData, error) {
 		return nil, err
 	}
 	// unmarshals the byte values and fill up the file read map
-	err = json.Unmarshal(byteValue, &fileReadMap)
+	err = json.Unmarshal(byteValue, &fi)
 	if err != nil {
-		return nil, err
+		// This is added here for backward compatibility because
+		// if the marshalling fails then that means we are trying to
+		// read a very old version of the index and hence we can just
+		// ignore it and reset index
+		// TODO: we need to remove this later
+		return NewFileIndex(), err
 	}
-	return fileReadMap, nil
+	return &fi, nil
 }
 
 // resolveFilePath resolves the filepath of the odo index file in the .odo folder
@@ -72,12 +96,12 @@ func RunIndexer(directory string, ignoreRules []string) (filesChanged []string, 
 	}
 
 	// read the odo index file
-	readData, err := read(resolvedPath)
+	existingFileIndex, err := readFileIndex(resolvedPath)
 	if err != nil {
 		return filesChanged, filesDeleted, err
 	}
 
-	filesMap := make(map[string]FileData)
+	newFileMap := make(map[string]FileData)
 	walk := func(fn string, fi os.FileInfo, err error) error {
 		if fi.IsDir() {
 
@@ -106,18 +130,18 @@ func RunIndexer(directory string, ignoreRules []string) (filesChanged []string, 
 			return err
 		}
 
-		if _, ok := readData[relFn]; !ok {
+		if _, ok := existingFileIndex.FileMap[relFn]; !ok {
 			filesChanged = append(filesChanged, fn)
 			glog.V(4).Infof("file added: %s", fn)
-		} else if !fi.ModTime().Equal(readData[relFn].LastModifiedDate) {
+		} else if !fi.ModTime().Equal(existingFileIndex.FileMap[relFn].LastModifiedDate) {
 			filesChanged = append(filesChanged, fn)
 			glog.V(4).Infof("last modified date changed: %s", fn)
-		} else if fi.Size() != readData[relFn].Size {
+		} else if fi.Size() != existingFileIndex.FileMap[relFn].Size {
 			filesChanged = append(filesChanged, fn)
 			glog.V(4).Infof("size changed: %s", fn)
 		}
 
-		filesMap[relFn] = FileData{
+		newFileMap[relFn] = FileData{
 			Size:             fi.Size(),
 			LastModifiedDate: fi.ModTime(),
 		}
@@ -130,8 +154,8 @@ func RunIndexer(directory string, ignoreRules []string) (filesChanged []string, 
 	}
 
 	// find files which are deleted/renamed
-	for fileName := range readData {
-		if _, ok := filesMap[fileName]; !ok {
+	for fileName := range existingFileIndex.FileMap {
+		if _, ok := newFileMap[fileName]; !ok {
 			glog.V(4).Infof("file deleted: %s", fileName)
 			// we return the absolute path of the files eventhough we store relative
 			filesDeleted = append(filesDeleted, filepath.Join(directory, fileName))
@@ -141,7 +165,9 @@ func RunIndexer(directory string, ignoreRules []string) (filesChanged []string, 
 
 	// if there are added/deleted/modified/renamed files or folders, write it to the odo index file
 	if len(filesChanged) > 0 || len(filesDeleted) > 0 {
-		err = write(resolvedPath, filesMap)
+		newfi := NewFileIndex()
+		newfi.FileMap = newFileMap
+		err = write(resolvedPath, newfi)
 		if err != nil {
 			return filesChanged, filesDeleted, err
 		}
@@ -152,8 +178,8 @@ func RunIndexer(directory string, ignoreRules []string) (filesChanged []string, 
 
 // writes the map of walked files and info about them, in a file
 // filepath is the location of the file to which it is supposed to be written
-func write(filePath string, writeMap map[string]FileData) error {
-	jsonData, err := json.Marshal(writeMap)
+func write(filePath string, fi *FileIndex) error {
+	jsonData, err := json.Marshal(fi)
 	if err != nil {
 		return err
 	}
