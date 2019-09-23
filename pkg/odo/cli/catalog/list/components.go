redhat-developer/odo

commit 6afe7b652fcbd8e93fd5c48d5c493378adfc350c

add unsupported and supported components (#2029)
* added code unsupported and supported components

* used a single tab writer for both catalogLists

* added comment for imageStreamRef

* comments at places and some refactor

* renamed nonSup to unSup

* changed Types to Component types

* renamed createImageMap to createImageTagMap

* Splice to Slice

* adding comments

* resolved cdrage's comments

* use different title for list components

pkg/odo/cli/catalog/list/components.go
@@ -2,13 +2,15 @@ package list
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/tabwriter"
+
 	"github.com/openshift/odo/pkg/catalog"
 	"github.com/openshift/odo/pkg/odo/cli/catalog/util"
 	"github.com/openshift/odo/pkg/odo/genericclioptions"
 	"github.com/spf13/cobra"
-	"os"
-	"strings"
-	"text/tabwriter"
 )
 
 const componentsRecommendedCommandName = "components"
@@ -53,24 +55,33 @@ func (o *ListComponentsOptions) Validate() (err error) {
 // Run contains the logic for the command associated with ListComponentsOptions
 func (o *ListComponentsOptions) Run() (err error) {
 	w := tabwriter.NewWriter(os.Stdout, 5, 2, 3, ' ', tabwriter.TabIndent)
-	fmt.Fprintln(w, "NAME", "\t", "PROJECT", "\t", "TAGS")
-	for _, component := range o.catalogList {
-		componentName := component.Name
-		if component.Namespace == o.Project {
-			/*
-				If current namespace is same as the current component namespace,
-				Loop through every other component,
-				If there exists a component with same name but in different namespaces,
-				mark the one from current namespace with (*)
-			*/
-			for _, comp := range o.catalogList {
-				if comp.Name == component.Name && component.Namespace != comp.Namespace {
-					componentName = fmt.Sprintf("%s (*)", component.Name)
-				}
-			}
+	var supCatalogList, unsupCatalogList []catalog.CatalogImage
+
+	for _, image := range o.catalogList {
+		supported, unsupported := catalog.SliceSupportedTags(image)
+
+		if len(supported) != 0 {
+			image.NonHiddenTags = supported
+			supCatalogList = append(supCatalogList, image)
 		}
-		fmt.Fprintln(w, componentName, "\t", component.Namespace, "\t", strings.Join(component.NonHiddenTags, ","))
+		if len(unsupported) != 0 {
+			image.NonHiddenTags = unsupported
+			unsupCatalogList = append(unsupCatalogList, image)
+		}
 	}
+
+	if len(supCatalogList) != 0 {
+		fmt.Fprintln(w, "Odo Supported OpenShift Components:")
+		o.printCatalogList(w, supCatalogList)
+		fmt.Fprintln(w)
+
+	}
+
+	if len(unsupCatalogList) != 0 {
+		fmt.Fprintln(w, "OpenShift Components:")
+		o.printCatalogList(w, unsupCatalogList)
+	}
+
 	w.Flush()
 	return
 }
@@ -81,12 +92,34 @@ func NewCmdCatalogListComponents(name, fullName string) *cobra.Command {
 
 	return &cobra.Command{
 		Use:     name,
-		Short:   "List all components available.",
-		Long:    "List all available component types from OpenShift's Image Builder.",
+		Short:   "List all components",
+		Long:    "List all available component types from OpenShift's Ima",
 		Example: fmt.Sprintf(componentsExample, fullName),
 		Run: func(cmd *cobra.Command, args []string) {
 			genericclioptions.GenericRun(o, cmd, args)
 		},
 	}
 
+}
+
+func (o *ListComponentsOptions) printCatalogList(w io.Writer, catalogList []catalog.CatalogImage) {
+	fmt.Fprintln(w, "NAME", "\t", "PROJECT", "\t", "TAGS")
+
+	for _, component := range catalogList {
+		componentName := component.Name
+		if component.Namespace == o.Project {
+			/*
+				If current namespace is same as the current component namespace,
+				Loop through every other component,
+				If there exists a component with same name but in different namespaces,
+				mark the one from current namespace with (*)
+			*/
+			for _, comp := range catalogList {
+				if comp.Name == component.Name && component.Namespace != comp.Namespace {
+					componentName = fmt.Sprintf("%s (*)", component.Name)
+				}
+			}
+		}
+		fmt.Fprintln(w, componentName, "\t", component.Namespace, "\t", strings.Join(component.NonHiddenTags, ","))
+	}
 }

pkg/catalog/catalog.go
@@ -15,11 +15,13 @@ type CatalogImage struct {
 	Namespace     string
 	AllTags       []string
 	NonHiddenTags []string
+
+	// this is used to get metadata like image URL for each tag present in the image stream
+	imageStreamRef imagev1.ImageStream
 }
 
 // List lists all the available component types
 func List(client *occlient.Client) ([]CatalogImage, error) {
-
 	catalogList, err := getDefaultBuilderImages(client)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to get image streams")
@@ -129,8 +131,73 @@ func getDefaultBuilderImages(client *occlient.Client) ([]CatalogImage, error) {
 		imageStreamTagMap[imageStreamTag.Name] = imageStreamTag
 	}
 
-	var builderImages []CatalogImage
+	builderImages := getBuildersFromImageStreams(imageStreams, imageStreamTagMap)
 
+	return builderImages, nil
+}
+
+// SliceSupportedTags splits the tags in to fully supported and unsupported tags
+func SliceSupportedTags(catalogImage CatalogImage) ([]string, []string) {
+
+	var supTag, unSupTag []string
+	tagMap := createImageTagMap(catalogImage.imageStreamRef.Spec.Tags)
+	for _, tag := range catalogImage.NonHiddenTags {
+		imageName := tagMap[tag]
+		if isSupportedImage(imageName) {
+			supTag = append(supTag, tag)
+		} else {
+			unSupTag = append(unSupTag, tag)
+		}
+	}
+	return supTag, unSupTag
+}
+
+// createImageTagMap takes a list of image TagReferences and creates a map of type tag name => image name e.g. 1.11 => openshift/nodejs-11
+func createImageTagMap(tagRefs []imagev1.TagReference) map[string]string {
+	tagMap := make(map[string]string)
+	for _, tagRef := range tagRefs {
+		imageName := tagRef.From.Name
+		if tagRef.From.Kind == "DockerImage" {
+			// we get the image name from the repo url e.g. registry.redhat.com/openshift/nodejs:10 will give openshift/nodejs:10
+			urlImageName := strings.SplitN(imageName, "/", 2)[1]
+			// here we remove the tag and digest
+			ns, img, _, _, _ := occlient.ParseImageName(urlImageName)
+			imageName = ns + "/" + img
+		} else if tagRef.From.Kind == "ImageStreamTag" {
+			tagList := strings.Split(imageName, ":")
+			tag := tagList[len(tagList)-1]
+			// if the kind is a image stream tag that means its pointing to an existing dockerImage or image stream image
+			// we just look it up from the tapMap we already have
+			imageName = tagMap[tag]
+		}
+		tagMap[tagRef.Name] = imageName
+	}
+	return tagMap
+}
+
+// isSupportedImages returns if the image is supported or not. the supported images have been provided here
+// https://github.com/openshift/odo-init-image/blob/master/language-scripts/image-mappings.json
+func isSupportedImage(imgName string) bool {
+	supportedImages := []string{
+		"redhat-openjdk-18/openjdk18-openshift",
+		"openjdk/openjdk-11-rhel8",
+		"openjdk/openjdk-11-rhel7",
+		"rhscl/nodejs-8-rhel7",
+		"rhoar-nodejs/nodejs-8",
+		"rhoar-nodejs/nodejs-10",
+	}
+	for _, supImage := range supportedImages {
+		if supImage == imgName {
+			return true
+		}
+	}
+	return false
+}
+
+// getBuildersFromImageStreams returns all the builder Images from the image streams provided and also hides the builder images
+// which have hidden annotation attached to it
+func getBuildersFromImageStreams(imageStreams []imagev1.ImageStream, imageStreamTagMap map[string]imagev1.ImageStreamTag) []CatalogImage {
+	var builderImages []CatalogImage
 	// Get builder images from the available imagestreams
 	for _, imageStream := range imageStreams {
 		var allTags []string
@@ -139,7 +206,6 @@ func getDefaultBuilderImages(client *occlient.Client) ([]CatalogImage, error) {
 
 		for _, tagReference := range imageStream.Spec.Tags {
 			allTags = append(allTags, tagReference.Name)
-
 			// Check to see if it is a "builder" image
 			if _, ok := tagReference.Annotations["tags"]; ok {
 				for _, t := range strings.Split(tagReference.Annotations["tags"], ",") {
@@ -172,18 +238,18 @@ func getDefaultBuilderImages(client *occlient.Client) ([]CatalogImage, error) {
 			}
 
 			catalogImage := CatalogImage{
-				Name:          imageStream.Name,
-				Namespace:     imageStream.Namespace,
-				AllTags:       allTags,
-				NonHiddenTags: getAllNonHiddenTags(allTags, hiddenTags),
+				Name:           imageStream.Name,
+				Namespace:      imageStream.Namespace,
+				AllTags:        allTags,
+				NonHiddenTags:  getAllNonHiddenTags(allTags, hiddenTags),
+				imageStreamRef: imageStream,
 			}
 			builderImages = append(builderImages, catalogImage)
 			glog.V(5).Infof("Found builder image: %#v", catalogImage)
 		}
 
 	}
-
-	return builderImages, nil
+	return builderImages
 }
 
 func getAllNonHiddenTags(allTags []string, hiddenTags []string) []string {

pkg/catalog/catalog_test.go
@@ -4,8 +4,11 @@ import (
 	"reflect"
 	"testing"
 
+	imagev1 "github.com/openshift/api/image/v1"
 	"github.com/openshift/odo/pkg/occlient"
 	"github.com/openshift/odo/pkg/testingutil"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ktesting "k8s.io/client-go/testing"
 )
@@ -135,5 +138,59 @@ func TestList(t *testing.T) {
 			}
 
 		})
+	}
+}
+
+func TestSliceSupportedTags(t *testing.T) {
+
+	tags := map[string]string{
+		"10": "docker.io/rhoar-nodejs/nodejs-10",
+		"8":  "docker.io/rhoar-nodejs/nodejs-8",
+		"6":  "docker.io/centos/nodejs-6-centos7:latest",
+	}
+
+	imageStream := &imagev1.ImageStream{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nodejs",
+			Namespace: "openshift",
+		},
+	}
+
+	for tagName, imageName := range tags {
+		imageTag := imagev1.TagReference{
+			Name:        tagName,
+			Annotations: map[string]string{"tags": "builder"},
+			From: &corev1.ObjectReference{
+				Kind: "DockerImage",
+				Name: imageName,
+			},
+		}
+		imageStream.Spec.Tags = append(imageStream.Spec.Tags, imageTag)
+	}
+
+	imageStream.Spec.Tags = append(imageStream.Spec.Tags,
+		imagev1.TagReference{
+			Name:        "latest",
+			Annotations: map[string]string{"tags": "builder"},
+			From: &corev1.ObjectReference{
+				Kind: "ImageStreamTag",
+				Name: "10",
+			},
+		})
+
+	img := CatalogImage{
+		Name:      "nodejs",
+		Namespace: "openshift",
+		NonHiddenTags: []string{
+			"10", "8", "6", "latest",
+		},
+		imageStreamRef: *imageStream,
+	}
+
+	supTags, unSupTags := SliceSupportedTags(img)
+
+	if !reflect.DeepEqual(supTags, []string{"10", "8", "latest"}) ||
+		!reflect.DeepEqual(unSupTags, []string{"6"}) {
+		t.Fatal("supported or unsupported tags are not as expected")
 	}
 }

pkg/testingutil/imagestreams.go
@@ -28,7 +28,10 @@ func fakeImageStream(imageName string, namespace string, tags []string) *imagev1
 	}
 
 	for _, tag := range tags {
-		imageTag := imagev1.TagReference{Name: tag, Annotations: map[string]string{"tags": "builder"}}
+		imageTag := imagev1.TagReference{
+			Name:        tag,
+			Annotations: map[string]string{"tags": "builder"},
+		}
 		image.Spec.Tags = append(image.Spec.Tags, imageTag)
 	}
 

commit 903af92556cf7c34a176bf802cccc6c3cb9e5493

Fix spelling errors in catalog list components
Updates the spelling errors introduced in:
https://github.com/openshift/odo/pull/2029/files#diff-1898d8a28a2de7aa682a9de52faa269dR96

pkg/odo/cli/catalog/list/components.go
@@ -100,7 +100,7 @@ func NewCmdCatalogListComponents(name, fullName string) *cobra.Command {
 	return &cobra.Command{
 		Use:     name,
 		Short:   "List all components",
-		Long:    "List all available component types from OpenShift's Ima",
+		Long:    "List all available component types from OpenShift's Image Builder",
 		Example: fmt.Sprintf(componentsExample, fullName),
 		Run: func(cmd *cobra.Command, args []string) {
 			genericclioptions.GenericRun(o, cmd, args)