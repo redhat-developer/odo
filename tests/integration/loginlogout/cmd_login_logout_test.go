3add7da28fda42e779544251a461001e332390af
Amit Rout <arout@redhat.com>
2019-09-25 18:19:25 +0530

    Fixing login flake (#2164)
    * Fixing login falke
    
    * Moving dependent spec to a single spec
    
    * Added step comment

--- tests/integration/loginlogout/cmd_login_logout_test.go
+++ tests/integration/loginlogout/cmd_login_logout_test.go
@@ -11,13 +11,11 @@ import (
 var _ = Describe("odo login and logout command tests", func() {
 	// user related constants
 	const loginTestUserForNoProject = "odologinnoproject"
-	const loginTestUserForSingleProject1 = "odologinsingleproject1"
-	const odoTestProjectForSingleProject1 = "odologintestproject1"
 	const loginTestUserPassword = "developer"
 	var session1 string
-	var testUserToken1 string
+	var testUserToken string
 	var oc helper.OcRunner
-	var currentUserToken1 string
+	var currentUserToken string
 
 	BeforeEach(func() {
 		SetDefaultEventuallyTimeout(10 * time.Minute)
@@ -39,32 +37,36 @@ var _ = Describe("odo login and logout command tests", func() {
 		})
 	})
 
-	Context("Run login tests with no active projects, having default is also considered as not having active project", func() {
-		It("Should login successfully with username and password without any projects with appropriate message", func() {
-			currentUserToken1 = oc.GetToken()
+	Context("when running login tests", func() {
+		It("should successful with correct credentials and fails with incorrect token", func() {
+			// Current user login token
+			currentUserToken = oc.GetToken()
+
+			// Login successful without any projects with appropriate message
 			session1 = helper.CmdShouldPass("odo", "login", "-u", loginTestUserForNoProject, "-p", loginTestUserPassword)
 			Expect(session1).To(ContainSubstring("Login successful"))
 			Expect(session1).To(ContainSubstring("You don't have any projects. You can try to create a new project, by running"))
 			Expect(session1).To(ContainSubstring("odo project create <project-name>"))
 			session1 = oc.GetLoginUser()
 			Expect(session1).To(ContainSubstring(loginTestUserForNoProject))
-			// One initialization needs one login, hence it happens here
-			testUserToken1 = oc.GetToken()
-		})
 
-		It("Should login successfully with token without any projects with appropriate message", func() {
-			session1 = helper.CmdShouldPass("odo", "login", "-t", testUserToken1)
+			// odologinnoproject user login token
+			testUserToken = oc.GetToken()
+
+			// Login successful with token without any projects with appropriate message
+			session1 = helper.CmdShouldPass("odo", "login", "-t", testUserToken)
 			Expect(session1).To(ContainSubstring("Logged into"))
 			Expect(session1).To(ContainSubstring("You don't have any projects. You can try to create a new project, by running"))
 			Expect(session1).To(ContainSubstring("odo project create <project-name>"))
 			session1 = oc.GetLoginUser()
 			Expect(session1).To(ContainSubstring(loginTestUserForNoProject))
-		})
 
-		It("Should fail login on invalid token with appropriate message", func() {
+			// Login fails on invalid token with appropriate message
 			sessionErr := helper.CmdShouldFail("odo", "login", "-t", "verybadtoken")
 			Expect(sessionErr).To(ContainSubstring("The token provided is invalid or expired"))
-			helper.CmdShouldPass("odo", "login", "--token", currentUserToken1)
+
+			// loging back to current user
+			helper.CmdShouldPass("odo", "login", "--token", currentUserToken)
 		})
 	})
 })
