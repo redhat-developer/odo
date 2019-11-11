de26405161722b6ae735051c5963e97e2d1adbab
Yaakov Selkowitz <yselkowi@redhat.com>
2019-11-11 03:31:07 -0500

    Build and package for all RHEL architectures (#2384)

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -11,11 +11,11 @@ if [[ -z "${COMMON_FLAGS}" ]]; then
     exit 1
 fi
 
-for platform in linux darwin windows ; do
-  echo "Cross compiling $platform-amd64 and placing binary at dist/bin/$platform-amd64/"
-  if [ $platform == "windows" ]; then
-    GOARCH=amd64 GOOS=$platform go build -o dist/bin/$platform-amd64/odo.exe -ldflags="$COMMON_FLAGS" ./cmd/odo/
+for platform in linux-amd64 linux-arm64 linux-ppc64le linux-s390x darwin-amd64 windows-amd64 ; do
+  echo "Cross compiling $platform and placing binary at dist/bin/$platform/"
+  if [ $platform == "windows-amd64" ]; then
+    GOARCH=amd64 GOOS=windows go build -o dist/bin/$platform/odo.exe -ldflags="$COMMON_FLAGS" ./cmd/odo/
   else
-    GOARCH=amd64 GOOS=$platform go build -o dist/bin/$platform-amd64/odo -ldflags="$COMMON_FLAGS" ./cmd/odo/
+    GOARCH=${platform#*-} GOOS=${platform%-*} go build -o dist/bin/$platform/odo -ldflags="$COMMON_FLAGS" ./cmd/odo/
   fi
 done
