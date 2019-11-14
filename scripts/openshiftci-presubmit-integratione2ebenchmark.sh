609594d5b2c27cae6e6557504bc1a8740cad4067
arout <arout@redhat.com>
2019-11-14 20:31:27 +0530

    Add all test target to a single OpenShift CI presubmit file

---
+++ scripts/openshiftci-presubmit-integratione2ebenchmark.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# fail if some commands fails
+set -e
+# show commands
+set -x
+
+export CI="openshift"
+export TIMEOUT="30m"
+export GINKGO_VERBOSE_MODE="-v"
+make configure-installer-tests-cluster
+make bin
+mkdir -p $GOPATH/bin
+go get -u github.com/onsi/ginkgo/ginkgo
+export PATH="$PATH:$(pwd):$GOPATH/bin"
+export ARTIFACTS_DIR="/tmp/artifacts"
+export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
+
+# Integration tests
+make test-generic
+make test-cmd-login-logout
+make test-cmd-cmp
+make test-cmd-cmp-sub
+make test-cmd-pref-config
+make test-cmd-watch
+make test-cmd-debug
+make test-cmd-storage
+make test-cmd-app
+make test-cmd-project
+make test-cmd-url
+make test-cmd-push
+make test-cmd-link-unlink
+
+# E2e tests
+make test-e2e-beta
+make test-e2e-java
+make test-e2e-source
+
+# Benchmark tests
+make test-benchmark
+
+odo logout
+
+# upload the junit test reports
+cp -r reports $ARTIFACTS_DIR
