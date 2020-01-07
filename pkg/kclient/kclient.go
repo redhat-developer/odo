16ac5db1d8602772a3d6a1fffba6cf742b34a644
John Collier <John.J.Collier@ibm.com>
2020-01-07 08:08:10 -0500

    Implement a stub kclient package for odo (#2458)
    * Implement a stub kclient package for odo
    
    Signed-off-by: John Collier <John.J.Collier@ibm.com>
    
    * Address review comments for kclient
    
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

---
+++ pkg/kclient/kclient.go
@@ -0,0 +1,51 @@
+package kclient
+
+import (
+	"github.com/pkg/errors"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// errorMsg is the message for user when invalid configuration error occurs
+const errorMsg = `
+Please ensure you have an active kubernetes context to your cluster. 
+Consult your Kubernetes distribution's documentation for more details
+`
+
+// Client is a collection of fields used for client configuration and interaction
+type Client struct {
+	KubeClient       kubernetes.Interface
+	KubeConfig       clientcmd.ClientConfig
+	KubeClientConfig *rest.Config
+	Namespace        string
+}
+
+// New creates a new client
+func New() (*Client, error) {
+	var client Client
+	var err error
+
+	// initialize client-go clients
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	client.KubeConfig = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+
+	client.KubeClientConfig, err = client.KubeConfig.ClientConfig()
+	if err != nil {
+		return nil, errors.Wrapf(err, errorMsg)
+	}
+
+	client.KubeClient, err = kubernetes.NewForConfig(client.KubeClientConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client.Namespace, _, err = client.KubeConfig.Namespace()
+	if err != nil {
+		return nil, err
+	}
+
+	return &client, nil
+}
---
+++ pkg/kclient/pods_test.go
@@ -0,0 +1,84 @@
+package kclient
+
+import (
+	"fmt"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/watch"
+
+	ktesting "k8s.io/client-go/testing"
+)
+
+func TestWaitAndGetPod(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		podName string
+		status  corev1.PodPhase
+		wantErr bool
+	}{
+		{
+			name:    "phase: running",
+			podName: "ruby",
+			status:  corev1.PodRunning,
+			wantErr: false,
+		},
+
+		{
+			name:    "phase: failed",
+			podName: "ruby",
+			status:  corev1.PodFailed,
+			wantErr: true,
+		},
+
+		{
+			name: "phase:	unknown",
+			podName: "ruby",
+			status:  corev1.PodUnknown,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			fkclient, fkclientset := FakeNew()
+			fkWatch := watch.NewFake()
+
+			// Change the status
+			go func() {
+				fkWatch.Modify(fakePodStatus(tt.status, tt.podName))
+			}()
+
+			fkclientset.Kubernetes.PrependWatchReactor("pods", func(action ktesting.Action) (handled bool, ret watch.Interface, err error) {
+				return true, fkWatch, nil
+			})
+
+			podSelector := fmt.Sprintf("deployment=%s", tt.podName)
+			timeout := int64(10)
+			watchOptions := metav1.ListOptions{
+				LabelSelector:  podSelector,
+				TimeoutSeconds: &timeout,
+			}
+			pod, err := fkclient.WaitAndGetPod(watchOptions, corev1.PodRunning, "Waiting for component to start")
+
+			if !tt.wantErr == (err != nil) {
+				t.Fatalf(" client.WaitAndGetPod(string) unexpected error %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if len(fkclientset.Kubernetes.Actions()) != 1 {
+				t.Errorf("expected 1 action in WaitAndGetPod got: %v", fkclientset.Kubernetes.Actions())
+			}
+
+			if err == nil {
+				if pod.Name != tt.podName {
+					t.Errorf("pod name is not matching to expected name, expected: %s, got %s", tt.podName, pod.Name)
+				}
+			}
+
+		})
+	}
+}
