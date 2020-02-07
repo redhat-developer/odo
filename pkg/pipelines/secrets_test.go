4672302c91a7aa23438629127637df96d8b944dc
Kevin McDermott <kmcdermo@redhat.com>
2020-02-07 16:32:48 +0000

    Include basic driver script.
    This is not intended for final use, but as a way to test out the generated resources.
    
    Co-authored-by: Ishita Sequeira <isequeir@redhat.com>

--- pkg/pipelines/secrets_test.go
+++ pkg/pipelines/secrets_test.go
@@ -2,6 +2,8 @@ package pipelines
 
 import (
 	"bytes"
+	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -33,4 +35,61 @@ func TestCreateOpaqueSecret(t *testing.T) {
 	if diff := cmp.Diff(want, secret); diff != "" {
 		t.Fatalf("CreateOpaqueSecret() failed got\n%s", diff)
 	}
+}
+
+func TestCreateOpaqueSecretWithErrorReading(t *testing.T) {
+	testErr := errors.New("test failure")
+	_, err := CreateOpaqueSecret("github-auth", errorReader{testErr})
+	if !matchError(t, "failed to read .* test failure", err) {
+		t.Fatalf("got an unexpected error: %#v", err)
+	}
+}
+
+func TestCreateDockerConfigSecret(t *testing.T) {
+	data := []byte(`abcdefghijklmnop`)
+	secret, err := CreateDockerConfigSecret("regcred", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "regcred",
+		},
+		Type: corev1.SecretTypeDockerConfigJson,
+		Data: map[string][]byte{
+			".dockerconfigjson": data,
+		},
+	}
+
+	if diff := cmp.Diff(want, secret); diff != "" {
+		t.Fatalf("CreateDockerConfigSecret() failed got\n%s", diff)
+	}
+}
+
+type errorReader struct {
+	err error
+}
+
+func (e errorReader) Read(p []byte) (int, error) {
+	return 0, e.err
+}
+
+func matchError(t *testing.T, s string, e error) bool {
+	t.Helper()
+	if s == "" && e == nil {
+		return true
+	}
+	if s != "" && e == nil {
+		return false
+	}
+	match, err := regexp.MatchString(s, e.Error())
+	if err != nil {
+		t.Fatal(err)
+	}
+	return match
 }
