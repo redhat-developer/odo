8a4025c048a9b92f897fde86ee4ad74506c352b6
William Tam <email.wtam@gmail.com>
2020-02-14 01:10:15 -0500

    Kill commented import
    Co-authored-by: Ishita Sequelira  <isequeir@redhat.com>

--- pkg/pipelines/tekton_checks_test.go
+++ pkg/pipelines/tekton_checks_test.go
@@ -8,7 +8,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
-	//	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
 )
 
