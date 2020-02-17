218c0538c3ac3adc313d68720cfcf4b9523ab7ef
chetan-rns <chetanrns1997@gmail.com>
2020-02-17 21:18:41 +0530

    Add functions to generate Tekton task objects
    - Added functions and test cases to create
    1) deploy-from-source-task
    2) deploy-using-kubectl-task
    3) github-status-task
    
    Co-Authored-By: Gagan Hegde <ghegde@redhat.com>

---
+++ pkg/pipelines/tasks/deploy_using_kubectl_task.go
@@ -0,0 +1,91 @@
+package tasks
+
+import (
+	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha2"
+)
+
+func generateDeployUsingKubectlTask() pipelinev1.Task {
+	return pipelinev1.Task{
+		TypeMeta:   createTaskTypeMeta(),
+		ObjectMeta: createTaskObjectMeta("deploy-using-kubectl-task"),
+		Spec: pipelinev1.TaskSpec{
+			Inputs: createInputsForDeployKubectlTask(),
+			TaskSpec: v1alpha2.TaskSpec{
+				Steps: createStepsForDeployKubectlTask(),
+			},
+		},
+	}
+}
+
+var argsForReplaceImageStep = []string{
+	"w",
+	"-i",
+	"$(inputs.params.PATHTODEPLOYMENT)/deployment.yaml",
+	"$(inputs.params.YAMLPATHTOIMAGE)",
+	"$(inputs.resources.image.url)",
+}
+
+var argsForKubectlStep = []string{
+	"apply",
+	"-n",
+	"$(inputs.params.NAMESPACE)",
+	"-k",
+	"$(inputs.params.PATHTODEPLOYMENT)",
+}
+
+func createStepsForDeployKubectlTask() []pipelinev1.Step {
+	return []pipelinev1.Step{
+		pipelinev1.Step{
+			Container: createContainer(
+				"replace-image",
+				"mikefarah/yq",
+				"/workspace/source",
+				[]string{"yq"},
+				argsForReplaceImageStep,
+			),
+		},
+		pipelinev1.Step{
+			Container: createContainer(
+				"run-kubectl",
+				"quay.io/kmcdermo/k8s-kubectl:latest",
+				"/workspace/source",
+				[]string{"kubectl"},
+				argsForKubectlStep,
+			),
+		},
+	}
+}
+
+func createInputsForDeployKubectlTask() *pipelinev1.Inputs {
+	return &pipelinev1.Inputs{
+		Resources: []pipelinev1.TaskResource{
+			createTaskResource("source", "git"),
+			createTaskResource("image", "image"),
+		},
+		Params: []pipelinev1.ParamSpec{
+			createTaskParamWithDefault(
+				"PATHTODEPLOYMENT",
+				"Path to the manifest to apply",
+				"string",
+				"deploy",
+			),
+			createTaskParam(
+				"NAMESPACE",
+				"Namespace to deploy into",
+				"string",
+			),
+			createTaskParamWithDefault(
+				"DRYRUN",
+				"If true run a server-side dryrun.",
+				"string",
+				"false",
+			),
+			createTaskParam(
+				"YAMLPATHTOIMAGE",
+				"The path to the image to replace in the yaml manifest (arg to yq)",
+				"string",
+			),
+		},
+	}
+}
