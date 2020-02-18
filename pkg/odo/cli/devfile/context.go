ea54ac9668c65f1b64ef3a753e036f766d6a504a
Rajiv Senthilnathan <rajivsen@ca.ibm.com>
2020-02-17 22:26:18 -0500

    Generate a component name that satisfies DNS-1123 requirement due to deployment name

--- pkg/odo/cli/devfile/context.go
+++ pkg/odo/cli/devfile/context.go
@@ -1,21 +1,24 @@
 package devfile
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/openshift/odo/pkg/devfile/adapters/common"
+	"github.com/openshift/odo/pkg/util"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 // NewDevfileContext creates a new Context struct populated with the current state based on flags specified for the provided command
 func NewDevfileContext(command *cobra.Command) (*Context, error) {
-	workingDir, err := os.Getwd()
-	if err != nil {
-		return nil, err
+	componentName, err := GetComponentName()
+
+	if err != nil || len(componentName) == 0 {
+		return nil, fmt.Errorf("Unable to generate a valid component name. Component names are based on the component directory name and must consist of lower case alphanumeric characters, '-' or '.' and follow DNS-1123 subdomain rules")
 	}
-	componentName := strings.ToLower(filepath.Base(workingDir))
 
 	odoComponent := common.OdoComponent{
 		Name: componentName,
@@ -25,6 +28,19 @@ func NewDevfileContext(command *cobra.Command) (*Context, error) {
 		OdoComponent: odoComponent,
 	}
 	return context, nil
+}
+
+// GetComponentName returns component name
+// Returns: directory name or error
+func GetComponentName() (string, error) {
+	retVal := ""
+	currDir, err := os.Getwd()
+	if err != nil {
+		return "", errors.Wrapf(err, "unable to get component because getting current directory failed")
+	}
+	retVal = filepath.Base(currDir)
+	retVal = strings.TrimSpace(util.GetDNS1123Name(strings.ToLower(retVal)))
+	return retVal, nil
 }
 
 // Context contains contextual information for Devfile commands
