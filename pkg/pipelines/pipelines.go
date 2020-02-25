aadb05e8b2338be3f38bb92ee4d6302d76b89412
Dhanashri Palodkar <dpalodkar14@gmail.com>
2020-02-24 22:43:56 -0500

    Gitops issue 39 (#16)
    * added pipeline.go
    
    * added pipeline_test.go
    
    * added cd pipeline yaml
    
    * updated bootstrap
    
    * upadated deployment path in bootstrap
    
    * add deployment-path flag, called from bootstrap driver, fixed a bunch of other things
    
    Co-authored-by: William Tam <wtam@redhat.com>

---
+++ pkg/pipelines/pipelines.go
@@ -0,0 +1,221 @@
+package pipelines
+
+import (
+	"github.com/openshift/odo/pkg/pipelines/meta"
+	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var (
+	// PipelineTypeMeta Pipeline TypeMeta
+	PipelineTypeMeta = v1.TypeMeta{
+		Kind:       "Pipeline",
+		APIVersion: "tekton.dev/v1alpha1",
+	}
+)
+
+// createCIPipeline creates the dev-ci-pipeline.
+func createDevCIPipeline(name types.NamespacedName) *pipelinev1.Pipeline {
+	return &pipelinev1.Pipeline{
+		TypeMeta:   PipelineTypeMeta,
+		ObjectMeta: meta.ObjectMeta(name),
+		Spec: pipelinev1.PipelineSpec{
+			Params: []pipelinev1.ParamSpec{
+				createParamSpec("REPO", "string"),
+				createParamSpec("COMMIT_SHA", "string"),
+			},
+
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				createPipelineDeclaredResource("source-repo", "git"),
+				createPipelineDeclaredResource("runtime-image", "image"),
+			},
+
+			Tasks: []pipelinev1.PipelineTask{
+				createGitHubStatusTask("create-pending-status", "", "pending", "Starting dev-ci-pipeline"),
+				createBuildImageTask("build-image"),
+				createGitHubStatusTask("create-success-status", "build-image", "success", "Completed dev-ci-pipeline"),
+			},
+		},
+	}
+}
+
+// createStageCIPipeline creates the stage-ci-pipeline
+func createStageCIPipeline(name types.NamespacedName, stageNamespace string) *pipelinev1.Pipeline {
+	return &pipelinev1.Pipeline{
+		TypeMeta:   PipelineTypeMeta,
+		ObjectMeta: meta.ObjectMeta(name),
+		Spec: pipelinev1.PipelineSpec{
+
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				createPipelineDeclaredResource("source-repo", "git"),
+			},
+
+			Tasks: []pipelinev1.PipelineTask{
+				createStageCIPipelineTask("apply-source", stageNamespace),
+			},
+		},
+	}
+}
+
+// createDevCDPipeline creates the dev-cd-pipeline
+func createDevCDPipeline(name types.NamespacedName, deploymentPath, devNamespace string) *pipelinev1.Pipeline {
+	return &pipelinev1.Pipeline{
+		TypeMeta:   PipelineTypeMeta,
+		ObjectMeta: meta.ObjectMeta(name),
+		Spec: pipelinev1.PipelineSpec{
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				createPipelineDeclaredResource("source-repo", "git"),
+				createPipelineDeclaredResource("runtime-image", "image"),
+			},
+			Tasks: []pipelinev1.PipelineTask{
+				createDevCDBuildImageTask("build-image"),
+				createDevCDDeployImageTask("deploy-image", devNamespace, deploymentPath),
+			},
+		},
+	}
+}
+
+// createStageCDPipeline creates the stage-cd-pipeline
+func createStageCDPipeline(name types.NamespacedName, stageNamespace string) *pipelinev1.Pipeline {
+	return &pipelinev1.Pipeline{
+		TypeMeta:   PipelineTypeMeta,
+		ObjectMeta: meta.ObjectMeta(name),
+		Spec: pipelinev1.PipelineSpec{
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				createPipelineDeclaredResource("source-repo", "git"),
+			},
+			Tasks: []pipelinev1.PipelineTask{
+				createStageCDPipelineTask("apply-source", stageNamespace),
+			},
+		},
+	}
+}
+
+func createParamSpec(name string, paramType pipelinev1.ParamType) pipelinev1.ParamSpec {
+	return pipelinev1.ParamSpec{Name: name, Type: paramType}
+}
+
+func createPipelineDeclaredResource(name string, resourceType string) pipelinev1.PipelineDeclaredResource {
+	return pipelinev1.PipelineDeclaredResource{Name: name, Type: resourceType}
+}
+
+func createGitHubStatusTask(name, runAfter, state, description string) pipelinev1.PipelineTask {
+	t := pipelinev1.PipelineTask{
+		Name:    name,
+		TaskRef: createTaskRef("create-github-status-task"),
+		Params: []pipelinev1.Param{
+			createTaskParam("REPO", "$(params.REPO)"),
+			createTaskParam("COMMIT_SHA", "$(params.COMMIT_SHA)"),
+			createTaskParam("STATE", state),
+			createTaskParam("DESCRIPTION", description),
+			createTaskParam("CONTEXT", "dev-ci-pipeline"),
+		},
+	}
+	if runAfter != "" {
+		t.RunAfter = []string{runAfter}
+	}
+	return t
+}
+
+func createBuildImageTask(name string) pipelinev1.PipelineTask {
+	return pipelinev1.PipelineTask{
+		Name:     name,
+		TaskRef:  createTaskRef("buildah-task"),
+		RunAfter: []string{"create-pending-status"},
+		Resources: &pipelinev1.PipelineTaskResources{
+			Inputs:  []pipelinev1.PipelineTaskInputResource{createInputTaskResource("source", "source-repo")},
+			Outputs: []pipelinev1.PipelineTaskOutputResource{createOutputTaskResource("image", "runtime-image")},
+		},
+	}
+
+}
+
+func createDevCDBuildImageTask(name string) pipelinev1.PipelineTask {
+	return pipelinev1.PipelineTask{
+		Name:    name,
+		TaskRef: createTaskRef("buildah-task"),
+		Resources: &pipelinev1.PipelineTaskResources{
+			Inputs:  []pipelinev1.PipelineTaskInputResource{createInputTaskResource("source", "source-repo")},
+			Outputs: []pipelinev1.PipelineTaskOutputResource{createOutputTaskResource("image", "runtime-image")},
+		},
+	}
+}
+
+func createDevCDDeployImageTask(name, devNamespace, deploymentPath string) pipelinev1.PipelineTask {
+	return pipelinev1.PipelineTask{
+		Name:     name,
+		TaskRef:  createTaskRef("deploy-using-kubectl-task"),
+		RunAfter: []string{"build-image"},
+		Resources: &pipelinev1.PipelineTaskResources{
+			Inputs: []pipelinev1.PipelineTaskInputResource{
+				createInputTaskResource("source", "source-repo"),
+				createInputTaskResource("image", "runtime-image"),
+			},
+		},
+		Params: []pipelinev1.Param{
+			createTaskParam("PATHTODEPLOYMENT", deploymentPath),
+			createTaskParam("YAMLPATHTOIMAGE", "spec.template.spec.containers[0].image"),
+			createTaskParam("NAMESPACE", devNamespace),
+		},
+	}
+}
+
+func createStageCIPipelineTask(taskName, stageNamespace string) pipelinev1.PipelineTask {
+	return pipelinev1.PipelineTask{
+		Name:    taskName,
+		TaskRef: createTaskRef("deploy-from-source-task"),
+		Resources: &pipelinev1.PipelineTaskResources{
+			Inputs: []pipelinev1.PipelineTaskInputResource{createInputTaskResource("source", "source-repo")},
+		},
+		Params: []pipelinev1.Param{
+			createTaskParam("NAMESPACE", stageNamespace),
+			createTaskParam("DRYRUN", "true"),
+		},
+	}
+}
+
+func createStageCDPipelineTask(taskName, stageNamespace string) pipelinev1.PipelineTask {
+	return pipelinev1.PipelineTask{
+		Name:    taskName,
+		TaskRef: createTaskRef("deploy-from-source-task"),
+		Resources: &pipelinev1.PipelineTaskResources{
+			Inputs: []pipelinev1.PipelineTaskInputResource{createInputTaskResource("source", "source-repo")},
+		},
+		Params: []pipelinev1.Param{
+			createTaskParam("NAMESPACE", stageNamespace),
+		},
+	}
+}
+
+func createTaskParam(name, value string) pipelinev1.Param {
+	return pipelinev1.Param{
+		Name: name,
+
+		Value: pipelinev1.ArrayOrString{
+			Type:      pipelinev1.ParamTypeString,
+			StringVal: value,
+		},
+	}
+}
+
+func createTaskRef(name string) *pipelinev1.TaskRef {
+	return &pipelinev1.TaskRef{
+		Name: name,
+	}
+}
+
+func createInputTaskResource(name string, resource string) pipelinev1.PipelineTaskInputResource {
+	return pipelinev1.PipelineTaskInputResource{
+		Name:     name,
+		Resource: resource,
+	}
+
+}
+
+func createOutputTaskResource(name string, resource string) pipelinev1.PipelineTaskOutputResource {
+	return pipelinev1.PipelineTaskOutputResource{
+		Name:     name,
+		Resource: resource,
+	}
+}
---
+++ pkg/pipelines/pipelines_test.go
@@ -0,0 +1,252 @@
+package pipelines
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/openshift/odo/pkg/pipelines/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+)
+
+func TestCreateDevCIPipeline(t *testing.T) {
+
+	DevCIpipeline := createDevCIPipeline(meta.NamespacedName("cicd-environment", "dev-ci-pipeline"))
+
+	want := &pipelinev1.Pipeline{
+		TypeMeta: PipelineTypeMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dev-ci-pipeline",
+			Namespace: "cicd-environment",
+		},
+		Spec: pipelinev1.PipelineSpec{
+			Params: []pipelinev1.ParamSpec{
+				pipelinev1.ParamSpec{
+					Name: "REPO",
+					Type: "string",
+				},
+				pipelinev1.ParamSpec{
+					Name: "COMMIT_SHA",
+					Type: "string",
+				},
+			},
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				pipelinev1.PipelineDeclaredResource{
+					Name: "source-repo",
+					Type: "git",
+				},
+				pipelinev1.PipelineDeclaredResource{
+					Name: "runtime-image",
+					Type: "image",
+				},
+			},
+			Tasks: []pipelinev1.PipelineTask{
+				pipelinev1.PipelineTask{
+					Name: "create-pending-status",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "create-github-status-task",
+					},
+					Params: []pipelinev1.Param{
+						{Name: "REPO", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "$(params.REPO)"}},
+						{Name: "COMMIT_SHA", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "$(params.COMMIT_SHA)"}},
+						{Name: "STATE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "pending"}},
+						{Name: "DESCRIPTION", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "Starting dev-ci-pipeline"}},
+						{Name: "CONTEXT", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "dev-ci-pipeline"}},
+					},
+				},
+
+				pipelinev1.PipelineTask{
+					Name: "build-image",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "buildah-task",
+					},
+					RunAfter: []string{"create-pending-status"},
+					Resources: &pipelinev1.PipelineTaskResources{
+						Inputs: []pipelinev1.PipelineTaskInputResource{
+							{Name: "source",
+								Resource: "source-repo"},
+						},
+						Outputs: []pipelinev1.PipelineTaskOutputResource{
+							{Name: "image",
+								Resource: "runtime-image"},
+						},
+					},
+				},
+
+				pipelinev1.PipelineTask{
+					Name: "create-success-status",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "create-github-status-task",
+					},
+					RunAfter: []string{"build-image"},
+					Params: []pipelinev1.Param{
+						{Name: "REPO", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "$(params.REPO)"}},
+						{Name: "COMMIT_SHA", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "$(params.COMMIT_SHA)"}},
+						{Name: "STATE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "success"}},
+						{Name: "DESCRIPTION", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "Completed dev-ci-pipeline"}},
+						{Name: "CONTEXT", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "dev-ci-pipeline"}},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, DevCIpipeline); diff != "" {
+		t.Fatalf("TestCreateDevCIPipeline() failed got\n%s", diff)
+	}
+}
+
+func TestCreateStageCIPipeline(t *testing.T) {
+
+	stageCIpipeline := createStageCIPipeline(meta.NamespacedName("cicd-environment", "stage-ci-pipeline"), "stage-environment")
+
+	want := &pipelinev1.Pipeline{
+		TypeMeta: PipelineTypeMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "stage-ci-pipeline",
+			Namespace: "cicd-environment",
+		},
+		Spec: pipelinev1.PipelineSpec{
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				pipelinev1.PipelineDeclaredResource{
+					Name: "source-repo",
+					Type: "git",
+				},
+			},
+
+			Tasks: []pipelinev1.PipelineTask{
+				pipelinev1.PipelineTask{
+					Name: "apply-source",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "deploy-from-source-task",
+					},
+					Resources: &pipelinev1.PipelineTaskResources{
+						Inputs: []pipelinev1.PipelineTaskInputResource{
+							{Name: "source",
+								Resource: "source-repo"},
+						},
+					},
+					Params: []pipelinev1.Param{
+						{Name: "NAMESPACE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "stage-environment"}},
+						{Name: "DRYRUN", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "true"}},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, stageCIpipeline); diff != "" {
+		t.Fatalf("TestcreateStageCIPipeline() failed got\n%s", diff)
+	}
+
+}
+
+func TestCreateDevCDPipeline(t *testing.T) {
+	DevCDpipeline := createDevCDPipeline(meta.NamespacedName("cicd-environment", "dev-cd-pipeline"), "usr/path/", "dev-environment")
+	want := &pipelinev1.Pipeline{
+		TypeMeta: PipelineTypeMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dev-cd-pipeline",
+			Namespace: "cicd-environment",
+		},
+
+		Spec: pipelinev1.PipelineSpec{
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				pipelinev1.PipelineDeclaredResource{
+					Name: "source-repo",
+					Type: "git",
+				},
+				pipelinev1.PipelineDeclaredResource{
+					Name: "runtime-image",
+					Type: "image",
+				},
+			},
+			Tasks: []pipelinev1.PipelineTask{
+
+				pipelinev1.PipelineTask{
+					Name: "build-image",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "buildah-task",
+					},
+
+					Resources: &pipelinev1.PipelineTaskResources{
+						Inputs: []pipelinev1.PipelineTaskInputResource{
+							{Name: "source",
+								Resource: "source-repo"},
+						},
+						Outputs: []pipelinev1.PipelineTaskOutputResource{
+							{Name: "image",
+								Resource: "runtime-image"},
+						},
+					},
+				},
+
+				pipelinev1.PipelineTask{
+					Name: "deploy-image",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "deploy-using-kubectl-task",
+					},
+					RunAfter: []string{"build-image"},
+					Resources: &pipelinev1.PipelineTaskResources{
+						Inputs: []pipelinev1.PipelineTaskInputResource{
+							{Name: "source", Resource: "source-repo"},
+							{Name: "image", Resource: "runtime-image"},
+						},
+					},
+					Params: []pipelinev1.Param{
+						{Name: "PATHTODEPLOYMENT", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "usr/path/"}},
+						{Name: "YAMLPATHTOIMAGE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "spec.template.spec.containers[0].image"}},
+						{Name: "NAMESPACE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "dev-environment"}},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, DevCDpipeline); diff != "" {
+		t.Fatalf("TestCreateDevCDPipeline() failed got\n%s", diff)
+	}
+}
+
+func TestCreateStageCDPipeline(t *testing.T) {
+	stageCDpipeline := createStageCDPipeline(meta.NamespacedName("cicd-environment", "stage-cd-pipeline"), "stage-environment")
+	want := &pipelinev1.Pipeline{
+		TypeMeta: PipelineTypeMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "stage-cd-pipeline",
+			Namespace: "cicd-environment",
+		},
+		Spec: pipelinev1.PipelineSpec{
+			Resources: []pipelinev1.PipelineDeclaredResource{
+				pipelinev1.PipelineDeclaredResource{
+					Name: "source-repo",
+					Type: "git",
+				},
+			},
+
+			Tasks: []pipelinev1.PipelineTask{
+				pipelinev1.PipelineTask{
+					Name: "apply-source",
+					TaskRef: &pipelinev1.TaskRef{
+						Name: "deploy-from-source-task",
+					},
+					Resources: &pipelinev1.PipelineTaskResources{
+						Inputs: []pipelinev1.PipelineTaskInputResource{
+							{Name: "source",
+								Resource: "source-repo"},
+						},
+					},
+					Params: []pipelinev1.Param{
+						{Name: "NAMESPACE", Value: pipelinev1.ArrayOrString{Type: "string", StringVal: "stage-environment"}},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, stageCDpipeline); diff != "" {
+		t.Fatalf("TestcreateStageCSPipeline() failed got\n%s", diff)
+	}
+
+}
