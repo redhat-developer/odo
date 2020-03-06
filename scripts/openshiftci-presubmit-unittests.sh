2685365d518b1ca27481f66e27da46660bd65365
Tomas Kral <tkral@redhat.com>
2020-03-06 13:22:05 +0100

    Update openshiftci-presubmit-unittests.sh

--- scripts/openshiftci-presubmit-unittests.sh
+++ scripts/openshiftci-presubmit-unittests.sh
@@ -8,13 +8,12 @@ set -x
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 export PATH=$PATH:$GOPATH/bin
-# set localtion for golangci-lint cache
+# set location for golangci-lint cache
 # otherwise /.cache is used, and it fails on permission denied
 export GOLANGCI_LINT_CACHE="/tmp/.cache"
 
 make goget-tools
 make validate
-make golint
 make test
 
 # crosscompile and publish artifacts
