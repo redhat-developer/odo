bb84433e28e67cf022724531ffda3169d26bbdea
John Collier <John.J.Collier@ibm.com>
2020-03-16 15:04:04 -0400

    Remove license that was added via copy and paste
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- pkg/lclient/fakeclient.go
+++ pkg/lclient/fakeclient.go
@@ -1,14 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-
 package lclient
 
 import (
