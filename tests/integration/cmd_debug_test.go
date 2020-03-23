1aa65cc31907548a71cb59501e3c39999d29094e
arout <arout@redhat.com>
2020-03-23 17:06:55 +0530

    Covering java component debug test

--- tests/integration/cmd_debug_test.go
+++ tests/integration/cmd_debug_test.go
@@ -1,7 +1,6 @@
 package integration
 
 import (
-	"github.com/openshift/odo/pkg/util"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -12,6 +11,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/openshift/odo/pkg/util"
 	"github.com/openshift/odo/tests/helper"
 )
 
@@ -101,6 +101,47 @@ var _ = Describe("odo debug command tests", func() {
 			stopChannel <- true
 		})
 
+		It("should expect a ws connection when tried to connect on different debug port locally and remotely", func() {
+			oc.ImportJavaIS(project)
+			helper.CopyExample(filepath.Join("binary", "java", "openjdk"), context)
+			helper.CmdShouldPass("odo", "component", "create", "java:8", "--project", project, "--context", context)
+			helper.CmdShouldPass("odo", "config", "set", "--force", "DebugPort", "9292", "--context", context)
+			dbgPort := helper.GetConfigValueWithContext("DebugPort", context)
+			Expect(dbgPort).To(Equal("9292"))
+			helper.CmdShouldPass("odo", "push", "--context", context)
+
+			httpPort, err := util.HttpGetFreePort()
+			Expect(err).NotTo(HaveOccurred())
+			freePort := strconv.Itoa(httpPort)
+
+			stopChannel := make(chan bool)
+			go func() {
+				helper.CmdShouldRunAndTerminate(60*time.Second, stopChannel, "odo", "debug", "port-forward", "--local-port", freePort, "--context", context)
+			}()
+
+			// 400 response expected because the endpoint expects a websocket request and we are doing a HTTP GET
+			// We are just using this to validate if nodejs agent is listening on the other side
+			helper.HttpWaitForWithStatus("http://localhost:"+freePort, "WebSockets request was expected", 12, 5, 400)
+			stopChannel <- true
+		})
+
+		It("should expect a ws connection when tried to connect on default debug port locally", func() {
+			oc.ImportJavaIS(project)
+			helper.CopyExample(filepath.Join("binary", "java", "openjdk"), context)
+			helper.CmdShouldPass("odo", "component", "create", "java:8", "--project", project, "--context", context)
+			helper.CmdShouldPass("odo", "push", "--context", context)
+
+			stopChannel := make(chan bool)
+			go func() {
+				helper.CmdShouldRunAndTerminate(60*time.Second, stopChannel, "odo", "debug", "port-forward", "--context", context)
+			}()
+
+			// 400 response expected because the endpoint expects a websocket request and we are doing a HTTP GET
+			// We are just using this to validate if nodejs agent is listening on the other side
+			helper.HttpWaitForWithStatus("http://localhost:5858", "WebSockets request was expected", 12, 5, 400)
+			stopChannel <- true
+		})
+
 	})
 
 	Context("odo debug info should work on a odo component", func() {
@@ -160,6 +201,59 @@ var _ = Describe("odo debug command tests", func() {
 			// CTRL_C_EVENTS from the terminal works fine https://github.com/golang/go/issues/6720#issuecomment-66087737
 			// here's a hack to generate the event https://golang.org/cl/29290044
 			// but the solution is unacceptable https://github.com/golang/go/issues/6720#issuecomment-66087749
+			if runtime.GOOS != "windows" {
+				Expect(helper.ListFilesInDir(os.TempDir())).To(Not(ContainElement(project + "-app" + "-nodejs-cmp-" + project + "-odo-debug.json")))
+			}
+
+		})
+		It("should start a debug session and run debug info on a running debug session", func() {
+			oc.ImportJavaIS(project)
+			helper.CopyExample(filepath.Join("binary", "java", "openjdk"), context)
+			helper.CmdShouldPass("odo", "component", "create", "java:8", "--project", project, "--context", context)
+			helper.CmdShouldPass("odo", "push", "--context", context)
+
+			httpPort, err := util.HttpGetFreePort()
+			Expect(err).NotTo(HaveOccurred())
+			freePort := strconv.Itoa(httpPort)
+
+			stopChannel := make(chan bool)
+			go func() {
+				helper.CmdShouldRunAndTerminate(60*time.Second, stopChannel, "odo", "debug", "port-forward", "--local-port", freePort, "--context", context)
+			}()
+
+			// 400 response expected because the endpoint expects a websocket request and we are doing a HTTP GET
+			// We are just using this to validate if nodejs agent is listening on the other side
+			helper.HttpWaitForWithStatus("http://localhost:"+freePort, "WebSockets request was expected", 12, 5, 400)
+			runningString := helper.CmdShouldPass("odo", "debug", "info", "--context", context)
+			Expect(runningString).To(ContainSubstring(freePort))
+			Expect(helper.ListFilesInDir(os.TempDir())).To(ContainElement(project + "-app" + "-nodejs-cmp-" + project + "-odo-debug.json"))
+			stopChannel <- true
+		})
+
+		It("should start a debug session and run debug info on a closed debug session", func() {
+			oc.ImportJavaIS(project)
+			helper.CopyExample(filepath.Join("binary", "java", "openjdk"), context)
+			helper.CmdShouldPass("odo", "component", "create", "java:8", "--project", project, "--context", context)
+			helper.CmdShouldPass("odo", "push", "--context", context)
+
+			httpPort, err := util.HttpGetFreePort()
+			Expect(err).NotTo(HaveOccurred())
+			freePort := strconv.Itoa(httpPort)
+
+			stopChannel := make(chan bool)
+			go func() {
+				helper.CmdShouldRunAndTerminate(60*time.Second, stopChannel, "odo", "debug", "port-forward", "--local-port", freePort, "--context", context)
+			}()
+
+			// 400 response expected because the endpoint expects a websocket request and we are doing a HTTP GET
+			// We are just using this to validate if nodejs agent is listening on the other side
+			helper.HttpWaitForWithStatus("http://localhost:"+freePort, "WebSockets request was expected", 12, 5, 400)
+			runningString := helper.CmdShouldPass("odo", "debug", "info", "--context", context)
+			Expect(runningString).To(ContainSubstring(freePort))
+			stopChannel <- true
+			failString := helper.CmdShouldPass("odo", "debug", "info", "--context", context)
+			Expect(failString).To(ContainSubstring("not running"))
+
 			if runtime.GOOS != "windows" {
 				Expect(helper.ListFilesInDir(os.TempDir())).To(Not(ContainElement(project + "-app" + "-nodejs-cmp-" + project + "-odo-debug.json")))
 			}
