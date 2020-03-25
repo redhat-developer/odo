2a8212dccd0ff9f61a521fa222b09f608bbd4dab
jingfu wang <jingfu.j.wang@ibm.com>
2020-03-25 18:46:00 -0400

    Update integration test
    Signed-off-by: jingfu wang <jingfu.j.wang@ibm.com>

--- tests/integration/cmd_devfile_create_test.go
+++ tests/integration/cmd_devfile_create_test.go
@@ -1,6 +1,8 @@
 package integration
 
 import (
+	"os"
+	"path/filepath"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -11,35 +13,36 @@ import (
 var _ = Describe("odo devfile create command tests", func() {
 	var project string
 	var context string
-	var originalDir string
+	var currentWorkingDirectory string
 
 	// This is run after every Spec (It)
 	var _ = BeforeEach(func() {
 		SetDefaultEventuallyTimeout(10 * time.Minute)
-		SetDefaultConsistentlyDuration(30 * time.Second)
 		project = helper.CreateRandProject()
-		context = helper.CreateNewContext()
-		originalDir = helper.Getwd()
+		context = helper.CreateNewDevfileContext()
+		currentWorkingDirectory = helper.Getwd()
 		helper.Chdir(context)
-		helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
+		os.Setenv("GLOBALODOCONFIG", filepath.Join(context, "config.yaml"))
 	})
 
 	// This is run after every Spec (It)
 	var _ = AfterEach(func() {
 		helper.DeleteProject(project)
-		helper.Chdir(originalDir)
+		helper.Chdir(currentWorkingDirectory)
 		helper.DeleteDir(context)
-		helper.CmdShouldPass("odo", "preference", "set", "Experimental", "false")
+		os.Unsetenv("GLOBALODOCONFIG")
 	})
 
 	Context("When executing odo create with devfile component type argument", func() {
 		It("should successfully create the devfile component", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			helper.CmdShouldPass("odo", "create", "openLiberty")
 		})
 	})
 
 	Context("When executing odo create with devfile component type and component name arguments", func() {
 		It("should successfully create the devfile component", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			componentName := helper.RandString(6)
 			helper.CmdShouldPass("odo", "create", "openLiberty", componentName)
 		})
@@ -47,6 +50,7 @@ var _ = Describe("odo devfile create command tests", func() {
 
 	Context("When executing odo create with devfile component type argument and --project flag", func() {
 		It("should successfully create the devfile component", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			componentNamespace := helper.RandString(6)
 			helper.CmdShouldPass("odo", "create", "openLiberty", "--project", componentNamespace)
 		})
@@ -54,6 +58,7 @@ var _ = Describe("odo devfile create command tests", func() {
 
 	Context("When executing odo create with devfile component name that contains unsupported character", func() {
 		It("should failed with component name is not valid and prompt supported character", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			componentName := "BAD@123"
 			output := helper.CmdShouldFail("odo", "create", "openLiberty", componentName)
 			helper.MatchAllInOutput(output, []string{"Contain only lowercase alphanumeric characters or ‘-’"})
@@ -62,6 +67,7 @@ var _ = Describe("odo devfile create command tests", func() {
 
 	Context("When executing odo create with devfile component name that contains all numeric values", func() {
 		It("should failed with component name is not valid and prompt container name must not contain all numeric values", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			componentName := "123456"
 			output := helper.CmdShouldFail("odo", "create", "openLiberty", componentName)
 			helper.MatchAllInOutput(output, []string{"Must not contain all numeric values"})
@@ -70,6 +76,7 @@ var _ = Describe("odo devfile create command tests", func() {
 
 	Context("When executing odo create with devfile component name that contains more than 63 characters ", func() {
 		It("should failed with component name is not valid and prompt container name contains at most 63 characters", func() {
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			componentName := helper.RandString(64)
 			output := helper.CmdShouldFail("odo", "create", "openLiberty", componentName)
 			helper.MatchAllInOutput(output, []string{"Contain at most 63 characters"})
