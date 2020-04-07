b5e4cb08bc1621b4b0a4019d92182d6f4b7934da
Charlie Drage <charlie@charliedrage.com>
2020-04-07 15:18:18 -0400

    Adds disclaimer that experimental mode is enabled
    **What type of PR is this?**
    
    /kind documentation
    
    **What does does this PR do / why we need it**:
    
    Adds a disclaimer that experimental mode is enabled when using `odo
    create` or `odo push`.
    
    See asciinema:
    
    [![asciicast](https://asciinema.org/a/A6ITI1wNQL9hahhLsCz4DT6wQ.svg)](https://asciinema.org/a/A6ITI1wNQL9hahhLsCz4DT6wQ)
    
    **Which issue(s) this PR fixes**:
    
    N/A
    
    **How to test changes / Special notes to the reviewer**:
    
    ```sh
    odo preference set experimental true
    git clone https://github.com/openshift/nodejs-ex
    odo create nodejs
    odo push
    ```
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- pkg/odo/cli/component/create.go
+++ pkg/odo/cli/component/create.go
@@ -285,7 +285,12 @@ func (co *CreateOptions) setResourceLimits() error {
 
 // Complete completes create args
 func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+
 	if experimental.IsExperimentalModeEnabled() {
+
+		// Add a disclaimer that we are in *experimental mode*
+		log.Experimental("Experimental mode is enabled, use at your own risk")
+
 		if len(args) == 0 {
 			co.interactive = true
 		}
@@ -361,10 +366,13 @@ func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string
 		co.devfileMetadata.componentName = strings.ToLower(componentName)
 		co.devfileMetadata.componentNamespace = componentNamespace
 
+		// Categorize the sections
+		log.Info("Validation")
+
 		// Since we need to support both devfile and s2i, so we have to check if the component type is
 		// supported by devfile, if it is supported we return, but if it is not supported we still need to
 		// run all codes related with s2i
-		spinner := log.Spinner("Checking if the specified component type is supported devfile component type")
+		spinner := log.Spinner("Checking Devfile compatibility")
 
 		for _, devfileComponent := range catalogDevfileList.Items {
 			if co.devfileMetadata.componentType == devfileComponent.Name && devfileComponent.Support {
@@ -579,10 +587,11 @@ func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string
 
 // Validate validates the create parameters
 func (co *CreateOptions) Validate() (err error) {
+
 	if experimental.IsExperimentalModeEnabled() {
 		if co.devfileMetadata.devfileSupport {
 			// Validate if the devfile component that user wants to create already exists
-			spinner := log.Spinner("Validating component")
+			spinner := log.Spinner("Validating Devfile")
 			defer spinner.End(false)
 
 			if util.CheckPathExists(EnvFilePath) {
@@ -599,6 +608,8 @@ func (co *CreateOptions) Validate() (err error) {
 			return nil
 		}
 	}
+
+	log.Info("Validation")
 
 	supported, err := catalog.IsComponentTypeSupported(co.Context.Client, *co.componentSettings.Type)
 	if err != nil {
