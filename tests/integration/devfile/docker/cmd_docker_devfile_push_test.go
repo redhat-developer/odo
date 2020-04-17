016029ba17e30fa1ab82e8c9ac054ba017d27a00
John Collier <John.J.Collier@ibm.com>
2020-04-16 22:58:42 -0400

    Add integration tests
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- tests/integration/devfile/docker/cmd_docker_devfile_push_test.go
+++ tests/integration/devfile/docker/cmd_docker_devfile_push_test.go
@@ -74,6 +74,50 @@ var _ = Describe("odo docker devfile push command tests", func() {
 			helper.CmdShouldPass("odo", "push", "--devfile", "devfile.yaml")
 		})
 
+		It("Check that odo push works with a devfile that has volumes defined", func() {
+			// Local devfile push requires experimental mode to be set and the pushtarget set to docker
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
+			helper.CmdShouldPass("odo", "preference", "set", "pushtarget", "docker")
+
+			helper.CmdShouldPass("odo", "create", "nodejs", "--context", context, cmpName)
+
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs"), context)
+			helper.RenameFile("devfile.yaml", "devfile-old.yaml")
+			helper.RenameFile("devfile-with-volumes.yaml", "devfile.yaml")
+
+			output := helper.CmdShouldPass("odo", "push", "--devfile", "devfile.yaml")
+			Expect(output).To(ContainSubstring("Changes successfully pushed to component"))
+
+			// Verify the volumes got created successfully (and 3 volumes exist: one source and two defined in devfile)
+			label := "component=" + cmpName
+			volumes := dockerClient.GetVolumesByLabel(label)
+			Expect(len(volumes)).To(Equal(3))
+		})
+
+		It("Check that odo push mounts the docker volumes in the container", func() {
+			// Local devfile push requires experimental mode to be set and the pushtarget set to docker
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
+			helper.CmdShouldPass("odo", "preference", "set", "pushtarget", "docker")
+
+			helper.CmdShouldPass("odo", "create", "nodejs", "--context", context, cmpName)
+
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs"), context)
+			helper.RenameFile("devfile.yaml", "devfile-old.yaml")
+			helper.RenameFile("devfile-with-volumes.yaml", "devfile.yaml")
+
+			output := helper.CmdShouldPass("odo", "push", "--devfile", "devfile.yaml")
+			Expect(output).To(ContainSubstring("Changes successfully pushed to component"))
+
+			// Retrieve the volume from one of the aliases in the devfile
+			volumes := dockerClient.GetVolumesByCompStorageName(cmpName, "myvol")
+			Expect(len(volumes)).To(Equal(1))
+			vol := volumes[0]
+
+			// Verify the volume is mounted
+			volMounted := dockerClient.IsVolumeMountedInContainer(vol, cmpName, "runtime")
+			Expect(volMounted).To(Equal(true))
+		})
+
 	})
 
 })
