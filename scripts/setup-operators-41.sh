be3df8d0ec9b827246feff7c3a84bac5ddc06749
Dharmit Shah <shahdharmit@gmail.com>
2020-04-21 15:09:29 +0530

    Make sure that desired operators are installed in CI
    The flake we're seeing is not reproducible so I've added a while loop
    which will make sure that the operators we're interested in are
    installed. The yaml definition works fine so there's no issue on that
    front.

--- scripts/setup-operators-41.sh
+++ scripts/setup-operators-41.sh
@@ -2,6 +2,47 @@
 set -x
 
 CI_OPERATOR_HUB_PROJECT=ci-operator-hub-project
+
+install_mongo_operator() {
+  # Subscription for mongo
+  oc create -f -<<EOF
+  apiVersion: operators.coreos.com/v1alpha1
+  kind: Subscription
+  metadata:
+    labels:
+      csc-owner-name: mongo-csc
+      csc-owner-namespace: openshift-marketplace
+    name: mongodb-enterprise
+    namespace: openshift-operators
+  spec:
+    channel: stable
+    installPlanApproval: Automatic
+    name: mongodb-enterprise
+    source: mongo-csc
+    sourceNamespace: openshift-operators
+EOF
+}
+
+
+install_etcd_operator(){
+  oc create -f -<<EOF
+  apiVersion: operators.coreos.com/v1alpha1
+  kind: Subscription
+  metadata:
+    labels:
+      csc-owner-name: etcd-csc
+      csc-owner-namespace: openshift-marketplace
+    name: etcd
+    namespace: ${CI_OPERATOR_HUB_PROJECT}
+  spec:
+    channel: singlenamespace-alpha
+    installPlanApproval: Automatic
+    name: etcd
+    source: etcd-csc
+    sourceNamespace: ${CI_OPERATOR_HUB_PROJECT}
+EOF
+}
+
 # First, install cluster-wide operator
 # CatalogSourceConfig for mongodb
 oc create -f -<<EOF
@@ -16,23 +57,18 @@ spec:
   packages: mongodb-enterprise
   targetNamespace: openshift-operators
 EOF
-# Subscription for mongo
-oc create -f -<<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  labels:
-    csc-owner-name: mongo-csc
-    csc-owner-namespace: openshift-marketplace
-  name: mongodb-enterprise
-  namespace: openshift-operators
-spec:
-  channel: stable
-  installPlanApproval: Automatic
-  name: mongodb-enterprise
-  source: mongo-csc
-  sourceNamespace: openshift-operators
-EOF
+
+# install mongo operator
+while :
+do
+    if oc get csv -n openshift-operators | grep mongo; then
+        break
+    else
+        install_mongo_operator
+        sleep 15
+    fi
+done
+
 # Now onto namespace bound operator
 # Create OperatorGroup
 oc create -f -<<EOF
@@ -65,20 +101,14 @@ spec:
   packages: etcd
   targetNamespace: ${CI_OPERATOR_HUB_PROJECT}
 EOF
-# Next, create a subscription
-oc create -f -<<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  labels:
-    csc-owner-name: etcd-csc
-    csc-owner-namespace: openshift-marketplace
-  name: etcd
-  namespace: ${CI_OPERATOR_HUB_PROJECT}
-spec:
-  channel: singlenamespace-alpha
-  installPlanApproval: Automatic
-  name: etcd
-  source: etcd-csc
-  sourceNamespace: ${CI_OPERATOR_HUB_PROJECT}
-EOF
+
+# install etcd operator
+while :
+do
+    if oc get csv -n ${CI_OPERATOR_HUB_PROJECT} | grep etcd; then
+        break
+    else
+        install_etcd_operator
+        sleep 15
+    fi
+done
--- scripts/setup-operators.sh
+++ scripts/setup-operators.sh
@@ -2,21 +2,53 @@
 set -x
 
 CI_OPERATOR_HUB_PROJECT=ci-operator-hub-project
-# First, enable a cluster-wide mongo operator
-oc create -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  generation: 1
-  name: mongodb-enterprise
-  namespace: openshift-operators
-spec:
-  channel: stable
-  installPlanApproval: Automatic
-  name: mongodb-enterprise
-  source: certified-operators
-  sourceNamespace: openshift-marketplace
+
+install_mongo_operator() {
+  # First, enable a cluster-wide mongo operator
+  oc create -f - <<EOF
+  apiVersion: operators.coreos.com/v1alpha1
+  kind: Subscription
+  metadata:
+    generation: 1
+    name: mongodb-enterprise
+    namespace: openshift-operators
+  spec:
+    channel: stable
+    installPlanApproval: Automatic
+    name: mongodb-enterprise
+    source: certified-operators
+    sourceNamespace: openshift-marketplace
 EOF
+}
+
+install_etcd_operator(){
+  # Create subscription
+  oc create -f - <<EOF
+  apiVersion: operators.coreos.com/v1alpha1
+  kind: Subscription
+  metadata:
+    name: etcd
+    namespace: ${OPERATOR_HUB_PROJECT}
+  spec:
+    channel: singlenamespace-alpha
+    installPlanApproval: Automatic
+    name: etcd
+    source: community-operators
+    sourceNamespace: openshift-marketplace
+EOF
+}
+
+# install mongo operator
+while :
+do
+    if oc get csv -n openshift-operators | grep mongo; then
+        break
+    else
+        install_mongo_operator
+        sleep 15
+    fi
+done
+
 # Now onto namespace bound operator
 # Create OperatorGroup
 oc create -f - <<EOF
@@ -30,17 +62,14 @@ spec:
   targetNamespaces:
   - ${CI_OPERATOR_HUB_PROJECT}
 EOF
-# Create subscription
-oc create -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: etcd
-  namespace: ${OPERATOR_HUB_PROJECT}
-spec:
-  channel: singlenamespace-alpha
-  installPlanApproval: Automatic
-  name: etcd
-  source: community-operators
-  sourceNamespace: openshift-marketplace
-EOF
+
+# install etcd operator
+while :
+do
+    if oc get csv -n ${CI_OPERATOR_HUB_PROJECT} | grep etcd; then
+        break
+    else
+        install_etcd_operator
+        sleep 15
+    fi
+done
