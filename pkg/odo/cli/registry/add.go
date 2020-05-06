be57e72bc889051acac7a55a9b8ff2c7d15370d5
jingfu wang <jingfu.j.wang@ibm.com>
2020-05-06 14:29:57 -0400

    Update help page
    Signed-off-by: jingfu wang <jingfu.j.wang@ibm.com>

--- pkg/odo/cli/registry/add.go
+++ pkg/odo/cli/registry/add.go
@@ -23,9 +23,7 @@ var (
 	addLongDesc = ktemplates.LongDesc(`Add devfile registry`)
 
 	addExample = ktemplates.Examples(`# Add devfile registry
-	%[1]s CheRegistry https://che-devfile-registry.openshift.io/
-
-	%[1]s CheRegistryFromGitHub https://raw.githubusercontent.com/eclipse/che-devfile-registry/master/
+	%[1]s CheRegistry https://che-devfile-registry.openshift.io
 	`)
 )
 
