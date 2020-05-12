2210a75d80189d7770b456f0b11978d87f8a9c9c
Girish Ramnani <gramnani@redhat.com>
2020-05-12 19:38:00 +0530

    fixed ordering in watch - should fix some stuff

--- pkg/odo/cli/component/watch.go
+++ pkg/odo/cli/component/watch.go
@@ -107,7 +107,7 @@ func (wo *WatchOptions) Complete(name string, cmd *cobra.Command, args []string)
 		} else {
 			platformContext = nil
 		}
-		wo.devfileHandler, err = adapters.NewPlatformAdapter(wo.Application, wo.componentName, wo.componentContext, devObj, platformContext)
+		wo.devfileHandler, err = adapters.NewPlatformAdapter(wo.componentName, wo.Application, wo.componentContext, devObj, platformContext)
 
 		return err
 	}
