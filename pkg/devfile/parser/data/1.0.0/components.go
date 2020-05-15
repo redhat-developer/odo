f8d9883bd4584d4571f8fa097545708ed8786365
Aditi Sharma <adsharma@redhat.com>
2020-05-15 13:39:44 +0530

    Add Devfile Parser V2, Update Common Structs (#3188)
    * Add Devfile Parser for Version 2.0.0
    
    Added Devfile V2 Go Structures
    Added converter for v2 ro common types
    
    Signed-off-by: adisky <adsharma@redhat.com>
    
    * Add example V2 devfile
    
    added example nodejs V2 devfile
    
    * Add Common Types as V2
    
    Add common types as v2
    Add Converter from common to v1
    
    * Updated example devfile with group

--- pkg/devfile/parser/data/1.0.0/components.go
+++ pkg/devfile/parser/data/1.0.0/components.go
@@ -1,21 +1,37 @@
 package version100
 
 import (
-	"strings"
-
 	"github.com/openshift/odo/pkg/devfile/parser/data/common"
 )
 
-// GetComponents returns the slice of DevfileComponent objects parsed from the Devfile
+func (d *Devfile100) GetMetadata() common.DevfileMetadata {
+	// No GenerateName field in V2
+	return common.DevfileMetadata{
+		Name: *d.Metadata.Name,
+		//Version: No field in V1
+	}
+}
+
+/// GetComponents returns the slice of DevfileComponent objects parsed from the Devfile
 func (d *Devfile100) GetComponents() []common.DevfileComponent {
-	return d.Components
+	var comps []common.DevfileComponent
+	for _, v := range d.Components {
+		comps = append(comps, convertV1ComponentToCommon(v))
+	}
+	return comps
 }
 
 // GetAliasedComponents returns the slice of DevfileComponent objects that each have an alias
 func (d *Devfile100) GetAliasedComponents() []common.DevfileComponent {
+	// TODO(adi): we might not need this for V2 as name is a required field now.
+	var comps []common.DevfileComponent
+	for _, v := range d.Components {
+		comps = append(comps, convertV1ComponentToCommon(v))
+	}
+
 	var aliasedComponents = []common.DevfileComponent{}
-	for _, comp := range d.Components {
-		if comp.Alias != nil {
+	for _, comp := range comps {
+		if comp.Container.Name != "" {
 			aliasedComponents = append(aliasedComponents, comp)
 		}
 	}
@@ -24,17 +40,157 @@ func (d *Devfile100) GetAliasedComponents() []common.DevfileComponent {
 
 // GetProjects returns the slice of DevfileProject objects parsed from the Devfile
 func (d *Devfile100) GetProjects() []common.DevfileProject {
-	return d.Projects
+
+	var projects []common.DevfileProject
+	for _, v := range d.Projects {
+		// We are only supporting ProjectType git in V1
+		if v.Source.Type == ProjectTypeGit {
+			projects = append(projects, convertV1ProjectToCommon(v))
+		}
+	}
+
+	return projects
 }
 
 // GetCommands returns the slice of DevfileCommand objects parsed from the Devfile
 func (d *Devfile100) GetCommands() []common.DevfileCommand {
-	var commands []common.DevfileCommand
 
-	for _, command := range d.Commands {
-		command.Name = strings.ToLower(command.Name)
-		commands = append(commands, command)
+	var commands []common.DevfileCommand
+	for _, v := range d.Commands {
+		cmd := convertV1CommandToCommon(v)
+
+		commands = append(commands, cmd)
 	}
 
 	return commands
+}
+
+func (d *Devfile100) GetParent() common.DevfileParent {
+	return common.DevfileParent{}
+
+}
+
+func (d *Devfile100) GetEvents() common.DevfileEvents {
+	return common.DevfileEvents{}
+
+}
+
+func convertV1CommandToCommon(c Command) (d common.DevfileCommand) {
+	var exec common.Exec
+
+	for _, action := range c.Actions {
+
+		if *action.Type == DevfileCommandTypeExec {
+			exec = common.Exec{
+				Attributes:  c.Attributes,
+				CommandLine: *action.Command,
+				Component:   *action.Component,
+				Group:       getGroup(c.Name),
+				Id:          c.Name,
+				WorkingDir:  action.Workdir,
+				// Env:
+				// Label:
+			}
+		}
+
+	}
+
+	// TODO: Previewurl
+	return common.DevfileCommand{
+		//TODO(adi): Type
+		Exec: &exec,
+	}
+}
+
+func convertV1ComponentToCommon(c Component) (d common.DevfileComponent) {
+
+	var endpoints []*common.Endpoint
+	for _, v := range c.ComponentDockerimage.Endpoints {
+		endpoints = append(endpoints, convertV1EndpointsToCommon(v))
+	}
+
+	var envs []*common.Env
+	for _, v := range c.ComponentDockerimage.Env {
+		envs = append(envs, convertV1EnvToCommon(v))
+	}
+
+	var volumes []*common.VolumeMount
+	for _, v := range c.ComponentDockerimage.Volumes {
+		volumes = append(volumes, convertV1VolumeToCommon(v))
+	}
+
+	container := common.Container{
+		Name:         *c.Alias,
+		Endpoints:    endpoints,
+		Env:          envs,
+		Image:        *c.ComponentDockerimage.Image,
+		MemoryLimit:  *c.ComponentDockerimage.MemoryLimit,
+		MountSources: c.MountSources,
+		VolumeMounts: volumes,
+		// SourceMapping: Not present in V1
+	}
+
+	d = common.DevfileComponent{Container: &container}
+
+	return d
+}
+
+func convertV1EndpointsToCommon(e DockerimageEndpoint) *common.Endpoint {
+	return &common.Endpoint{
+		// Attributes:
+		// Configuration:
+		Name:       *e.Name,
+		TargetPort: *e.Port,
+	}
+}
+
+func convertV1EnvToCommon(e DockerimageEnv) *common.Env {
+	return &common.Env{
+		Name:  *e.Name,
+		Value: *e.Value,
+	}
+}
+
+func convertV1VolumeToCommon(v DockerimageVolume) *common.VolumeMount {
+	return &common.VolumeMount{
+		Name: *v.Name,
+		Path: *v.ContainerPath,
+	}
+}
+
+func convertV1ProjectToCommon(p Project) common.DevfileProject {
+
+	git := common.Git{
+		Branch:            *p.Source.Branch,
+		Location:          p.Source.Location,
+		SparseCheckoutDir: *p.Source.SparseCheckoutDir,
+		StartPoint:        *p.Source.StartPoint,
+	}
+
+	return common.DevfileProject{
+		ClonePath: p.ClonePath,
+		Name:      p.Name,
+		Git:       &git,
+	}
+
+}
+
+func getGroup(name string) *common.Group {
+	var kind common.DevfileCommandGroupType
+
+	switch name {
+	case "devRun":
+		kind = common.RunCommandGroupType
+	case "devBuild":
+		kind = common.BuildCommandGroupType
+	case "devInit":
+		kind = common.InitCommandGroupType
+	default:
+		kind = ""
+	}
+
+	return &common.Group{
+		// TODO(adi): IsDefault:
+		Kind: kind,
+	}
 }
