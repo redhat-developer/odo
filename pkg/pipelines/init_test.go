b004fd0cb1e5bfa6cc0a835762c5fa59e2b0cc14
William Tam <wtam@redhat.com>
2020-05-15 02:54:11 -0400

    Add back TestValidateImageRepo() (#93)

--- pkg/pipelines/init_test.go
+++ pkg/pipelines/init_test.go
@@ -3,6 +3,7 @@ package pipelines
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -145,4 +146,178 @@ func TestMerge(t *testing.T) {
 		t.Fatalf("original map changed %s\n", diff)
 	}
 
+}
+
+func TestValidateImageRepo(t *testing.T) {
+
+	errorMsg := "failed to parse image repo:%s, expected image repository in the form <registry>/<username>/<repository> or <project>/<app> for internal registry"
+
+	tests := []struct {
+		description                string
+		options                    InitParameters
+		expectedError              string
+		expectedIsInternalRegistry bool
+		expectedImageRepo          string
+	}{
+		{
+			"Valid image regsitry URL",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "quay.io/sample-user/sample-repo",
+			},
+			"",
+			false,
+			"quay.io/sample-user/sample-repo",
+		},
+		{
+			"Valid image regsitry URL random registry",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "random.io/sample-user/sample-repo",
+			},
+			"",
+			false,
+			"random.io/sample-user/sample-repo",
+		},
+		{
+			"Valid image regsitry URL docker.io",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "docker.io/sample-user/sample-repo",
+			},
+			"",
+			false,
+			"docker.io/sample-user/sample-repo",
+		},
+		{
+			"Invalid image registry URL with missing repo name",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "quay.io/sample-user",
+			},
+			fmt.Sprintf(errorMsg, "quay.io/sample-user"),
+			false,
+			"",
+		},
+		{
+			"Invalid image registry URL with missing repo name docker.io",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "docker.io/sample-user",
+			},
+			fmt.Sprintf(errorMsg, "docker.io/sample-user"),
+			false,
+			"",
+		},
+		{
+			"Invalid image registry URL with whitespaces",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "quay.io/sample-user/ ",
+			},
+			fmt.Sprintf(errorMsg, "quay.io/sample-user/ "),
+			false,
+			"",
+		},
+		{
+			"Invalid image registry URL with whitespaces in between",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "quay.io/sam\tple-user/",
+			},
+			fmt.Sprintf(errorMsg, "quay.io/sam\tple-user/"),
+			false,
+			"",
+		},
+		{
+			"Invalid image registry URL with leading whitespaces",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "quay.io/ sample-user/",
+			},
+			fmt.Sprintf(errorMsg, "quay.io/ sample-user/"),
+			false,
+			"",
+		},
+		{
+			"Valid internal registry URL",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "image-registry.openshift-image-registry.svc:5000/project/app",
+			},
+			"",
+			true,
+			"image-registry.openshift-image-registry.svc:5000/project/app",
+		},
+		{
+			"Invalid internal registry URL implicit starts with '/'",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "/project/app",
+			},
+			fmt.Sprintf(errorMsg, "/project/app"),
+			false,
+			"",
+		},
+		{
+			"Valid internal registry URL implicit",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "project/app",
+			},
+			"",
+			true,
+			"image-registry.openshift-image-registry.svc:5000/project/app",
+		},
+		{
+			"Invalid too many URL components docker",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "docker.io/foo/project/app",
+			},
+			fmt.Sprintf(errorMsg, "docker.io/foo/project/app"),
+			false,
+			"",
+		},
+		{
+			"Invalid too many URL components internal",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "image-registry.openshift-image-registry.svc:5000/project/app/foo",
+			},
+			fmt.Sprintf(errorMsg, "image-registry.openshift-image-registry.svc:5000/project/app/foo"),
+			false,
+			"",
+		},
+		{
+			"Invalid not enough URL components, no slash",
+			InitParameters{
+				InternalRegistryHostname: "image-registry.openshift-image-registry.svc:5000",
+				ImageRepo:                "docker.io",
+			},
+			fmt.Sprintf(errorMsg, "docker.io"),
+			false,
+			"",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			isInternalRegistry, imageRepo, error := validateImageRepo(test.options.ImageRepo,
+				test.options.InternalRegistryHostname,
+			)
+			if diff := cmp.Diff(isInternalRegistry, test.expectedIsInternalRegistry); diff != "" {
+				t.Errorf("validateImageRepo() failed:\n%s", diff)
+			}
+			if diff := cmp.Diff(imageRepo, test.expectedImageRepo); diff != "" {
+				t.Errorf("validateImageRepo() failed:\n%s", diff)
+			}
+			errorString := ""
+			if error != nil {
+				errorString = error.Error()
+			}
+			if diff := cmp.Diff(errorString, test.expectedError); diff != "" {
+				t.Errorf("validateImageRepo() failed:\n%s", diff)
+			}
+		})
+	}
 }
