cd8d466b4597cc112317e6074308f4e3260f86ea
Aditi Sharma <adsharma@redhat.com>
2020-05-19 18:22:31 +0530

    Avoid String Pointers (#3209)
    While converting v1 to v2 types, string pointers are prone to cause
    null pointer error. This PR updates struct fields from string
    pointers to string

--- pkg/sync/adapter.go
+++ pkg/sync/adapter.go
@@ -197,14 +197,14 @@ func getSyncFolder(projects []versionsCommon.DevfileProject) (string, error) {
 		project := projects[0]
 		// If the clonepath is set to a value, set it to be the sync folder
 		// As some devfiles rely on the code being synced to the folder in the clonepath
-		if project.ClonePath != nil {
-			if strings.HasPrefix(*project.ClonePath, "/") {
+		if project.ClonePath != "" {
+			if strings.HasPrefix(project.ClonePath, "/") {
 				return "", fmt.Errorf("the clonePath in the devfile must be a relative path")
 			}
-			if strings.Contains(*project.ClonePath, "..") {
+			if strings.Contains(project.ClonePath, "..") {
 				return "", fmt.Errorf("the clonePath in the devfile cannot escape the projects root. Don't use .. to try and do that")
 			}
-			return filepath.ToSlash(filepath.Join(kclient.OdoSourceVolumeMount, *project.ClonePath)), nil
+			return filepath.ToSlash(filepath.Join(kclient.OdoSourceVolumeMount, project.ClonePath)), nil
 		}
 		return filepath.ToSlash(filepath.Join(kclient.OdoSourceVolumeMount, projects[0].Name)), nil
 	}
