b4d1cf95d93b5e29ae77b2cd60a75b2cbd1409bd
Tomas Kral <tkral@redhat.com>
2020-05-19 16:29:54 +0200

    TEST

--- scripts/openshiftci-presubmit-integration-tests.sh
+++ scripts/openshiftci-presubmit-integration-tests.sh
@@ -14,11 +14,16 @@ export PATH="$PATH:$(pwd):$GOPATH/bin"
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 
+
+echo $KUBECONFIG
+oc whoami
+oc config view
+
 # Integration tests
-make test-integration
+# make test-integration
 make test-integration-devfile
-make test-cmd-login-logout
-make test-cmd-project
-make test-operator-hub
+# make test-cmd-login-logout
+# make test-cmd-project
+# make test-operator-hub
 
 odo logout
