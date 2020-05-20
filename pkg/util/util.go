47c03d5f21ad2c62845dfab85dca57d28010ba55
Enrique Lacal Bereslawski <Enrique.Lacal.Bereslawski@ibm.com>
2020-05-20 13:43:38 +0100

    John's Requested Branches

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -46,11 +46,13 @@ const maxAllowedNamespacedStringLength = 63 - len("-s2idata") - 1
 // note for mocking purpose ONLY
 var customHomeDir = os.Getenv("CUSTOM_HOMEDIR")
 
-const defaultGithubRef = "master"
-const githubBranchKey = "Branch"
-const githubTagKey = "Tag"
-const githubCommitIDKey = "CommitID"
-const githubStartPointKey = "StartPoint"
+const (
+	defaultGithubRef    = "master"
+	githubBranchKey     = "Branch"
+	githubTagKey        = "Tag"
+	githubCommitIDKey   = "CommitID"
+	githubStartPointKey = "StartPoint"
+)
 
 // ResourceRequirementInfo holds resource quantity before transformation into its appropriate form in container spec
 type ResourceRequirementInfo struct {
