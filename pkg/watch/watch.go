0dde0fd96a8af3a4f0d65616280a516f9e11942f
Maysun J Faisal <31771087+maysunfaisal@users.noreply.github.com>
2020-05-27 13:49:08 -0400

    Add devfile command flags to watch (#3075)
    * save non default devfile cmd in env.yaml for push and watch
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Add and update tests for envinfo
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Update watch test to incl new watch param
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * watch integration test
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Display msg when devfile commands are changed via odo push
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Change Logic for watch flag + feedback 1
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Update Watch tests with feedback
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * disable watch test on kube since we now use oc
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>
    
    * Watch test feedback 2
    
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>

--- pkg/watch/watch.go
+++ pkg/watch/watch.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/openshift/odo/pkg/devfile/adapters/common"
+	"github.com/openshift/odo/pkg/envinfo"
 	"github.com/openshift/odo/pkg/util"
 
 	"github.com/openshift/odo/pkg/occlient"
@@ -30,7 +31,7 @@ type WatchParameters struct {
 	FileIgnores []string
 	// Custom function that can be used to push detected changes to remote pod. For more info about what each of the parameters to this function, please refer, pkg/component/component.go#PushLocal
 	WatchHandler func(*occlient.Client, string, string, string, io.Writer, []string, []string, bool, []string, bool) error
-	// Custom function that can be used to push detected changes to remote devfile pod. For more info about what each of the parameters to this function, please refer, pkg/component/component.go#PushLocal
+	// Custom function that can be used to push detected changes to remote devfile pod. For more info about what each of the parameters to this function, please refer, pkg/devfile/adapters/interface.go#PlatformAdapter
 	DevfileWatchHandler func(common.PushParameters) error
 	// This is a channel added to signal readiness of the watch command to the external channel listeners
 	StartChan chan bool
@@ -40,6 +41,14 @@ type WatchParameters struct {
 	PushDiffDelay int
 	// Parameter whether or not to show build logs
 	Show bool
+	// EnvSpecificInfo contains infomation of env.yaml file
+	EnvSpecificInfo *envinfo.EnvSpecificInfo
+	// DevfileInitCmd takes the init command through the command line and overwrites devfile init command
+	DevfileInitCmd string
+	// DevfileBuildCmd takes the build command through the command line and overwrites devfile build command
+	DevfileBuildCmd string
+	// DevfileRunCmd takes the run command through the command line and overwrites devfile run command
+	DevfileRunCmd string
 }
 
 // addRecursiveWatch handles adding watches recursively for the path provided
@@ -309,6 +318,10 @@ func WatchAndPush(client *occlient.Client, out io.Writer, parameters WatchParame
 							WatchDeletedFiles: deletedPaths,
 							IgnoredFiles:      parameters.FileIgnores,
 							ForceBuild:        false,
+							DevfileInitCmd:    parameters.DevfileInitCmd,
+							DevfileBuildCmd:   parameters.DevfileBuildCmd,
+							DevfileRunCmd:     parameters.DevfileRunCmd,
+							EnvSpecificInfo:   *parameters.EnvSpecificInfo,
 						}
 
 						err = parameters.DevfileWatchHandler(pushParams)
@@ -326,6 +339,10 @@ func WatchAndPush(client *occlient.Client, out io.Writer, parameters WatchParame
 							WatchDeletedFiles: deletedPaths,
 							IgnoredFiles:      parameters.FileIgnores,
 							ForceBuild:        false,
+							DevfileInitCmd:    parameters.DevfileInitCmd,
+							DevfileBuildCmd:   parameters.DevfileBuildCmd,
+							DevfileRunCmd:     parameters.DevfileRunCmd,
+							EnvSpecificInfo:   *parameters.EnvSpecificInfo,
 						}
 
 						err = parameters.DevfileWatchHandler(pushParams)
--- tests/integration/devfile/cmd_devfile_watch_test.go
+++ tests/integration/devfile/cmd_devfile_watch_test.go
@@ -9,12 +9,16 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/openshift/odo/tests/helper"
+	"github.com/openshift/odo/tests/integration/devfile/utils"
 )
 
 var _ = Describe("odo devfile watch command tests", func() {
 	var namespace string
 	var context string
+	var cmpName string
 	var currentWorkingDirectory string
+
+	var cliRunner helper.CliRunner
 
 	// Setup up state for each test spec
 	// create new project (not set as active) and new context directory for each test spec
@@ -27,11 +31,17 @@ var _ = Describe("odo devfile watch command tests", func() {
 			homeDir := helper.GetUserHomeDir()
 			kubeConfigFile := helper.CopyKubeConfigFile(filepath.Join(homeDir, ".kube", "config"), filepath.Join(context, "config"))
 			namespace = helper.CreateRandNamespace(kubeConfigFile)
+			cliRunner = helper.NewKubectlRunner("kubectl")
 		} else {
 			namespace = helper.CreateRandProject()
+			cliRunner = helper.NewOcRunner("oc")
 		}
 		currentWorkingDirectory = helper.Getwd()
+		cmpName = helper.RandString(6)
 		helper.Chdir(context)
+
+		// Set experimental mode to true
+		helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 	})
 
 	// Clean up after the test
@@ -50,9 +60,6 @@ var _ = Describe("odo devfile watch command tests", func() {
 
 	Context("when running help for watch command", func() {
 		It("should display the help", func() {
-			// Devfile push requires experimental mode to be set
-			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
-
 			appHelp := helper.CmdShouldPass("odo", "watch", "-h")
 			Expect(appHelp).To(ContainSubstring("Watch for changes"))
 		})
@@ -60,8 +67,6 @@ var _ = Describe("odo devfile watch command tests", func() {
 
 	Context("when executing watch without pushing a devfile component", func() {
 		It("should fail", func() {
-			// Devfile push requires experimental mode to be set
-			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			cmpName := helper.RandString(6)
 			helper.Chdir(currentWorkingDirectory)
 			helper.CmdShouldPass("odo", "create", "nodejs", "--project", namespace, "--context", context, cmpName)
@@ -72,8 +77,6 @@ var _ = Describe("odo devfile watch command tests", func() {
 
 	Context("when executing watch without a valid devfile", func() {
 		It("should fail", func() {
-			// Devfile push requires experimental mode to be set
-			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "true")
 			output := helper.CmdShouldFail("odo", "watch", "--devfile", "fake-devfile.yaml")
 			Expect(output).To(ContainSubstring("The current directory does not represent an odo component"))
 		})
@@ -81,9 +84,51 @@ var _ = Describe("odo devfile watch command tests", func() {
 
 	Context("when executing odo watch with devfile flag without experimental mode", func() {
 		It("should fail", func() {
-			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs"), context)
+			helper.CmdShouldPass("odo", "preference", "set", "Experimental", "false", "-f")
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), context)
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", "devfile.yaml"), filepath.Join(context, "devfile.yaml"))
 			output := helper.CmdShouldFail("odo", "watch", "--devfile", filepath.Join(context, "devfile.yaml"))
 			Expect(output).To(ContainSubstring("Error: unknown flag: --devfile"))
+		})
+	})
+
+	Context("when executing odo watch after odo push", func() {
+		It("should listen for file changes", func() {
+			helper.CmdShouldPass("odo", "create", "nodejs", "--project", namespace, cmpName)
+
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), context)
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", "devfile.yaml"), filepath.Join(context, "devfile.yaml"))
+
+			output := helper.CmdShouldPass("odo", "push", "--devfile", "devfile.yaml", "--project", namespace)
+			Expect(output).To(ContainSubstring("Changes successfully pushed to component"))
+
+			watchFlag := ""
+			odoV2Watch := utils.OdoV2Watch{
+				CmpName:            cmpName,
+				StringsToBeMatched: []string{"Executing devbuild command", "Executing devrun command"},
+			}
+			// odo watch and validate
+			utils.OdoWatch(utils.OdoV1Watch{}, odoV2Watch, namespace, context, watchFlag, cliRunner, "kube")
+		})
+	})
+
+	Context("when executing odo watch after odo push with custom commands", func() {
+		It("should listen for file changes", func() {
+			helper.CmdShouldPass("odo", "create", "nodejs", "--project", namespace, cmpName)
+
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), context)
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", "devfile.yaml"), filepath.Join(context, "devfile.yaml"))
+
+			output := helper.CmdShouldPass("odo", "push", "--build-command", "build", "--run-command", "run", "--devfile", "devfile.yaml", "--project", namespace)
+			Expect(output).To(ContainSubstring("Changes successfully pushed to component"))
+
+			watchFlag := "--build-command build --run-command run"
+			odoV2Watch := utils.OdoV2Watch{
+				CmpName:            cmpName,
+				StringsToBeMatched: []string{"Executing build command", "Executing run command"},
+			}
+			// odo watch and validate
+			utils.OdoWatch(utils.OdoV1Watch{}, odoV2Watch, namespace, context, watchFlag, cliRunner, "kube")
 		})
 	})
 })
