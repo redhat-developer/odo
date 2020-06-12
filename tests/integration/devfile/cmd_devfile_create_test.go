aacab1798ad0381453f24aae26dd0dea4583b338
jingfu wang <jingfu.j.wang@ibm.com>
2020-06-12 15:55:39 -0400

    test: improve registry tests
    Signed-off-by: jingfu wang <jingfu.j.wang@ibm.com>

--- tests/integration/devfile/cmd_devfile_create_test.go
+++ tests/integration/devfile/cmd_devfile_create_test.go
@@ -128,7 +128,8 @@ var _ = Describe("odo devfile create command tests", func() {
 
 		It("should fail to create the devfile component if specified registry is invalid", func() {
 			componentRegistry := "fake"
-			helper.CmdShouldFail("odo", "create", "java-openliberty", "--registry", componentRegistry)
+			output := helper.CmdShouldFail("odo", "create", "java-openliberty", "--registry", componentRegistry)
+			helper.MatchAllInOutput(output, []string{"Registry fake doesn't exist, please specify a valid registry via --registry"})
 		})
 	})
 
