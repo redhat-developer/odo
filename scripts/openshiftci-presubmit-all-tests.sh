447e1cd86bdb15506d836d26fbb125787425b9fc
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-06-22 19:06:43 +0530

    [skip ci] Updating prow scripts to skip ci if commits contain keyword
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/openshiftci-presubmit-all-tests.sh
+++ scripts/openshiftci-presubmit-all-tests.sh
@@ -5,7 +5,17 @@ set -e
 # show commands
 set -x
 
+export GIT_LOG_FILE="/tmp/git-log"
 export CI="openshift"
+
+git log master.. > $GIT_LOG_FILE
+cat $GIT_LOG_FILE | grep "skip ci"
+
+if [[ $? -ne 0 ]]; then
+        echo "Keyword 'skip ci' detected Skipping CI run"
+	exit 0
+fi
+
 make configure-installer-tests-cluster
 make bin
 mkdir -p $GOPATH/bin
