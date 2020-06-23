807479090b9ec5be3a3e485368afced9d8549106
Maysun J Faisal <maysun.j.faisal@ibm.com>
2020-06-23 14:07:42 -0400

    Update Registry integration tests for v2
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>

--- tests/integration/devfile/cmd_devfile_registry_test.go
+++ tests/integration/devfile/cmd_devfile_registry_test.go
@@ -13,7 +13,7 @@ import (
 var _ = Describe("odo devfile registry command tests", func() {
 	var project, context, currentWorkingDirectory, originalKubeconfig string
 	const registryName string = "RegistryName"
-	const addRegistryURL string = "https://raw.githubusercontent.com/GeekArthur/registry/master"
+	const addRegistryURL string = "https://raw.githubusercontent.com/odo-devfiles/registry/master"
 	const updateRegistryURL string = "http://www.example.com/update"
 
 	// Using program commmand according to cliRunner in devfile
@@ -50,7 +50,6 @@ var _ = Describe("odo devfile registry command tests", func() {
 
 		It("Should fail with an error with no registries", func() {
 			helper.CmdShouldPass("odo", "registry", "delete", "DefaultDevfileRegistry", "-f")
-			helper.CmdShouldPass("odo", "registry", "delete", "CheDevfileRegistry", "-f")
 			output := helper.CmdShouldFail("odo", "registry", "list")
 			helper.MatchAllInOutput(output, []string{"No devfile registries added to the configuration. Refer `odo registry add -h` to add one"})
 
