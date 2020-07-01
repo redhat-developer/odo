c03c1f3aa1379b1ee663fb2cae3b5667de059b98
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-07-01 11:27:20 +0530

    Building redistributable only on x86_64
    We dont need redistributable package building
    on multiple architectures as it is happening right
    now (case in point internal scratch build)
    
    Eg:
      - openshift-odo-redistributable-1.2.3-1.el8.s390x.rpm
      - openshift-odo-redistributable-1.2.3-1.el8.ppc64le.rpm
      - openshift-odo-redistributable-1.2.3-1.el8.x86_64.rpm
    
    We only need last one as individual rpms for specific architectures
    is fine but not the redistributable ones.
    
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/rpm-local-build.sh
+++ scripts/rpm-local-build.sh
@@ -6,6 +6,10 @@ if [[ ! -d dist/rpmbuild  ]]; then
 	exit 1
 fi
 
+echo "Cleaning up old rpmcontent"
+rm -f ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
 echo "Copying content to local rpmbuild"
 cp -avrf dist/rpmbuild/SOURCES/* $HOME/rpmbuild/SOURCES/
 cp -avrf dist/rpmbuild/SPECS/* $HOME/rpmbuild/SPECS/
