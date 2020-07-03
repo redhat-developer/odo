95cb37546dada17b44b5b8a85f9f5ffd7d99acc8
Priti Kumari <pkumari@redhat.com>
2020-07-03 11:54:11 +0530

    Updating copy kubeconfig to temp dir on the basis of review comment

--- scripts/configure-installer-tests-cluster.sh
+++ scripts/configure-installer-tests-cluster.sh
@@ -13,18 +13,13 @@ KUBEADMIN_PASSWORD_FILE=${KUBEADMIN_PASSWORD_FILE:-"${DEFAULT_INSTALLER_ASSETS_D
 # Default values
 OC_STABLE_LOGIN="false"
 CI_OPERATOR_HUB_PROJECT="ci-operator-hub-project"
-# Copy kubeconfig to temporary kubeconfig file
-# Read and Write permission to temporary kubeconfig file
-mkdir -p ${DEFAULT_INSTALLER_ASSETS_DIR}/tmp
-cp ${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeconfig ${DEFAULT_INSTALLER_ASSETS_DIR}/tmp/kubeconfig
-chmod 644 ${DEFAULT_INSTALLER_ASSETS_DIR}/tmp/kubeconfig
 # Exported to current env
-export KUBECONFIG=${KUBECONFIG:-"${DEFAULT_INSTALLER_ASSETS_DIR}/tmp/kubeconfig"}
+export KUBECONFIG=${KUBECONFIG:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeconfig"}
 
 # List of users to create
 USERS="developer odonoprojectattemptscreate odosingleprojectattemptscreate odologinnoproject odologinsingleproject1"
 
-# Attempt resolution of kubeadmin, only if a CI is set
+# Attempt resolution of kubeadmin, only if a CI is not set
 if [ -z $CI ]; then
     # Check if nessasary files exist
     if [ ! -f $KUBEADMIN_PASSWORD_FILE ]; then
@@ -42,6 +37,13 @@ if [ -z $CI ]; then
 
     # Login as admin user
     oc login -u $KUBEADMIN_USER -p $KUBEADMIN_PASSWORD
+else
+    # Copy kubeconfig to temporary kubeconfig file
+    # Read and Write permission to temporary kubeconfig file
+    TMP_DIR=$(mktemp -d)
+    cp $KUBECONFIG $TMP_DIR/kubeconfig
+    chmod 640 $TMP_DIR/kubeconfig
+    export KUBECONFIG=$TMP_DIR/kubeconfig
 fi
 
 # Setup the cluster for Operator tests
