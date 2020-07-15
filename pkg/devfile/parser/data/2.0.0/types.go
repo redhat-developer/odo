fa3dc1ad35932a934ef880be8535b8f5b888b4e6
Aditi Sharma <adsharma@redhat.com>
2020-07-15 19:42:06 +0530

    Preserve yaml order
    update 2.0 go struct to preserve yaml order

--- pkg/devfile/parser/data/2.0.0/types.go
+++ pkg/devfile/parser/data/2.0.0/types.go
@@ -15,15 +15,8 @@ const (
 
 // Devfile200 Devfile schema.
 type Devfile200 struct {
-
-	// Predefined, ready-to-use, workspace-related commands
-	Commands []common.DevfileCommand `json:"commands,omitempty" yaml:"commands,omitempty"`
-
-	// List of the workspace components, such as editor and plugins, user-provided containers, or other types of components
-	Components []common.DevfileComponent `json:"components,omitempty" yaml:"components,omitempty"`
-
-	// Bindings of commands to events. Each command is referred-to by its name.
-	Events common.DevfileEvents `json:"events,omitempty" yaml:"events,omitempty"`
+	// Devfile schema version
+	SchemaVersion string `json:"schemaVersion" yaml:"schemaVersion"`
 
 	// Optional metadata
 	Metadata common.DevfileMetadata `json:"metadata,omitempty" yaml:"metadata,omitempty"`
@@ -34,11 +27,17 @@ type Devfile200 struct {
 	// Projects worked on in the workspace, containing names and sources locations
 	Projects []common.DevfileProject `json:"projects,omitempty" yaml:"projects,omitempty"`
 
-	// Devfile schema version
-	SchemaVersion string `json:"schemaVersion" yaml:"schemaVersion"`
-
 	// StarterProjects is a project that can be used as a starting point when bootstrapping new projects
-	StarterProjects []StarterProject `json:"starterProjects, yaml:"schemaVersion", "omitempty"`
+	StarterProjects []StarterProject `json:"starterProjects,omitempty" yaml:"starterProjects,omitempty"`
+
+	// List of the workspace components, such as editor and plugins, user-provided containers, or other types of components
+	Components []common.DevfileComponent `json:"components,omitempty" yaml:"components,omitempty"`
+
+	// Predefined, ready-to-use, workspace-related commands
+	Commands []common.DevfileCommand `json:"commands,omitempty" yaml:"commands,omitempty"`
+
+	// Bindings of commands to events. Each command is referred-to by its name.
+	Events common.DevfileEvents `json:"events,omitempty" yaml:"events,omitempty"`
 }
 
 // Command
