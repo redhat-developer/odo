b72a812ad722392cf82c1576c3ee93dce28657fe
John Collier <John.J.Collier@ibm.com>
2020-07-16 12:18:13 -0400

    Add unit tests for command help funcs
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

---
+++ pkg/devfile/parser/data/common/command_helper_test.go
@@ -0,0 +1,144 @@
+package common
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetID(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		command DevfileCommand
+		want    string
+	}{
+		{
+			name: "Case 1: Exec command ID",
+			command: DevfileCommand{
+				Exec: &Exec{
+					Id: "exec1",
+				},
+			},
+			want: "exec1",
+		},
+		{
+			name: "Case 2: Composite command ID",
+			command: DevfileCommand{
+				Composite: &Composite{
+					Id: "composite1",
+				},
+			},
+			want: "composite1",
+		},
+		{
+			name:    "Case 3: Empty command",
+			command: DevfileCommand{},
+			want:    "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commandID := tt.command.GetID()
+			if commandID != tt.want {
+				t.Errorf("expected %v, actual %v", tt.want, commandID)
+			}
+		})
+	}
+
+}
+
+func TestGetKind(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		command DevfileCommand
+		want    DevfileCommandGroupType
+	}{
+		{
+			name: "Case 1: Exec command kind",
+			command: DevfileCommand{
+				Exec: &Exec{
+					Id: "exec1",
+					Group: &Group{
+						IsDefault: true,
+						Kind:      RunCommandGroupType,
+					},
+				},
+			},
+			want: RunCommandGroupType,
+		},
+		{
+			name: "Case 2: Composite command kind",
+			command: DevfileCommand{
+				Composite: &Composite{
+					Id: "composite1",
+					Group: &Group{
+						IsDefault: true,
+						Kind:      BuildCommandGroupType,
+					},
+				},
+			},
+			want: BuildCommandGroupType,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commandKind := tt.command.GetKind()
+			if commandKind != tt.want {
+				t.Errorf("expected %v, actual %v", tt.want, commandKind)
+			}
+		})
+	}
+
+}
+
+func TestGetGroup(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		command DevfileCommand
+		want    Group
+	}{
+		{
+			name: "Case 1: Exec command group",
+			command: DevfileCommand{
+				Exec: &Exec{
+					Id: "exec1",
+					Group: &Group{
+						IsDefault: true,
+						Kind:      RunCommandGroupType,
+					},
+				},
+			},
+			want: Group{
+				IsDefault: true,
+				Kind:      RunCommandGroupType,
+			},
+		},
+		{
+			name: "Case 2: Composite command group",
+			command: DevfileCommand{
+				Composite: &Composite{
+					Id: "composite1",
+					Group: &Group{
+						IsDefault: true,
+						Kind:      BuildCommandGroupType,
+					},
+				},
+			},
+			want: Group{
+				IsDefault: true,
+				Kind:      BuildCommandGroupType,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commandGroup := tt.command.GetGroup()
+			if !reflect.DeepEqual(*commandGroup, tt.want) {
+				t.Errorf("expected %v, actual %v", tt.want, commandGroup)
+			}
+		})
+	}
+
+}
