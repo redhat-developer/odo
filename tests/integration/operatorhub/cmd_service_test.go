3eb640fe7976df194feaa6b2a2cb74a73f117052
Dharmit Shah <shahdharmit@gmail.com>
2020-07-18 11:20:40 +0530

    Move 'odo link' tests under operator hub tests

--- tests/integration/operatorhub/cmd_service_test.go
+++ tests/integration/operatorhub/cmd_service_test.go
@@ -299,4 +299,83 @@ spec:
 			helper.MatchAllInOutput(jsonOut, []string{msg, msgWithQuote})
 		})
 	})
+
+	Context("When linking devfile component with Operator backed service", func() {
+		var context, currentWorkingDirectory, devfilePath string
+		const devfile = "devfile.yaml"
+
+		JustBeforeEach(func() {
+			preSetup()
+			context = helper.CreateNewContext()
+			devfilePath = filepath.Join(context, devfile)
+			currentWorkingDirectory = helper.Getwd()
+			helper.Chdir(context)
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", devfile), devfilePath)
+		})
+
+		JustAfterEach(func() {
+			cleanPreSetup()
+			helper.Chdir(currentWorkingDirectory)
+			helper.DeleteDir(context)
+		})
+
+		It("should fail if service name doesn't adhere to <service-type>/<service-name> format", func() {
+			if os.Getenv("KUBERNETES") == "true" {
+				Skip("This is a OpenShift specific scenario, skipping")
+			}
+
+			componentName := helper.RandString(6)
+			helper.CmdShouldPass("odo", "create", componentName)
+
+			stdOut := helper.CmdShouldFail("odo", "link", "EtcdCluster")
+			Expect(stdOut).To(ContainSubstring("Invalid service name"))
+
+			stdOut = helper.CmdShouldFail("odo", "link", "EtcdCluster/")
+			Expect(stdOut).To(ContainSubstring("Invalid service name"))
+
+			stdOut = helper.CmdShouldFail("odo", "link", "/example")
+			Expect(stdOut).To(ContainSubstring("Invalid service name"))
+		})
+
+		It("should fail if the provided service doesn't exist in the namespace", func() {
+			if os.Getenv("KUBERNETES") == "true" {
+				Skip("This is a OpenShift specific scenario, skipping")
+			}
+
+			componentName := helper.RandString(6)
+			helper.CmdShouldPass("odo", "create", componentName)
+			helper.CmdShouldPass("odo", "push")
+
+			stdOut := helper.CmdShouldFail("odo", "link", "EtcdCluster/example")
+			Expect(stdOut).To(ContainSubstring("Couldn't find the requested service"))
+		})
+
+		It("should successfully connect a component with an existing service", func() {
+			if os.Getenv("KUBERNETES") == "true" {
+				Skip("This is a OpenShift specific scenario, skipping")
+			}
+
+			componentName := helper.RandString(6)
+			helper.CmdShouldPass("odo", "create", componentName)
+			helper.CmdShouldPass("odo", "push")
+
+			// start the Operator backed service first
+			operators := helper.CmdShouldPass("odo", "catalog", "list", "services")
+			etcdOperator := regexp.MustCompile(`etcdoperator\.*[a-z][0-9]\.[0-9]\.[0-9]-clusterwide`).FindString(operators)
+			helper.CmdShouldPass("odo", "service", "create", etcdOperator, "--crd", "EtcdCluster")
+
+			// now verify if the pods for the operator have started
+			pods := helper.CmdShouldPass("oc", "get", "pods", "-n", project)
+			// Look for pod with example name because that's the name etcd will give to the pods.
+			etcdPod := regexp.MustCompile(`example-.[a-z0-9]*`).FindString(pods)
+
+			ocArgs := []string{"get", "pods", etcdPod, "-o", "template=\"{{.status.phase}}\"", "-n", project}
+			helper.WaitForCmdOut("oc", ocArgs, 1, true, func(output string) bool {
+				return strings.Contains(output, "Running")
+			})
+
+			stdOut := helper.CmdShouldPass("odo", "link", "EtcdCluster/example")
+			Expect(stdOut).To(ContainSubstring("Successfully created link between component"))
+		})
+	})
 })
