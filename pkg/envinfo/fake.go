13a94adce19ea23e9e71206dfcadb1675e310b9c
mik-dass <mrinald7@gmail.com>
2020-07-21 13:40:39 +0530

    Adds comment for GetFakeEnvInfo

--- pkg/envinfo/fake.go
+++ pkg/envinfo/fake.go
@@ -1,5 +1,6 @@
 package envinfo
 
+// GetFakeEnvInfo gets a fake envInfo using the given componentSettings
 func GetFakeEnvInfo(settings ComponentSettings) *EnvInfo {
 	return &EnvInfo{
 		componentSettings: settings,
