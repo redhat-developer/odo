0c9dc4cbc5d69862ee9eac0ae2d4c69157a8b901
Neeraj Laad <neeraj.laad@gmail.com>
2020-07-21 04:39:10 -0700

    Fix paths in github zip

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -932,10 +932,16 @@ func Unzip(src, dest, pathToUnzip string) ([]string, error) {
 	// change path separator to correct character
 	pathToUnzip = filepath.FromSlash(pathToUnzip)
 
+	// removes first slash of pathToUnzip if present, adds trailing slash
+	pathToUnzip = strings.TrimPrefix(pathToUnzip, string(os.PathSeparator))
+	if pathToUnzip != "" && !strings.HasSuffix(pathToUnzip, string(os.PathSeparator)) {
+		pathToUnzip = pathToUnzip + string(os.PathSeparator)
+	}
+
 	for _, f := range r.File {
 		// Store filename/path for returning and using later on
-		index := strings.Index(f.Name, string(os.PathSeparator))
-		filename := f.Name[index+1:]
+		index := strings.Index(f.Name, "/")
+		filename := filepath.FromSlash(f.Name[index+1:])
 		if filename == "" {
 			continue
 		}
@@ -946,11 +952,6 @@ func Unzip(src, dest, pathToUnzip string) ([]string, error) {
 			return filenames, err
 		}
 
-		// removes first slash of pathToUnzip if present, adds trailing slash
-		pathToUnzip = strings.TrimPrefix(pathToUnzip, string(os.PathSeparator))
-		if pathToUnzip != "" && !strings.HasSuffix(pathToUnzip, string(os.PathSeparator)) {
-			pathToUnzip = pathToUnzip + string(os.PathSeparator)
-		}
 		// destination filepath before trim
 		fpath := filepath.Join(dest, filename)
 
