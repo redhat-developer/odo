8a786bb54fa247bf70b43aded82a8d1f6aff86b0
Jonathan West <jgwest@ca.ibm.com>
2020-07-27 07:35:39 -0400

    Sync operation within the Push command does not remove deleted/renamed files

--- pkg/sync/adapter.go
+++ pkg/sync/adapter.go
@@ -329,7 +329,7 @@ func isRemoteFolderEmpty(a Adapter, compInfo common.ComponentInfo, syncFolder st
 	stdoutWriter, stdoutOutputChan := exec.CreateConsoleOutputWriterAndChannel()
 	stderrWriter, stderrOutputChan := exec.CreateConsoleOutputWriterAndChannel()
 
-	for err := exec.ExecuteCommand(a.Client, compInfo, []string{"ls", "-a", syncFolder}, false, stdoutWriter, stderrWriter); err != nil {
+	if err := exec.ExecuteCommand(a.Client, compInfo, []string{"ls", "-a", syncFolder}, false, stdoutWriter, stderrWriter); err != nil {
 		return false, err
 	}
 
