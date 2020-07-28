0002edae0b5d6f9fa9077af024d9faa5b4e3cb9c
Chris Laprun <metacosm@gmail.com>
2020-07-28 13:57:25 +0200

    chore: re-add some tests

--- pkg/testingutil/devfile.go
+++ pkg/testingutil/devfile.go
@@ -10,6 +10,7 @@ type TestDevfileData struct {
 	Components        []versionsCommon.DevfileComponent
 	ExecCommands      []versionsCommon.Exec
 	CompositeCommands []versionsCommon.Composite
+	Commands          []versionsCommon.DevfileCommand
 	Events            common.DevfileEvents
 }
 
@@ -75,19 +76,21 @@ func (d TestDevfileData) GetProjects() []versionsCommon.DevfileProject {
 
 // GetCommands is a mock function to get the commands from a devfile
 func (d TestDevfileData) GetCommands() []versionsCommon.DevfileCommand {
+	if d.Commands == nil {
+		var commands []versionsCommon.DevfileCommand
 
-	var commands []versionsCommon.DevfileCommand
+		for i := range d.ExecCommands {
+			commands = append(commands, versionsCommon.DevfileCommand{Exec: &d.ExecCommands[i]})
+		}
 
-	for i := range d.ExecCommands {
-		commands = append(commands, versionsCommon.DevfileCommand{Exec: &d.ExecCommands[i]})
+		for i := range d.CompositeCommands {
+			commands = append(commands, versionsCommon.DevfileCommand{Composite: &d.CompositeCommands[i]})
+		}
+
+		return commands
+	} else {
+		return d.Commands
 	}
-
-	for i := range d.CompositeCommands {
-		commands = append(commands, versionsCommon.DevfileCommand{Composite: &d.CompositeCommands[i]})
-	}
-
-	return commands
-
 }
 
 // Validate is a mock validation that always validates without error
