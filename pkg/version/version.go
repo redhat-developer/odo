8b213dc09d87527e483fb3e9afb18112ff54bcd3
Charlie Drage <charlie@charliedrage.com>
2020-08-04 14:35:52 -0400

    Update version to 2.0.0-alpha

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -12,7 +12,7 @@ Changing these values will change the versioning information when releasing odo.
 
 var (
 	// VERSION  is version number that will be displayed when running ./odo version
-	VERSION = "v1.2.5"
+	VERSION = "v2.0.0-alpha"
 
 	// GITCOMMIT is hash of the commit that will be displayed when running ./odo version
 	// this will be overwritten when running  build like this: go build -ldflags="-X github.com/openshift/odo/cmd.GITCOMMIT=$(GITCOMMIT)"
