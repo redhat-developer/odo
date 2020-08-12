c76e0fe3cfb3fc9d3fcb02902994f6defa011c49
Aditi Sharma <adsharma@redhat.com>
2020-08-12 16:53:53 +0530

    Simplify implementation

--- pkg/devfile/adapters/common/generic.go
+++ pkg/devfile/adapters/common/generic.go
@@ -149,18 +149,8 @@ func (a GenericAdapter) ExecDevfile(commandsMap PushCommandsMap, componentExists
 
 		// if we need to restart, issue supervisor command to stop all running commands first
 		if componentExists {
-			if restart {
-				klog.V(4).Infof("restart:true, restarting %s", defaultCmd)
-				if cmd, err := newSupervisorStopCommand(command, a); cmd != nil {
-					if err != nil {
-						return err
-					}
-					commands = append(commands, cmd)
-				}
-			} else if !restart && previousMode != currentMode {
-				// If we are switching from odo push to odo push --debug, even if restart:false
-				// We need to stop the previous command
-				klog.V(4).Infof("restart:false, switching modes stopping previous %s command", previousMode)
+			if restart || (previousMode != currentMode) {
+				klog.V(4).Infof("supervisord stop command %s to restart or start other command", previousMode)
 				if cmd, err := newSupervisorStopCommand(command, a); cmd != nil {
 					if err != nil {
 						return err
@@ -170,29 +160,6 @@ func (a GenericAdapter) ExecDevfile(commandsMap PushCommandsMap, componentExists
 			} else {
 				klog.V(4).Infof("restart:false, not restarting %s", defaultCmd)
 			}
-
-		}
-
-		if componentExists && restart {
-			klog.V(4).Infof("restart:true, restarting %s", defaultCmd)
-			if cmd, err := newSupervisorStopCommand(command, a); cmd != nil {
-				if err != nil {
-					return err
-				}
-				commands = append(commands, cmd)
-			}
-		} else if componentExists && previousMode != currentMode && !restart {
-			// If we are switching from odo push to odo push --debug, even if restart:false
-			// We need to stop the previous command
-			klog.V(4).Infof("restart:false, switching modes stopping previous %s command", previousMode)
-			if cmd, err := newSupervisorStopCommand(command, a); cmd != nil {
-				if err != nil {
-					return err
-				}
-				commands = append(commands, cmd)
-			}
-		} else {
-			klog.V(4).Infof("restart:false, not restarting %s", defaultCmd)
 		}
 
 		// with restart false, executing only supervisord start command, if the command is already running, supvervisord will not restart it.
