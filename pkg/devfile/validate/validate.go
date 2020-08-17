e2c857de55a0fbb0c6fbf8ace4b211df1b7dcc13
John Collier <John.J.Collier@ibm.com>
2020-08-17 16:51:20 -0400

    Remove leftover v1 devfile check
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- pkg/devfile/validate/validate.go
+++ pkg/devfile/validate/validate.go
@@ -6,7 +6,6 @@ import (
 	"github.com/openshift/odo/pkg/devfile/parser/data/common"
 	"k8s.io/klog"
 
-	v100 "github.com/openshift/odo/pkg/devfile/parser/data/1.0.0"
 	v200 "github.com/openshift/odo/pkg/devfile/parser/data/2.0.0"
 )
 
@@ -15,8 +14,6 @@ func ValidateDevfileData(data interface{}) error {
 	var components []common.DevfileComponent
 
 	switch d := data.(type) {
-	case *v100.Devfile100:
-		components = d.GetComponents()
 	case *v200.Devfile200:
 		components = d.GetComponents()
 
@@ -25,7 +22,7 @@ func ValidateDevfileData(data interface{}) error {
 			return err
 		}
 	default:
-		return fmt.Errorf("unknown devfile type %T", d)
+		return fmt.Errorf("invalid devfile type %T", d)
 	}
 
 	// Validate Components
