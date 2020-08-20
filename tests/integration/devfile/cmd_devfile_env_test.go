c3667019231783fcd1bfe75a8c33a67e3c550fff
Girish Ramnani <girishramnani95@gmail.com>
2020-08-20 12:45:04 +0530

    cleaned up devfile env integration tests

--- tests/integration/devfile/cmd_devfile_env_test.go
+++ tests/integration/devfile/cmd_devfile_env_test.go
@@ -63,8 +63,8 @@ var _ = Describe("odo devfile env command tests", func() {
 		})
 	})
 
-	Context("When executing env set", func() {
-		It("Should successfully set the parameters", func() {
+	Context("When executing env set and unset", func() {
+		It("Should successfully set and unset the parameters", func() {
 			helper.CmdShouldPass("odo", "create", "nodejs")
 			helper.CmdShouldPass("odo", "env", "set", "Name", testName, "-f")
 			helper.CmdShouldPass("odo", "env", "set", "Namespace", testNamepace, "-f")
@@ -81,26 +81,6 @@ var _ = Describe("odo devfile env command tests", func() {
 				testDebugPort,
 			}
 			helper.MatchAllInOutput(output, wantOutput)
-		})
-
-		It("Should fail to set the invalid parameter", func() {
-			helper.CmdShouldPass("odo", "create", "nodejs")
-			helper.CmdShouldFail("odo", "env", "set", fakeParameter, fakeParameter, "-f")
-		})
-	})
-
-	Context("When executing env unset", func() {
-		It("Should successfully unset the parameters after set the parameters", func() {
-			helper.CmdShouldPass("odo", "create", "nodejs")
-			helper.CmdShouldPass("odo", "env", "set", "DebugPort", testDebugPort, "-f")
-			output := helper.CmdShouldPass("odo", "env", "view")
-			wantOutput := []string{
-				"PARAMETER NAME",
-				"PARAMETER VALUE",
-				"DebugPort",
-				testDebugPort,
-			}
-			helper.MatchAllInOutput(output, wantOutput)
 
 			helper.CmdShouldPass("odo", "env", "unset", "DebugPort", "-f")
 			output = helper.CmdShouldPass("odo", "env", "view")
@@ -111,9 +91,11 @@ var _ = Describe("odo devfile env command tests", func() {
 			helper.CmdShouldPass("odo", "push", "--project", project)
 		})
 
-		It("Should fail to unset the invalid parameter", func() {
+		It("Should fail to set and unset an invalid parameter", func() {
 			helper.CmdShouldPass("odo", "create", "nodejs")
+			helper.CmdShouldFail("odo", "env", "set", fakeParameter, fakeParameter, "-f")
 			helper.CmdShouldFail("odo", "env", "unset", fakeParameter, "-f")
 		})
 	})
+
 })
