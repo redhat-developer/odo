25b80bfbc90986a3fced5b7c95c8f179a9a69831
arout <arout@redhat.com>
2020-08-24 16:00:14 +0530

    Updated travis file with respect to the changes

--- scripts/openshiftci-periodic-tests.sh
+++ scripts/openshiftci-periodic-tests.sh
@@ -29,7 +29,9 @@ oc whoami
 
 # Integration tests
 make test-integration
+make test-cmd-debug-serial
 make test-integration-devfile
+make test-cmd-devfile-debug-serial
 make test-cmd-login-logout
 make test-cmd-project
 make test-operator-hub
