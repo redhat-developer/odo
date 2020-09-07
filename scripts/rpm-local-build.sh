be3a49442098ecec9abc11d2efe011f2d07b0bda
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-09-07 12:03:56 +0530

    Fixing local build script to use dist/rpmbuild and topdir for build
     - This prevents polluting users main rpmbuild
    
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/rpm-local-build.sh
+++ scripts/rpm-local-build.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 set -e
+set +x
 
 if [[ ! -d dist/rpmbuild  ]]; then
 	echo "Cannot build as artifacts are not generated. Run scrips/rpm-prepare.sh first"
 	exit 1
 fi
 
-echo "Cleaning up old rpmcontent"
-rm -f ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-
-echo "Copying content to local rpmbuild"
-cp -avrf dist/rpmbuild/SOURCES/* $HOME/rpmbuild/SOURCES/
-cp -avrf dist/rpmbuild/SPECS/* $HOME/rpmbuild/SPECS/
-
+top_dir="`pwd`/dist/rpmbuild"
 echo "Building locally"
-rpmbuild -ba $HOME/rpmbuild/SPECS/openshift-odo.spec
+rpmbuild --define "_topdir `echo $top_dir`" -ba dist/rpmbuild/SPECS/openshift-odo.spec
