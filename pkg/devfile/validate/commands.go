b54070bdc7165eb0f36138a956779b8f144d7444
Maysun J Faisal <maysunaneek@gmail.com>
2020-09-08 17:07:51 -0400

    Update validation errors
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pkg/devfile/validate/commands.go
+++ pkg/devfile/validate/commands.go
@@ -1,11 +1,9 @@
 package validate
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/openshift/odo/pkg/devfile/parser/data/common"
-	"github.com/pkg/errors"
 )
 
 // validateCommands validates all the devfile commands
@@ -16,20 +14,18 @@ func validateCommands(commands map[string]common.DevfileCommand, components []co
 	for _, command := range commands {
 		err = validateCommand(command, commands, components)
 		if err != nil {
-			// return errors.Wrapf(err, "command %s error", command.GetID())
-			// return fmt.Errorf("command %s has validation error: %v", command.GetID(), err)
 			return err
 		}
 	}
 
-	return /* fmt.Errorf("should err out") */
+	return
 }
 
 func validateCommand(command common.DevfileCommand, devfileCommands map[string]common.DevfileCommand, components []common.DevfileComponent) (err error) {
 
 	// type must be exec or composite
 	if command.Exec == nil && command.Composite == nil {
-		return fmt.Errorf("command %q must be of type \"exec\" or \"composite\"", command.GetID())
+		return &UnsupportedOdoCommandError{commandId: command.GetID()}
 	}
 
 	// If the command is a composite command, need to validate that it is valid
@@ -41,17 +37,15 @@ func validateCommand(command common.DevfileCommand, devfileCommands map[string]c
 
 	// component must be specified
 	if command.Exec.Component == "" {
-		return fmt.Errorf("exec command %q must reference a component", command.GetID())
+		return &ExecCommandMissingComponentError{commandId: command.GetID()}
 	}
 
 	// must specify a command
 	if command.Exec.CommandLine == "" {
-		return fmt.Errorf("exec command %q must have a command", command.GetID())
+		return &ExecCommandMissingCommandLineError{commandId: command.GetID()}
 	}
 
 	// must map to a container component
-	// components := GetDevfileContainerComponents(data)
-
 	isComponentValid := false
 	for _, component := range components {
 		if command.Exec.Component == component.Container.Name {
@@ -59,7 +53,7 @@ func validateCommand(command common.DevfileCommand, devfileCommands map[string]c
 		}
 	}
 	if !isComponentValid {
-		return fmt.Errorf("the command %q does not map to a supported component", command.GetID())
+		return &ExecCommandInvalidContainerError{commandId: command.GetID()}
 	}
 
 	return
@@ -68,7 +62,7 @@ func validateCommand(command common.DevfileCommand, devfileCommands map[string]c
 // validateCompositeCommand checks that the specified composite command is valid
 func validateCompositeCommand(compositeCommand *common.Composite, parentCommands map[string]string, devfileCommands map[string]common.DevfileCommand, components []common.DevfileComponent) error {
 	if compositeCommand.Group != nil && compositeCommand.Group.Kind == common.RunCommandGroupType {
-		return fmt.Errorf("composite commands of run Kind are not supported currently")
+		return &CompositeRunKindError{}
 	}
 
 	// Store the command ID in a map of parent commands
@@ -77,18 +71,18 @@ func validateCompositeCommand(compositeCommand *common.Composite, parentCommands
 	// Loop over the commands and validate that each command points to a command that's in the devfile
 	for _, command := range compositeCommand.Commands {
 		if strings.ToLower(command) == compositeCommand.Id {
-			return fmt.Errorf("the composite command %q cannot reference itself", compositeCommand.Id)
+			return &CompositeDirectReferenceError{commandId: compositeCommand.Id}
 		}
 
 		// Don't allow commands to indirectly reference themselves, so check if the command equals any of the parent commands in the command tree
 		_, ok := parentCommands[strings.ToLower(command)]
 		if ok {
-			return fmt.Errorf("the composite command %q cannot indirectly reference itself", compositeCommand.Id)
+			return &CompositeIndirectReferenceError{commandId: compositeCommand.Id}
 		}
 
 		subCommand, ok := devfileCommands[strings.ToLower(command)]
 		if !ok {
-			return fmt.Errorf("the command %q mentioned in the composite command %q does not exist in the devfile", command, compositeCommand.Id)
+			return &CompositeMissingSubCommandError{commandId: compositeCommand.Id, subCommand: command}
 		}
 
 		if subCommand.Composite != nil {
@@ -101,7 +95,7 @@ func validateCompositeCommand(compositeCommand *common.Composite, parentCommands
 		} else {
 			err := validateCommand(subCommand, devfileCommands, components)
 			if err != nil {
-				return errors.Wrapf(err, "the composite command %q references an invalid command %q", compositeCommand.Id, subCommand.GetID())
+				return &CompositeInvalidSubCommandError{commandId: compositeCommand.Id, subCommandId: subCommand.GetID()}
 			}
 		}
 	}
