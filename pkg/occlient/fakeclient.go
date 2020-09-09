8803e51c0baf1415e2cc75544d849d105ba6982b
Chris Laprun <metacosm@gmail.com>
2020-09-09 12:15:26 +0200

    feat: always set discovery client on fakeclient

--- pkg/occlient/fakeclient.go
+++ pkg/occlient/fakeclient.go
@@ -7,7 +7,13 @@ import (
 	fakeImageClientset "github.com/openshift/client-go/image/clientset/versioned/fake"
 	fakeProjClientset "github.com/openshift/client-go/project/clientset/versioned/fake"
 	fakeRouteClientset "github.com/openshift/client-go/route/clientset/versioned/fake"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery/fake"
 	fakeKubeClientset "k8s.io/client-go/kubernetes/fake"
+	"os"
+	"sync"
 )
 
 // FakeClientset holds fake ClientSets
@@ -53,5 +59,48 @@ func FakeNew() (*Client, *FakeClientset) {
 	fkclientset.ServiceCatalogClientSet = fakeServiceCatalogClientSet.NewSimpleClientset()
 	client.serviceCatalogClient = fkclientset.ServiceCatalogClientSet.ServicecatalogV1beta1()
 
+	if os.Getenv("KUBERNETES") != "true" {
+		client.SetDiscoveryInterface(fakeDiscoveryWithRoute)
+	} else {
+		client.SetDiscoveryInterface(&fake.FakeDiscovery{})
+	}
+
 	return &client, &fkclientset
+}
+
+type resourceMapEntry struct {
+	list *metav1.APIResourceList
+	err  error
+}
+
+type fakeDiscovery struct {
+	*fake.FakeDiscovery
+
+	lock        sync.Mutex
+	resourceMap map[string]*resourceMapEntry
+}
+
+var fakeDiscoveryWithRoute = &fakeDiscovery{
+	resourceMap: map[string]*resourceMapEntry{
+		"project.openshift.io/v1": {
+			list: &metav1.APIResourceList{
+				GroupVersion: "project.openshift.io/v1",
+				APIResources: []metav1.APIResource{{
+					Name:         "routes",
+					SingularName: "route",
+					Namespaced:   true,
+					Kind:         "route",
+				}},
+			},
+		},
+	},
+}
+
+func (c *fakeDiscovery) ServerResourcesForGroupVersion(groupVersion string) (*metav1.APIResourceList, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if rl, ok := c.resourceMap[groupVersion]; ok {
+		return rl.list, rl.err
+	}
+	return nil, kerrors.NewNotFound(schema.GroupResource{}, "")
 }
