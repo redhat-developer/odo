22143a56765bd69fdf4b1547e2c6440f71921fca
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-08 16:52:37 +0530

    Some script updates
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,15 +1,48 @@
 #!/usr/bin/env bash
+
+shout() {
+  set +x
+  echo -e "\n!!!!!!!!!!!!!!!!!!!!\n${1}\n!!!!!!!!!!!!!!!!!!!!\n"
+  set -x
+}
+
 set -ex
 
-# Run unit tests
-GOFLAGS='-mod=vendor' make test
+shout "Setting up"
 
+mkdir bin
 GOBIN="`pwd`/bin"
 if [[ $BASE_OS == "windows" ]]; then
     GOBIN="$(cygpath -pw $GOBIN)"
 fi
-
 PATH=$PATH:$GOBIN
+
+#-----------------------------------------------------------------------------
+
+shout "Testing against 4x cluster"
+
+if [[ $BASE_OS == "linux"  ]]; then
+    set +x
+	curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
+    set -x
+	tar -C $GOBIN -xvf ./oc.tar && rm -rf ./oc.tar
+else
+    set +x
+    curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
+    set -x
+    gunzip -c ./oc.zip > $GOBIN/oc && rm -rf ./oc.zip && chmod +x $GOBIN/oc
+    if [[ $BASE_OS == "windows" ]]; then
+        mv -f $GOBIN/oc $GOBIN/oc.exe
+    fi
+fi
+
+shout "Logging into 4x cluster as developer (logs hidden)"
+set +x
+oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
+set -x
+
+# Run unit tests
+GOFLAGS='-mod=vendor' make test
 
 # Prep for int
 echo "getting ginkgo"
@@ -26,4 +59,3 @@ if [[ $run_all -eq 0 ]]; then
 else
     echo "Ginkgo does not exist, skipping integration/e2e"
 fi
-
--- scripts/setup_all_tests.sh
+++ scripts/setup_all_tests.sh
@@ -1,36 +1,4 @@
 #!/usr/bin/env bash
 
-# Base Setup
-
-shout() {
-  set +x
-  echo -e "\n!!!!!!!!!!!!!!!!!!!!\n${1}\n!!!!!!!!!!!!!!!!!!!!\n"
-  set -x
-}
-
-set -ex
-
-mkdir bin
-BINDIR="`pwd`/bin"
-
-if [[ $BASE_OS == "linux"  ]]; then
-    set +x
-	curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
-    set -x
-	tar -C $BINDIR -xvf ./oc.tar && rm -rf ./oc.tar
-else
-    set +x
-    curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
-    set -x
-    gunzip -c ./oc.zip > $BINDIR/oc && rm -rf ./oc.zip && chmod +x $BINDIR/oc
-    if [[ $BASE_OS == "windows" ]]; then
-        mv -f $BINDIR/oc $BINDIR/oc.exe
-    fi
-fi
-
-## 4.x tests
-shout "Logging into 4x cluster as developer (logs hidden)"
-set +x
-oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
-set -x
+echo "No setup"
 
