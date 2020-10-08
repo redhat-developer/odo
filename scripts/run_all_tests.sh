1dbdc948ae5e217e48a12f7871e0f6c3ca73f3de
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-08 15:52:06 +0530

    updating testing scripts
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
+set -ex
+
+# Run unit tests
+GOFLAGS='-mod=vendor' make test
+
+GOBIN="`pwd`/bin"
+if [[ $BASE_OS == "windows" ]]; then
+    GOBIN="$(cygpath -pw $GOBIN)"
+fi
+
+PATH=$PATH:$GOBIN
+
+# Prep for int
+echo "getting ginkgo"
+GOBIN="$GOBIN" make goget-ginkgo
+
+set +e
+ls -a $GOBIN
+ginkgo version
+run_all=$?
 set -e
-make test
+# Integration tests
+if [[ $run_all -eq 0 ]]; then
+    make test-e2e-all
+else
+    echo "Ginkgo does not exist, skipping integration/e2e"
+fi
+
