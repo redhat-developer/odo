801d57bee657957bb3506863f991fbd310cbec05
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-09 18:57:42 +0530

    Some fixes
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -35,30 +35,30 @@ ginkgo version
 run_all=$?
 set -e
 
-# Integration tests
-shout "Testing against 4x cluster"
-shout "Getting oc binary"
-if [[ $BASE_OS == "linux"  ]]; then
-    set +x
-	curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
-    set -x
-	tar -C $GOBIN -xvf ./oc.tar && rm -rf ./oc.tar
-else
-    set +x
-    curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
-    set -x
-    gunzip -c ./oc.zip > $GOBIN/oc && rm -rf ./oc.zip && chmod +x $GOBIN/oc
-    if [[ $BASE_OS == "windows" ]]; then
-        mv -f $GOBIN/oc $GOBIN/oc.exe
-    fi
-fi
-
-shout "Logging into 4x cluster as developer (logs hidden)"
-set +x
-oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
-set -x
-
 if [[ $run_all -eq 0 ]]; then
+    # Integration tests
+    shout "Testing against 4x cluster"
+    shout "Getting oc binary"
+    if [[ $BASE_OS == "linux"  ]]; then
+        set +x
+    	curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
+        set -x
+    	tar -C $GOBIN -xvf ./oc.tar && rm -rf ./oc.tar
+    else
+        set +x
+        curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
+        set -x
+        gunzip -c ./oc.zip > $GOBIN/oc && rm -rf ./oc.zip && chmod +x $GOBIN/oc
+        if [[ $BASE_OS == "windows" ]]; then
+            mv -f $GOBIN/oc $GOBIN/oc.exe
+        fi
+    fi
+
+    shout "Logging into 4x cluster as developer (logs hidden)"
+    set +x
+    oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
+    set -x
+    
     shout "Running integration/e2e tests"
     make test-e2e-all
 else
