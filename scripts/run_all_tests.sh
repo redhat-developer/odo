713dd8d8a0ed8b09a3ac5f90d39dee7c1fad0a9e
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-27 16:36:01 +0530

    Cleaning up the script
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -17,7 +17,28 @@ if [[ $BASE_OS == "windows" ]]; then
 fi
 PATH=$PATH:$GOBIN
 
+shout "Getting oc binary"
+if [[ $BASE_OS == "linux"  ]]; then
+    set +x
+    curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
+    set -x
+    tar -C $GOBIN -xvf ./oc.tar && rm -rf ./oc.tar
+else
+    set +x
+    curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
+    set -x
+    tar -C $GOBIN -xvf ./oc.zip && rm -rf ./oc.zip && chmod +x $GOBIN/oc
+    if [[ $BASE_OS == "windows" ]]; then
+        mv -f $GOBIN/oc $GOBIN/oc.exe
+    fi
+    if [[ $BASE_OS == "mac" ]]; then
+        PATH="$PATH:/usr/local/bin:/usr/local/go/bin"
+    fi
+fi
+
 #-----------------------------------------------------------------------------
+
+shout "Testing"
 
 # Run unit tests
 GOFLAGS='-mod=vendor' make test
@@ -29,38 +50,13 @@ cp -avrf ./odo $GOBIN/
 shout "getting ginkgo"
 GOBIN="$GOBIN" make goget-ginkgo
 
-set +e
-ls -a $GOBIN
-ginkgo version
-run_all=$?
-set -e
-
-if [[ $run_all -eq 0 ]]; then
-    # Integration tests
-    shout "Testing against 4x cluster"
-    shout "Getting oc binary"
-    if [[ $BASE_OS == "linux"  ]]; then
-        set +x
-    	curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.tar -o ./oc.tar
-        set -x
-    	tar -C $GOBIN -xvf ./oc.tar && rm -rf ./oc.tar
-    else
-        set +x
-        curl -k ${OC4X_DOWNLOAD_URL}/${ARCH}/${BASE_OS}/oc.zip -o ./oc.zip
-        set -x
-        gunzip -c ./oc.zip > $GOBIN/oc && rm -rf ./oc.zip && chmod +x $GOBIN/oc
-        if [[ $BASE_OS == "windows" ]]; then
-            mv -f $GOBIN/oc $GOBIN/oc.exe
-        fi
-    fi
-
-    shout "Logging into 4x cluster as developer (logs hidden)"
-    #set +x
-    oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
-    #set -x
+# Integration tests
+shout "Testing against 4x cluster"
     
-    shout "Running integration/e2e tests"
-    make test-e2e-all
-else
-    shout "Ginkgo does not exist, skipping integration/e2e"
-fi
+shout "Logging into 4x cluster as developer (logs hidden)"
+set +x
+oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
+set -x
+    
+shout "Running integration/e2e tests"
+make test-e2e-all
