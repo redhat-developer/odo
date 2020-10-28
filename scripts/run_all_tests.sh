cd6e884ca09e9261c5e62b5b0dd5c2a28c4394ed
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-28 16:02:53 +0530

    Updating script for kubeadmin login and handling some setup
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -54,10 +54,28 @@ GOBIN="$GOBIN" make goget-ginkgo
 
 # Integration tests
 shout "Testing against 4x cluster"
-    
+
+shout "Logging into 4x cluster for some setup (logs hidden)"
+set +x
+oc login -u kubeadmin -p ${OCP4X_KUBEADMIN_PASSWORD} --insecure-skip-tls-verify  ${OCP4X_API_URL}
+set -x
+
+shout "Doing some presetup"
+REDHAT_OPENJDK_PROJECT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+REDHAT_NODEJS_PROJECT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+# Create the namespace for e2e image test apply pull secret to the namespace
+for i in `echo "$REDHAT_OPENJDK_PROJECT $REDHAT_NODEJS_PROJECT"`; do
+    # create the namespace
+    oc new-project $i
+    # Applying pull secret to the namespace which will be used for pulling images from authenticated registry
+    oc get secret pull-secret -n openshift-config -o yaml | sed "s/openshift-config/$i/g" | oc apply -f -
+    # Let developer user have access to the project
+    oc adm policy add-role-to-user edit developer
+done
+
 shout "Logging into 4x cluster as developer (logs hidden)"
 set +x
-oc login -u developer -p password@123 --insecure-skip-tls-verify  ${OCP4X_API_URL}
+oc login -u developer -p ${OCP4X_DEVELOPER_PASSWORD} --insecure-skip-tls-verify  ${OCP4X_API_URL}
 set -x
     
 shout "Running integration/e2e tests"
