18fddfb457ae6f5fcc032c2223df733cda52d1d9
Dharmit Shah <shahdharmit@gmail.com>
2020-11-11 11:54:02 +0530

    Remove gitmodules file and add the example

---
+++ tests/examples/source/web-nodejs-sample/README.md
@@ -0,0 +1,16 @@
+# web-nodejs-sample
+
+ExpressJS Sample Application
+
+# Developer Workspace
+[![Contribute](http://beta.codenvy.com/factory/resources/codenvy-contribute.svg)](http://beta.codenvy.com/f?id=r8et9w6vohmqvro8)
+
+# Stack to use
+
+FROM [codenvy/node](https://hub.docker.com/r/codenvy/node/)
+
+# How to run
+
+| #       | Description           | Command  |
+| :------------- |:-------------| :-----|
+| 1      | Run | `cd ${current.project.path}/app && node app.js` |
---
+++ tests/examples/source/web-nodejs-sample/app/app.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2012-2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+/*eslint-env node*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
