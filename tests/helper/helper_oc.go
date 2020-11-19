7a7470c8c67dedcb5058407142f4ff5a3add6005
anandrkskd <anandrkskd@gmail.com>
2020-11-19 11:42:21 +0530

    windows path seperator fix
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- tests/helper/helper_oc.go
+++ tests/helper/helper_oc.go
@@ -312,7 +312,7 @@ func (oc OcRunner) checkForImageStream(name string, tag string) bool {
 
 // ImportImageFromRegistry import the required image of the respective component type from the specified registry
 func (oc OcRunner) ImportImageFromRegistry(registry, image, cmpType, project string) {
-	CmdShouldPass(oc.path, "--request-timeout", "5m", "import-image", cmpType, "--namespace="+project, "--from="+filepath.ToSlash(filepath.Join(registry, image)), "--confirm")
+	CmdShouldPass(oc.path, "--request-timeout", "5m", "import-image", cmpType, "--namespace="+project, "--from="+registry+"/"+image, "--confirm")
 	CmdShouldPass(oc.path, "annotate", filepath.ToSlash(filepath.Join("istag", cmpType)), "--namespace="+project, "tags=builder", "--overwrite")
 
 }
