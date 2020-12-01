a738a098a866a368dd8d9b915d4495d738ac4f82
Rodolfo Napoles <rnapoles@redhat.com>
2020-12-01 10:37:11 -0500

    4062 fix syntax error

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -33,7 +33,7 @@ for arch in `ls -1 $BIN_DIR/`;do
 
     # Create a tar.gz of the binary
     # echo "gzipping binary $source_file as $target_file"
-    if [["$suffix" == ".exe"]]; then
+    if [[$suffix == .exe]]; then
         echo "zipping binary $source_file as $target_file"
         zip -9 -y -r -q $target_file.zip $source_dir/$source_filename
     else
