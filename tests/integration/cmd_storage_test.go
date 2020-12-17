d1f82e24ce2070bb7b12f303054de14ad7db55a5
Rodolfo Napoles <rnapoles@redhat.com>
2020-12-17 11:49:26 -0500

    fixed required flags for expected failure

--- tests/integration/cmd_storage_test.go
+++ tests/integration/cmd_storage_test.go
@@ -34,7 +34,7 @@ var _ = Describe("odo storage command tests", func() {
 
 	Context("when running storage command without required flag(s)", func() {
 		It("should fail", func() {
-			requiredFlags := []string{"size", "path", "context"}
+			requiredFlags := []string{"size", "path"}
 			helper.CopyExample(filepath.Join("source", "nodejs"), commonVar.Context)
 			helper.CmdShouldPass("odo", "component", "create", "--s2i", "nodejs", "nodejs", "--app", "nodeapp", "--project", commonVar.Project, "--context", commonVar.Context)
 			stdErr := helper.CmdShouldFail("odo", "storage", "create", "pv1", "--size", "1Gi", "--context", commonVar.Context)
