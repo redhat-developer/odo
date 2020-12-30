46a745694fc6758e2ba8d05696ff65ab83e64545
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-12-30 11:19:40 +0530

    Moving operator hub tests to top
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -103,8 +103,8 @@ set -x
 
 # Integration tests
 shout "Running integration Tests"
-make test-integration || error=true
 make test-operator-hub || error=true
+make test-integration || error=true
 make test-integration-devfile || error=true
 make test-cmd-login-logout || error=true
 make test-cmd-project || error=true
