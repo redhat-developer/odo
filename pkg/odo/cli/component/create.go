b7aa6cdc73ca6a9c6840ea61d2cbe5535b16bb37
Girish Ramnani <girishramnani95@gmail.com>
2021-01-11 17:19:43 +0530

    reversed a bug side-fix

--- pkg/odo/cli/component/create.go
+++ pkg/odo/cli/component/create.go
@@ -383,9 +383,8 @@ func (co *CreateOptions) Complete(name string, cmd *cobra.Command, args []string
 		return errors.New("this directory already contains a devfile, you can't specify devfile via --devfile")
 	}
 
-	// we check if the devfile is already present or not, we also check if the envfile doesn't exist because someone could just have shared a zip of the component
-	// and the other developer runs odo create on it
-	if util.CheckPathExists(co.DevfilePath) && !util.CheckPathExists(EnvFilePath) {
+	// we check if the devfile is already present or not, this location is important - check should happen early
+	if util.CheckPathExists(co.DevfilePath) {
 		co.devfileMetadata.devfileAlreadyPresent = true
 	}
 
