e3ebaa6c7937f54877c522d30bf4cedf5836cb57
Rodolfo Napoles <rnapoles@redhat.com>
2021-02-03 13:17:34 -0500

    Scripts for PSI minishift tests

---
+++ scripts/minishift-all-tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+executing() {
+   set +x
+  echo -e "\n------------------------------\n${1}\n------------------------------\n"
+   set -x
+}
+
+set -ex
+
+#Export GitHub token to avoid 
+export MINISHIFT_GITHUB_API_TOKEN="03d362b0f2a7ecf02b30bfbdd40fe720a4d6b8b3"
+executing "Setting up environment..."
+
+export PATH="$PATH:/usr/local/go/bin/"
+export GOPATH=$HOME/go
+git clone https://github.com/openshift/odo.git openshift/odo
+cd openshift/odo
+
+mkdir -p $GOPATH/bin
+make goget-ginkgo
+export PATH="$PATH:$(pwd):$GOPATH/bin"
+
+executing "Building ODO..."
+make bin
+sudo cp odo /usr/bin
+
+executing "Stopping minishift..."
+minishift stop
+yes | minishift delete
+MINISHIFT_ENABLE_EXPERIMENTAL=y 
+
+executing "Starting minishift..."
+minishift start 
+executing "Adding components: service-catalog, automation-service-broker, and template-service-broker ..."
+minishift openshift component add service-catalog
+minishift openshift component add automation-service-broker
+minishift openshift component add template-service-broker
+sleep 3m
+eval $(minishift oc-env)
+
+executing "Executing tests..."
+make test-cmd-project
+make test-cmd-service
+
+executing "Removing cloned repo..."
+rm -Rf openshift
