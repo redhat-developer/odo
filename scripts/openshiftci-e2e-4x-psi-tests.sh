7b3b5115b84262fbcc22e0a8b31660e70c28f891
Mohammed Ahmed <mohammed.zee1000@gmail.com>
2021-02-12 15:44:59 +0530

    Renaming entrypoint scripts to use new job (#4419)
    - 4.6 will now run the old presubmit-all script
     - psi will run as 4.6-e2e-4x-psi
    
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/openshiftci-e2e-4x-psi-tests.sh
+++ scripts/openshiftci-e2e-4x-psi-tests.sh
@@ -1,3 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "For now. I am a placeholder. Please update me in psi poc after updating prow configs with new job for me"
+# fail if some commands fails
+set -e
+
+# Hide command that deals with secrets
+set +x
+if [[ -f $ODO_RABBITMQ_AMQP_URL ]]; then
+    export AMQP_URI=$(cat $ODO_RABBITMQ_AMQP_URL)
+fi
+export AMQP_URI=${AMQP_URI:?"Please set AMQP_URI env with amqp uri or provide path of file containing it as ODO_RABBITMQ_AMQP_URL env"}
+
+# show commands
+set -x
+
+export JOB_NAME="odo-pr-build"
+export REPO_URL="https://github.com/openshift/odo"
+# Extract PR NUMBER from prow job spec, which is injected by prow.
+export TARGET="$(jq .refs.pulls[0].number <<< $(echo $JOB_SPEC))"
+export CUSTOM_HOMEDIR=$ARTIFACT_DIR
+
+##### ci-firewall parameters end
+# The version of CI_FIREWALL TO USE
+export CI_FIREWALL_VERSION="valpha"
+
+echo "Getting ci-firewall, see https://github.com,/mohammedzee1000/ci-firewall"
+curl -kLO https://github.com/mohammedzee1000/ci-firewall/releases/download/valpha/ci-firewall-linux-amd64.tar.gz
+tar -xzf ci-firewall-linux-amd64.tar.gz
+./ci-firewall request --runscript scripts/run_all_tests.sh --timeout 2h15m
