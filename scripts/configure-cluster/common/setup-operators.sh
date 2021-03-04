5d914523def09b8e1b64e7687f39873f116f7836
Dharmit Shah <shahdharmit@gmail.com>
2021-03-04 19:39:22 +0530

    Change API & Operator version for Serice Binding Operator (#4484)
    * Change API & Operator version for Serice Binding Operator
    
    This is to be compatible with SBO 0.5.0
    
    * Remove startingCSV info from Operator setup script
    
    This will lead to installation of latest available version of the
    Operator

--- scripts/configure-cluster/common/setup-operators.sh
+++ scripts/configure-cluster/common/setup-operators.sh
@@ -15,7 +15,6 @@ install_etcd_operator(){
     name: etcd
     source: community-operators
     sourceNamespace: openshift-marketplace
-    startingCSV: etcdoperator.v0.9.4-clusterwide
 EOF
 }
 
@@ -34,7 +33,6 @@ install_service_binding_operator(){
     name: rh-service-binding-operator
     source: redhat-operators
     sourceNamespace: openshift-marketplace
-    startingCSV: service-binding-operator.v0.3.0
 EOF
 }
 
