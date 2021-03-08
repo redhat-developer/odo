41f3d1617f0d5ac61db085a9360bc65f33c71661
anandrkskd <anandrkskd@gmail.com>
2021-03-08 16:50:33 +0530

    fixing typo
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- scripts/openshiftci-unit-psi-tests_win.sh
+++ scripts/openshiftci-unit-psi-tests_win.sh
@@ -3,6 +3,6 @@
 export SENDQUEUE=amqp.ci.queue.win.unit.send
 export SENDTOPIC=amqp.ci.topic.win.unit.send
 export SETUPSCRIPT=${SETUPSCRIPT:-"scripts/setup_script_unit.sh"}
-export RUNSCRIPT=${RUNSCRIPT:-"script/run_script_unit.sh"}
+export RUNSCRIPT=${RUNSCRIPT:-"scripts/run_script_unit.sh"}
 
 . scripts/openshift-e2e-4x-psi-tests.sh
