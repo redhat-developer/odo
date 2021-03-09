bcad6e0fb390c62a5d70145cf8b1100d44a8b1fb
Rodolfo Napoles <rnapoles@redhat.com>
2021-03-08 19:58:36 -0500

    Generalized scripts to reduce duplication of code

--- scripts/minishift-setup-env.sh
+++ scripts/minishift-setup-env.sh
@@ -17,7 +17,6 @@ export PATH="$PATH:$(pwd):$GOPATH/bin"
 
 export ARTIFACTS_DIR="`pwd`/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACT_DIR
-
 # This si one of the variables injected by ci-firewall. Its purpose is to allow scripts to handle uniqueness as needed
 SCRIPT_IDENTITY=${SCRIPT_IDENTITY:-"def-id"}
 
