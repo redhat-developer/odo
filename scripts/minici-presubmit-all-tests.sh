4797f238054ed0bc5d7043468d0519b64104ca01
Rodolfo Napoles <72166315+rnapoles-rh@users.noreply.github.com>
2021-03-13 04:35:39 -0500

    Scripts for PSI minishift tests (#4406)
    * Scripts for PSI minishift tests
    
    * added line at end of script
    
    * added line at end of script
    
    * emoved info as requested
    
    * Updates as per review feedback
    
    * Updated presubmit script, adding new scripts
    
    * Changes to minishift start script and removal of one script
    
    * copyin odo to $GOPATH/bin instead of /usr/bin
    
    * deleting unwanted script as it was replaced with other files
    
    * Generilized scripst to reduce duplication of code
    
    * Generalized scripts to reduce duplication of code
    
    * small changes
    
    * Scripts for PSI minishift tests
    
    * added line at end of script
    
    * added line at end of script
    
    * emoved info as requested
    
    * Updates as per review feedback
    
    * Made required changes
    Updated presubmit script, adding new scripts
    
    * deleting unwanted script as it was replaced with other files
    
    * :Generilized scripst to reduce duplication of code
    
    * Generalized scripts to reduce duplication of code
    
    * Merging kubernetes and minishift istead of having scripts for each
    
    * Changes to check if required components are installed
    
    * Parametrized firewall version
    
    * Deleted delete sh
    
    * Fixed CI_FIREWALL_VERSION being defined twice

---
+++ scripts/minici-presubmit-all-tests.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# yaml file must call this script with parameter values minikube or minishift
+# fail if some commands fails
+set -e
+# Hide command that deals with secrets
+set +x
+if [[ -f $ODO_RABBITMQ_AMQP_URL ]]; then
+    export AMQP_URI=$(cat $ODO_RABBITMQ_AMQP_URL)
+fi
+
+##### These are varialbes used by ci-firewall as one of the ways to get its parameters
+# If AMQP_URI is not set by the time we reach here, show error message and exit.
+export AMQP_URI=${AMQP_URI:?"Please set AMQP_URI env with amqp uri or provide path of file containing it as ODO_RABBITMQ_AMQP_URL env"}
+
+case $1 in
+    minikube)
+        export JOB_NAME="odo-minikube-pr-build"
+        export SENDQUEUE="amqp.ci.queue.minikube.send"
+        export SENDTOPIC="amqp.ci.topic.minikube.send"
+        export SETUP_SCRIPT="scripts/minikube-minishift-setup-env.sh minikube"
+        export RUN_SCRIPT="scripts/minikube-minishift-all-tests.sh minikube"
+        export TIMEOUT="2h15m"
+        ;;
+    minishift)
+        export JOB_NAME="odo-minishift-pr-tests"
+        export SENDQUEUE="amqp.ci.queue.minishift.send"
+        export SENDTOPIC="amqp.ci.topic.minishift.send"
+        export SETUP_SCRIPT="scripts/minikube-minishift-setup-env.sh minishift"
+        export RUN_SCRIPT="scripts/minikube-minishift-all-tests.sh minishift"
+        export TIMEOUT="4h00m"
+        ;;
+    *)
+        echo "Must pass minikube or minishift as paramater"
+        exit 1
+        ;;
+esac
+
+export REPO_URL="https://github.com/openshift/odo"
+# Extract PR NUMBER from prow job spec, which is injected by prow.
+export TARGET="$(jq .refs.pulls[0].number <<< $(echo $JOB_SPEC))"
+##### ci-firewall parameters end
+
+# The version of CI_FIREWALL TO USE
+export CI_FIREWALL_VERSION="valpha"
+
+echo "Getting ci-firewall, see https://github.com,/mohammedzee1000/ci-firewall"
+# show commands
+set -x
+
+curl -kLO https://github.com/mohammedzee1000/ci-firewall/releases/download/$CI_FIREWALL_VERSION/ci-firewall-linux-amd64.tar.gz
+tar -xzf ci-firewall-linux-amd64.tar.gz
+
+./ci-firewall request --sendqueue $SENDQUEUE --sendtopic $SENDTOPIC --setupscript $SETUP_SCRIPT --runscript $RUN_SCRIPT --timeout $TIMEOUT
