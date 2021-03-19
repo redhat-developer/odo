69e9319b02146b8053ecedc5e8c73e7a81f6c599
Parthvi Vala <pvala@redhat.com>
2021-03-19 16:07:01 +0530

    Modify tests as requested

--- pkg/segment/segment_test.go
+++ pkg/segment/segment_test.go
@@ -9,11 +9,12 @@ import (
 	"net/http/httptest"
 	"os"
 	"os/user"
-	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/openshift/odo/pkg/testingutil/filesystem"
 
 	"github.com/openshift/odo/pkg/log"
 	"github.com/openshift/odo/pkg/preference"
@@ -66,23 +67,21 @@ func TestClientUploadWithoutConsent(t *testing.T) {
 			},
 		},
 	}
-
-	c, err := newCustomClient(cfg, filepath.Join(os.TempDir(), "telemetry"), server.URL)
-	//	assert err != nil
+	c, err := newCustomClient(cfg, createConfigDir(t), server.URL)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	// run a command, odo preference view
-	if err := c.Upload("odo preference view", time.Second, errors.New("an error occured")); err != nil {
-		t.Fatal(err)
+	if err = c.Upload("odo preference view", time.Second, errors.New("an error occurred")); err != nil {
+		t.Error(err)
 	}
-	if err := c.Close(); err != nil {
-		t.Fatal(err)
+	if err = c.Close(); err != nil {
+		t.Error(err)
 	}
 
 	select {
 	case <-body:
-		t.Fatal("server should not receive data")
+		t.Error("server should not receive data")
 	default:
 	}
 }
@@ -109,14 +108,12 @@ func TestClientUploadWithConsent(t *testing.T) {
 		errType  string
 	}{
 		{
-			cmd:      "odo preference view",
 			testName: "command ran successfully",
 			err:      nil,
 			success:  true,
 			errType:  "",
 		},
 		{
-			cmd:      "odo prfnc view",
 			testName: "command failed",
 			err:      errors.New("some error occurred"),
 			success:  false,
@@ -126,12 +123,12 @@ func TestClientUploadWithConsent(t *testing.T) {
 	for _, tt := range tests {
 		t.Log("Running test: ", tt.testName)
 		t.Run(tt.testName, func(t *testing.T) {
-			c, err := newCustomClient(cfg, filepath.Join(os.TempDir(), "telemetry"), server.URL)
+			c, err := newCustomClient(cfg, createConfigDir(t), server.URL)
 			if err != nil {
 				t.Error(err)
 			}
 			//run a command, odo preference view
-			if err := c.Upload(tt.cmd, time.Second, tt.err); err != nil {
+			if err := c.Upload("odo create", time.Second, tt.err); err != nil {
 				t.Error(err)
 			}
 			if err := c.Close(); err != nil {
@@ -177,19 +174,49 @@ func TestClientUploadWithConsent(t *testing.T) {
 }
 
 func TestSetError(t *testing.T) {
-	err := errors.New("this is an error string")
-	if SetError(err) != err.Error() {
-		t.Errorf("got: %s, want: %s", SetError(err), err.Error())
-	}
-
 	user, err := user.Current()
 	if err != nil {
 		t.Error(err.Error())
 	}
 
-	err = fmt.Errorf("cannot access the preference file '/home/%s/.odo/preference.yaml'", user.Username)
-
-	if SetError(err) == err.Error() || strings.Contains(SetError(err), user.Username) {
-		t.Error("User ID is not sanitized properly.")
+	tests := []struct {
+		name   string
+		err    error
+		hasPII bool
+	}{
+		{
+			name:   "error without PII information",
+			err:    errors.New("this is an error string"),
+			hasPII: false,
+		},
+		{
+			name:   "error with PII information",
+			err:    fmt.Errorf("cannot access the preference file '/home/%s/.odo/preference.yaml'", user.Username),
+			hasPII: true,
+		},
 	}
+
+	for _, tt := range tests {
+		got := SetError(tt.err)
+
+		// if error has PII, string returned by SetError must not be the same as the error since it was sanitized
+		// else it will be the same.
+		if (tt.hasPII && got == tt.err.Error()) || (!tt.hasPII && got != tt.err.Error()) {
+			if tt.hasPII && strings.Contains(got, user.Username) {
+				t.Error("PII was not sanitized properly.")
+			} else {
+				t.Errorf("got: %s, want: %s", got, tt.err.Error())
+			}
+		}
+	}
+}
+
+// createConfigDir creates a mock filesystem
+func createConfigDir(t *testing.T) string {
+	fs := filesystem.NewFakeFs()
+	configDir, err := fs.TempDir(os.TempDir(), "telemetry")
+	if err != nil {
+		t.Error(err)
+	}
+	return configDir
 }
