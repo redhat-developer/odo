c7d4f2f6c2a99d65b1af875921a6cf9dee894bbc
Parthvi Vala <pvala@redhat.com>
2021-04-06 11:36:45 +0530

    Clean cmd_devfile_describe_test.go

--- tests/integration/devfile/cmd_devfile_describe_test.go
+++ tests/integration/devfile/cmd_devfile_describe_test.go
@@ -19,7 +19,6 @@ var _ = Describe("odo devfile describe command tests", func() {
 		}
 
 		commonVar = helper.CommonBeforeEach()
-		helper.Chdir(commonVar.Context)
 	})
 
 	// This is run after every Spec (It)
@@ -28,6 +27,12 @@ var _ = Describe("odo devfile describe command tests", func() {
 	})
 
 	Context("When executing odo describe", func() {
+		JustAfterEach(func() {
+			// odo delete requires changing directory because it does not work as intended with --context
+			helper.Chdir(commonVar.Context)
+			helper.CmdShouldPass("odo", "delete", "-f", "--all")
+		})
+
 		It("should describe the component when it is not pushed", func() {
 			helper.CmdShouldPass("odo", "create", "nodejs", "cmp-git", "--project", commonVar.Project, "--context", commonVar.Context, "--app", "testing")
 			helper.CmdShouldPass("odo", "url", "create", "url-1", "--port", "3000", "--host", "example.com", "--context", commonVar.Context)
@@ -51,8 +56,6 @@ var _ = Describe("odo devfile describe command tests", func() {
 			// odo should describe not pushed component if component name is given.
 			helper.CmdShouldPass("odo", "describe", "cmp-git", "--context", commonVar.Context)
 			Expect(cmpDescribe).To(ContainSubstring("cmp-git"))
-
-			helper.CmdShouldPass("odo", "delete", "-f", "--all", "--context", commonVar.Context)
 		})
 
 		It("should describe the component when it is pushed", func() {
@@ -79,8 +82,6 @@ var _ = Describe("odo devfile describe command tests", func() {
 			// odo should describe not pushed component if component name is given.
 			helper.CmdShouldPass("odo", "describe", "cmp-git", "--context", commonVar.Context)
 			Expect(cmpDescribe).To(ContainSubstring("cmp-git"))
-
-			helper.CmdShouldPass("odo", "delete", "-f", "--all", "--context", commonVar.Context)
 		})
 	})
 
