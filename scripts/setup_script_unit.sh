313016db59e484a40aff787f1e83ae429d3ed389
anandrkskd <anandrkskd@gmail.com>
2021-04-14 14:16:40 +0530

    fixing unit test
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- scripts/setup_script_unit.sh
+++ scripts/setup_script_unit.sh
@@ -17,8 +17,7 @@ mkdir bin artifacts
 export GOBIN="`pwd`/bin"
 # Set kubeconfig to current dir. This ensures no clashes with other test runs
 export KUBECONFIG="`pwd`/config"
-export ARTIFACTS_DIR="`pwd`/artifacts"
-export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
+export CUSTOM_HOMEDIR=$ARTIFACT_DIR
 
 # This si one of the variables injected by ci-firewall. Its purpose is to allow scripts to handle uniqueness as needed
 SCRIPT_IDENTITY=${SCRIPT_IDENTITY:-"def-id"}
@@ -26,7 +25,6 @@ SCRIPT_IDENTITY=${SCRIPT_IDENTITY:-"def-id"}
 if [[ $BASE_OS == "windows" ]]; then
     shout "Setting GOBIN for windows"
     GOBIN="$(cygpath -pw $GOBIN)"
-    ARTIFACTS_DIR="$(cygpath -pw $ARTIFACTS_DIR)"
     CUSTOM_HOMEDIR="$(cygpath -pw $CUSTOM_HOMEDIR)"
 elif [[ $BASE_OS == "mac" ]]; then
     PATH="$PATH:/usr/local/bin:/usr/local/go/bin"                           #Path to `go` command as `/usr/local/go/bin:/usr/local/bin` is not included in $PATH while running test
