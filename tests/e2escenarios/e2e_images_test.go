919882c0549f1d94bdfbb73ace75d66b9699ffa1
Girish Ramnani <girishramnani95@gmail.com>
2021-04-15 14:07:59 +0530

    more images

--- tests/e2escenarios/e2e_images_test.go
+++ tests/e2escenarios/e2e_images_test.go
@@ -129,6 +129,12 @@ var _ = Describe("odo supported images e2e tests", func() {
 			verifySupportedImage("ubi8/nodejs-12:latest", "nodejs", "nodejs:latest", redhatNodejs12UBI8Project, appName, commonVar.Context)
 		})
 
+		It("Should be able to verify the nodejs-12-rhel7 image", func() {
+			redhatNodejs12RHEL7Project := util.GetEnvWithDefault("REDHAT_NODEJS12_RHEL7_PROJECT", "nodejs-12-rhel7")
+			oc.ImportImageFromRegistry("registry.redhat.io", "rhscl/nodejs-12-rhel7:latest", "nodejs:latest", redhatNodejs12RHEL7Project)
+			verifySupportedImage("rhscl/nodejs-12-rhel7:latest", "nodejs", "nodejs:latest", redhatNodejs12RHEL7Project, appName, commonVar.Context)
+		})
+
 		It("Should be able to verify the openjdk-11 image", func() {
 			redhatOpenjdk11UBI8Project := util.GetEnvWithDefault("REDHAT_OPENJDK11_UBI8_PROJECT", "openjdk-11")
 			oc.ImportImageFromRegistry("registry.redhat.io", "ubi8/openjdk-11:latest", "java:8", redhatOpenjdk11UBI8Project)
