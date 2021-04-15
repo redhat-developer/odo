24038662e108dbfc79189afcf48ea4dcc6253575
jingfu wang <jingfwan@redhat.com>
2021-04-15 10:59:56 -0400

    Merge branch 'master' of github.com:openshift/odo into odoMigration
    Signed-off-by: jingfu wang <jingfwan@redhat.com>

--- pkg/kclient/deployments.go
+++ pkg/kclient/deployments.go
@@ -226,6 +226,9 @@ func (c *Client) CreateDynamicResource(exampleCustomResource map[string]interfac
 		Object: exampleCustomResource,
 	}
 
+	debugOut, _ := json.MarshalIndent(deployment, " ", " ")
+	klog.V(5).Infoln("Creating resource:")
+	klog.V(5).Infoln(string(debugOut))
 	// Create the dynamic resource based on the alm-example for the CRD
 	_, err := c.DynamicClient.Resource(deploymentRes).Namespace(c.Namespace).Create(deployment, metav1.CreateOptions{})
 	if err != nil {
