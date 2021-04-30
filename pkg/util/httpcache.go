67b3bc442be3cd665804efd693677c2a0e63a4f3
Girish Ramnani <girishramnani95@gmail.com>
2021-04-30 16:32:04 +0530

    invalidate the cache if there is an error (#4660)
    * invalidate the cache if there is an error
    
    * removed infinite loop and added one more retry
    
    * add comment
    
    * addressed mrinal's commit

--- pkg/util/httpcache.go
+++ pkg/util/httpcache.go
@@ -6,8 +6,30 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/devfile/library/pkg/testingutil/filesystem"
 	"k8s.io/klog"
 )
+
+// CleanDefaultHTTPCacheDir cleans the default directory used for HTTP caching
+func CleanDefaultHTTPCacheDir() error {
+	return cleanDefaultHTTPCacheDir(filesystem.DefaultFs{})
+}
+
+func cleanDefaultHTTPCacheDir(fs filesystem.Filesystem) error {
+	cacheFiles, err := fs.ReadDir(httpCacheDir)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range cacheFiles {
+		klog.V(4).Infof("Removing cache file %s", f.Name())
+		err := fs.Remove(filepath.Join(httpCacheDir, f.Name()))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 // cleanHttpCache checks cacheDir and deletes all files that were modified more than cacheTime back
 func cleanHttpCache(cacheDir string, cacheTime time.Duration) error {
---
+++ pkg/util/httpcache_test.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/devfile/library/pkg/testingutil/filesystem"
+)
+
+func TestCleanDefaultHTTPCacheDir(t *testing.T) {
+	fakeFs := filesystem.NewFakeFs()
+	filesToGenerate := 10
+	for i := 0; i < filesToGenerate; i++ {
+		err := fakeFs.WriteFile(filepath.Join(httpCacheDir, GenerateRandomString(10)), []byte(GenerateRandomString(10)), os.ModePerm)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	files, err := fakeFs.ReadDir(httpCacheDir)
+	if err != nil {
+		t.Error(err)
+	}
+	// checking the file count before the run
+	if len(files) != filesToGenerate {
+		t.Error("the file count in the httpCacheDir don't match files generated")
+	}
+	err = cleanDefaultHTTPCacheDir(fakeFs)
+	if err != nil {
+		t.Error(err)
+	}
+
+	newFiles, err := fakeFs.ReadDir(httpCacheDir)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(newFiles) != 0 {
+		t.Error("httpCacheDir is not empty after cleanup")
+	}
+
+}
