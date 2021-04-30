5f99f6cd9f2b924a518b14bcb4f435b7e68c13d1
Girish Ramnani <girishramnani95@gmail.com>
2021-04-30 14:22:28 +0530

    addressed mrinal's commit

--- pkg/util/httpcache.go
+++ pkg/util/httpcache.go
@@ -6,19 +6,24 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/devfile/library/pkg/testingutil/filesystem"
 	"k8s.io/klog"
 )
 
 // CleanDefaultHTTPCacheDir cleans the default directory used for HTTP caching
 func CleanDefaultHTTPCacheDir() error {
-	cacheFiles, err := ioutil.ReadDir(httpCacheDir)
+	return cleanDefaultHTTPCacheDir(filesystem.DefaultFs{})
+}
+
+func cleanDefaultHTTPCacheDir(fs filesystem.Filesystem) error {
+	cacheFiles, err := fs.ReadDir(httpCacheDir)
 	if err != nil {
 		return err
 	}
 
 	for _, f := range cacheFiles {
 		klog.V(4).Infof("Removing cache file %s", f.Name())
-		err := os.Remove(filepath.Join(httpCacheDir, f.Name()))
+		err := fs.Remove(filepath.Join(httpCacheDir, f.Name()))
 		if err != nil {
 			return err
 		}
