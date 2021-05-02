a71b5cf9824eb09c1a2e8ef0e204e17268fa9690
Girish Ramnani <girishramnani95@gmail.com>
2021-05-02 19:10:03 +0530

    create a catalog interface

---
+++ pkg/odo/cli/catalog/describe/operator_backend.go
@@ -0,0 +1,89 @@
+package describe
+
+import (
+	"fmt"
+
+	"github.com/openshift/odo/pkg/log"
+	"github.com/openshift/odo/pkg/machineoutput"
+	"github.com/openshift/odo/pkg/service"
+	svc "github.com/openshift/odo/pkg/service"
+	olm "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+type operatorBackend struct {
+}
+
+func NewOperatorBackend() *operatorBackend {
+	return &operatorBackend{}
+}
+
+func (ohb *operatorBackend) CompleteDescribeService(dso *DescribeServiceOptions, args []string) error {
+	oprType, CR, err := service.SplitServiceKindName(args[0])
+	if err != nil {
+		return err
+	}
+	// we check if the cluster supports ClusterServiceVersion or not.
+	isCSVSupported, err := svc.IsCSVSupported()
+	if err != nil {
+		// if there is an error checking it, we return the error.
+		return err
+	}
+	// if its not supported then we return an error
+	if !isCSVSupported {
+		return errors.New("it seems the cluster doesn't support Operators. Please install OLM and try again")
+	}
+	dso.OperatorType = oprType
+	dso.CustomResource = CR
+	return nil
+}
+
+func (ohb *operatorBackend) ValidateDescribeService(dso *DescribeServiceOptions) error {
+	var err error
+	if dso.OperatorType == "" || dso.CustomResource == "" {
+		return fmt.Errorf("invalid service name, use the format <operator-type>/<crd-name>")
+	}
+	// make sure that CSV of the specified OperatorType exists
+	dso.CSV, err = dso.KClient.GetClusterServiceVersion(dso.OperatorType)
+	if err != nil {
+		// error only occurs when OperatorHub is not installed.
+		// k8s does't have it installed by default but OCP does
+		return err
+	}
+
+	// Get the specific CR that matches "kind"
+	crs := dso.KClient.GetCustomResourcesFromCSV(&dso.CSV)
+
+	var cr *olm.CRDDescription
+	hasCR := false
+	for _, custRes := range *crs {
+		c := custRes
+		if c.Kind == dso.CustomResource {
+			cr = &c
+			hasCR = true
+			break
+		}
+	}
+	if !hasCR {
+		return fmt.Errorf("the %s resource doesn't exist in specified %s operator", dso.CustomResource, dso.OperatorType)
+	}
+
+	dso.CR = cr
+	return nil
+
+}
+
+func (ohb *operatorBackend) RunDescribeService(dso *DescribeServiceOptions) error {
+	if log.IsJSON() {
+		machineoutput.OutputSuccess(dso.CR)
+		return nil
+	}
+	output, err := yaml.Marshal(svc.ConvertCRDToRepr(dso.CR))
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(string(output))
+	return nil
+}
--- pkg/odo/cli/catalog/describe/service.go
+++ pkg/odo/cli/catalog/describe/service.go
@@ -1,19 +1,13 @@
 package describe
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/olekukonko/tablewriter"
-	"github.com/openshift/odo/pkg/log"
-	"github.com/openshift/odo/pkg/machineoutput"
 	"github.com/openshift/odo/pkg/odo/genericclioptions"
+	"github.com/openshift/odo/pkg/service"
 	svc "github.com/openshift/odo/pkg/service"
 	olm "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 	ktemplates "k8s.io/kubectl/pkg/util/templates"
 )
 
@@ -46,9 +40,9 @@ type DescribeServiceOptions struct {
 	// generic context options common to all commands
 	*genericclioptions.Context
 
+	backend CatalogProviderBackend
+
 	// Operator backed services params
-	// split the name provided on CLI and populate servicetype & customresource
-	isOperator bool
 	// the operator name
 	OperatorType   string
 	CustomResource string
@@ -63,164 +57,30 @@ func NewDescribeServiceOptions() *DescribeServiceOptions {
 
 // Complete completes DescribeServiceOptions after they've been created
 func (o *DescribeServiceOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
-
-	// if the argument contains "/" then we assume the user wants to describe a CRD.
-	if strings.Contains(args[0], "/") {
-		// we check if the cluster supports ClusterServiceVersion or not.
-		isCSVSupported, err := svc.IsCSVSupported()
-		if err != nil {
-			// if there is an error checking it, we return the error.
-			return err
-		}
-		// if its not supported then we return an error
-		if !isCSVSupported {
-			return errors.New("it seems the cluster doesn't support Operators. Please install OLM and try again")
-		}
-		tmpOptrList := strings.Split(args[0], "/")
-		o.OperatorType = tmpOptrList[0]
-		o.CustomResource = tmpOptrList[1]
-		o.isOperator = true
-	} else {
-		o.serviceName = args[0]
+	o.Context, err = genericclioptions.NewContext(cmd, true)
+	if err != nil {
+		return err
 	}
 
-	o.Context, err = genericclioptions.NewContext(cmd, true)
+	// if the argument contains "/" then we assume the user wants to describe a CRD.
+	if _, _, err := service.SplitServiceKindName(args[0]); err != nil {
+		o.backend = NewOperatorBackend()
+	} else {
+		o.backend = NewServiceCatalogBackend()
+	}
 
-	return
+	return o.backend.CompleteDescribeService(o, args)
 }
 
 // Validate validates the DescribeServiceOptions based on completed values
 func (o *DescribeServiceOptions) Validate() (err error) {
-
-	if o.isOperator {
-
-		if o.OperatorType == "" || o.CustomResource == "" {
-			return fmt.Errorf("invalid service name, use the format <operator-type>/<crd-name>")
-		}
-		// make sure that CSV of the specified OperatorType exists
-		o.CSV, err = o.KClient.GetClusterServiceVersion(o.OperatorType)
-		if err != nil {
-			// error only occurs when OperatorHub is not installed.
-			// k8s does't have it installed by default but OCP does
-			return err
-		}
-
-		// Get the specific CR that matches "kind"
-		crs := o.KClient.GetCustomResourcesFromCSV(&o.CSV)
-
-		var cr *olm.CRDDescription
-		hasCR := false
-		for _, custRes := range *crs {
-			c := custRes
-			if c.Kind == o.CustomResource {
-				cr = &c
-				hasCR = true
-				break
-			}
-		}
-		if !hasCR {
-			return fmt.Errorf("the %s resource doesn't exist in specified %s operator", o.CustomResource, o.OperatorType)
-		}
-
-		o.CR = cr
-		return nil
-	}
-	o.service, o.plans, err = svc.GetServiceClassAndPlans(o.Client, o.serviceName)
-	return err
+	return o.backend.ValidateDescribeService(o)
 
 }
 
 // Run contains the logic for the command associated with DescribeServiceOptions
 func (o *DescribeServiceOptions) Run() (err error) {
-	if o.isOperator {
-		return o.operatorRun()
-	}
-	return o.serviceCatalogRun()
-
-}
-func (o *DescribeServiceOptions) operatorRun() (err error) {
-	if log.IsJSON() {
-		machineoutput.OutputSuccess(o.CR)
-		return
-	}
-	output, err := yaml.Marshal(svc.ConvertCRDToRepr(o.CR))
-	if err != nil {
-		return err
-	}
-
-	fmt.Print(string(output))
-	return nil
-}
-
-func (o *DescribeServiceOptions) serviceCatalogRun() (err error) {
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetBorder(false)
-	table.SetAlignment(tablewriter.ALIGN_LEFT)
-
-	serviceData := [][]string{
-		{"Name", o.service.Name},
-		{"Bindable", fmt.Sprint(o.service.Bindable)},
-		{"Operated by the broker", o.service.ServiceBrokerName},
-		{"Short Description", o.service.ShortDescription},
-		{"Long Description", o.service.LongDescription},
-		{"Versions Available", strings.Join(o.service.VersionsAvailable, ",")},
-		{"Tags", strings.Join(o.service.Tags, ",")},
-	}
-
-	table.AppendBulk(serviceData)
-
-	table.Append([]string{""})
-
-	if len(o.plans) > 0 {
-		table.Append([]string{"PLANS"})
-
-		for _, plan := range o.plans {
-
-			// create the display values for required  and optional parameters
-			requiredWithMandatoryUserInputParameterNames := []string{}
-			requiredWithOptionalUserInputParameterNames := []string{}
-			optionalParameterDisplay := []string{}
-			for _, parameter := range plan.Parameters {
-				if parameter.Required {
-					// until we have a better solution for displaying the plan data (like a separate table perhaps)
-					// this is simplest thing to do
-					if len(parameter.Default) > 0 {
-						requiredWithOptionalUserInputParameterNames = append(
-							requiredWithOptionalUserInputParameterNames,
-							fmt.Sprintf("%s (default: '%s')", parameter.Name, parameter.Default))
-					} else {
-						requiredWithMandatoryUserInputParameterNames = append(requiredWithMandatoryUserInputParameterNames, parameter.Name)
-					}
-
-				} else {
-					optionalParameterDisplay = append(optionalParameterDisplay, parameter.Name)
-				}
-			}
-
-			table.Append([]string{"***********************", "*****************************************************"})
-			planLineSeparator := []string{"-----------------", "-----------------"}
-
-			planData := [][]string{
-				{"Name", plan.Name},
-				planLineSeparator,
-				{"Display Name", plan.DisplayName},
-				planLineSeparator,
-				{"Short Description", plan.Description},
-				planLineSeparator,
-				{"Required Params without a default value", strings.Join(requiredWithMandatoryUserInputParameterNames, ", ")},
-				planLineSeparator,
-				{"Required Params with a default value", strings.Join(requiredWithOptionalUserInputParameterNames, ", ")},
-				planLineSeparator,
-				{"Optional Params", strings.Join(optionalParameterDisplay, ", ")},
-				{"", ""},
-			}
-			table.AppendBulk(planData)
-		}
-		table.Render()
-	} else {
-		return fmt.Errorf("no plans found for service %s", o.serviceName)
-	}
-	return
+	return o.backend.RunDescribeService(o)
 }
 
 // NewCmdCatalogDescribeService implements the odo catalog describe service command
