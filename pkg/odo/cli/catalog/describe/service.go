bad22e0531a8ef281ac81ed2e77b5f809bae27bc
Girish Ramnani <girishramnani95@gmail.com>
2021-05-05 09:51:14 +0530

    odo catalog describe service for operators (#4671)
    * init
    
    * working json output
    
    * add docs in long description
    
    * add human readable output
    
    * add tests
    
    * addressed comments
    
    * create a catalog interface
    
    * fix bug
    
    * add typemeta and objectmeta
    
    * Update pkg/odo/cli/catalog/describe/operator_backend.go
    
    Co-authored-by: Dharmit Shah <shahdharmit@gmail.com>
    
    * addressed comments
    
    Co-authored-by: Dharmit Shah <shahdharmit@gmail.com>

--- pkg/odo/cli/catalog/describe/service.go
+++ pkg/odo/cli/catalog/describe/service.go
@@ -2,11 +2,9 @@ package describe
 
 import (
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/olekukonko/tablewriter"
 	"github.com/openshift/odo/pkg/odo/genericclioptions"
+	"github.com/openshift/odo/pkg/service"
 	svc "github.com/openshift/odo/pkg/service"
 	"github.com/spf13/cobra"
 	ktemplates "k8s.io/kubectl/pkg/util/templates"
@@ -15,12 +13,19 @@ import (
 const serviceRecommendedCommandName = "service"
 
 var (
-	serviceExample = ktemplates.Examples(`  # Describe a service
-    %[1]s mysql-persistent`)
+	serviceExample = ktemplates.Examples(`  # Describe a service catalog service
+    %[1]s mysql-persistent
+	
+	# Describe a Operator backed service
+	%[1]s 
+	`)
 
-	serviceLongDesc = ktemplates.LongDesc(`Describe a service type.
+	serviceLongDesc = ktemplates.LongDesc(`Describes a service type.
+	This command supports both Service Catalog services and Operator backed services.
+	A user can describe an Operator backed service by providing the full identifier for an Operand i.e. <operator_type>/<cr_name> which they can find by running "odo catalog list services".
 
-This describes the service and the associated plans.
+	If the format doesn't match <operator_type>/<cr_name> then service catalog services would be searched.  
+
 `)
 )
 
@@ -33,6 +38,8 @@ type DescribeServiceOptions struct {
 	plans   []svc.ServicePlan
 	// generic context options common to all commands
 	*genericclioptions.Context
+
+	backend CatalogProviderBackend
 }
 
 // NewDescribeServiceOptions creates a new DescribeServiceOptions instance
@@ -43,98 +50,41 @@ func NewDescribeServiceOptions() *DescribeServiceOptions {
 // Complete completes DescribeServiceOptions after they've been created
 func (o *DescribeServiceOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
 	o.Context, err = genericclioptions.NewContext(cmd, true)
-	o.serviceName = args[0]
+	if err != nil {
+		return err
+	}
 
-	return
+	// if the argument contains "/" then we assume the user wants to describe a CRD.
+	if _, _, err := service.SplitServiceKindName(args[0]); err == nil {
+		o.backend = NewOperatorBackend()
+	} else {
+		o.backend = NewServiceCatalogBackend()
+	}
+
+	return o.backend.CompleteDescribeService(o, args)
 }
 
 // Validate validates the DescribeServiceOptions based on completed values
 func (o *DescribeServiceOptions) Validate() (err error) {
-	o.service, o.plans, err = svc.GetServiceClassAndPlans(o.Client, o.serviceName)
-	return err
+	return o.backend.ValidateDescribeService(o)
+
 }
 
 // Run contains the logic for the command associated with DescribeServiceOptions
 func (o *DescribeServiceOptions) Run() (err error) {
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetBorder(false)
-	table.SetAlignment(tablewriter.ALIGN_LEFT)
-
-	serviceData := [][]string{
-		{"Name", o.service.Name},
-		{"Bindable", fmt.Sprint(o.service.Bindable)},
-		{"Operated by the broker", o.service.ServiceBrokerName},
-		{"Short Description", o.service.ShortDescription},
-		{"Long Description", o.service.LongDescription},
-		{"Versions Available", strings.Join(o.service.VersionsAvailable, ",")},
-		{"Tags", strings.Join(o.service.Tags, ",")},
-	}
-
-	table.AppendBulk(serviceData)
-
-	table.Append([]string{""})
-
-	if len(o.plans) > 0 {
-		table.Append([]string{"PLANS"})
-
-		for _, plan := range o.plans {
-
-			// create the display values for required  and optional parameters
-			requiredWithMandatoryUserInputParameterNames := []string{}
-			requiredWithOptionalUserInputParameterNames := []string{}
-			optionalParameterDisplay := []string{}
-			for _, parameter := range plan.Parameters {
-				if parameter.Required {
-					// until we have a better solution for displaying the plan data (like a separate table perhaps)
-					// this is simplest thing to do
-					if len(parameter.Default) > 0 {
-						requiredWithOptionalUserInputParameterNames = append(
-							requiredWithOptionalUserInputParameterNames,
-							fmt.Sprintf("%s (default: '%s')", parameter.Name, parameter.Default))
-					} else {
-						requiredWithMandatoryUserInputParameterNames = append(requiredWithMandatoryUserInputParameterNames, parameter.Name)
-					}
-
-				} else {
-					optionalParameterDisplay = append(optionalParameterDisplay, parameter.Name)
-				}
-			}
-
-			table.Append([]string{"***********************", "*****************************************************"})
-			planLineSeparator := []string{"-----------------", "-----------------"}
-
-			planData := [][]string{
-				{"Name", plan.Name},
-				planLineSeparator,
-				{"Display Name", plan.DisplayName},
-				planLineSeparator,
-				{"Short Description", plan.Description},
-				planLineSeparator,
-				{"Required Params without a default value", strings.Join(requiredWithMandatoryUserInputParameterNames, ", ")},
-				planLineSeparator,
-				{"Required Params with a default value", strings.Join(requiredWithOptionalUserInputParameterNames, ", ")},
-				planLineSeparator,
-				{"Optional Params", strings.Join(optionalParameterDisplay, ", ")},
-				{"", ""},
-			}
-			table.AppendBulk(planData)
-		}
-		table.Render()
-	} else {
-		return fmt.Errorf("no plans found for service %s", o.serviceName)
-	}
-	return
+	return o.backend.RunDescribeService(o)
 }
 
 // NewCmdCatalogDescribeService implements the odo catalog describe service command
 func NewCmdCatalogDescribeService(name, fullName string) *cobra.Command {
 	o := NewDescribeServiceOptions()
 	command := &cobra.Command{
-		Use:     name,
-		Short:   "Describe a service",
-		Long:    serviceLongDesc,
-		Example: fmt.Sprintf(serviceExample, fullName),
-		Args:    cobra.ExactArgs(1),
+		Use:         name,
+		Short:       "Describe a service",
+		Long:        serviceLongDesc,
+		Example:     fmt.Sprintf(serviceExample, fullName),
+		Annotations: map[string]string{"machineoutput": "json"},
+		Args:        cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			genericclioptions.GenericRun(o, cmd, args)
 		},
