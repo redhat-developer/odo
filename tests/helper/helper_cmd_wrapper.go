99ea4de291be09d87f842751207b6787a66c8f9d
anandrkskd <anandrkskd@gmail.com>
2021-05-10 14:56:44 +0530

    Adding helper wrapper functions
    Adding WithTimeout, WithTerminate, ShouldFail, WithRetry wrapper functions
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- tests/helper/helper_cmd_wrapper.go
+++ tests/helper/helper_cmd_wrapper.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os/exec"
 	"path/filepath"
+	"runtime"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,11 +13,15 @@ import (
 )
 
 type CmdWrapper struct {
-	Cmd     *exec.Cmd
-	program string
-	args    []string
-	writer  *gexec.PrefixedWriter
-	session *gexec.Session
+	Cmd      *exec.Cmd
+	program  string
+	args     []string
+	writer   *gexec.PrefixedWriter
+	session  *gexec.Session
+	timeout  time.Duration
+	stopChan chan bool
+	err      error
+	maxRetry int
 }
 
 func Cmd(program string, args ...string) *CmdWrapper {
@@ -30,11 +36,73 @@ func Cmd(program string, args ...string) *CmdWrapper {
 	}
 }
 
-func (cw *CmdWrapper) ShouldPass() *CmdWrapper {
+func (cw *CmdWrapper) Runner() *CmdWrapper {
 	fmt.Fprintln(GinkgoWriter, runningCmd(cw.Cmd))
-	session, err := gexec.Start(cw.Cmd, cw.writer, cw.writer)
-	Expect(err).NotTo(HaveOccurred())
-	cw.session = session
+	cw.session, cw.err = gexec.Start(cw.Cmd, cw.writer, cw.writer)
+	timeout := time.After(cw.timeout)
+	if cw.timeout > 0 {
+		select {
+		case <-cw.stopChan:
+			if cw.session != nil {
+				if runtime.GOOS == "windows" {
+					cw.session.Kill()
+				} else {
+					cw.session.Terminate()
+				}
+			}
+		case <-timeout:
+			if cw.session != nil {
+				if runtime.GOOS == "windows" {
+					cw.session.Kill()
+				} else {
+					cw.session.Terminate()
+				}
+			}
+		}
+	}
+	return cw
+}
+
+func (cw *CmdWrapper) WithRetry(maxRetry int, intervalSeconds time.Duration) string {
+	for i := 0; i < cw.maxRetry; i++ {
+		fmt.Fprintf(GinkgoWriter, "try %d of %d\n", i, cw.maxRetry)
+
+		cw.Runner()
+		cw.session.Wait()
+		// if exit code is 0 which means the program succeeded and hence we retry
+		if cw.session.ExitCode() == 0 {
+			time.Sleep(time.Duration(intervalSeconds) * time.Second)
+		} else {
+			Consistently(cw.session).ShouldNot(gexec.Exit(0), runningCmd(cw.session.Command))
+			return string(cw.session.Err.Contents())
+		}
+	}
+	Fail(fmt.Sprintf("Failed after %d retries", maxRetry))
+	return ""
+}
+
+func (cw *CmdWrapper) ShouldPass() *CmdWrapper {
+	cw.Runner()
+	Expect(cw.err).NotTo(HaveOccurred())
+	Eventually(cw.session).Should(gexec.Exit(0), runningCmd(cw.session.Command))
+	return cw
+}
+
+func (cw *CmdWrapper) ShouldFail() *CmdWrapper {
+	cw.Runner()
+	Expect(cw.err).To(HaveOccurred())
+	Consistently(cw.session).ShouldNot(gexec.Exit(0), runningCmd(cw.session.Command))
+	return cw
+}
+
+func (cw *CmdWrapper) WithTerminate(timeoutAfter time.Duration, stop <-chan bool) *CmdWrapper {
+	cw.timeout = time.Duration(timeoutAfter) * time.Second
+	cw.stopChan <- <-stop
+	return cw
+}
+
+func (cw *CmdWrapper) WithTimeout(timeoutAfter time.Duration) *CmdWrapper {
+	cw.timeout = time.Duration(timeoutAfter) * time.Second
 	return cw
 }
 
