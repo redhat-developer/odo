4144b0a3c4528411559791b3706193e9f165fa94
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-05-18 15:57:22 +0530

    Moving declarations to just before each
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- tests/integration/devfile/cmd_devfile_registry_test.go
+++ tests/integration/devfile/cmd_devfile_registry_test.go
@@ -86,9 +86,13 @@ var _ = Describe("odo devfile registry command tests", func() {
 	})
 
 	Context("when working with git based registries", func() {
+		var deprecated, docLink string
+		JustBeforeEach(func() {
+			deprecated = "Deprecated"
+			docLink = "https://github.com/openshift/odo/tree/main/docs/public/git-registry-deprecation.adoc"
+		})
 		It("should show deprecation warning when the git based registry is used", func() {
-			deprecated := "Deprecated"
-			docLink := "https://github.com/openshift/odo/tree/main/docs/public/git-registry-deprecation.adoc"
+
 			outstr, errstr := helper.CmdShouldPassIncludeErrStream("odo", "registry", "add", "RegistryFromGitHub", "https://github.com/odo-devfiles/registry")
 			co := fmt.Sprintln(outstr, errstr)
 			helper.MatchAllInOutput(co, []string{deprecated, docLink})
@@ -100,13 +104,11 @@ var _ = Describe("odo devfile registry command tests", func() {
 			helper.MatchAllInOutput(co, []string{deprecated, docLink})
 		})
 		It("should not show deprecation warning if non-git-based registry is used", func() {
-			deprecated := "Deprecated"
-			docLink := "https://github.com/openshift/odo/tree/main/docs/public/git-registry-deprecation.adoc"
-			_, err := helper.CmdShouldPassIncludeErrStream("odo", "registry", "list")
-			helper.DontMatchAllInOutput(err, []string{deprecated, docLink})
+			out, err := helper.CmdShouldPassIncludeErrStream("odo", "registry", "list")
+			helper.DontMatchAllInOutput(fmt.Sprintln(out, err), []string{deprecated, docLink})
 			helper.CmdShouldPass("odo", "registry", "add", "RegistryFromGitHub", "https://github.com/odo-devfiles/registry")
-			_, err = helper.CmdShouldPassIncludeErrStream("odo", "create", "nodejs", "--registry", "DefaultDevfileRegistry")
-			helper.DontMatchAllInOutput(err, []string{deprecated, docLink})
+			out, err = helper.CmdShouldPassIncludeErrStream("odo", "create", "nodejs", "--registry", "DefaultDevfileRegistry")
+			helper.DontMatchAllInOutput(fmt.Sprintln(out, err), []string{deprecated, docLink})
 		})
 	})
 })
