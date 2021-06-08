9b88443f81fd3a1fd63a392d6252ad40130645f8
Parthvi Vala <pvala@redhat.com>
2021-06-08 12:43:49 +0530

    Move tests

--- tests/integration/cmd_cmp_test.go
+++ tests/integration/cmd_cmp_test.go
@@ -3,11 +3,73 @@
 package integration
 
 import (
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/openshift/odo/pkg/application/labels"
+	"github.com/openshift/odo/pkg/odo/genericclioptions"
+	"github.com/openshift/odo/tests/helper"
 )
 
 func componentTestsNoSub() {
 	componentTests()
+	Context("when components are not created/managed by odo", func() {
+		var commonVar helper.CommonVar
+
+		JustBeforeEach(func() {
+			/*
+				Copy the deployment-label.yaml and dc-label.yaml file.
+				Create a deployment with deployment-label.yaml.
+				Create a dc with dc-label.yaml.
+			*/
+			commonVar = helper.CommonBeforeEach()
+			helper.CopyManifestFile("dc-label.yaml", filepath.Join(commonVar.Context, "dc-label.yaml"))
+			helper.CopyManifestFile("deployment-label.yaml", filepath.Join(commonVar.Context, "deployment-label.yaml"))
+			helper.CmdShouldPass("oc", "apply", "-f", "dc-label.yaml")
+			helper.CmdShouldPass("oc", "apply", "-f", "deployment-label.yaml")
+		})
+		JustAfterEach(func() {
+			//DeleteNonOdoComponent
+			client, _ := genericclioptions.Client()
+			label := map[string]string{
+				labels.ManagedBy: "!odo",
+			}
+			Expect(client.Delete(label, true)).To(BeNil())
+			Expect(client.GetKubeClient().DeleteDeployment(label)).To(BeNil())
+			helper.CommonAfterEach(commonVar)
+		})
+		FIt("should list the components", func() {
+			output := helper.CmdShouldPass("odo", "list")
+			Expect(output).To(ContainSubstring("Other Components running on the cluster(read-only)"))
+		})
+		It("should list the components with --all-apps flag", func() {
+			output := helper.CmdShouldPass("odo", "list", "--all-apps")
+			Expect(output).To(ContainSubstring("Other Components running on the cluster(read-only)"))
+
+		})
+		It("should list the components with --app flag", func() {
+			output := helper.CmdShouldPass("odo", "list", "--app", "httpd")
+			Expect(output).To(ContainSubstring("Other Components running on the cluster(read-only)"))
+		})
+		It("should list the components in json format with -o json flag", func() {
+			output := helper.CmdShouldPass("odo", "list", "-o", "json")
+			Expect(output).To(ContainSubstring("Other Components running on the cluster(read-only)"))
+		})
+		When("executing odo list from other project", func() {
+			JustBeforeEach(func() {
+				helper.CmdShouldPass("odo", "project", "set", "default")
+			})
+			JustAfterEach(func() {
+				helper.CmdShouldPass("odo", "project", "set", commonVar.Project)
+			})
+			It("should list the components with --project flag", func() {
+				output := helper.CmdShouldPass("odo", "list", "--project", commonVar.Project)
+				Expect(output).To(ContainSubstring("Other Components running on the cluster(read-only)"))
+			})
+
+		})
+	})
 }
 
 var _ = Describe("odo component command tests", componentTestsNoSub)
--- tests/integration/component.go
+++ tests/integration/component.go
@@ -1,7 +1,6 @@
 package integration
 
 import (
-	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,16 +8,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"time"
-
-	"github.com/openshift/odo/pkg/occlient"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	componentlabels "github.com/openshift/odo/pkg/component/labels"
-
-	"github.com/devfile/library/pkg/devfile/generator"
-	applabels "github.com/openshift/odo/pkg/application/labels"
-	"github.com/openshift/odo/pkg/kclient"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -169,62 +158,6 @@ func componentTests(args ...string) {
 			cmpList := helper.CmdShouldPass("odo", append(args, "list", "--context", commonVar.Context)...)
 			helper.MatchAllInOutput(cmpList, []string{"cmp-git", "Not Pushed"})
 			helper.CmdShouldPass("odo", append(args, "delete", "-f", "--all", "--context", commonVar.Context)...)
-		})
-		Context("when components are not created/managed by odo", func() {
-			var appName, compName string
-			var kc *kclient.Client
-			var c *occlient.Client
-			var err error
-			var labels map[string]string
-			JustBeforeEach(func() {
-				compName = "nodejs-ex"
-				application := "sample-app"
-				appName = application
-				labels = map[string]string{
-					applabels.ApplicationLabel:         application,
-					applabels.App:                      application,
-					applabels.ManagerVersion:           "v4.7",
-					applabels.ManagedBy:                "console",
-					"component":                        compName,
-					componentlabels.ComponentTypeLabel: "smh",
-				}
-				selectorLabels := map[string]string{
-					"component": compName,
-				}
-				objectMeta := generator.GetObjectMeta(compName, commonVar.Project, labels, nil)
-				deployParams := generator.DeploymentParams{
-					TypeMeta:          generator.GetTypeMeta(kclient.DeploymentKind, kclient.DeploymentAPIVersion),
-					ObjectMeta:        objectMeta,
-					PodSelectorLabels: selectorLabels,
-				}
-
-				deploy := generator.GetDeployment(deployParams)
-				c, err = occlient.New()
-				kc = c.GetKubeClient()
-				if err != nil {
-					return
-				}
-				_, err = kc.KubeClient.AppsV1().Deployments(c.Namespace).Create(context.TODO(), deploy, metav1.CreateOptions{FieldManager: "console"})
-				if err != nil {
-					return
-				}
-				//CreateNonOdoComponent
-			})
-			JustAfterEach(func() {
-
-				//DeleteNonOdoComponent
-				kc.DeleteDeployment(labels)
-			})
-			It("should list the component with --app flag", func() {
-				output := helper.CmdShouldPass("odo", "list", "--app", appName)
-				Expect(output).To(ContainSubstring(compName))
-				Expect(output).To(ContainSubstring(appName))
-			})
-			It("should list the component with --all-apps flag", func() {
-				output := helper.CmdShouldPass("odo", "list", "--all-apps")
-				Expect(output).To(ContainSubstring(compName))
-				Expect(output).To(ContainSubstring(appName))
-			})
 		})
 
 		// It("should list the state as unknown for disconnected cluster", func() {
