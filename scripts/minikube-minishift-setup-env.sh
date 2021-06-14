8258a5f009563c13c0ecca1dfbd3ccf171c3d02b
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-06-14 18:10:44 +0530

    Using homedir
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/minikube-minishift-setup-env.sh
+++ scripts/minikube-minishift-setup-env.sh
@@ -16,7 +16,7 @@ export GOBIN="$GOPATH/bin"
 # Set kubeconfig to current dir. This ensures no clashes with other test runs
 export KUBECONFIG="`pwd`/config"
 export ARTIFACTS_DIR="`pwd`/artifacts"
-export CUSTOM_HOMEDIR=$ARTIFACT_DIR
+export HOME="`pwd`/home"
 
 # This si one of the variables injected by ci-firewall. Its purpose is to allow scripts to handle uniqueness as needed
 SCRIPT_IDENTITY=${SCRIPT_IDENTITY:-"def-id"}
