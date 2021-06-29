bb2a2e4d7fa8da494d23c308111f7b8e48c2a42a
anandrkskd <anandrkskd@gmail.com>
2021-06-29 14:24:27 +0530

    refactor cmd-storage
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- tests/integration/cmd_storage_test.go
+++ tests/integration/cmd_storage_test.go
@@ -23,56 +23,95 @@ var _ = Describe("odo storage command tests", func() {
 		helper.CommonAfterEach(commonVar)
 	})
 
-	Context("when running help for storage command", func() {
-		It("should display the help", func() {
-			appHelp := helper.Cmd("odo", "storage", "-h").ShouldPass().Out()
-			Expect(appHelp).To(ContainSubstring("Perform storage operations"))
-		})
+	It("should display the help for storage command", func() {
+		appHelp := helper.Cmd("odo", "storage", "-h").ShouldPass().Out()
+		Expect(appHelp).To(ContainSubstring("Perform storage operations"))
 	})
 
-	Context("when using storage command with -o json", func() {
-		It("should create and list output in json format", func() {
+	When("creating a new component", func() {
+
+		BeforeEach(func() {
 			helper.CopyExample(filepath.Join("source", "wildfly"), commonVar.Context)
 			helper.Cmd("odo", "component", "create", "--s2i", "wildfly", "wildfly", "--app", "wildflyapp", "--project", commonVar.Project, "--context", commonVar.Context).ShouldPass()
-			actualJSONStorage := helper.Cmd("odo", "storage", "create", "mystorage", "--path=/opt/app-root/src/storage/", "--size=1Gi", "--context", commonVar.Context, "-o", "json").ShouldPass().Out()
-
-			valuesStoreC := gjson.GetMany(actualJSONStorage, "kind", "metadata.name", "spec.size", "spec.path")
-			expectedStoreC := []string{"storage", "mystorage", "1Gi", "/opt/app-root/src/storage/"}
-			Expect(helper.GjsonMatcher(valuesStoreC, expectedStoreC)).To(Equal(true))
-
-			actualStorageList := helper.Cmd("odo", "storage", "list", "--context", commonVar.Context, "-o", "json").ShouldPass().Out()
-
-			valuesStoreL := gjson.GetMany(actualStorageList, "kind", "items.0.kind", "items.0.metadata.name", "items.0.spec.size")
-			expectedStoreL := []string{"List", "storage", "mystorage", "1Gi"}
-			Expect(helper.GjsonMatcher(valuesStoreL, expectedStoreL)).To(Equal(true))
-
-			helper.Cmd("odo", "storage", "delete", "mystorage", "--context", commonVar.Context, "-f").ShouldPass()
 		})
+
+		When("creating storage", func() {
+
+			actualJSONStorage := ""
+
+			BeforeEach(func() {
+				actualJSONStorage = helper.Cmd("odo", "storage", "create", "mystorage", "--path=/opt/app-root/src/storage/", "--size=1Gi", "--context", commonVar.Context, "-o", "json").ShouldPass().Out()
+			})
+
+			AfterEach(func() {
+				helper.Cmd("odo", "storage", "delete", "mystorage", "--context", commonVar.Context, "-f").ShouldPass()
+			})
+
+			It("should create", func() {
+				valuesStoreC := gjson.GetMany(actualJSONStorage, "kind", "metadata.name", "spec.size", "spec.path")
+				expectedStoreC := []string{"storage", "mystorage", "1Gi", "/opt/app-root/src/storage/"}
+				Expect(helper.GjsonMatcher(valuesStoreC, expectedStoreC)).To(Equal(true))
+
+			})
+
+			When("listing storage in json", func() {
+
+				actualStorageList := ""
+
+				BeforeEach(func() {
+					actualStorageList = helper.Cmd("odo", "storage", "list", "--context", commonVar.Context, "-o", "json").ShouldPass().Out()
+				})
+
+				It("should list output in json format", func() {
+					valuesStoreL := gjson.GetMany(actualStorageList, "kind", "items.0.kind", "items.0.metadata.name", "items.0.spec.size")
+					expectedStoreL := []string{"List", "storage", "mystorage", "1Gi"}
+					Expect(helper.GjsonMatcher(valuesStoreL, expectedStoreL)).To(Equal(true))
+
+				})
+			})
+		})
+
 	})
 
-	Context("when running storage list command to check state", func() {
-		It("should list storage with correct state", func() {
+	When("component is created and pushed", func() {
 
+		BeforeEach(func() {
 			helper.CopyExample(filepath.Join("source", "nodejs"), commonVar.Context)
 			helper.Cmd("odo", "component", "create", "--s2i", "nodejs", "nodejs", "--app", "nodeapp", "--project", commonVar.Project, "--context", commonVar.Context).ShouldPass()
 			helper.Cmd("odo", "push", "--context", commonVar.Context).ShouldPass()
+		})
 
-			// create storage, list storage should have state "Not Pushed"
-			helper.Cmd("odo", "storage", "create", "pv1", "--path=/tmp1", "--size=1Gi", "--context", commonVar.Context).ShouldPass()
-			StorageList := helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
-			Expect(StorageList).To(ContainSubstring("Not Pushed"))
+		When("creating storage", func() {
+			BeforeEach(func() {
+				// create storage, list storage should have state "Not Pushed"
+				helper.Cmd("odo", "storage", "create", "pv1", "--path=/tmp1", "--size=1Gi", "--context", commonVar.Context).ShouldPass()
+			})
 
-			// Push storage, list storage should have state "Pushed"
-			helper.Cmd("odo", "push", "--context", commonVar.Context).ShouldPass()
-			StorageList = helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
-			Expect(StorageList).To(ContainSubstring("Pushed"))
+			It("should list storage as Not Pushed", func() {
 
-			// Delete storage, list storage should have state "Locally Deleted"
-			helper.Cmd("odo", "storage", "delete", "pv1", "-f", "--context", commonVar.Context).ShouldPass()
-			StorageList = helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
-			Expect(StorageList).To(ContainSubstring("Locally Deleted"))
+				StorageList := helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
+				Expect(StorageList).To(ContainSubstring("Not Pushed"))
+			})
+			When("storage is pushed", func() {
+				// Push storage, list storage should have state "Pushed"
+				helper.Cmd("odo", "push", "--context", commonVar.Context).ShouldPass()
+				It("should have state push", func() {
 
+					StorageList := helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
+					Expect(StorageList).To(ContainSubstring("Pushed"))
+
+				})
+				When("storage is deleted", func() {
+					BeforeEach(func() {
+						helper.Cmd("odo", "storage", "delete", "pv1", "-f", "--context", commonVar.Context).ShouldPass()
+					})
+					It("should have state Locally Deleted", func() {
+						// Delete storage, list storage should have state "Locally Deleted"
+						StorageList := helper.Cmd("odo", "storage", "list", "--context", commonVar.Context).ShouldPass().Out()
+						Expect(StorageList).To(ContainSubstring("Locally Deleted"))
+					})
+				})
+			})
 		})
 	})
-
 })
