ed251b34f3c86ebf4962766918807b5154528650
Tomas Kral <tkral@redhat.com>
2021-06-30 16:23:24 +0200

    format service parameters as a table (#4812)
    * format sevice parameters as a table
    
    * fix errors after rebase

--- pkg/odo/cli/catalog/describe/operator_backend.go
+++ pkg/odo/cli/catalog/describe/operator_backend.go
@@ -3,11 +3,12 @@ package describe
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/openshift/odo/pkg/log"
 	"github.com/openshift/odo/pkg/machineoutput"
 	"github.com/openshift/odo/pkg/service"
-	svc "github.com/openshift/odo/pkg/service"
 	olm "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
@@ -31,7 +32,7 @@ func (ohb *operatorBackend) CompleteDescribeService(dso *DescribeServiceOptions,
 		return err
 	}
 	// we check if the cluster supports ClusterServiceVersion or not.
-	isCSVSupported, err := svc.IsCSVSupported()
+	isCSVSupported, err := service.IsCSVSupported()
 	if err != nil {
 		// if there is an error checking it, we return the error.
 		return err
@@ -83,7 +84,7 @@ func (ohb *operatorBackend) ValidateDescribeService(dso *DescribeServiceOptions)
 func (ohb *operatorBackend) RunDescribeService(dso *DescribeServiceOptions) error {
 
 	if dso.isExample {
-		almExample, err := svc.GetAlmExample(ohb.CSV, ohb.CustomResource, ohb.OperatorType)
+		almExample, err := service.GetAlmExample(ohb.CSV, ohb.CustomResource, ohb.OperatorType)
 		if err != nil {
 			return err
 		}
@@ -108,14 +109,29 @@ func (ohb *operatorBackend) RunDescribeService(dso *DescribeServiceOptions) erro
 	}
 
 	if log.IsJSON() {
-		machineoutput.OutputSuccess(svc.ConvertCRDToJSONRepr(ohb.CR))
+		machineoutput.OutputSuccess(service.ConvertCRDToJSONRepr(ohb.CR))
 		return nil
 	}
-	output, err := yaml.Marshal(svc.ConvertCRDToRepr(ohb.CR))
-	if err != nil {
-		return err
-	}
+	repr := service.ConvertCRDToRepr(ohb.CR)
 
-	fmt.Print(string(output))
+	fmt.Printf("Kind: %s\n", repr.Kind)
+	fmt.Printf("Version: %s\n", repr.Version)
+	fmt.Printf("Description: %s\n", repr.Description)
+	fmt.Println("Parameters:")
+
+	tableString := &strings.Builder{}
+	table := tablewriter.NewWriter(tableString)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetCenterSeparator("")
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+
+	table.SetHeader([]string{"Path", "DisplayName", "Description"})
+	for _, params := range repr.Parameters {
+		table.Append([]string{params.Path, params.DisplayName, params.Description})
+	}
+	table.Render()
+	fmt.Print(tableString)
 	return nil
 }
