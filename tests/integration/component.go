7d58b7d23789d7ab83474c09bd61d1d08ca5459f
Anand Kumar Singh <anandrkskd@gmail.com>
2021-07-09 13:28:46 +0530

    Update tests/integration/component.go
    Co-authored-by: Parthvi Vala <pvala@redhat.com>

--- tests/integration/component.go
+++ tests/integration/component.go
@@ -390,7 +390,7 @@ func componentTests(args ...string) {
 					binaryFilePath = filepath.Join("/private", binaryFilePath)
 				}
 				helper.Cmd("odo", append(args, "create", "--s2i", "java:8", cmpName, "--project",
-					commonVar.Project, "--binary", filepath.Join(binaryFilePath, "sb.jar"))...).ShouldPass()
+					commonVar.Project, "--binary", binaryFilePath...).ShouldPass()
 				info := helper.LocalEnvInfo(commonVar.Context)
 				Expect(info.GetName(), cmpName)
 			})
