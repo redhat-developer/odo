83c6a14416850132fa7d6e7a5428a27b0729702e
Parthvi Vala <pvala@redhat.com>
2021-07-14 22:52:05 +0530

    Refactor tests

--- tests/integration/cmd_link_unlink_test.go
+++ tests/integration/cmd_link_unlink_test.go
@@ -11,84 +11,79 @@ import (
 
 var _ = Describe("odo link and unlink command tests", func() {
 
-	var frontendContext, backendContext string
-	var oc helper.OcRunner
-
 	var commonVar helper.CommonVar
 
-	// This is run before every Spec (It)
 	var _ = BeforeEach(func() {
-		oc = helper.NewOcRunner("oc")
 		commonVar = helper.CommonBeforeEach()
 	})
 
-	// Clean up after the test
-	// This is run after every Spec (It)
 	var _ = AfterEach(func() {
 		helper.CommonAfterEach(commonVar)
 	})
 
-	Context("when running help for link and unlink command", func() {
-		It("should display the help", func() {
+	Context("Running the help command", func() {
+		By("for link command", func() {
 			appHelp := helper.Cmd("odo", "link", "-h").ShouldPass().Out()
-			helper.MatchAllInOutput(appHelp, []string{"Link component to a service ", "backed by an Operator or Service Catalog", "or component"})
-			appHelp = helper.Cmd("odo", "unlink", "-h").ShouldPass().Out()
+			helper.MatchAllInOutput(appHelp, []string{"Link component to a service ", "backed by an Operator or Service Catalog", "or component", "works only with s2i components"})
+		})
+		By("for unlink command", func() {
+			appHelp := helper.Cmd("odo", "unlink", "-h").ShouldPass().Out()
 			Expect(appHelp).To(ContainSubstring("Unlink component or service from a component"))
 		})
 	})
 
-	Context("When handling link/unlink between components", func() {
+	When("Two components are deployed", func() {
+		var frontendContext, backendContext, frontendURL string
+		var oc helper.OcRunner
+
 		JustBeforeEach(func() {
+			oc = helper.NewOcRunner("oc")
 			frontendContext = helper.CreateNewContext()
 			backendContext = helper.CreateNewContext()
+			helper.CopyExample(filepath.Join("source", "nodejs"), frontendContext)
+			helper.Cmd("odo", "create", "nodejs", "frontend", "--context", frontendContext, "--project", commonVar.Project).ShouldPass()
+			helper.Cmd("odo", "url", "create", "--port", "8080", "--context", frontendContext).ShouldPass()
+			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
+			frontendURL = helper.DetermineRouteURL(frontendContext)
+			helper.CopyExample(filepath.Join("source", "python"), backendContext)
+			helper.Cmd("odo", "create", "python", "backend", "--project", commonVar.Project, "--context", backendContext).ShouldPass()
+			helper.Cmd("odo", "url", "create", "--port", "8080", "--context", backendContext).ShouldPass()
+			helper.Cmd("odo", "push", "--context", backendContext).ShouldPass()
 		})
 		JustAfterEach(func() {
 			helper.DeleteDir(frontendContext)
 			helper.DeleteDir(backendContext)
 		})
-		It("should link the frontend application to the backend and then unlink successfully", func() {
-			helper.CopyExample(filepath.Join("source", "nodejs"), frontendContext)
-			helper.Cmd("odo", "create", "nodejs", "frontend", "--context", frontendContext, "--project", commonVar.Project).ShouldPass()
-			helper.Cmd("odo", "url", "create", "--port", "8080", "--context", frontendContext).ShouldPass()
-			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
-			frontendURL := helper.DetermineRouteURL(frontendContext)
-			helper.CopyExample(filepath.Join("source", "python"), backendContext)
-			helper.Cmd("odo", "create", "python", "backend", "--project", commonVar.Project, "--context", backendContext).ShouldPass()
-			helper.Cmd("odo", "url", "create", "--port", "8080", "--context", backendContext).ShouldPass()
-			helper.Cmd("odo", "push", "--context", backendContext).ShouldPass()
+		When("linking the two components", func() {
+			JustBeforeEach(func() {
+				// we link
+				helper.Cmd("odo", "link", "backend", "--context", frontendContext).ShouldPass()
+				helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
+			})
+			JustAfterEach(func() {})
 
-			// we link
-			helper.Cmd("odo", "link", "backend", "--context", frontendContext).ShouldPass()
-			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
-			// ensure that the proper envFrom entry was created
-			envFromOutput := oc.GetEnvFromEntry("frontend", "app", commonVar.Project, "deployment")
-			Expect(envFromOutput).To(ContainSubstring("backend"))
+			It("should successfully link", func() {
+				By("ensuring that the proper envFrom entry was created", func() {
+					envFromOutput := oc.GetEnvFromEntry("frontend", "app", commonVar.Project, "deployment")
+					Expect(envFromOutput).To(ContainSubstring("backend"))
+					helper.HttpWaitFor(frontendURL, "Hello world from node.js!", 20, 1)
+				})
+				By("not allowing re-linking", func() {
+					outputErr := helper.Cmd("odo", "link", "backend", "--context", frontendContext).ShouldFail().Err()
+					Expect(outputErr).To(ContainSubstring("already linked"))
+				})
+			})
 
-			helper.HttpWaitFor(frontendURL, "Hello world from node.js!", 20, 1)
-			outputErr := helper.Cmd("odo", "link", "backend", "--context", frontendContext).ShouldFail().Err()
-			Expect(outputErr).To(ContainSubstring("already linked"))
-			helper.Cmd("odo", "unlink", "backend", "--context", frontendContext).ShouldPass()
-			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
-		})
+			It("should successfully unlink", func() {
+				helper.Cmd("odo", "unlink", "backend", "--context", frontendContext).ShouldPass()
+				helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
+			})
 
-		It("should successfully delete component after linked component is deleted", func() {
-			// first create the two components
-			helper.CopyExample(filepath.Join("source", "nodejs"), frontendContext)
-			helper.Cmd("odo", "create", "nodejs", "frontend", "--context", frontendContext, "--project", commonVar.Project).ShouldPass()
-			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
-			helper.CopyExample(filepath.Join("source", "python"), backendContext)
-			helper.Cmd("odo", "create", "python", "backend", "--context", backendContext, "--project", commonVar.Project).ShouldPass()
-			helper.Cmd("odo", "push", "--context", backendContext).ShouldPass()
-
-			// now link frontend to the backend component
-			helper.Cmd("odo", "link", "backend", "--context", frontendContext).ShouldPass()
-			helper.Cmd("odo", "push", "--context", frontendContext).ShouldPass()
-
-			// now delete the backend component and then the frontend component
-			// this didn't work earlier: https://github.com/openshift/odo/issues/2355
-			helper.Cmd("odo", "delete", "-f", "--context", backendContext).ShouldPass()
-			helper.Cmd("odo", "delete", "-f", "--context", frontendContext).ShouldPass()
+			It("should successfully delete component after linked component is deleted", func() {
+				// Testing: https://github.com/openshift/odo/issues/2355
+				helper.Cmd("odo", "delete", "-f", "--context", backendContext).ShouldPass()
+				helper.Cmd("odo", "delete", "-f", "--context", frontendContext).ShouldPass()
+			})
 		})
 	})
-
 })
