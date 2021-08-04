0b9d35099e4ebf6693c96ad0ec4409b6df6c35ae
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-08-04 12:30:49 +0530

    Moving interactive mode error to top
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/odo/cli/service/create.go
+++ pkg/odo/cli/service/create.go
@@ -82,6 +82,9 @@ func NewCreateOptions() *CreateOptions {
 
 // Complete completes CreateOptions after they've been created
 func (o *CreateOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	if len(args) < 1 {
+		return fmt.Errorf("odo doesn't support interactive mode for creating Operator backed service")
+	}
 	o.Context, err = genericclioptions.New(genericclioptions.CreateParameters{
 		Cmd:              cmd,
 		DevfilePath:      component.DevfilePath,
@@ -107,9 +110,7 @@ func (o *CreateOptions) Complete(name string, cmd *cobra.Command, args []string)
 		return err
 	}
 	//if no args are provided, user wants interactive mode
-	if len(args) < 1 {
-		return fmt.Errorf("odo doesn't support interactive mode for creating Operator backed service")
-	}
+
 	o.Backend = NewOperatorBackend()
 	o.interactive = false
 	return o.Backend.CompleteServiceCreate(o, cmd, args)
