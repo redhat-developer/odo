63073ebdb11862893358c57be5949428f3da8f8c
Priti Kumari <pkumari@redhat.com>
2021-08-04 17:04:57 +0530

    Nightly CI and Operator Setup script (#4958)
    * Nightly CI and Operator Setup script
    
    * Fix syntax error
    
    * Addressed review comment
    
    * Update changelog and few review comment

---
+++ scripts/openshiftci-Nightly-SBO-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# fail if some commands fails
+set -e
+# show commands
+set -x
+
+export CI="openshift"
+export NIGHTLY=true
+make configure-installer-tests-cluster
+make bin
+mkdir -p $GOPATH/bin
+make goget-ginkgo
+export PATH="$PATH:$(pwd):$GOPATH/bin"
+export CUSTOM_HOMEDIR=$ARTIFACT_DIR
+
+# Copy kubeconfig to temporary kubeconfig file
+# Read and Write permission to temporary kubeconfig file
+TMP_DIR=$(mktemp -d)
+cp $KUBECONFIG $TMP_DIR/kubeconfig
+chmod 640 $TMP_DIR/kubeconfig
+export KUBECONFIG=$TMP_DIR/kubeconfig
+
+# Login as developer
+oc login -u developer -p password@123
+
+# Check login user name for debugging purpose
+oc whoami
+
+# Operatorhub integration tests
+make test-operator-hub
+
+cp -r reports $ARTIFACT_DIR 
+
+oc logout
