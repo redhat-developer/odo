0bc764a5df0cffe65f070cc74df0fab29bca5ef8
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-08-10 12:22:03 +0530

    Fixing error check
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/odo/cli/catalog/list/components.go
+++ pkg/odo/cli/catalog/list/components.go
@@ -42,7 +42,7 @@ func NewListComponentsOptions() *ListComponentsOptions {
 func (o *ListComponentsOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
 	tasks := util.NewConcurrentTasks(2)
 
-	if util.IsValidKubeConfigPath() {
+	if err1 := util.IsValidKubeConfigPath(); err1 == nil {
 		o.Context, err = genericclioptions.NewContext(cmd)
 		if err != nil {
 			return err
