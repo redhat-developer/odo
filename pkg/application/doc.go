1c7587f887709d9ff80d16a4e9a848d418cf98ac
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-08-16 12:39:19 +0530

    Updating docs based changes as per review
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/application/doc.go
+++ pkg/application/doc.go
@@ -1,5 +1,5 @@
 // Package application provides functions to list, check existence of, delete and get machine readable description of applications.
 // An application is a set of components and services.
 // An application is materialized by the `app:` label in `deployments`, `deploymentconfigs`,
-// or service instances (service instances from Service Catalog or from Operator Backed Services).
+// or service instances (service instances from Operator Backed Services).
 package application
