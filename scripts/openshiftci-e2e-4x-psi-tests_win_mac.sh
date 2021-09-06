8ad4b0921e1180f30eec61caf417cf8df0e5ffa0
Anand Kumar Singh <anandrkskd@gmail.com>
2021-09-06 13:38:33 +0530

    add PSI and IBM cloud to the script for test (#4946)
    * add PSI and IBM cloud to the script for test
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * fix syntax error
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * fix syntax error
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * disable operator hub test
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * cleaning NS before exiting build
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * uncomment ibmlogin
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * remove debug code
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * typo resolved
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * update cluster login for IbmCloud openshift cluster
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * login for IBM cloud
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * copy test artifacts
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * copy test artifacts for operator-hub, devfile
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * fix typo
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

---
+++ scripts/openshiftci-e2e-4x-psi-tests_win_mac.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+￼
+￼case $1 in
+￼
+￼  win)
+￼    export SENDQUEUE=amqp.ci.queue.win.e2e.send
+￼    export SENDTOPIC=amqp.ci.topic.win.e2e.send
+￼    export JOB_NAME=odo-windows-e2e-pr-build
+￼    export SENDEXCHANGE=amqp.ci.exchange.win.e2e.send
+￼    ;;
+￼
+￼  mac)
+￼    export SENDQUEUE=amqp.ci.queue.mac.e2e.send
+￼    export SENDTOPIC=amqp.ci.topic.mac.e2e.send
+￼    export JOB_NAME=odo-mac-e2e-pr-build
+￼    export SENDEXCHANGE=amqp.ci.exchange.mac.e2e.send
+￼    ;;
+￼esac
+￼
+￼. scripts/openshiftci-e2e-4x-psi-tests.sh 
