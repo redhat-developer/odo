26bc269dd0862a6e31bc1a1d718739cd7634ffdc
Philippe Martin <phmartin@redhat.com>
2021-09-07 15:51:11 +0200

    Refactor odo exec integration tests + remove unused utils functions

--- tests/integration/devfile/cmd_devfile_exec_test.go
+++ tests/integration/devfile/cmd_devfile_exec_test.go
@@ -1,8 +1,9 @@
 package devfile
 
 import (
+	"path/filepath"
+
 	"github.com/openshift/odo/tests/helper"
-	"github.com/openshift/odo/tests/integration/devfile/utils"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -25,25 +26,58 @@ var _ = Describe("odo devfile exec command tests", func() {
 		helper.CommonAfterEach(commonVar)
 	})
 
-	Context("When devfile exec command is executed", func() {
+	When("a component is created", func() {
 
-		It("should execute the given command successfully in the container", func() {
-			utils.ExecCommand(commonVar.Context, cmpName)
-			podName := commonVar.CliRunner.GetRunningPodNameByComponent(cmpName, commonVar.Project)
-			listDir := commonVar.CliRunner.ExecListDir(podName, commonVar.Project, "/projects")
-			Expect(listDir).To(ContainSubstring("blah.js"))
-		})
+		BeforeEach(func() {
+			args := []string{"create", "nodejs", cmpName, "--context", commonVar.Context}
+			helper.Cmd("odo", args...).ShouldPass()
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), commonVar.Context)
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", "devfile.yaml"), filepath.Join(commonVar.Context, "devfile.yaml"))
 
-		It("should error out when no command is given by the user", func() {
-			utils.ExecWithoutCommand(commonVar.Context, cmpName)
-		})
-
-		It("should error out when a invalid command is given by the user", func() {
-			utils.ExecWithInvalidCommand(commonVar.Context, cmpName, "kube")
 		})
 
 		It("should error out when a component is not present or when a devfile flag is used", func() {
-			utils.ExecCommandWithoutComponentAndDevfileFlag(commonVar.Context, cmpName)
+			args := []string{"exec", "--context", commonVar.Context}
+			args = append(args, []string{"--", "touch", "/projects/blah.js"}...)
+			helper.Cmd("odo", args...).ShouldFail()
+
+			args = []string{"exec", "--context", commonVar.Context, "--devfile", "invalid.yaml"}
+			args = append(args, []string{"--", "touch", "/projects/blah.js"}...)
+			helper.Cmd("odo", args...).ShouldFail()
 		})
+
+		When("odo push is executed", func() {
+			BeforeEach(func() {
+				args := []string{"push", "--context", commonVar.Context}
+				helper.Cmd("odo", args...).ShouldPass()
+			})
+
+			It("should execute the given command successfully in the container", func() {
+				args := []string{"exec", "--context", commonVar.Context}
+				args = append(args, []string{"--", "touch", "/projects/blah.js"}...)
+				helper.Cmd("odo", args...).ShouldPass()
+
+				podName := commonVar.CliRunner.GetRunningPodNameByComponent(cmpName, commonVar.Project)
+				listDir := commonVar.CliRunner.ExecListDir(podName, commonVar.Project, "/projects")
+				Expect(listDir).To(ContainSubstring("blah.js"))
+			})
+
+			It("should error out when no command is given by the user", func() {
+				args := []string{"exec", "--context", commonVar.Context}
+				args = append(args, "--")
+				output := helper.Cmd("odo", args...).ShouldFail().Err()
+
+				Expect(output).To(ContainSubstring("no command was given"))
+			})
+
+			It("should error out when a invalid command is given by the user", func() {
+				args := []string{"exec", "--context", commonVar.Context}
+				args = append(args, "--", "invalidCommand")
+				output := helper.Cmd("odo", args...).ShouldFail().Out()
+
+				Expect(output).To(ContainSubstring("executable file not found in $PATH"))
+			})
+		})
+
 	})
 })
