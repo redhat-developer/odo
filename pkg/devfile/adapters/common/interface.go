67033410d4875f503951efbabd4d5541434dd5c9
Philippe Martin <phmartin@redhat.com>
2021-09-27 09:51:26 +0200

    Fix renaming

--- pkg/devfile/adapters/common/interface.go
+++ pkg/devfile/adapters/common/interface.go
@@ -13,7 +13,7 @@ type ComponentAdapter interface {
 	DoesComponentExist(cmpName string, app string) (bool, error)
 	Delete(labels map[string]string, show bool, wait bool) error
 	Test(testCmd string, show bool) error
-	CheckSupervisordCtlStatus(command devfilev1.Command) error
+	CheckSupervisordCommandStatus(command devfilev1.Command) error
 	StartContainerStatusWatch()
 	StartSupervisordCtlStatusWatch()
 	Log(follow bool, command devfilev1.Command) (io.ReadCloser, error)
