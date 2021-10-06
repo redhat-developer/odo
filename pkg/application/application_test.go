24ebb39f948a85df9df613718a59e45177bf91ea
Parthvi Vala <pvala@redhat.com>
2021-10-06 19:57:02 +0530

    Comment the failing unit test

--- pkg/application/application_test.go
+++ pkg/application/application_test.go
@@ -7,130 +7,131 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-//func TestGetMachineReadableFormat(t *testing.T) {
-//	type args struct {
-//		appName     string
-//		projectName string
-//		active      bool
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want App
-//	}{
-//		{
-//
-//			name: "Test Case: machine readable output for application",
-//			args: args{
-//				appName:     "myapp",
-//				projectName: "myproject",
-//				active:      true,
-//			},
-//			want: App{
-//				TypeMeta: metav1.TypeMeta{
-//					Kind:       appKind,
-//					APIVersion: appAPIVersion,
-//				},
-//				ObjectMeta: metav1.ObjectMeta{
-//					Name:      "myapp",
-//					Namespace: "myproject",
-//				},
-//				Spec: AppSpec{
-//					Components: []string{"frontend"},
-//				},
-//			},
-//		},
-//	}
-//	deploymentList := appsv1.DeploymentList{
-//		Items: []appsv1.Deployment{
-//			{
-//				ObjectMeta: metav1.ObjectMeta{
-//					Name:      "frontend-myapp",
-//					Namespace: "myproject",
-//					Labels: map[string]string{
-//						applabels.ApplicationLabel:         "myapp",
-//						componentlabels.ComponentLabel:     "frontend",
-//						componentlabels.ComponentTypeLabel: "nodejs",
-//						applabels.ManagedBy:                "odo",
-//						applabels.ManagerVersion:           version.VERSION,
-//					},
-//				},
-//				Spec: appsv1.DeploymentSpec{
-//					Template: corev1.PodTemplateSpec{
-//						Spec: corev1.PodSpec{
-//							Containers: []corev1.Container{
-//								{
-//									Name: "dummyContainer",
-//								},
-//							},
-//						},
-//					},
-//				},
-//			},
-//			{
-//				ObjectMeta: metav1.ObjectMeta{
-//					Name:      "backend-app",
-//					Namespace: "myproject",
-//					Labels: map[string]string{
-//						applabels.ApplicationLabel:         "app",
-//						componentlabels.ComponentLabel:     "backend",
-//						componentlabels.ComponentTypeLabel: "java",
-//						applabels.ManagedBy:                "odo",
-//						applabels.ManagerVersion:           version.VERSION,
-//					},
-//				},
-//				Spec: appsv1.DeploymentSpec{
-//					Template: corev1.PodTemplateSpec{
-//						Spec: corev1.PodSpec{
-//							Containers: []corev1.Container{
-//								{
-//									Name: "dummyContainer",
-//								},
-//							},
-//						},
-//					},
-//				},
-//			},
-//		},
-//	}
-//	//deploymentList := appsv1.DeploymentList{
-//	//	Items: []appsv1.Deployment{
-//	//		*testingutil.CreateFakeDeploymentsWithContainers("frontend", []corev1.Container{
-//	//			{Name: "dummyContainer"},
-//	//		}, []corev1.Container{}),
-//	//		*testingutil.CreateFakeDeploymentsWithContainers("backend", []corev1.Container{
-//	//			{Name: "dummyContainer"},
-//	//		}, []corev1.Container{}),
-//	//	},
-//	//}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			// Fake the client with the appropriate arguments
-//			client, fakeClientSet := occlient.FakeNew()
-//
-//			// fake the project
-//			fakeClientSet.ProjClientset.PrependReactor("get", "projects", func(action ktesting.Action) (handled bool, ret runtime.Object, err error) {
-//				return true, &testingutil.FakeOnlyOneExistingProjects().Items[0], nil
-//			})
-//
-//			//fake the deployments
-//			fakeClientSet.AppsClientset.PrependReactor("list", "deployments", func(action ktesting.Action) (bool, runtime.Object, error) {
-//				return true, &deploymentList, nil
-//			})
-//
-//			for i := range deploymentList.Items {
-//				fakeClientSet.AppsClientset.PrependReactor("get", "deployments", func(action ktesting.Action) (bool, runtime.Object, error) {
-//					return true, &deploymentList.Items[i], nil
-//				})
-//			}
-//			if got := GetMachineReadableFormat(client, tt.args.appName, tt.args.projectName); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("GetMachineReadableFormat() = %v,\n want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+/* TODO: Fix this unit test. Mock occlient pkg with mockgen and use it for the test.
+func TestGetMachineReadableFormat(t *testing.T) {
+	type args struct {
+		appName     string
+		projectName string
+		active      bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want App
+	}{
+		{
 
+			name: "Test Case: machine readable output for application",
+			args: args{
+				appName:     "myapp",
+				projectName: "myproject",
+				active:      true,
+			},
+			want: App{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       appKind,
+					APIVersion: appAPIVersion,
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "myapp",
+					Namespace: "myproject",
+				},
+				Spec: AppSpec{
+					Components: []string{"frontend"},
+				},
+			},
+		},
+	}
+	deploymentList := appsv1.DeploymentList{
+		Items: []appsv1.Deployment{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "frontend-myapp",
+					Namespace: "myproject",
+					Labels: map[string]string{
+						applabels.ApplicationLabel:         "myapp",
+						componentlabels.ComponentLabel:     "frontend",
+						componentlabels.ComponentTypeLabel: "nodejs",
+						applabels.ManagedBy:                "odo",
+						applabels.ManagerVersion:           version.VERSION,
+					},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name: "dummyContainer",
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "backend-app",
+					Namespace: "myproject",
+					Labels: map[string]string{
+						applabels.ApplicationLabel:         "app",
+						componentlabels.ComponentLabel:     "backend",
+						componentlabels.ComponentTypeLabel: "java",
+						applabels.ManagedBy:                "odo",
+						applabels.ManagerVersion:           version.VERSION,
+					},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name: "dummyContainer",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	//deploymentList := appsv1.DeploymentList{
+	//	Items: []appsv1.Deployment{
+	//		*testingutil.CreateFakeDeploymentsWithContainers("frontend", []corev1.Container{
+	//			{Name: "dummyContainer"},
+	//		}, []corev1.Container{}),
+	//		*testingutil.CreateFakeDeploymentsWithContainers("backend", []corev1.Container{
+	//			{Name: "dummyContainer"},
+	//		}, []corev1.Container{}),
+	//	},
+	//}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Fake the client with the appropriate arguments
+			client, fakeClientSet := occlient.FakeNew()
+
+			// fake the project
+			fakeClientSet.ProjClientset.PrependReactor("get", "projects", func(action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				return true, &testingutil.FakeOnlyOneExistingProjects().Items[0], nil
+			})
+
+			//fake the deployments
+			fakeClientSet.AppsClientset.PrependReactor("list", "deployments", func(action ktesting.Action) (bool, runtime.Object, error) {
+				return true, &deploymentList, nil
+			})
+
+			for i := range deploymentList.Items {
+				fakeClientSet.AppsClientset.PrependReactor("get", "deployments", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, &deploymentList.Items[i], nil
+				})
+			}
+			if got := GetMachineReadableFormat(client, tt.args.appName, tt.args.projectName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetMachineReadableFormat() = %v,\n want %v", got, tt.want)
+			}
+		})
+	}
+}
+*/
 func TestGetMachineReadableFormatForList(t *testing.T) {
 	type args struct {
 		apps []App
