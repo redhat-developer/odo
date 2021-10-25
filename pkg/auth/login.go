e56129188f5358115005a497fa25a342d083088d
Tomasz Janiszewski <janiszt@gmail.com>
2021-10-25 23:18:11 +0200

    auth/login.go

--- pkg/auth/login.go
+++ pkg/auth/login.go
@@ -114,9 +114,8 @@ func copyAndFilter(w io.Writer, r io.Reader) ([]byte, error) {
 		if n > 0 {
 			d := buf[:n]
 			out = append(out, d...)
-			_, err := w.Write(filteredInformation(d))
-			if err != nil {
-				return out, err
+			if 	_, e := w.Write(filteredInformation(d)); e != nil {
+				return out, e
 			}
 		}
 		if err != nil {
