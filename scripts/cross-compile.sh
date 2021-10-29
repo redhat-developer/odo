57f076ffea7f0b93650a7241d365752ac762b34b
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-10-29 16:05:20 +0530

    Adding darwin-arm64 build for artifacts
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -8,7 +8,7 @@ if [[ -z "${*}" ]]; then
     exit 1
 fi
 
-for platform in linux-amd64 linux-arm64 linux-ppc64le linux-s390x darwin-amd64 windows-amd64 ; do
+for platform in linux-amd64 linux-arm64 linux-ppc64le linux-s390x darwin-amd64 darwin-arm64 windows-amd64 ; do
   echo "Cross compiling $platform and placing binary at dist/bin/$platform/"
   if [ $platform == "windows-amd64" ]; then
     GOARCH=amd64 GOOS=windows go build -o dist/bin/$platform/odo.exe "${@}" ./cmd/odo/
--- scripts/rpm-x86_64-test.sh
+++ scripts/rpm-x86_64-test.sh
@@ -19,7 +19,7 @@ ls ./usr/bin | grep odo
 ./usr/bin/odo version
 popd
 
-RL="odo-darwin-amd64 odo-linux-ppc64le odo-linux-arm64 odo-windows-amd64.exe odo-linux-amd64 odo-linux-s390x"
+RL="odo-darwin-amd64 odo-darwin-arm64 odo-linux-ppc64le odo-linux-arm64 odo-windows-amd64.exe odo-linux-amd64 odo-linux-s390x"
 echo "Validating odo-redistributable rpm"
 rpm2cpio dist/rpmbuild/RPMS/x86_64/`ls dist/rpmbuild/RPMS/x86_64/ | grep redistributable` > dist/rpmtest/redistributable/odo-redistribuable.cpio
 pushd dist/rpmtest/redistributable
