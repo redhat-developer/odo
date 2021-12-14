9ce5040d6b68b26c3420fc1678f35fb7715eea66
Philippe Martin <phmartin@redhat.com>
2021-12-14 12:19:39 +0100

    Use odo-init-image v1.1.11 (#5287)

--- pkg/devfile/adapters/common/utils.go
+++ pkg/devfile/adapters/common/utils.go
@@ -38,7 +38,7 @@ const (
 
 	// Default Image that will be used containing the supervisord binary and assembly scripts
 	// use GetBootstrapperImage() function instead of this variable
-	defaultBootstrapperImage = "registry.access.redhat.com/ocp-tools-4/odo-init-container-rhel8:1.1.10"
+	defaultBootstrapperImage = "registry.access.redhat.com/ocp-tools-4/odo-init-container-rhel8:1.1.11"
 
 	// SupervisordControlCommand sub command which stands for control
 	SupervisordControlCommand = "ctl"
