4dfedb4cbccd1368b822c5c56bde69ff7defb04a
anandrkskd <anandrkskd@gmail.com>
2021-12-14 14:39:06 +0530

    use env for windows IP instead
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- .ibm/pipelines/windows-test.sh
+++ .ibm/pipelines/windows-test.sh
@@ -1,28 +1,29 @@
 #!/bin/bash 
+
+###################################################
+# This script is used to run the test on windows
+# using the IBM DevOps Services.
+#
+
 set -x
-apt-get update -y
-apt-get install -y sshpass
 
 LOGFILE="pr-${GIT_PR_NUMBER}-windows-tests-${BUILD_NUMBER}"
-export REPO=${REPO:-"https://github.com/anandrkskd/odo"}
+export REPO=${REPO:-"https://github.com/redhat-developer/odo"}
 #copy test script inside /tmp/
-#sshpass -p $PASS scp -o StrictHostKeyChecking=no ./.ibm/pipelines/openshift-windows-tests.sh    Administrator@161.156.170.160:/tmp/openshift-windows-tests-${BUILD_NUMBER}.sh
-sshpass -p $PASS scp -o StrictHostKeyChecking=no ./.ibm/pipelines/windows-test-script.ps1    Administrator@161.156.170.160:/tmp/windows-test-script.ps1
+sshpass -p $WINDOWS_PASSWORD scp -o StrictHostKeyChecking=no ./.ibm/pipelines/windows-test-script.ps1    Administrator@$WINDOWS_IP:/tmp/windows-test-script.ps1
 
-#(
 #execute test from the test script
-#sshpass -p $PASS ssh Administrator@161.156.170.160 -o StrictHostKeyChecking=no /tmp/openshift-windows-tests-${BUILD_NUMBER}.sh "${GIT_PR_NUMBER}" "${BUILD_NUMBER}" "${API_KEY}" "${IBM_OPENSHIFT_ENDPOINT}" "${LOGFILE}" "${REPO}"
-sshpass -p $PASS ssh Administrator@161.156.170.160 -o StrictHostKeyChecking=no powershell /tmp/windows-test-script.ps1 "${GIT_PR_NUMBER}" "${BUILD_NUMBER}" "${API_KEY}" "${IBM_OPENSHIFT_ENDPOINT}" "${LOGFILE}" "${REPO}"
+sshpass -p $WINDOWS_PASSWORD ssh Administrator@$WINDOWS_IP -o StrictHostKeyChecking=no powershell /tmp/windows-test-script.ps1 "${GIT_PR_NUMBER}" "${BUILD_NUMBER}" "${API_KEY}" "${IBM_OPENSHIFT_ENDPOINT}" "${LOGFILE}" "${REPO}"
 RESULT=$?
 echo "RESULT: $RESULT"
 
 # save log
 source .ibm/pipelines/functions.sh
 ibmcloud login --apikey "${API_KEY}" -r "${IBM_REGION}"
-sshpass -p $PASS scp  -o StrictHostKeyChecking=no Administrator@161.156.170.160:/tmp/${LOGFILE}   /tmp/${LOGFILE}
+sshpass -p $WINDOWS_PASSWORD scp  -o StrictHostKeyChecking=no Administrator@$WINDOWS_IP:/tmp/${LOGFILE}   /tmp/${LOGFILE}
 save_logs "${LOGFILE}" "OpenShift Windows Tests"  $RESULT
 
-# # cleanup 
-sshpass -p $PASS ssh Administrator@161.156.170.160    -o StrictHostKeyChecking=no rm -rf /tmp/openshift-windows-tests-${BUILD_NUMBER}.sh
+# cleanup 
+sshpass -p $WINDOWS_PASSWORD ssh Administrator@$WINDOWS_IP    -o StrictHostKeyChecking=no rm -rf /tmp/windows-test-script.ps1
 
 exit ${RESULT}
