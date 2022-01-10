a2680f4c6c6f8dff6d5aaada794b27bde6fa7b60
Anand Kumar Singh <anandrkskd@gmail.com>
2022-01-10 15:06:50 +0530

    Remove scripts which are no longer in use (#5295)
    * remove scripts which are no longer in use
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * cleanup
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * removing sync-docs.sh as we have moved to new docs
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * cleanup
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    * remove FIt
    
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- scripts/minikube-minishift-all-tests.sh
+++ scripts/minikube-minishift-all-tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 shout() {
-  set +x
-  echo -e "\n.---------------------------------------\n${1}\n'---------------------------------------\n"
-  set -x
+    set +x
+    echo -e "\n.---------------------------------------\n${1}\n'---------------------------------------\n"
+    set -x
 }
 
 set -ex
@@ -11,47 +11,20 @@ set -ex
 # This is one of the variables injected by ci-firewall. Its purpose is to allow scripts to handle uniqueness as needed
 SCRIPT_IDENTITY=${SCRIPT_IDENTITY:-"def-id"}
 
-case ${1} in
-    minikube)
-        # Integration tests
-        shout "| Running integration Tests on MiniKube"
-        make test-operator-hub
-        make test-cmd-project
-        make test-integration-devfile
+# Integration tests
+shout "| Running integration Tests on MiniKube"
+make test-operator-hub
+make test-cmd-project
+make test-integration-devfile
 
-        shout "Cleaning up some leftover namespaces"
+shout "Cleaning up some leftover namespaces"
 
-        set +x
-        for i in $(kubectl get namespace -o name); do
-	        if [[ $i == "namespace/${SCRIPT_IDENTITY}"* ]]; then
-	            kubectl delete $i
-            fi
-        done
-        set -x
+set +x
+for i in $(kubectl get namespace -o name); do
+    if [[ $i == "namespace/${SCRIPT_IDENTITY}"* ]]; then
+        kubectl delete $i
+    fi
+done
+set -x
 
-        odo logout
-        ;;
-    minishift)
-        cd $HOME/redhat-developer/odo
-        eval $(minishift oc-env)
-
-        shout "| Logging in to minishift..."
-        oc login -u developer -p developer --insecure-skip-tls-verify $(minishift ip):8443
-
-        shout "| Executing on minishift: generic, login, component command and plugin handler integration tests"
-        make test-integration
-
-
-        shout "| Executing on minishift: devfile catalog, create, push, watch, delete, registry, exec, test, env, status, config, debug and log command integration tests"
-        make test-integration-devfile
-
-        shout "| Executing on minishift: core beta, java, source e2e tests"
-        make test-e2e-devfile
-
-        odo logout
-        ;;
-    *)
-        echo "Need parameter set to minikube or minishift"
-        exit 1
-        ;;
-esac
+odo logout
