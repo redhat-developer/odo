b84d823beccdc048f50ed34d65bba7ece9043817
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2022-01-17 15:00:25 +0530

    Updating tests
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/odo/cli/component/create.go
+++ pkg/odo/cli/component/create.go
@@ -146,16 +146,17 @@ func (co *CreateOptions) Complete(cmdline cmdline.Cmdline, args []string) (err e
 	}
 	// CONFLICT CHECK
 	// Check if a component exists
-	if util.CheckPathExists(envFilePath) && util.CheckPathExists(co.DevfilePath) {
-		//if --devfile arg is provided, return arror
-		if co.devfileMetadata.devfilePath.value != "" && !util.PathEqual(co.DevfilePath, co.devfileMetadata.devfilePath.value) {
+	if util.CheckPathExists(co.DevfilePath) {
+		if util.CheckPathExists(envFilePath) {
+			return errors.New("this directory already contains a component")
+		} else if co.devfileMetadata.devfilePath.value != "" && !util.PathEqual(co.DevfilePath, co.devfileMetadata.devfilePath.value) {
 			return errors.New("this directory already contains a devfile, you can't specify devfile via --devfile")
 		} else if co.devfileMetadata.starter != "" && len(args) == 0 {
 			//if devfile already exists, then don't allow --starter
 			return fmt.Errorf("this directory already has a devfile so you cannot provide a starter. Please remove exisiting devfile and re-create")
 		}
-		return errors.New("this directory already contains a component")
 	}
+
 	// Check if there is a dangling env file; delete the env file if found
 	if util.CheckPathExists(envFilePath) && !util.CheckPathExists(co.DevfilePath) {
 		log.Warningf("Found a dangling env file without a devfile, overwriting it")
