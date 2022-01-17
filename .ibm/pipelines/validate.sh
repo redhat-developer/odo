c587b0cf5bf80dcd3ff694df9e0fc8507546bd62
anandrkskd <anandrkskd@gmail.com>
2022-01-17 16:55:11 +0530

    split unit test into unit test and validate test
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- .ibm/pipelines/validate.sh
+++ .ibm/pipelines/validate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-LOGFILE="pr-${GIT_PR_NUMBER}-unit-tests-${BUILD_NUMBER}"
+LOGFILE="pr-${GIT_PR_NUMBER}-Validate-tests-${BUILD_NUMBER}"
 
 source .ibm/pipelines/functions.sh
 
 (
     set -e
     make goget-tools
-    make test
+    make validate
 ) |& tee "/tmp/$LOGFILE"
 RESULT=${PIPESTATUS[0]}
 
 ibmcloud login --apikey "${API_KEY}" -r "${IBM_REGION}"
-save_logs "${LOGFILE}" "Unit Tests" ${RESULT}
+save_logs "${LOGFILE}" "Validate Tests" ${RESULT}
 
 exit ${RESULT}
