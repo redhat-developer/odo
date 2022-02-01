722feb8f868119a228ec756e5e32f5b68dc7611a
Rodolfo Napoles <rnapoles@redhat.com>
2022-01-31 21:30:08 -0500

    refactoring test generic-test

--- tests/integration/generic_test.go
+++ tests/integration/generic_test.go
@@ -2,7 +2,6 @@ package integration
 
 import (
 	"regexp"
-	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -28,40 +27,57 @@ var _ = Describe("odo generic", func() {
 		helper.CommonAfterEach(commonVar)
 	})
 
-	Context("Check the help usage for odo", func() {
+	When("running odo --help", func() {
+		It("retuns full help contents including usage, examples, commands, utility commands, component shortcuts, and flags sections", func() {
+			output := helper.Cmd("odo", "--help").ShouldPass().Out()
+			Expect(output).To(ContainSubstring("Usage:"))
+			Expect(output).To(ContainSubstring("Examples:"))
+			Expect(output).To(ContainSubstring("Commands:"))
+			Expect(output).To(ContainSubstring("Utility Commands:"))
+			Expect(output).To(ContainSubstring("Component Shortcuts:"))
+			Expect(output).To(ContainSubstring("Flags:"))
+		})
 
-		It("Makes sure that we have the long-description when running odo and we dont error", func() {
+	})
+
+	When("running odo without subcommand and flags", func() {
+		It("a short vesion of help contents is returned, an error is not expected", func() {
 			output := helper.Cmd("odo").ShouldPass().Out()
 			Expect(output).To(ContainSubstring("To see a full list of commands, run 'odo --help'"))
 		})
+	})
 
-		It("Make sure we have the full description when performing odo --help", func() {
-			output := helper.Cmd("odo", "--help").ShouldPass().Out()
-			Expect(output).To(ContainSubstring("Use \"odo [command] --help\" for more information about a command."))
+	When("using an invalid subcommand", func() {
+		It("an error message and help including Usage, Examples, Available commands, and flags is returned", func() {
+			output := helper.Cmd("odo", "hello").ShouldFail().Err()
+			Expect(output).To(ContainSubstring("Usage:"))
+			Expect(output).To(ContainSubstring("Examples:"))
+			Expect(output).To(ContainSubstring("Commands:"))
+			Expect(output).To(ContainSubstring("Flags:"))
 		})
+	})
 
+	When("trying to create a component with an invalid component name", func() {
 		It("Fail when entering an incorrect name for a component", func() {
 			output := helper.Cmd("odo", "component", "foobar").ShouldFail().Err()
-			Expect(output).To(ContainSubstring("Subcommand not found, use one of the available commands"))
+			Expect(output).To(ContainSubstring("Error: Subcommand not found, use one of the available commands:"))
+			Expect(output).To(ContainSubstring("Usage:"))
+			Expect(output).To(ContainSubstring("Examples:"))
+			Expect(output).To(ContainSubstring("Available Commands:"))
+			Expect(output).To(ContainSubstring("Flags:"))
+			Expect(output).To(ContainSubstring("Additional Flags:"))
 		})
-
-		It("Fail with showing help only once for incorrect command", func() {
-			output := helper.Cmd("odo", "hello").ShouldFail().Err()
-			Expect(strings.Count(output, "odo [flags]")).Should(Equal(1))
-		})
-
 	})
 
-	Context("When executing catalog list without component directory", func() {
-		It("should list all component catalogs", func() {
+	When("executing catalog list without component directory", func() {
+		It("should list all devfile components", func() {
 			stdOut := helper.Cmd("odo", "catalog", "list", "components").ShouldPass().Out()
-			helper.MatchAllInOutput(stdOut, []string{"nodejs", "python", "php", "go", "java"})
+			helper.MatchAllInOutput(stdOut, []string{"Devfile", "nodejs", "python", "php", "go", "java"})
 		})
-
 	})
 
-	Context("check catalog component search functionality", func() {
-		It("check that a component does not exist", func() {
+	When("searching the catalog for a non existing component", func() {
+		It("searches for the component and returs message indicating that no component matched the query", func() {
 			componentRandomName := helper.RandString(7)
 			output := helper.Cmd("odo", "catalog", "search", "component", componentRandomName).ShouldFail().Err()
 			Expect(output).To(ContainSubstring("no component matched the query: " + componentRandomName))
@@ -69,7 +85,7 @@ var _ = Describe("odo generic", func() {
 	})
 
 	// Test machine readable output
-	Context("when creating project -o json", func() {
+	When("creating an application using -o json flag", func() {
 		var projectName string
 		JustBeforeEach(func() {
 			projectName = helper.RandString(6)
@@ -77,26 +93,13 @@ var _ = Describe("odo generic", func() {
 		JustAfterEach(func() {
 			helper.DeleteProject(projectName)
 		})
-
-		// odo project create foobar -o json
-		It("should be able to create project and show output in json format", func() {
+		It("should create the application and show output in json format", func() {
 			actual := helper.Cmd("odo", "project", "create", projectName, "-o", "json").ShouldPass().Out()
 			values := gjson.GetMany(actual, "kind", "metadata.name", "status.active")
 			expected := []string{"Project", projectName, "true"}
 			Expect(helper.GjsonMatcher(values, expected)).To(Equal(true))
 		})
-	})
-
-	Context("Creating same project twice with flag -o json", func() {
-		var projectName string
-		JustBeforeEach(func() {
-			projectName = helper.RandString(6)
-		})
-		JustAfterEach(func() {
-			helper.DeleteProject(projectName)
-		})
-		// odo project create foobar -o json (x2)
-		It("should fail along with proper machine readable output", func() {
+		It("should fail if trying to create twice along with proper machine readable output", func() {
 			helper.Cmd("odo", "project", "create", projectName).ShouldPass()
 			actual := helper.Cmd("odo", "project", "create", projectName, "-o", "json").ShouldFail().Err()
 			valuesC := gjson.GetMany(actual, "kind", "message")
@@ -106,48 +109,45 @@ var _ = Describe("odo generic", func() {
 		})
 	})
 
-	Context("Delete the project with flag -o json", func() {
+	When("deleting a project with flag -o json", func() {
 		var projectName string
 		JustBeforeEach(func() {
 			projectName = helper.RandString(6)
 		})
-
-		// odo project delete foobar -o json
-		It("should be able to delete project and show output in json format", func() {
+		It("should delete the project and show output in json format", func() {
 			helper.Cmd("odo", "project", "create", projectName, "-o", "json").ShouldPass()
-
 			actual := helper.Cmd("odo", "project", "delete", projectName, "-o", "json").ShouldPass().Out()
 			valuesDel := gjson.GetMany(actual, "kind", "message")
 			expectedDel := []string{"Status", "Deleted project"}
 			Expect(helper.GjsonMatcher(valuesDel, expectedDel)).To(Equal(true))
-
 		})
+
 	})
 
-	Context("When deleting two project one after the other", func() {
-		It("should be able to delete sequentially", func() {
+	When("deleting two project one after the other", func() {
+		It("should be able to delete them sequentially", func() {
 			project1 := helper.CreateRandProject()
 			project2 := helper.CreateRandProject()
 
-			helper.DeleteProject(project2)
 			helper.DeleteProject(project1)
+			helper.DeleteProject(project2)
 		})
 	})
 
-	Context("When deleting three project one after the other in opposite order", func() {
-		It("should be able to delete", func() {
+	When("deleting several projects", func() {
+		It("should be able to delete them in any order", func() {
 			project1 := helper.CreateRandProject()
 			project2 := helper.CreateRandProject()
 			project3 := helper.CreateRandProject()
 
-			helper.DeleteProject(project1)
 			helper.DeleteProject(project2)
+			helper.DeleteProject(project1)
 			helper.DeleteProject(project3)
 
 		})
 	})
 
-	Context("when executing odo version command", func() {
+	When("executing odo version command", func() {
 		It("should show the version of odo major components including server login URL", func() {
 			odoVersion := helper.Cmd("odo", "version").ShouldPass().Out()
 			reOdoVersion := regexp.MustCompile(`^odo\s*v[0-9]+.[0-9]+.[0-9]+(?:-\w+)?\s*\(\w+\)`)
