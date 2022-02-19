e197594e368dfefbb24ba0ae129152771b4e8493
anandrkskd <anandrkskd@gmail.com>
2022-02-19 20:22:44 +0530

    modify periodic test script for v2 branch
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- scripts/openshiftci-periodic-tests.sh
+++ scripts/openshiftci-periodic-tests.sh
@@ -5,6 +5,41 @@ set -e
 # show commands
 set -x
 
+# check if devfiles are updated and if so, update and test them
+check_devfile() {
+    NOTEQUAL= false
+    # Languages for which devfiles preset in examples dir
+    LANGUAGES=('nodejs' 'python' 'springboot')
+
+    for LANGUAGE in "${LANGUAGES[@]}"; do
+        Example_devfile_path=./tests/examples/source/devfiles/$LANGUAGE/devfile-registry.yaml
+        TEMPDIR=$(mktemp -d)
+# download devfiles with odo
+        if [[ $LANGUAGE == "springboot" ]]; then
+            odo create java-$LANGUAGE language --context $TEMPDIR
+        else
+            odo create $LANGUAGE language --context $TEMPDIR
+        fi
+
+        Devfile_path=$TEMPDIR/devfile.yaml
+# check if devfiles differ then the one in examples dir
+        diff $Devfile_path $Example_devfile_path
+        if [ $? != 0 ] ; then # $status is 0 if the files are the same
+            echo "$LANGUAGE Devfile is not same as example"
+            # copy if devfile differs
+            cp $Devfile_path $Example_devfile_path
+            NOTEQUAL= true
+        fi
+
+    done
+
+    if [ $NOTEQUAL == true ]; then
+        return -1
+    else
+        return 0
+    fi
+}
+
 export CI="openshift"
 make configure-installer-tests-cluster
 make bin
@@ -27,19 +62,17 @@ oc login -u developer -p password@123
 oc whoami
 
 # Integration tests
-make test-integration || error=true
-make test-integration-devfile || error=true
-make test-cmd-login-logout || error=true
-make test-cmd-project || error=true
-make test-operator-hub || error=true
+check_devfile
+if [[ $? == -1 ]]; then
+    make test-integration-devfile || error=true
+fi
 
-# E2e tests
-make test-e2e-all || error=true
+make test-operator-hub || error=true
 
 if [ $error ]; then
     exit -1
 fi
 
-cp -r reports tests/reports $ARTIFACT_DIR 
+cp -r reports tests/reports $ARTIFACT_DIR
 
 oc logout
