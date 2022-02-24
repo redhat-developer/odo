78ad27a95db9c8a91a8559c6d5da2ab8120e1b8e
Parthvi Vala <pvala@redhat.com>
2022-02-24 16:01:54 +0530

    Add test
    Signed-off-by: Parthvi Vala <pvala@redhat.com>

---
+++ pkg/init/asker/asker_test.go
@@ -0,0 +1,85 @@
+package asker
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildPersonalizedConfigurationOptions(t *testing.T) {
+	type args struct {
+		configuration ContainerConfiguration
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantOptions []string
+		wantTracker []OperationOnContainer
+	}{
+		{
+			name: "default options",
+			args: args{configuration: ContainerConfiguration{
+				Ports: []string{},
+				Envs:  map[string]string{},
+			}},
+			wantOptions: []string{"NOTHING - configuration is correct", "Add new port", "Add new environment variable"},
+			wantTracker: []OperationOnContainer{{
+				Ops:  "Nothing",
+				Kind: "",
+				Key:  "",
+			}, {
+				Ops:  "Add",
+				Kind: "Port",
+				Key:  "",
+			}, {
+				Ops:  "Add",
+				Kind: "EnvVar",
+				Key:  "",
+			}},
+		},
+		{
+			name: "all options",
+			args: args{configuration: ContainerConfiguration{
+				Ports: []string{"7000", "8000"},
+				Envs:  map[string]string{"env1": "env2"},
+			}},
+			wantOptions: []string{"NOTHING - configuration is correct", "Delete port \"7000\"", "Delete port \"8000\"", "Add new port", "Delete environment variable \"env1\"", "Add new environment variable"},
+			wantTracker: []OperationOnContainer{
+				{
+					Ops:  "Nothing",
+					Kind: "",
+					Key:  "",
+				}, {
+					Ops:  "Delete",
+					Kind: "Port",
+					Key:  "7000",
+				}, {
+					Ops:  "Delete",
+					Kind: "Port",
+					Key:  "8000",
+				}, {
+					Ops:  "Add",
+					Kind: "Port",
+					Key:  "",
+				}, {
+					Ops:  "Delete",
+					Kind: "EnvVar",
+					Key:  "env1",
+				}, {
+					Ops:  "Add",
+					Kind: "EnvVar",
+					Key:  "",
+				}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOptions, gotTracker := buildPersonalizedConfigurationOptions(tt.args.configuration)
+			if !reflect.DeepEqual(gotOptions, tt.wantOptions) {
+				t.Errorf("buildPersonalizedConfigurationOptions() gotOptions = %v, want %v", gotOptions, tt.wantOptions)
+			}
+			if !reflect.DeepEqual(gotTracker, tt.wantTracker) {
+				t.Errorf("buildPersonalizedConfigurationOptions() gotTracker = %v, want %v", gotTracker, tt.wantTracker)
+			}
+		})
+	}
+}
