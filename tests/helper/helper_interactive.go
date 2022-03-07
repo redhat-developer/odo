0dba1fcdf9285aae963da0f25fa03e1f845d9974
Armel Soro <asoro@redhat.com>
2022-03-07 19:36:54 +0100

    Add welcoming messages to `odo init` (#5514)
    * Add test specs meeting the acceptance criteria
    
    * Add welcoming messages to odo init
    
    * Purposely set the log level to 0 in the integration tests
    
    This is to check that the welcoming messages are the first
    to be displayed to the end users.
    
    Ideally, changing the log level would be done right at the
    command level in each of those tests (e.g.:, odo init -v 0),
    but this unfortunately cannot be done at this time due to [1].
    
    [1] https://github.com/redhat-developer/odo/issues/5513
    
    * Make helper.RunInteractive log the command output in case unexpected errors occur
    
    This should hopefully help debug the CI issues
    
    * Initialize the test interactive command sub-process env with a copy of the current process env
    
    Otherwise, as was the case here, we might end up with
    cases where the first prompt displayed by an interactive command
    is the telemetry consent (unless the appropriate env var is set).

--- tests/helper/helper_interactive.go
+++ tests/helper/helper_interactive.go
@@ -5,19 +5,47 @@ package helper
 
 import (
 	"bytes"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"time"
 
 	"github.com/Netflix/go-expect"
 	"github.com/hinshun/vt10x"
 	"github.com/kr/pty"
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-// RunInteractive runs the command in interactive mode and returns the output, and error
-// It takes command as array of strings, and a function `test` that contains steps to run the test as an argument
-func RunInteractive(command []string, test func(*expect.Console, *bytes.Buffer)) (string, error) {
+// InteractiveContext represents the context of an interactive command to be run.
+type InteractiveContext struct {
+
+	//Command represents the original command ran
+	Command []string
+
+	// console is the internal interface used by the interactive command
+	console *expect.Console
+
+	// buffer is the internal bytes buffer containing the console output.
+	// Its content will get updated as long as there are interactions with the console, like sending lines or
+	// expecting lines.
+	buffer *bytes.Buffer
+}
+
+// Tester represents the function that contains all steps to test the given interactive command.
+// The InteractiveContext argument needs to be passed to the various helper.SendLine and helper.ExpectString methods.
+type Tester func(InteractiveContext)
+
+// RunInteractive runs the command in interactive mode and returns the output, and error.
+// It takes command as array of strings, and a function `tester` that contains steps to run the test as an argument.
+// The command is executed as a separate process, the environment of which is controlled via the `env` argument.
+// The initial value of the sub-process environment is a copy of the environment of the current process.
+// If `env` is not `nil`, it will be appended to the end of the sub-process environment.
+// If there are duplicate environment keys, only the last value in the slice for each duplicate key is used.
+func RunInteractive(command []string, env []string, tester Tester) (string, error) {
+
+	fmt.Fprintln(GinkgoWriter, "running command", command, "with env", env)
 
 	ptm, pts, err := pty.Open()
 	if err != nil {
@@ -38,13 +66,21 @@ func RunInteractive(command []string, test func(*expect.Console, *bytes.Buffer))
 	cmd.Stdin = c.Tty()
 	cmd.Stdout = c.Tty()
 	cmd.Stderr = c.Tty()
+	if env != nil {
+		cmd.Env = append(os.Environ(), env...)
+	}
 	err = cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	buf := new(bytes.Buffer)
-	test(c, buf)
+	ctx := InteractiveContext{
+		Command: command,
+		console: c,
+		buffer:  buf,
+	}
+	tester(ctx)
 	err = cmd.Wait()
 	if err != nil {
 		log.Fatal(err)
@@ -55,13 +91,26 @@ func RunInteractive(command []string, test func(*expect.Console, *bytes.Buffer))
 	return buf.String(), err
 }
 
-func SendLine(c *expect.Console, line string) {
-	_, err := c.SendLine(line)
-	Expect(err).ShouldNot(HaveOccurred())
+// expectDescriptionSupplier returns a function intended to be used as description supplier
+// when checking errors do not occur in ExpectString and SendLine.
+// Note that the function returned is evaluated lazily, only in case an error occurs.
+func expectDescriptionSupplier(ctx InteractiveContext, line string) func() string {
+	return func() string {
+		return fmt.Sprintf("error while sending or expecting line: \"%s\"\n"+
+			"=== output of command '%+q' read so far ===\n%v\n======================",
+			line,
+			ctx.Command,
+			ctx.buffer)
+	}
 }
 
-func ExpectString(c *expect.Console, line string) string {
-	res, err := c.ExpectString(line)
-	Expect(err).ShouldNot(HaveOccurred())
-	return res
+func SendLine(ctx InteractiveContext, line string) {
+	_, err := ctx.console.SendLine(line)
+	Expect(err).ShouldNot(HaveOccurred(), expectDescriptionSupplier(ctx, line))
+}
+
+func ExpectString(ctx InteractiveContext, line string) {
+	res, err := ctx.console.ExpectString(line)
+	fmt.Fprint(ctx.buffer, res)
+	Expect(err).ShouldNot(HaveOccurred(), expectDescriptionSupplier(ctx, line))
 }
--- tests/integration/interactive/cmd_init_test.go
+++ tests/integration/interactive/cmd_init_test.go
@@ -4,13 +4,12 @@
 package interactive
 
 import (
-	"bytes"
 	"fmt"
-
-	"github.com/Netflix/go-expect"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/redhat-developer/odo/tests/helper"
+	"log"
+	"path/filepath"
 )
 
 var _ = Describe("odo init interactive command tests", func() {
@@ -32,31 +31,151 @@ var _ = Describe("odo init interactive command tests", func() {
 	It("should download correct devfile", func() {
 
 		command := []string{"odo", "init"}
-		output, err := helper.RunInteractive(command, func(c *expect.Console, output *bytes.Buffer) {
+		output, err := helper.RunInteractive(command, nil, func(ctx helper.InteractiveContext) {
 
-			res := helper.ExpectString(c, "Select language")
-			fmt.Fprintln(output, res)
-			helper.SendLine(c, "go")
+			helper.ExpectString(ctx, "Select language")
+			helper.SendLine(ctx, "go")
 
-			res = helper.ExpectString(c, "Select project type")
-			fmt.Fprintln(output, res)
-			helper.SendLine(c, "\n")
+			helper.ExpectString(ctx, "Select project type")
+			helper.SendLine(ctx, "\n")
 
-			res = helper.ExpectString(c, "Which starter project do you want to use")
-			fmt.Fprintln(output, res)
-			helper.SendLine(c, "\n")
+			helper.ExpectString(ctx, "Which starter project do you want to use")
+			helper.SendLine(ctx, "\n")
 
-			res = helper.ExpectString(c, "Enter component name")
-			fmt.Fprintln(output, res)
-			helper.SendLine(c, "my-go-app")
+			helper.ExpectString(ctx, "Enter component name")
+			helper.SendLine(ctx, "my-go-app")
 
-			res = helper.ExpectString(c, "Your new component \"my-go-app\" is ready in the current directory.")
-			fmt.Fprintln(output, res)
+			helper.ExpectString(ctx, "Your new component \"my-go-app\" is ready in the current directory.")
 
 		})
 
 		Expect(err).To(BeNil())
 		Expect(output).To(ContainSubstring("Your new component \"my-go-app\" is ready in the current directory."))
 		Expect(helper.ListFilesInDir(commonVar.Context)).To(ContainElements("devfile.yaml"))
+	})
+
+	Describe("displaying welcoming messages", func() {
+
+		// testFunc is a function that returns a `Tester` function (intended to be used via `helper.RunInteractive`),
+		// which first expects all messages in `welcomingMsgs` to be read from the console,
+		// then runs an `additionalTester` and finally expects the asking of a component name
+		// (based on the `language` specified)
+		testFunc := func(language string, welcomingMsgs []string, additionalTester helper.Tester) helper.Tester {
+			return func(ctx helper.InteractiveContext) {
+				for _, msg := range welcomingMsgs {
+					helper.ExpectString(ctx, msg)
+				}
+
+				if additionalTester != nil {
+					additionalTester(ctx)
+				}
+
+				helper.ExpectString(ctx, "Enter component name")
+				helper.SendLine(ctx, fmt.Sprintf("my-%s-app", language))
+
+				helper.ExpectString(ctx,
+					fmt.Sprintf("Your new component \"my-%s-app\" is ready in the current directory.", language))
+			}
+		}
+
+		assertBehavior := func(language string, output string, err error, msgs []string, additionalAsserter func()) {
+			Expect(err).To(BeNil())
+
+			lines, err := helper.ExtractLines(output)
+			if err != nil {
+				log.Fatal(err)
+			}
+			Expect(len(lines)).To(BeNumerically(">", len(msgs)))
+			Expect(lines[0:len(msgs)]).To(Equal(msgs))
+			Expect(lines).To(
+				ContainElement(fmt.Sprintf("Your new component \"my-%s-app\" is ready in the current directory.", language)))
+
+			Expect(helper.ListFilesInDir(commonVar.Context)).To(ContainElements("devfile.yaml"))
+
+			if additionalAsserter != nil {
+				additionalAsserter()
+			}
+		}
+
+		testRunner := func(language string, welcomingMsgs []string, tester helper.Tester) (string, error) {
+			command := []string{"odo", "init"}
+			return helper.RunInteractive(command,
+				// Setting verbosity level to 0, because we would be asserting the welcoming message is the first
+				// message displayed to the end user. So we do not want any potential debug lines to be printed first.
+				// Using envvars here (and not via the -v flag), because of https://github.com/redhat-developer/odo/issues/5513
+				[]string{"ODO_LOG_LEVEL=0"},
+				testFunc(language, welcomingMsgs, tester))
+		}
+
+		When("directory is empty", func() {
+
+			BeforeEach(func() {
+				Expect(helper.ListFilesInDir(commonVar.Context)).To(HaveLen(0))
+			})
+
+			It("should display appropriate welcoming messages", func() {
+				language := "java"
+				welcomingMsgs := []string{
+					"The current directory is empty. odo will help you start a new project.",
+				}
+
+				output, err := testRunner(language, welcomingMsgs, func(ctx helper.InteractiveContext) {
+					helper.ExpectString(ctx, "Select language")
+					helper.SendLine(ctx, language)
+
+					helper.ExpectString(ctx, "Select project type")
+					helper.SendLine(ctx, "\n")
+
+					helper.ExpectString(ctx, "Which starter project do you want to use")
+					helper.SendLine(ctx, "\n")
+				})
+
+				assertBehavior(language, output, err, welcomingMsgs, nil)
+			})
+		})
+
+		When("directory is not empty", func() {
+
+			BeforeEach(func() {
+				helper.CopyExample(filepath.Join("source", "python"), commonVar.Context)
+				Expect(helper.ListFilesInDir(commonVar.Context)).To(
+					SatisfyAll(
+						HaveLen(2),
+						ContainElements("requirements.txt", "wsgi.py")))
+			})
+
+			It("should display appropriate welcoming messages", func() {
+				language := "python"
+				welcomingMsgs := []string{
+					"The current directory already contains source code. " +
+						"odo will try to autodetect the language and project type in order to select the best suited Devfile for your project.",
+				}
+
+				output, err := testRunner(language, welcomingMsgs, func(ctx helper.InteractiveContext) {
+					helper.ExpectString(ctx, "Based on the files in the current directory odo detected")
+
+					helper.ExpectString(ctx, fmt.Sprintf("Language: %s", language))
+
+					helper.ExpectString(ctx, fmt.Sprintf("Project type: %s", language))
+
+					helper.ExpectString(ctx,
+						fmt.Sprintf("The devfile \"%s\" from the registry \"DefaultDevfileRegistry\" will be downloaded.", language))
+
+					helper.ExpectString(ctx, "Is this correct")
+					helper.SendLine(ctx, "\n")
+
+					helper.ExpectString(ctx, "Select container for which you want to change configuration")
+					helper.SendLine(ctx, "\n")
+				})
+
+				assertBehavior(language, output, err, welcomingMsgs, func() {
+					// Make sure the original source code files are still present
+					Expect(helper.ListFilesInDir(commonVar.Context)).To(
+						SatisfyAll(
+							HaveLen(3),
+							ContainElements("devfile.yaml", "requirements.txt", "wsgi.py")))
+				})
+			})
+		})
 	})
 })
