b395348235a9ba1aafdc53c87475413feea9e912
Dharmit Shah <shahdharmit@gmail.com>
2022-03-07 16:51:04 +0530

    Use callback and implement watch handler at CLI level (#5516)
    * Use callback and implement watch handler at CLI level
    
    * Find devfile instead of hard coding

--- pkg/dev/interface.go
+++ pkg/dev/interface.go
@@ -1,12 +1,21 @@
 package dev
 
 import (
+	"io"
+
+	"github.com/redhat-developer/odo/pkg/devfile/adapters/common"
+
+	"github.com/redhat-developer/odo/pkg/watch"
+
 	"github.com/devfile/library/pkg/devfile/parser"
 	"github.com/redhat-developer/odo/pkg/devfile/adapters/kubernetes"
-	"io"
 )
 
 type Client interface {
-	Start(d parser.DevfileObj, platformContext kubernetes.KubernetesContext, ignorePaths []string, path string, w io.Writer) error
+	Start(d parser.DevfileObj, platformContext kubernetes.KubernetesContext, ignorePaths []string, path string, w io.Writer, h Handler) error
 	Cleanup() error
+}
+
+type Handler interface {
+	RegenerateAdapterAndPush(common.PushParameters, watch.WatchParameters) error
 }
