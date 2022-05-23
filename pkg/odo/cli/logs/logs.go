fb5e434d81836a67ba0b936d2c938adc8630e6a5
Dharmit Shah <shahdharmit@gmail.com>
2022-05-23 15:07:19 +0530

    Nolint for random number generation

--- pkg/odo/cli/logs/logs.go
+++ pkg/odo/cli/logs/logs.go
@@ -132,7 +132,7 @@ func printLogs(containerName string, rd io.ReadCloser, out io.Writer) error {
 func selectRandomColor() color.Attribute {
 	colors := []color.Attribute{color.FgRed, color.FgGreen, color.FgYellow, color.FgBlue, color.FgMagenta, color.FgCyan, color.FgWhite}
 	rand.Seed(time.Now().UnixNano())
-	i := rand.Intn(len(colors))
+	i := rand.Intn(len(colors)) //nolint
 	return colors[i]
 }
 func NewCmdLogs(name, fullname string) *cobra.Command {
