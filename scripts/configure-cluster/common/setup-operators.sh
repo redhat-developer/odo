fa2a1e0b14280fc50be6c5ae134a2bbd75127f21
Surender Yadav <surender.yadav1@ibm.com>
2022-05-25 12:33:29 +0530

    Removed setup operator code block for s390x (#5761)

--- scripts/configure-cluster/common/setup-operators.sh
+++ scripts/configure-cluster/common/setup-operators.sh
@@ -37,15 +37,6 @@ if [ "$KUBERNETES" == "true" ]; then
 
   # install "service-binding-operator" using "kubectl" in "operators" namespace; use "operatorhubio-catalog" catalog source from "olm" namespace
   install_service_binding_operator kubectl operators service-binding-operator operatorhubio-catalog olm
-elif [ $(uname -m) == "s390x" ]; then
-  # create "operator-ibm-catalog" CatalogSource for s390x
-  oc apply -f https://raw.githubusercontent.com/redhat-developer/odo/main/docs/website/manifests/catalog-source-$(uname -m).yaml
-
-  # install "cloud-native-postgresql" using "oc" in "openshift-operators" namespace; use "operator-ibm-catalog" catalog source from "openshift-marketplace" namespace
-  install_postgres_operator oc openshift-operators operator-ibm-catalog openshift-marketplace
-
-  # install "service-binding-operator" using "oc" in "openshift-operators" namespace; use "operator-ibm-catalog" catalog source from "openshift-marketplace" namespace
-  install_service_binding_operator oc openshift-operators service-binding-operator operator-ibm-catalog openshift-marketplace
 else
   # install "cloud-native-postgresql" using "oc" in "openshift-operators" namespace; use "certified-operators" catalog source from "openshift-marketplace" namespace
   install_postgres_operator oc openshift-operators certified-operators openshift-marketplace
