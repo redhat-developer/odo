3b40c629982fc3d613cf70a42bb590314c06f3d1
Dharmit Shah <shahdharmit@gmail.com>
2022-06-15 16:18:59 +0530

    Update pkg/logs/interface.go
    Co-authored-by: Philippe Martin <contact@elol.fr>

--- pkg/logs/interface.go
+++ pkg/logs/interface.go
@@ -3,8 +3,10 @@ package logs
 import "io"
 
 type Client interface {
-	// GetLogsForMode gets logs of the containers for the specified mode (Dev or Deploy) of the provided
+	// GetLogsForMode gets logs of the containers for the specified mode (Dev, Deploy or both) of the provided
 	// component name and namespace. It returns a slice of maps where container name is the key and its logs are
-	// the value. Each map is a key-value pair of container name and its logs
+	// the value. Each map is a key-value pair of container name and its logs for a specific pod
+	// The accepted values for mode are ComponentDevMode, ComponentDeployMode and ComponentAnyMode
+	// found in the pkg/labels package
 	GetLogsForMode(mode string, componentName string, namespace string) ([]map[string]io.ReadCloser, error)
 }
