c8b5b24a8606b1c31f0a5fd880e2a8661fce9a24
Armel Soro <asoro@redhat.com>
2022-06-19 21:35:32 +0200

    Add integration test supporting composite debug commands

--- tests/integration/devfile/cmd_dev_debug_test.go
+++ tests/integration/devfile/cmd_dev_debug_test.go
@@ -54,6 +54,56 @@ var _ = Describe("odo dev debug command tests", func() {
 		})
 	})
 
+	When("a composite command is used as debug command", func() {
+		const devfileCmpName = "nodejs"
+		var session helper.DevSession
+		var stdout []byte
+		var stderr []byte
+		var ports map[string]string
+		BeforeEach(func() {
+			helper.CopyExampleDevFile(filepath.Join("source", "devfiles", "nodejs", "devfileCompositeRunAndDebug.yaml"), filepath.Join(commonVar.Context, "devfile.yaml"))
+			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), commonVar.Context)
+			var err error
+			session, stdout, stderr, ports, err = helper.StartDevMode("--debug")
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			session.Stop()
+			session.WaitEnd()
+		})
+
+		It("should expect a ws connection when tried to connect on default debug port locally", func() {
+			helper.MatchAllInOutput(string(stdout), []string{
+				"Executing the application (command: mkdir)",
+				"Executing the application (command: echo)",
+				"Executing the application (command: install)",
+				"Executing the application (command: start-debug)",
+			})
+			helper.MatchAllInOutput(string(stderr), []string{
+				"Devfile command \"echo\" exited with an error status",
+				"intentional-error-message",
+			})
+
+			// Verify the command executed successfully
+			podName := commonVar.CliRunner.GetRunningPodNameByComponent(devfileCmpName, commonVar.Project)
+			res := commonVar.CliRunner.CheckCmdOpInRemoteDevfilePod(
+				podName,
+				"runtime",
+				commonVar.Project,
+				[]string{"stat", "/projects/testfolder"},
+				func(cmdOp string, err error) bool {
+					return err == nil
+				},
+			)
+			Expect(res).To(BeTrue())
+
+			// 400 response expected because the endpoint expects a websocket request and we are doing a HTTP GET
+			// We are just using this to validate if nodejs agent is listening on the other side
+			helper.HttpWaitForWithStatus("http://"+ports["5858"], "WebSockets request was expected", 12, 5, 400)
+		})
+	})
+
 	When("a component without debug command is bootstrapped", func() {
 		BeforeEach(func() {
 			helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), commonVar.Context)
