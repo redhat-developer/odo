bd6f37dfa70e361bb40138a4ffe4c0da22bf9f0d
Philippe Martin <phmartin@redhat.com>
2022-06-28 18:10:42 +0200

    add doc

--- pkg/portForward/interface.go
+++ pkg/portForward/interface.go
@@ -7,6 +7,10 @@ import (
 )
 
 type Client interface {
+	// StartPortForwarding starts port forwarding for the endpoints defined in the containers of the devfile
+	// componentName indicates the name of component in the Devfile
+	// randomPorts indicates to affect random ports, instead of stable ports starting at 40001
+	// output will be written to errOut writer
 	StartPortForwarding(
 		devFileObj parser.DevfileObj,
 		componenentName string,
@@ -14,5 +18,6 @@ type Client interface {
 		errOut io.Writer,
 	) error
 
+	// StopPortForwarding stops the port forwarding
 	StopPortForwarding()
 }
