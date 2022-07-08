7ddfcba9dd823b65b1674f60593c49108804f892
anandrkskd <anandrkskd@gmail.com>
2022-07-08 18:29:15 +0530

    download golint in test script
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>

--- scripts/openshiftci-presubmit-unittests.sh
+++ scripts/openshiftci-presubmit-unittests.sh
@@ -11,6 +11,18 @@ export PATH=$PATH:$GOPATH/bin
 # otherwise /.cache is used, and it fails on permission denied
 export GOLANGCI_LINT_CACHE="/tmp/.cache"
 
+ARCH=`uname -i`
+if [ "$ARCH" = "x86_64" ]; then
+  export GOARCH=amd64
+else
+  export GOARCH=$ARCH
+fi
+
+wget https://github.com/golangci/golangci-lint/releases/download/v1.37.0/golangci-lint-1.37.0-linux-$GOARCH.tar.gz
+tar --no-same-owner -xzf golangci-lint-1.37.0-linux-amd64.tar.gz
+mv golangci-lint-1.37.0-linux-amd64/golangci-lint $(go env GOPATH)/bin/golangci-lint
+chmod +x $(go env GOPATH)/bin/golangci-lint
+
 make validate
 make test
 
