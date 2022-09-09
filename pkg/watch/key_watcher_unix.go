09a50d7b5d8b916df7128af4ba2302752867911f
Philippe Martin <phmartin@redhat.com>
2022-09-09 09:33:01 +0200

    Document enableCharInput

--- pkg/watch/key_watcher_unix.go
+++ pkg/watch/key_watcher_unix.go
@@ -7,6 +7,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// enableCharInput is inspired from the Unix implementation of MakeRaw in golang.org/x/term
+// It enables the treatment of input stream char by char instead of line by line
+// See https://man7.org/linux/man-pages/man3/termios.3.html for reference
 func enableCharInput(fd int) error {
 	termios, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
 	if err != nil {
--- pkg/watch/key_watcher_windows.go
+++ pkg/watch/key_watcher_windows.go
@@ -4,9 +4,9 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// makeRaw is forked from golang.org/x/term
-// Differences are:
-// - ctrl-c is enabled
+// enableCharInput is inspired from Windows implementation of MakeRaw in golang.org/x/term
+// It enables the treatment of input stream char by char instead of line by line
+// See https://docs.microsoft.com/en-us/windows/console/setconsolemode for reference
 func enableCharInput(fd int) error {
 	var st uint32
 	if err := windows.GetConsoleMode(windows.Handle(fd), &st); err != nil {
