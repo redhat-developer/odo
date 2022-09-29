17119be55d9f0b19edb4b70a8adf2b59c1fc58de
Philippe Martin <phmartin@redhat.com>
2022-09-29 13:25:43 +0200

    prow jobs scripts

---
+++ .prow/jobs/kubernetes-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+ibmcloud login --apikey "${IBM_API_KEY}"
+ibmcloud target -r "${IBM_REGION}"
+ibmcloud ks cluster config --cluster "${IBM_KUBERNETES_ID}" --admin
+
+export DEVFILE_PROXY="$(kubectl get svc -n devfile-proxy nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' || true)"
+echo Using Devfile proxy: ${DEVFILE_PROXY}
+
+PATH=${PATH}:${GOPATH}/bin
+make install
+
+export KUBERNETES=true
+make test-integration
+make test-e2e
---
+++ .prow/jobs/validate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+make goget-tools
+make validate
