17886112f1e7eadbeb51bf2e87073b944e86823d
Philippe Martin <phmartin@redhat.com>
2022-10-13 10:49:43 +0200

    More doc on context.Get* functions

--- pkg/odo/context/odo.go
+++ pkg/odo/context/odo.go
@@ -23,11 +23,14 @@ var (
 )
 
 // WithApplication sets the value of the application in ctx
+// This function must be used before using GetApplication
 func WithApplication(ctx context.Context, val string) context.Context {
 	return context.WithValue(ctx, applicationKey, val)
 }
 
 // GetApplication gets the application value in ctx
+// This function will panic if the context does not contain the value
+// Use this function only with a context obtained from Complete/Validate/Run/... methods of Runnable interface
 func GetApplication(ctx context.Context) string {
 	value := ctx.Value(applicationKey)
 	if cast, ok := value.(string); ok {
@@ -37,11 +40,15 @@ func GetApplication(ctx context.Context) string {
 }
 
 // WithWorkingDirectory sets the value of the working directory in ctx
+// This function must be used before calling GetWorkingDirectory
 func WithWorkingDirectory(ctx context.Context, val string) context.Context {
 	return context.WithValue(ctx, cwdKey, val)
 }
 
 // GetWorkingDirectory gets the working directory value in ctx
+// This function will panic if the context does not contain the value
+// Use this function only with a context obtained from Complete/Validate/Run/... methods of Runnable interface
+// and only if the runnable have added the FILESYSTEM dependency to its clientset
 func GetWorkingDirectory(ctx context.Context) string {
 	value := ctx.Value(cwdKey)
 	if cast, ok := value.(string); ok {
@@ -51,11 +58,15 @@ func GetWorkingDirectory(ctx context.Context) string {
 }
 
 // WithDevfilePath sets the value of the devfile path in ctx
+// This function must be called before using GetDevfilePath
 func WithDevfilePath(ctx context.Context, val string) context.Context {
 	return context.WithValue(ctx, devfilePathKey, val)
 }
 
 // GetDevfilePath gets the devfile path value in ctx
+// This function will panic if the context does not contain the value
+// Use this function only with a context obtained from Complete/Validate/Run/... methods of Runnable interface
+// and only if the runnable have added the FILESYSTEM dependency to its clientset
 func GetDevfilePath(ctx context.Context) string {
 	value := ctx.Value(devfilePathKey)
 	if cast, ok := value.(string); ok {
@@ -65,11 +76,15 @@ func GetDevfilePath(ctx context.Context) string {
 }
 
 // WithDevfileObj sets the value of the devfile object in ctx
+// This function must be called before using GetDevfileObj
 func WithDevfileObj(ctx context.Context, val *parser.DevfileObj) context.Context {
 	return context.WithValue(ctx, devfileObjKey, val)
 }
 
 // GetDevfileObj gets the devfile object value in ctx
+// This function will panic if the context does not contain the value
+// Use this function only with a context obtained from Complete/Validate/Run/... methods of Runnable interface
+// and only if the runnable have added the FILESYSTEM dependency to its clientset
 func GetDevfileObj(ctx context.Context) *parser.DevfileObj {
 	value := ctx.Value(devfileObjKey)
 	if cast, ok := value.(*parser.DevfileObj); ok {
@@ -79,11 +94,15 @@ func GetDevfileObj(ctx context.Context) *parser.DevfileObj {
 }
 
 // WithComponentName sets the name of the component in ctx
+// This function must be called before using GetComponentName
 func WithComponentName(ctx context.Context, val string) context.Context {
 	return context.WithValue(ctx, componentNameKey, val)
 }
 
 // GetComponentName gets the name of the component in ctx
+// This function will panic if the context does not contain the value
+// Use this function only with a context obtained from Complete/Validate/Run/... methods of Runnable interface
+// and only if the runnable have added the FILESYSTEM dependency to its clientset
 func GetComponentName(ctx context.Context) string {
 	value := ctx.Value(componentNameKey)
 	if cast, ok := value.(string); ok {
