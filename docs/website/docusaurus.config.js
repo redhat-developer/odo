11d426799a21ff51d73d58aeccbb817c84d2dcaa
Armel Soro <armel@rm3l.org>
2022-10-20 12:11:42 +0200

    Apply suggestions from code review
    Co-authored-by: Parthvi Vala <pvala@redhat.com>

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -132,12 +132,12 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '3.0.0',
+              label: 'v3',
               badge: true,
               banner: 'none',
             },
             '2.5.0': {
-              label: '2.5.1',
+              label: 'v2',
               path: '2.5.0',
               badge: true,
               banner: 'none',
