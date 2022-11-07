546f88634f630eeb80027066a59773880d806ad0
Philippe Martin <phmartin@redhat.com>
2022-11-07 10:04:05 +0100

    Add more TODOs

--- pkg/podman/logs.go
+++ pkg/podman/logs.go
@@ -5,5 +5,6 @@ import "io"
 // GetPodLogs returns the logs of the specified pod container.
 // All logs for all containers part of the pod are returned if an empty string is provided as container name.
 func (o *PodmanCli) GetPodLogs(podName, containerName string, followLog bool) (io.ReadCloser, error) {
+	// TODO(feloy)
 	return nil, nil
 }
--- pkg/podman/pods.go
+++ pkg/podman/pods.go
@@ -7,21 +7,25 @@ import (
 
 // GetPodsMatchingSelector returns all pods matching the given label selector.
 func (o *PodmanCli) GetPodsMatchingSelector(selector string) (*corev1.PodList, error) {
+	// TODO(feloy) when pod is created with labels
 	return nil, nil
 }
 
 // GetAllResourcesFromSelector returns all resources of any kind matching the given label selector.
 func (o *PodmanCli) GetAllResourcesFromSelector(selector string, ns string) ([]unstructured.Unstructured, error) {
+	// TODO(feloy) when pod is created with labels
 	return nil, nil
 }
 
 // GetAllPodsInNamespaceMatchingSelector returns all pods matching the given label selector and in the specified namespace.
 func (o *PodmanCli) GetAllPodsInNamespaceMatchingSelector(selector string, ns string) (*corev1.PodList, error) {
+	// TODO(feloy) when pod is created with labels
 	return nil, nil
 }
 
 // GetRunningPodFromSelector returns any pod matching the given label selector.
 // If multiple pods are found, implementations might have different behavior, by either returning an error or returning any element.
 func (o *PodmanCli) GetRunningPodFromSelector(selector string) (*corev1.Pod, error) {
+	// TODO(feloy) when pod is created with labels
 	return nil, nil
 }
