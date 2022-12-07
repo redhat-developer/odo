95965d1b7fb8bc4a2f20a67143adb84c7d40c269
Philippe Martin <phmartin@redhat.com>
2022-12-07 18:50:52 +0100

    Display a warning when connection to cluster fails

--- pkg/odo/cli/errors/errors.go
+++ pkg/odo/cli/errors/errors.go
@@ -1,6 +1,9 @@
 package errors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type NoCommandInDevfileError struct {
 	command string
@@ -14,4 +17,29 @@ func NewNoCommandInDevfileError(command string) NoCommandInDevfileError {
 
 func (o NoCommandInDevfileError) Error() string {
 	return fmt.Sprintf("no command of kind %q found in the devfile", o.command)
+}
+
+type Warning struct {
+	msg string
+	err error
+}
+
+func NewWarning(msg string, err error) Warning {
+	return Warning{
+		msg: msg,
+		err: err,
+	}
+}
+
+func (o Warning) Error() string {
+	return fmt.Errorf("%s: %w", o.msg, o.err).Error()
+}
+
+func IsWarning(err error) bool {
+	_, ok := err.(Warning)
+	return ok
+}
+
+func AsWarning(err error) bool {
+	return errors.As(err, &Warning{})
 }
