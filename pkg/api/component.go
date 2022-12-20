ebbc1c1460331a24013d7178b09972da3064a0e0
Armel Soro <asoro@redhat.com>
2022-12-20 17:32:54 +0100

    Add RunningOn field to the Component API

--- pkg/api/component.go
+++ pkg/api/component.go
@@ -2,13 +2,19 @@ package api
 
 // Component describes the state of a devfile component
 type Component struct {
-	DevfilePath       string           `json:"devfilePath,omitempty"`
-	DevfileData       *DevfileData     `json:"devfileData,omitempty"`
-	DevForwardedPorts []ForwardedPort  `json:"devForwardedPorts,omitempty"`
-	RunningIn         RunningModes     `json:"runningIn"`
-	Ingresses         []ConnectionData `json:"ingresses,omitempty"`
-	Routes            []ConnectionData `json:"routes,omitempty"`
-	ManagedBy         string           `json:"managedBy"`
+	DevfilePath       string          `json:"devfilePath,omitempty"`
+	DevfileData       *DevfileData    `json:"devfileData,omitempty"`
+	DevForwardedPorts []ForwardedPort `json:"devForwardedPorts,omitempty"`
+	// RunningIn is the overall running mode map of the component;
+	// this is computing as a merge of RunningOn (all the different running modes
+	// for each platform the component is running on).
+	RunningIn RunningModes `json:"runningIn"`
+	// RunningOn represents the map of running modes for each platform the component is running on.
+	// The key is the platform, either cluster or podman.
+	RunningOn map[string]RunningModes `json:"runningOn,omitempty"`
+	Ingresses []ConnectionData        `json:"ingresses,omitempty"`
+	Routes    []ConnectionData        `json:"routes,omitempty"`
+	ManagedBy string                  `json:"managedBy"`
 }
 
 type ForwardedPort struct {
