0dcdf2bc4e40ccd73a393423e519f3806d78c253
Parthvi Vala <pvala@redhat.com>
2022-12-27 21:31:22 +0530

    Attempt at fixing validation test
    Signed-off-by: Parthvi Vala <pvala@redhat.com>

--- tests/helper/helper_documentation.go
+++ tests/helper/helper_documentation.go
@@ -49,8 +49,8 @@ func GetMDXContent(filePath string) (mdxContent string) {
 	mdxDir := filepath.Join(filepath.Dir(filename), "..", "..", "docs", "website", "docs")
 
 	readFile, err := os.Open(filepath.Join(mdxDir, filePath))
-	Expect(err).ToNot(HaveOccurred())
 	defer readFile.Close()
+	Expect(err).ToNot(HaveOccurred())
 
 	fileScanner := bufio.NewScanner(readFile)
 
