20ed3edf6ee8d78c08ca65be058a3fe693998d66
Philippe Martin <phmartin@redhat.com>
2023-01-09 16:26:03 +0100

    Implement GetPodLogs for podman

--- pkg/podman/logs.go
+++ pkg/podman/logs.go
@@ -1,10 +1,35 @@
 package podman
 
-import "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os/exec"
+
+	"k8s.io/klog"
+)
 
 // GetPodLogs returns the logs of the specified pod container.
 // All logs for all containers part of the pod are returned if an empty string is provided as container name.
 func (o *PodmanCli) GetPodLogs(podName, containerName string, followLog bool) (io.ReadCloser, error) {
-	// TODO(feloy)
-	return nil, nil
+	// TODO(feloy) implement followLog = true
+	args := []string{"pod", "logs"}
+	if containerName != "" {
+		args = append(args, "--container", podName+"-"+containerName)
+	}
+	args = append(args, podName)
+
+	cmd := exec.Command(o.podmanCmd, args...)
+	klog.V(3).Infof("executing %v", cmd.Args)
+	// We get the commbined output as podman logs outputs logs in stdout && stderr (when kubectl outputs all logs on stdout)
+	resultBytes, err := cmd.CombinedOutput()
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			err = fmt.Errorf("%s: %s", err, string(exiterr.Stderr))
+		}
+		return nil, err
+	}
+
+	rd := io.NopCloser(bytes.NewReader(resultBytes))
+	return rd, nil
 }
