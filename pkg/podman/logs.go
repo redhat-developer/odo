e635675b53904b3c0e84eefc55bcad631c245fef
Philippe Martin <contact@elol.fr>
2023-01-31 15:50:08 +0100

    Update pkg/podman/logs.go
    Co-authored-by: Armel Soro <armel@rm3l.org>

--- pkg/podman/logs.go
+++ pkg/podman/logs.go
@@ -10,7 +10,6 @@ import (
 // GetPodLogs returns the logs of the specified pod container.
 // All logs for all containers part of the pod are returned if an empty string is provided as container name.
 func (o *PodmanCli) GetPodLogs(podName, containerName string, followLog bool) (io.ReadCloser, error) {
-	// TODO(feloy) implement followLog = true
 	args := []string{"pod", "logs"}
 	if followLog {
 		args = append(args, "--follow")
