4af5213029e65c9bb1cd4f9d347f9b704ead668b
Parthvi Vala <pvala@redhat.com>
2023-02-08 16:13:33 +0530

    Add integration test
    Signed-off-by: Parthvi Vala <pvala@redhat.com>
    Co-authored-by: Armel Soro <asoro@redhat.com>

--- tests/integration/cmd_dev_test.go
+++ tests/integration/cmd_dev_test.go
@@ -2923,41 +2923,39 @@ CMD ["npm", "start"]
 			})
 		}))
 	}
-	for _, ctx := range []struct {
-		title           string
-		resources, args []string
-	}{
-		{
-			title:     "with run command",
-			resources: []string{"deploy-k8s-resource", "deploy-a-third-k8s-resource", "image-build-component"},
-		},
-		{
-			title:     "with debug command",
-			resources: []string{"deploy-another-k8s-resource", "deploy-a-third-k8s-resource", "image-build-component"},
-			args:      []string{"--debug"},
-		},
-	} {
-		ctx := ctx
-		When("using devfile that contains K8s resource to run it on podman", Label(helper.LabelPodman), func() {
-			BeforeEach(func() {
-				helper.CopyExample(filepath.Join("source", "devfiles", "nodejs", "project"), commonVar.Context)
-				helper.CopyExampleDevFile(
-					filepath.Join("source", "devfiles", "nodejs", "devfile-composite-apply-different-commandgk.yaml"),
-					filepath.Join(commonVar.Context, "devfile.yaml"),
-					helper.DevfileMetadataNameSetter(cmpName))
-			})
-			It(fmt.Sprintf("should show warning about being unable to create the resource when running odo dev %s on podman", ctx.title), func() {
-				err := helper.RunDevMode(helper.DevSessionOpts{RunOnPodman: true, CmdlineArgs: ctx.args}, func(session *gexec.Session, outContents, errContents []byte, ports map[string]string) {
-					Expect(string(errContents)).To(ContainSubstring("Kubernetes components are not supported on Podman. Skipping: "))
-					Expect(string(errContents)).To(ContainSubstring("Apply Kubernetes components are not supported on Podman. Skipping: "))
-					Expect(string(errContents)).To(ContainSubstring("Apply Image commands are not implemented on Podman. Skipping: "))
-					helper.MatchAllInOutput(string(errContents), ctx.resources)
-				})
-				Expect(err).ToNot(HaveOccurred())
-			})
+
+	When("using devfile that contains K8s resource to run it on podman", Label(helper.LabelPodman), func() {
+		var session helper.DevSession
+		var outContents, errContents []byte
+		BeforeEach(func() {
+			helper.CopyExample(filepath.Join("source", "nodejs"), commonVar.Context)
+			helper.CopyExampleDevFile(
+				filepath.Join("source", "devfiles", "nodejs", "devfile-composite-apply-different-commandgk.yaml"),
+				filepath.Join(commonVar.Context, "devfile.yaml"),
+				helper.DevfileMetadataNameSetter(cmpName),
+			)
+			var err error
+			session, outContents, errContents, _, err = helper.StartDevMode(
+				helper.DevSessionOpts{RunOnPodman: true, EnvVars: []string{"ODO_PUSH_IMAGES=false"}},
+			)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		AfterEach(func() {
+			session.Stop()
+			session.WaitEnd()
+		})
+		It(fmt.Sprintf("should show warning about being unable to create the resource when running odo dev on podman"), func() {
+			Expect(string(errContents)).To(ContainSubstring("Kubernetes components are not supported on Podman. Skipping: "))
+			Expect(string(errContents)).To(ContainSubstring("Apply Kubernetes components are not supported on Podman. Skipping: "))
+			helper.MatchAllInOutput(string(errContents), []string{"deploy-k8s-resource", "deploy-a-third-k8s-resource"})
 		})
 
-	}
+		It(fmt.Sprintf("should build the images when running odo dev on podman"), func() {
+			// we do not test push because then it becomes complex to setup image registry credentials to pull the image
+			// all pods created by odo have a `PullAlways` image policy.
+			Expect(outContents).To(ContainSubstring("Building & Pushing Container:"))
+		})
+	})
 
 	for _, podman := range []bool{true, false} {
 		podman := podman
