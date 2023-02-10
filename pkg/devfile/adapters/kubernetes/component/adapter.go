b07992d01529c240483938eaed6c82e2d1b5cf66
Armel Soro <asoro@redhat.com>
2023-02-10 15:41:04 +0100

    Update portForward.Client interface methods
    Specifically, the 'StartPortForwarding' method
    can now accept an explicit list of ports that needs to
    be forwarded, if the caller can compute provide such information.
    
    This is currently useful on Podman where the ports
    (even the random ones) are known in advance.

--- pkg/devfile/adapters/kubernetes/component/adapter.go
+++ pkg/devfile/adapters/kubernetes/component/adapter.go
@@ -338,10 +338,10 @@ func (a Adapter) Push(ctx context.Context, parameters adapters.PushParameters, c
 	}
 
 	if podChanged || portsChanged {
-		a.portForwardClient.StopPortForwarding()
+		a.portForwardClient.StopPortForwarding(a.ComponentName)
 	}
 
-	err = a.portForwardClient.StartPortForwarding(a.Devfile, a.ComponentName, parameters.Debug, parameters.RandomPorts, parameters.ErrOut)
+	err = a.portForwardClient.StartPortForwarding(a.Devfile, a.ComponentName, parameters.Debug, parameters.RandomPorts, log.GetStdout(), parameters.ErrOut, nil)
 	if err != nil {
 		return adapters.NewErrPortForward(err)
 	}
