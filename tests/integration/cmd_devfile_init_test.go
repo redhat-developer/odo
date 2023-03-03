dd6b84ffa0f2fff828dea36011e0e29f925cc592
Parthvi Vala <pvala@redhat.com>
2023-03-03 16:46:08 +0530

    Use proxy when available
    Signed-off-by: Parthvi Vala <pvala@redhat.com>
    
    Co-authored-by: Armel Soro <armel@rm3l.org>

--- tests/integration/cmd_devfile_init_test.go
+++ tests/integration/cmd_devfile_init_test.go
@@ -587,11 +587,28 @@ var _ = Describe("odo devfile init command tests", func() {
 
 		When("CR for devfileregistrieslists is installed in namespace", func() {
 			BeforeEach(func() {
-				command := commonVar.CliRunner.Run("apply", "-f", helper.GetExamplePath("manifests", "devfileRegistryListCR.yaml"))
+				content := fmt.Sprintf(`
+apiVersion: registry.devfile.io/v1alpha1
+kind: DevfileRegistriesList
+metadata:
+  name: namespace-list
+spec:
+  devfileRegistries:
+    - name: devfile-staging
+      url: '%[1]s'
+    - name: second-devfile-reg
+      url: '%[1]s'
+      skipTLSVerify: true
+`, helper.GetDevfileRegistryURL())
+				manifestFilePath := filepath.Join(commonVar.ConfigDir, "devfileRegistryListCR.yaml")
+				err := helper.CreateFileWithContent(manifestFilePath, content)
+				Expect(err).ToNot(HaveOccurred())
+				command := commonVar.CliRunner.Run("apply", "-f", manifestFilePath)
 				Expect(command.ExitCode()).To(BeEquivalentTo(0))
 			})
-			FIt("should be able to download devfile from the in-cluster registry", func() {
-				helper.Cmd("odo", "init", "--devfile-registry", inClusterDevfileRegName, "--devfile", "go", "--name", "go-devfile").ShouldPass()
+			It("should be able to download devfile from the in-cluster registry", func() {
+				out := helper.Cmd("odo", "init", "--devfile-registry", inClusterDevfileRegName, "--devfile", "go", "--name", "go-devfile").ShouldPass().Out()
+				Expect(out).To(ContainSubstring("Downloading devfile \"go\" from registry \"second-devfile-reg\""))
 				helper.VerifyFileExists(filepath.Join(commonVar.Context, "devfile.yaml"))
 			})
 		})
