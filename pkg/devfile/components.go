cbbd52a53bb3a773accb8586fbaca12f983d9e64
Armel Soro <asoro@redhat.com>
2023-03-10 10:02:06 +0100

    Handle deployByDefault on K8s and OpenShift components

--- pkg/devfile/components.go
+++ pkg/devfile/components.go
@@ -7,16 +7,19 @@ import (
 )
 
 // GetK8sAndOcComponentsToPush returns the list of Kubernetes and OpenShift components to push,
-// by getting the list of Kubernetes and OpenShift components and removing the ones
-// referenced from a command in the devfile
-// It takes an additional allowApply boolean, which set to true, will append the components from apply command to the list
+// The list returned is governed by the DeployByDefault field in each component.
+// All components with DeployByDefault set to true are included, along with those with no DeployByDefault set and not-referenced.
+// It takes an additional allowApply boolean, which set to true, will append the components referenced from apply commands to the list.
 func GetK8sAndOcComponentsToPush(devfileObj parser.DevfileObj, allowApply bool) ([]devfilev1.Component, error) {
+	var allComponents []devfilev1.Component
+
 	k8sComponents, err := devfileObj.Data.GetComponents(parsercommon.DevfileOptions{
 		ComponentOptions: parsercommon.ComponentOptions{ComponentType: devfilev1.KubernetesComponentType},
 	})
 	if err != nil {
 		return nil, err
 	}
+	allComponents = append(allComponents, k8sComponents...)
 
 	ocComponents, err := devfileObj.Data.GetComponents(parsercommon.DevfileOptions{
 		ComponentOptions: parsercommon.ComponentOptions{ComponentType: devfilev1.OpenshiftComponentType},
@@ -24,40 +27,60 @@ func GetK8sAndOcComponentsToPush(devfileObj parser.DevfileObj, allowApply bool) 
 	if err != nil {
 		return nil, err
 	}
-
-	allComponents := []devfilev1.Component{}
-	allComponents = append(allComponents, k8sComponents...)
 	allComponents = append(allComponents, ocComponents...)
 
-	componentsMap := map[string]devfilev1.Component{}
-	for _, component := range allComponents {
-		componentsMap[component.Name] = component
-	}
-
-	commands, err := devfileObj.Data.GetCommands(parsercommon.DevfileOptions{})
+	allApplyCommands, err := devfileObj.Data.GetCommands(parsercommon.DevfileOptions{
+		CommandOptions: parsercommon.CommandOptions{CommandType: devfilev1.ApplyCommandType},
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	for _, command := range commands {
-		componentName := ""
-		if command.Exec != nil {
-			componentName = command.Exec.Component
-		} else if !allowApply && command.Apply != nil {
-			componentName = command.Apply.Component
-		}
-		if componentName == "" {
+	m := make(map[string]devfilev1.Component)
+	for _, comp := range allComponents {
+		if comp.Kubernetes == nil && comp.Openshift == nil {
 			continue
 		}
-		delete(componentsMap, componentName)
+		var k devfilev1.K8sLikeComponent
+		if comp.Kubernetes != nil {
+			k = comp.Kubernetes.K8sLikeComponent
+		} else {
+			k = comp.Openshift.K8sLikeComponent
+		}
+		var add bool
+		if allowApply && isComponentReferenced(allApplyCommands, comp.Name) {
+			add = true
+		} else if k.DeployByDefault == nil {
+			// auto-created only if not referenced by any apply command
+			if !isComponentReferenced(allApplyCommands, comp.Name) {
+				add = true
+			}
+		} else if *k.DeployByDefault {
+			add = true
+		}
+		if !add {
+			continue
+		}
+		if _, present := m[comp.Name]; !present {
+			m[comp.Name] = comp
+		}
 	}
 
-	allComponentsToPush := make([]devfilev1.Component, len(componentsMap))
-	i := 0
-	for _, v := range componentsMap {
-		allComponentsToPush[i] = v
-		i++
+	var result []devfilev1.Component
+	for _, comp := range m {
+		result = append(result, comp)
 	}
+	return result, nil
+}
 
-	return allComponentsToPush, err
+func isComponentReferenced(allApplyCommands []devfilev1.Command, cmpName string) bool {
+	for _, cmd := range allApplyCommands {
+		if cmd.Apply == nil {
+			continue
+		}
+		if cmd.Apply.Component == cmpName {
+			return true
+		}
+	}
+	return false
 }
--- pkg/devfile/components_test.go
+++ pkg/devfile/components_test.go
@@ -3,8 +3,11 @@ package devfile
 import (
 	"testing"
 
+	devfilepkg "github.com/devfile/api/v2/pkg/devfile"
+	"github.com/devfile/library/v2/pkg/devfile/parser/data"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"k8s.io/utils/pointer"
 
 	devfiletesting "github.com/redhat-developer/odo/pkg/devfile/testing"
 
@@ -14,151 +17,213 @@ import (
 	devfileFileSystem "github.com/devfile/library/v2/pkg/testingutil/filesystem"
 )
 
-func TestGetKubernetesComponentsToPush(t *testing.T) {
+func TestGetK8sAndOcComponentsToPush(t *testing.T) {
 	fs := devfileFileSystem.NewFakeFs()
 
-	getDevfileWithoutApplyCommand := func() parser.DevfileObj {
-		devfileObj := parser.DevfileObj{
-			Data: devfiletesting.GetDevfileData(t, []devfiletesting.InlinedComponent{
-				{
-					Name:    "component1",
-					Inlined: "Component 1",
-				},
-			}, nil),
-			Ctx: devfileCtx.FakeContext(fs, parser.OutputDevfileYamlPath),
-		}
-		return devfileObj
-	}
-
-	getDevfileWithApplyCommand := func(applyComponentName string) parser.DevfileObj {
-		const defaultComponentName = "component1"
-		devfileObj := parser.DevfileObj{
-			Data: devfiletesting.GetDevfileData(t, []devfiletesting.InlinedComponent{
-				{
-					Name:    defaultComponentName,
-					Inlined: "Component 1",
-				},
-			}, nil),
-			Ctx: devfileCtx.FakeContext(fs, parser.OutputDevfileYamlPath),
-		}
-		applyCommand := devfilev1.Command{
-			CommandUnion: devfilev1.CommandUnion{
-				Apply: &devfilev1.ApplyCommand{
-					Component: applyComponentName,
-				},
+	buildK8sOrOcComponent := func(k8s bool, name string, deployByDefault *bool, referenced bool) (devfilev1.Component, devfilev1.Command) {
+		k8sLikeComponent := devfilev1.K8sLikeComponent{
+			DeployByDefault: deployByDefault,
+			K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
+				Inlined: name,
 			},
 		}
-		if applyComponentName != defaultComponentName {
-			_ = devfileObj.Data.AddComponents([]devfilev1.Component{
-				{
-					Name: applyComponentName,
-					ComponentUnion: devfilev1.ComponentUnion{
-						Kubernetes: &devfilev1.KubernetesComponent{
-							K8sLikeComponent: devfilev1.K8sLikeComponent{
-								K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
-									Inlined: applyComponentName,
-								},
-							},
+		comp := devfilev1.Component{Name: name}
+		if k8s {
+			comp.ComponentUnion.Kubernetes = &devfilev1.KubernetesComponent{K8sLikeComponent: k8sLikeComponent}
+		} else {
+			comp.ComponentUnion.Openshift = &devfilev1.OpenshiftComponent{K8sLikeComponent: k8sLikeComponent}
+		}
+		if referenced {
+			cmd := devfilev1.Command{
+				Id: "apply-" + name,
+				CommandUnion: devfilev1.CommandUnion{
+					Apply: &devfilev1.ApplyCommand{
+						Component: name,
+					},
+				},
+			}
+			return comp, cmd
+		}
+		return comp, devfilev1.Command{}
+	}
+
+	var (
+		k8sDeployByDefaultTrueReferenced, applyK8sDeployByDefaultTrueReferenced = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultTrueReferenced", pointer.Bool(true), true)
+		ocDeployByDefaultTrueReferenced, applyOcDeployByDefaultTrueReferenced = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultTrueReferenced", pointer.Bool(true), true)
+
+		k8sDeployByDefaultTrueNotReferenced, _ = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultTrueNotReferenced", pointer.Bool(true), false)
+		ocDeployByDefaultTrueNotReferenced, _ = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultTrueNotReferenced", pointer.Bool(true), false)
+
+		k8sDeployByDefaultFalseReferenced, applyK8sDeployByDefaultFalseReferenced = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultFalseReferenced", pointer.Bool(false), true)
+		ocDeployByDefaultFalseReferenced, applyOcDeployByDefaultFalseReferenced = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultFalseReferenced", pointer.Bool(false), true)
+
+		k8sDeployByDefaultFalseNotReferenced, _ = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultFalseNotReferenced", pointer.Bool(false), false)
+		ocDeployByDefaultFalseNotReferenced, _ = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultFalseNotReferenced", pointer.Bool(false), false)
+
+		k8sDeployByDefaultNotSetReferenced, applyK8sDeployByDefaultNotSetReferenced = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultNotSetReferenced", nil, true)
+		ocDeployByDefaultNotSetReferenced, applyOcDeployByDefaultNotSetReferenced = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultNotSetReferenced", nil, true)
+
+		k8sDeployByDefaultNotSetNotReferenced, _ = buildK8sOrOcComponent(
+			true, "k8sDeployByDefaultNotSetNotReferenced", nil, false)
+		ocDeployByDefaultNotSetNotReferenced, _ = buildK8sOrOcComponent(
+			false, "ocDeployByDefaultNotSetNotReferenced", nil, false)
+	)
+
+	buildFullDevfile := func() (parser.DevfileObj, error) {
+		devfileData, err := data.NewDevfileData(string(data.APISchemaVersion220))
+		if err != nil {
+			return parser.DevfileObj{}, err
+		}
+		devfileData.SetMetadata(devfilepkg.DevfileMetadata{Name: "my-devfile"})
+		err = devfileData.AddComponents([]devfilev1.Component{
+			k8sDeployByDefaultNotSetNotReferenced,
+			k8sDeployByDefaultNotSetReferenced,
+			ocDeployByDefaultNotSetReferenced,
+			ocDeployByDefaultNotSetNotReferenced,
+
+			k8sDeployByDefaultTrueNotReferenced,
+			k8sDeployByDefaultTrueReferenced,
+			ocDeployByDefaultTrueReferenced,
+			ocDeployByDefaultTrueNotReferenced,
+
+			k8sDeployByDefaultFalseNotReferenced,
+			k8sDeployByDefaultFalseReferenced,
+			ocDeployByDefaultFalseReferenced,
+			ocDeployByDefaultFalseNotReferenced,
+
+			//Add other kinds of components
+			{
+				Name: "my-image-component",
+				ComponentUnion: devfilev1.ComponentUnion{
+					Image: &devfilev1.ImageComponent{
+						Image: devfilev1.Image{
+							ImageName: "image-component-1",
 						},
 					},
 				},
-			})
+			},
+			{
+				Name: "container-component",
+				ComponentUnion: devfilev1.ComponentUnion{
+					Container: &devfilev1.ContainerComponent{
+						Container: devfilev1.Container{
+							DedicatedPod: pointer.Bool(true),
+							Image:        "my-container-image",
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			return parser.DevfileObj{}, err
 		}
-		_ = devfileObj.Data.AddCommands([]devfilev1.Command{applyCommand})
-		return devfileObj
+		err = devfileData.AddCommands([]devfilev1.Command{
+			applyK8sDeployByDefaultNotSetReferenced,
+			applyOcDeployByDefaultNotSetReferenced,
+			applyK8sDeployByDefaultTrueReferenced,
+			applyOcDeployByDefaultTrueReferenced,
+			applyK8sDeployByDefaultFalseReferenced,
+			applyOcDeployByDefaultFalseReferenced,
+
+			//Add other kinds of components
+			{
+				Id: "apply-image",
+				CommandUnion: devfilev1.CommandUnion{
+					Apply: &devfilev1.ApplyCommand{
+						Component: "my-image-component",
+					},
+				},
+			},
+			{
+				Id: "exec-command",
+				CommandUnion: devfilev1.CommandUnion{
+					Apply: &devfilev1.ApplyCommand{
+						Component: "my-image-component",
+					},
+					Exec: &devfilev1.ExecCommand{
+						CommandLine: "/path/to/my/command -success",
+						Component:   "container-component",
+					},
+				},
+			},
+		})
+		if err != nil {
+			return parser.DevfileObj{}, err
+		}
+		return parser.DevfileObj{
+			Ctx:  devfileCtx.FakeContext(fs, parser.OutputDevfileYamlPath),
+			Data: devfileData,
+		}, nil
+	}
+
+	type args struct {
+		devfileObj func() (parser.DevfileObj, error)
+		allowApply bool
 	}
 
 	tests := []struct {
-		name       string
-		devfileObj parser.DevfileObj
-		want       []devfilev1.Component
-		allowApply bool
-		wantErr    bool
+		name    string
+		args    args
+		want    []devfilev1.Component
+		wantErr bool
 	}{
 		{
 			name: "empty devfile",
-			devfileObj: parser.DevfileObj{
-				Data: devfiletesting.GetDevfileData(t, nil, nil),
-				Ctx:  devfileCtx.FakeContext(fs, parser.OutputDevfileYamlPath),
+			args: args{
+				devfileObj: func() (parser.DevfileObj, error) {
+					return parser.DevfileObj{
+						Data: devfiletesting.GetDevfileData(t, nil, nil),
+						Ctx:  devfileCtx.FakeContext(fs, parser.OutputDevfileYamlPath),
+					}, nil
+				},
 			},
 			want:    []devfilev1.Component{},
 			wantErr: false,
 		},
 		{
-			name:       "no apply command",
-			devfileObj: getDevfileWithoutApplyCommand(),
-			want: []devfilev1.Component{
-				{
-					Name: "component1",
-					ComponentUnion: devfilev1.ComponentUnion{
-						Kubernetes: &devfilev1.KubernetesComponent{
-							K8sLikeComponent: devfilev1.K8sLikeComponent{
-								K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
-									Inlined: "Component 1",
-								},
-							},
-						},
-					},
-				},
+			name: "allowApply=false => return components that need to be created automatically on startup",
+			args: args{
+				devfileObj: buildFullDevfile,
+				allowApply: false,
 			},
-			wantErr: false,
+			want: []devfilev1.Component{
+				k8sDeployByDefaultTrueNotReferenced,
+				k8sDeployByDefaultTrueReferenced,
+				ocDeployByDefaultTrueNotReferenced,
+				ocDeployByDefaultTrueReferenced,
+				k8sDeployByDefaultNotSetNotReferenced,
+				ocDeployByDefaultNotSetNotReferenced,
+			},
 		},
 		{
-			name:       "apply command referencing the component",
-			devfileObj: getDevfileWithApplyCommand("component1"),
-			want:       []devfilev1.Component{},
-			wantErr:    false,
-		},
-		{
-			name:       "apply command not referencing the component",
-			devfileObj: getDevfileWithApplyCommand("other component"),
-			want: []devfilev1.Component{
-				{
-					Name: "component1",
-					ComponentUnion: devfilev1.ComponentUnion{
-						Kubernetes: &devfilev1.KubernetesComponent{
-							K8sLikeComponent: devfilev1.K8sLikeComponent{
-								K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
-									Inlined: "Component 1",
-								},
-							},
-						},
-					},
-				},
+			name: "allowApply=true => return components that need to be created automatically on startup and those referenced",
+			args: args{
+				devfileObj: buildFullDevfile,
+				allowApply: true,
 			},
-			wantErr: false,
-		},
-		{
-			name:       "allow component referenced by apply command when allowApply is true",
-			devfileObj: getDevfileWithApplyCommand("component2"),
-			allowApply: true,
 			want: []devfilev1.Component{
-				{
-					Name: "component1",
-					ComponentUnion: devfilev1.ComponentUnion{
-						Kubernetes: &devfilev1.KubernetesComponent{
-							K8sLikeComponent: devfilev1.K8sLikeComponent{
-								K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
-									Inlined: "Component 1",
-								},
-							},
-						},
-					},
-				},
-				{
-					Name: "component2",
-					ComponentUnion: devfilev1.ComponentUnion{
-						Kubernetes: &devfilev1.KubernetesComponent{
-							K8sLikeComponent: devfilev1.K8sLikeComponent{
-								K8sLikeComponentLocation: devfilev1.K8sLikeComponentLocation{
-									Inlined: "component2",
-								},
-							},
-						},
-					},
-				},
+				k8sDeployByDefaultTrueNotReferenced,
+				k8sDeployByDefaultTrueReferenced,
+				ocDeployByDefaultTrueNotReferenced,
+				ocDeployByDefaultTrueReferenced,
+				k8sDeployByDefaultNotSetNotReferenced,
+				ocDeployByDefaultNotSetNotReferenced,
+
+				k8sDeployByDefaultFalseReferenced,
+				ocDeployByDefaultFalseReferenced,
+				k8sDeployByDefaultNotSetReferenced,
+				ocDeployByDefaultNotSetReferenced,
 			},
-			wantErr: false,
 		},
 	}
 
@@ -168,7 +233,12 @@ func TestGetKubernetesComponentsToPush(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetK8sAndOcComponentsToPush(tt.devfileObj, tt.allowApply)
+			devfileObj, err := tt.args.devfileObj()
+			if err != nil {
+				t.Errorf("unable to create Devfile object: %v", err)
+				return
+			}
+			got, err := GetK8sAndOcComponentsToPush(devfileObj, tt.args.allowApply)
 			gotErr := err != nil
 			if gotErr != tt.wantErr {
 				t.Errorf("Got error %v, expected %v\n", err, tt.wantErr)
@@ -178,7 +248,7 @@ func TestGetKubernetesComponentsToPush(t *testing.T) {
 				t.Errorf("Got %d components, expected %d\n", len(got), len(tt.want))
 			}
 
-			if diff := cmp.Diff(tt.want, got, cmpopts.SortSlices(lessFunc)); diff != "" {
+			if diff := cmp.Diff(tt.want, got, cmpopts.EquateEmpty(), cmpopts.SortSlices(lessFunc)); diff != "" {
 				t.Errorf("GetK8sAndOcComponentsToPush() mismatch (-want +got):\n%s", diff)
 			}
 		})
