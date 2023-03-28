18892a83d51a13e6b9905fae898941328caa6be7
Parthvi Vala <pvala@redhat.com>
2023-03-28 20:56:12 +0530

    Add a new line before warning, and use corev1.NamespaceDefault instead of default
    Signed-off-by: Parthvi Vala <pvala@redhat.com>

--- pkg/odo/genericclioptions/util.go
+++ pkg/odo/genericclioptions/util.go
@@ -1,9 +1,11 @@
 package genericclioptions
 
 import (
+	"fmt"
 	"github.com/redhat-developer/odo/pkg/kclient"
 	"github.com/redhat-developer/odo/pkg/log"
 	pkgUtil "github.com/redhat-developer/odo/pkg/util"
+	v1 "k8s.io/api/core/v1"
 
 	dfutil "github.com/devfile/library/v2/pkg/util"
 )
@@ -39,12 +41,12 @@ func ApplyIgnore(ignores *[]string, sourcePath string) (err error) {
 
 // WarnIfDefaultNamespace warns when user tries to run `odo dev` or `odo deploy` in the default namespace
 func WarnIfDefaultNamespace(namespace string, kubeClient kclient.ClientInterface) {
-	if namespace == "default" {
+	if namespace == v1.NamespaceDefault {
 		noun := "namespace"
 		if isOC, _ := kubeClient.IsProjectSupported(); isOC {
 			noun = "project"
 		}
-
+		fmt.Println()
 		log.Warningf("You are using \"default\" %[1]s, odo may not work as expected in the default %[1]s.", noun)
 		log.Warningf("You may set a new %[1]s by running `odo create %[1]s <name>`, or set an existing one by running `odo set %[1]s <name>`", noun)
 	}
