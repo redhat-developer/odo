a85e96902d298cb0a2002e8f72f497202be1a9e2
Armel Soro <asoro@redhat.com>
2023-04-05 17:42:58 +0200

    Simplify parsing the output on Podman in the tests when looking for K8s/OpenShift components

--- tests/helper/helper_podman.go
+++ tests/helper/helper_podman.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	. "github.com/onsi/gomega"
 )
@@ -17,4 +19,26 @@ namespace=%q
 `, ns))
 	Expect(err).ShouldNot(HaveOccurred())
 	os.Setenv("CONTAINERS_CONF", containersConfPath)
+}
+
+// ExtractK8sAndOcComponentsFromOutputOnPodman extracts the list of Kubernetes and OpenShift components from the "odo" output on Podman.
+func ExtractK8sAndOcComponentsFromOutputOnPodman(out string) []string {
+	lines, err := ExtractLines(out)
+	Expect(err).ShouldNot(HaveOccurred())
+
+	var handled []string
+	// Example lines to match:
+	// ⚠ Kubernetes components are not supported on Podman. Skipping: k8s-deploybydefault-true-and-referenced, k8s-deploybydefault-true-and-not-referenced.
+	// ⚠ OpenShift components are not supported on Podman. Skipping: ocp-deploybydefault-true-and-referenced.
+	// ⚠  Apply OpenShift components are not supported on Podman. Skipping: k8s-deploybydefault-true-and-referenced.
+	// ⚠  Apply OpenShift components are not supported on Podman. Skipping: k8s-deploybydefault-true-and-referenced.
+	re := regexp.MustCompile(`(?:Kubernetes|OpenShift) components are not supported on Podman\.\s*Skipping:\s*([^\n]+)\.`)
+	for _, l := range lines {
+		matches := re.FindStringSubmatch(l)
+		if len(matches) > 1 {
+			handled = append(handled, strings.Split(matches[1], ", ")...)
+		}
+	}
+
+	return handled
 }
