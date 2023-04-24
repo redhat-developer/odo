cc396bdfc14d743aa7a304fb8ba6d6b414182f11
Ritu Deshmukh <rideshmu@redhat.com>
2023-04-24 13:32:05 +0530

    Changes to be added
    Signed-off-by: Ritu Deshmukh <rideshmu@redhat.com>

--- tests/documentation/command-reference/doc_command_reference_deletenamespace_test.go
+++ tests/documentation/command-reference/doc_command_reference_deletenamespace_test.go
@@ -3,11 +3,11 @@ package docautomation
 import (
 	"fmt"
 	"path/filepath"
-	"strings"
 
 	"github.com/google/go-cmp/cmp"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	"github.com/redhat-developer/odo/tests/helper"
 )
 
@@ -28,26 +28,24 @@ var _ = Describe("doc command reference odo delete namespace", func() {
 
 	Context("To delete a namespace resource", func() {
 
-		BeforeEach(func() {
-			helper.Cmd("odo", "create", "namespace", "odo-dev").ShouldRun()
+		//BeforeEach(func() {
+		//	helper.Cmd("odo", "create", "namespace", "odo-dev").ShouldRun()
 
-		})
+		//})
 
-		AfterEach(func() {
-			commonVar.CliRunner.DeleteNamespaceProject("odo-dev", false)
-		})
+		//AfterEach(func() {
+		//	commonVar.CliRunner.DeleteNamespaceProject("odo-dev", false)
+		//})
 
 		It("Deletes a namespace resource for a kubernetes cluster", func() {
 			args := []string{"odo", "delete", "namespace", "odo-dev"}
 			out, err := helper.RunInteractive(args, []string{"ODO_LOG_LEVEL=0"}, func(ctx helper.InteractiveContext) {
-				helper.ExpectString(ctx, "Are you sure you want to delete project 'odo-dev'?(y/N)")
+				helper.ExpectString(ctx, "? Are you sure you want to delete namespace \"odo-dev\"?")
 				helper.SendLine(ctx, "Yes")
 
 			})
 			Expect(err).To(BeNil())
-			//out := helper.Cmd("odo", args...).ShouldPass().Out()
-
-			got := fmt.Sprintf(outputStringFormat, strings.Join(args, " "), helper.StripSpinner(out))
+			got := helper.StripAnsi(out)
 			got = helper.StripInteractiveQuestion(got)
 			got = fmt.Sprintf(outputStringFormat, args[1], helper.StripSpinner(got))
 			file := "delete_namespace.mdx"
@@ -55,15 +53,23 @@ var _ = Describe("doc command reference odo delete namespace", func() {
 			diff := cmp.Diff(want, got)
 			Expect(diff).To(BeEmpty(), file)
 		})
-	})
 
-	It("Deletes a project resource for a openshift cluster", func() {
-		args := []string{"delete", "project", "odo-dev"}
-		out := helper.Cmd("odo", args...).ShouldPass().Out()
-		got := fmt.Sprintf(outputStringFormat, strings.Join(args, " "), helper.StripSpinner(out))
-		file := "delete_project.mdx"
-		want := helper.GetMDXContent(filepath.Join(commonPath, file))
-		diff := cmp.Diff(want, got)
-		Expect(diff).To(BeEmpty(), file)
+		FIt("Deletes a project resource for a openshift cluster", func() {
+			args := []string{"odo", "delete", "project", "odo-dev"}
+			out, err := helper.RunInteractive(args, []string{"ODO_LOG_LEVEL=0"}, func(ctx helper.InteractiveContext) {
+				helper.ExpectString(ctx, "? Are you sure you want to delete project \"odo-dev\"?")
+				helper.SendLine(ctx, "Yes")
+
+			})
+			Expect(err).To(BeNil())
+			got := helper.StripAnsi(out)
+			got = helper.StripInteractiveQuestion(got)
+			got = fmt.Sprintf(outputStringFormat, args[1], helper.StripSpinner(got))
+			file := "delete_project.mdx"
+			want := helper.GetMDXContent(filepath.Join(commonPath, file))
+			diff := cmp.Diff(want, got)
+			Expect(diff).To(BeEmpty(), file)
+		})
+
 	})
 })
