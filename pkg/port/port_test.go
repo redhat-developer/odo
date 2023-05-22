68ab057441d5472112d5906e0d5a3d94f86184a8
Armel Soro <asoro@redhat.com>
2023-05-22 14:07:15 +0200

    Do  not error out in port detection if any of `/proc/net/{tc,ud}p{,6}` files are missing
    /proc/net/{tc,ud}p6 files might be missing if IPv6 is disabled in the host networking stack,
    as reported by a user on RHEL.
    
    Actually /proc/net/{tc,ud}p* files might be totally missing if network stats are disabled.

--- pkg/port/port_test.go
+++ pkg/port/port_test.go
@@ -22,7 +22,7 @@ const (
 
 var cmd = []string{
 	remotecmd.ShellExecutable, "-c",
-	"cat /proc/net/tcp /proc/net/udp /proc/net/tcp6 /proc/net/udp6",
+	"for f in tcp udp tcp6 udp6; do cat /proc/net/$f || true; done",
 }
 
 const aggregatedContentFromProcNetFiles = `
