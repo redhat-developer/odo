99fa17edd8e9fa34a147696f59af6e7f3f87f518
Philippe Martin <phmartin@redhat.com>
2023-07-06 08:30:35 +0200

    Make api-server an experimental mode command

--- pkg/odo/cli/cli.go
+++ pkg/odo/cli/cli.go
@@ -10,6 +10,7 @@ import (
 	"unicode"
 
 	"github.com/redhat-developer/odo/pkg/odo/cli/apiserver"
+	"github.com/redhat-developer/odo/pkg/odo/cli/feature"
 	"github.com/redhat-developer/odo/pkg/odo/cli/logs"
 	"github.com/redhat-developer/odo/pkg/odo/cli/run"
 	"github.com/redhat-developer/odo/pkg/odo/commonflags"
@@ -202,8 +203,10 @@ func odoRootCmd(ctx context.Context, name, fullName string, unknownCmdHandler fu
 		logs.NewCmdLogs(logs.RecommendedCommandName, util.GetFullName(fullName, logs.RecommendedCommandName), testClientset),
 		completion.NewCmdCompletion(completion.RecommendedCommandName, util.GetFullName(fullName, completion.RecommendedCommandName)),
 		run.NewCmdRun(run.RecommendedCommandName, util.GetFullName(fullName, run.RecommendedCommandName), testClientset),
-		apiserver.NewCmdApiServer(ctx, apiserver.RecommendedCommandName, util.GetFullName(fullName, apiserver.RecommendedCommandName), testClientset),
 	)
+	if feature.IsExperimentalModeEnabled(ctx) {
+		rootCmdList = append(rootCmdList, apiserver.NewCmdApiServer(ctx, apiserver.RecommendedCommandName, util.GetFullName(fullName, apiserver.RecommendedCommandName), testClientset))
+	}
 
 	// Add all subcommands to base commands
 	rootCmd.AddCommand(rootCmdList...)
