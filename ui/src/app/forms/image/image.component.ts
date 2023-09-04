2c6ad5f5c6dd33b410f622fdf42f4a847190a362
Philippe Martin <phmartin@redhat.com>
2023-09-04 11:32:43 +0200

    [ui] edit images

--- ui/src/app/forms/image/image.component.ts
+++ ui/src/app/forms/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PATTERN_COMPONENT_ID } from '../patterns';
 import { Image } from 'src/app/api-gen';
@@ -11,8 +11,11 @@ import { TelemetryService } from 'src/app/services/telemetry.service';
 })
 export class ImageComponent {
   @Input() cancelable: boolean = false;
+  @Input() image: Image | undefined;
+
   @Output() canceled = new EventEmitter<void>();
   @Output() created = new EventEmitter<Image>();
+  @Output() saved = new EventEmitter<Image>();
 
   form: FormGroup;
 
@@ -35,7 +38,29 @@ export class ImageComponent {
     this.created.emit(this.form.value);
   }
 
+  save() {
+    const newValue = this.form.value;
+    newValue.name = this.image?.name;
+    this.telemetry.track("[ui] edit volume");
+    this.saved.emit(this.form.value);
+  }
+
   cancel() {
     this.canceled.emit();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("changes", changes);
+    if (!changes['image']) {
+      return;
+    }
+    const img = changes['image'].currentValue;
+    if (img == undefined) {
+      this.form.get('name')?.enable();
+    } else {
+      this.form.reset();
+      this.form.patchValue(img);
+      this.form.get('name')?.disable();
+    }
   }
 }
