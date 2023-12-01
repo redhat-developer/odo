244e5ccca0e1fe8a2a01999a246249e0a0db6852
Armel Soro <asoro@redhat.com>
2023-12-01 12:30:23 +0100

    Make Interop tests rely on mock Devfile Registry Server started in the tests
    The goal is to decommission the long-running IBM Clusters,
    for cost purposes.

--- scripts/openshiftci-config.sh
+++ scripts/openshiftci-config.sh
@@ -1,4 +1,5 @@
-# This is used in openshiftci-presubmit-all-tests.sh, used by Interop tests.
-# Since these interop tests are managed by a different team, we intentionally want to use a stable Devfile registry.
-# But our own internal tests make use of the staging Devfile registry.
-export DEVFILE_REGISTRY=https://devfile-registry-ci-devfile-registry.odo-test-kubernete-449701-49529fc6e6a4a9fe7ebba9a3db5b55c4-0000.eu-de.containers.appdomain.cloud/
+## This is used in openshiftci-presubmit-all-tests.sh, used by Interop tests.
+## Since these interop tests are managed by a different team, we intentionally want to use a stable Devfile registry.
+## But our own internal tests make use of the staging Devfile registry.
+# NOTE: Since #7154, tests no longer depend on an external Devfile Registry. They are now able to start an HTTP Server emulating the Devfile Registry API.
+#export DEVFILE_REGISTRY=https://devfile-registry-ci-devfile-registry.odo-test-kubernete-449701-49529fc6e6a4a9fe7ebba9a3db5b55c4-0000.eu-de.containers.appdomain.cloud/
